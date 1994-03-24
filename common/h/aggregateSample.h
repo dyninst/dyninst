c46fe7bb7a058101cfb81ab09a49323e2fb8cca9
hollings <hollings>
1994-03-24 16:45:50 +0000

    Moved aggregation of samples here.

---
+++ common/h/aggregateSample.h
@@ -0,0 +1,44 @@
+
+//
+// Define a class for a sample value.
+//    Sample values can be a single value or may be the result of an aggregate.
+//    This is in the util library because both paradynd's and paradyn need to
+//      use it.
+//
+
+#ifndef UTIL_SAMPLE
+#define UTIL_SAMPLE
+
+#include "util/h/list.h"
+#include "util/h/hist.h"
+
+//
+// What gets returned when a newValue is called in sampleInfo;
+struct sampleInterval {
+    Boolean	valid;
+    timeStamp	start;
+    timeStamp	end;
+    sampleValue	value;
+};
+
+class sampleInfo {
+  public:
+    struct sampleInterval newValue(time64 wallTime, sampleValue value);
+    struct sampleInterval newValue(List<sampleInfo *> peers, 
+				   time64 wallTime, 
+				   sampleValue value);
+    sampleInfo() {
+        firstSampleReceived = FALSE;
+	value = 0.0;
+	lastSampleStart = 0.0;
+	lastSampleEnd = 0.0;
+	lastSample = 0.0;
+    }
+    Boolean firstSampleReceived;        // has first sample been recorded
+    sampleValue value;                  // cumlative value
+    timeStamp   lastSampleStart;        // start time for last sample
+    timeStamp   lastSampleEnd;          // end time for last sample
+    sampleValue lastSample;             // what was the last sample increment
+};
+
+#endif
---
+++ common/src/aggregateSample.C
@@ -0,0 +1,97 @@
+
+#include <assert.h>
+
+#include "util/h/aggregateSample.h"
+
+struct sampleInterval sampleInfo::newValue(time64 sampleTime, 
+						  sampleValue newVal)
+{
+    struct sampleInterval ret;
+
+    // use the first sample to define a baseline in time and value.
+    if (!firstSampleReceived) {
+	firstSampleReceived = True;
+	lastSampleStart = sampleTime;
+	lastSampleEnd = sampleTime;
+	lastSample = 0.0;
+	value = newVal;
+	ret.valid = FALSE;
+	return(ret);
+    }
+
+    ret.valid = TRUE;
+    ret.start = lastSampleEnd;
+    ret.end = sampleTime;
+    ret.value = newVal;
+
+    // used when it's a component of an aggregate.
+    lastSample += newVal;
+    lastSampleEnd = sampleTime;
+
+    return(ret);
+}
+
+struct sampleInterval sampleInfo::newValue(List<sampleInfo *> parts,
+					   time64 sampleTime, 
+					   sampleValue newVal)
+{
+    double fract;
+    sampleInfo *curr;
+    List<sampleInfo*> cp;
+    sampleValue component;
+    timeStamp earlyestTime;
+    struct sampleInterval ret;
+    sampleValue aggregateVal;
+
+    if (parts.count() <= 1) {
+       // not an aggregate.
+       return(newValue(sampleTime, newVal));
+    } else {
+	earlyestTime = (*parts)->lastSampleEnd;
+	for (cp=parts; curr = *cp; cp++) {
+	    if (curr->lastSampleEnd < earlyestTime) {
+		earlyestTime = curr->lastSampleEnd;
+	    }
+	}
+
+
+	if (earlyestTime > lastSampleEnd + 0.0001) {
+	    /* eat the first one to get a good interval basis */
+	    if (!firstSampleReceived) {
+		firstSampleReceived = True;
+		ret.valid = FALSE;
+		lastSampleEnd = earlyestTime;
+		return(ret);
+	    }
+
+	    aggregateVal = 0.0;
+	    for (cp=parts; curr=*cp; cp++) {
+		// assert(earlyestTime >= curr->lastSampleStart);
+
+		fract = (earlyestTime - lastSampleEnd)/
+		    (curr->lastSampleEnd - curr->lastSampleStart);
+		component = (curr->lastSample) * fract;
+
+		assert(fract > 0.0);
+		assert(fract <= 1.0);
+		assert(component >= -0.01);
+
+		curr->lastSample -= component;
+		aggregateVal += component;
+		/* move forward our time of our earliest sample */
+		curr->lastSampleStart = earlyestTime;
+	    }
+
+	    ret.valid = TRUE;
+	    ret.start = lastSampleEnd;
+	    ret.end = earlyestTime;
+	    ret.value = aggregateVal;
+
+	    lastSampleStart = lastSampleEnd;
+	    lastSampleEnd = earlyestTime;
+	} else {
+	    ret.valid = FALSE;
+	}
+    }
+    return(ret);
+}
