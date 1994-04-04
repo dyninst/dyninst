5ffa7b0bb3082465e1374693329c7ecb538b62ba
newhall <newhall>
1994-04-04 21:38:09 +0000

    *** empty log message ***

--- paradyn/src/pdMain/paradyn.h
+++ paradyn/src/pdMain/paradyn.h
@@ -1,7 +1,10 @@
 /* $Log: paradyn.h,v $
-/* Revision 1.1  1994/03/29 20:21:25  karavan
-/* initial version for testing.
-/* */
+/* Revision 1.2  1994/04/04 21:38:09  newhall
+/* *** empty log message ***
+/*
+ * Revision 1.1  1994/03/29  20:21:25  karavan
+ * initial version for testing.
+ * */
 /* paradyn.h */
 
 /* some global definitions for main.C */
@@ -10,7 +13,12 @@
 #ifndef _paradyn_h
 #define _paradyn_h
 
+#include "thread/h/thread.h"
+
 #include "dataManager.CLNT.h"
+#include "performanceConsultant.CLNT.h"
+//#include "UI.CLNT.h"
+//#include "VM.CLNT.h"
 
 struct CLargStruct {
   int clargc;
@@ -22,16 +30,24 @@ typedef struct CLargStruct CLargStruct;
 
 /* common MSG TAG definitions */
 
-unsigned int MSG_TAG_UIM_READY;
-unsigned int MSG_TAG_DM_READY;
-unsigned int MSG_TAG_VM_READY;
-unsigned int MSG_TAG_PC_READY;
-unsigned int MSG_TAG_ALL_CHILDREN_READY;
+#define MSG_TAG_UIM_READY 1001
+#define MSG_TAG_DM_READY 1002
+#define MSG_TAG_VM_READY 1003
+#define MSG_TAG_PC_READY 1004
+#define MSG_TAG_ALL_CHILDREN_READY 1005
 
-thread_t UIMtid;
-thread_t MAINtid;
-thread_t PCtid;
-thread_t DMtid;
-applicationContext *context;
+extern thread_t UIMtid;
+extern thread_t MAINtid;
+extern thread_t PCtid;
+extern thread_t DMtid;
+extern thread_t VMtid;
+extern applicationContext *context;
+
+extern dataManagerUser *dataMgr;
+extern performanceConsultantUser *perfConsult;
+//extern UIMUser *uiMgr;
+//extern VMUser  *vmMgr;
 
 #endif
+
+
