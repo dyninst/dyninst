7c7d69b10d15b287ae8fc3e6b02af984bf22b8bf
karavan <karavan>
1994-04-06 17:39:00 +0000

    changed interp to global

--- paradyn/src/UIthread/UIglobals.h
+++ paradyn/src/UIthread/UIglobals.h
@@ -1,7 +1,10 @@
 /* $Log: UIglobals.h,v $
-/* Revision 1.1  1994/04/05 04:42:34  karavan
-/* initial version of UI thread code and tcl paradyn command
-/* */
+/* Revision 1.2  1994/04/06 17:39:00  karavan
+/* changed interp to global
+/*
+ * Revision 1.1  1994/04/05  04:42:34  karavan
+ * initial version of UI thread code and tcl paradyn command
+ * */
 
 /* UIglobals.h 
      definitions used by UI thread */
@@ -12,8 +15,9 @@
 #include "dataManager.CLNT.h"
 #include "performanceConsultant.CLNT.h"
 #include "UI.SRVR.h"
-
-
+extern "C" {
+ #include "tk.h"
+}
 #define UIMBUFFSIZE 256
 
 
@@ -22,5 +26,7 @@ extern int                       uim_eid;
 extern List<metricInstance*>     uim_enabled;
 extern performanceStream         *uim_defaultStream;
 extern UIM                       *uim_server;
+
+extern Tcl_Interp *interp;
 
 #endif
