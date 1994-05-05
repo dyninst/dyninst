1cb27c2d661dd0f721f3de514f44b1ef6fc57835
karavan <karavan>
1994-05-05 19:53:28 +0000

    added structure defn

--- paradyn/src/UIthread/UIglobals.h
+++ paradyn/src/UIthread/UIglobals.h
@@ -1,7 +1,10 @@
 /* $Log: UIglobals.h,v $
-/* Revision 1.3  1994/05/05 02:13:28  karavan
-/* moved CmdTabEntry definition from paradyn.tcl.C to UIglobals.h
+/* Revision 1.4  1994/05/05 19:53:28  karavan
+/* added structure defn
 /*
+ * Revision 1.3  1994/05/05  02:13:28  karavan
+ * moved CmdTabEntry definition from paradyn.tcl.C to UIglobals.h
+ *
  * Revision 1.2  1994/04/06  17:39:00  karavan
  * changed interp to global
  *
@@ -29,6 +32,12 @@ struct cmdTabEntry
   char *cmdname;
   int (*func)(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[]);
 };
+
+typedef struct UIMReplyRec {
+  void *cb;
+  thread_t tid;
+} UIMReplyRec;
+
 
 extern resource                  *uim_rootRes;
 extern int                       uim_eid;
