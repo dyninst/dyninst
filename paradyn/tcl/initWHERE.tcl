b85f8ba80d7d3db51014c78bbb851f1b64ab6da2
karavan <karavan>
1994-06-12 22:35:29 +0000

    changed default font for WHERE axis nodes

--- paradyn/tcl/initWHERE.tcl
+++ paradyn/tcl/initWHERE.tcl
@@ -3,11 +3,29 @@
 # some default styles for nodes and edges
 
 # $Log: initWHERE.tcl,v $
-# Revision 1.2  1994/05/26 21:23:11  karavan
+# Revision 1.3  1994/06/12 22:35:29  karavan
+# changed default font for WHERE axis nodes
+#
+# Revision 1.2  1994/05/26  21:23:11  karavan
 # changed parent window name.
 #
 # Revision 1.1  1994/05/23  01:56:23  karavan
 # initial version.
+#
+
+#
+# Copyright (c) 1993, 1994 Barton P. Miller, Jeff Hollingsworth,
+#     Bruce Irvin, Jon Cargille, Krishna Kunchithapadam, Karen
+#     Karavanic, Tia Newhall, Mark Callaghan.  All rights reserved.
+# 
+#  This software is furnished under the condition that it may not be
+#  provided or otherwise made available to, or used by, any other
+#  person, except as provided for by the terms of applicable license
+#  agreements.  No title to or ownership of the software is hereby
+#  transferred.  The name of the principals may not be used in any
+#  advertising or publicity related to this software without specific,
+#  written prior authorization.  Any use of this software must include
+#  the above copyright notice.
 #
 
 proc initWHERE {wwindow} {
@@ -36,7 +54,7 @@ pack $WHEREname.d01 -side top -expand 1 -fill both
 
 # style 1: WHERE axis node 
 $WHEREname.d01 addNstyle 1 -bg #c99e5f54dcab \
-		-font "-adobe-times-bold-r-normal--*-100*" \
+		-font "-Adobe-times-bold-r-normal--*-80*" \
 		-text black  \
 		-stipple "" -width 1
 
