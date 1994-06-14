fe9ca19b6a38997bab9394d539a124f28ff712e4
markc <markc>
1994-06-14 15:36:49 +0000

    Added support for different types of aggregation (max, min, sum, avg). Previously, only summation had been done.

--- pdutil/src/aggregateSample.C
+++ pdutil/src/aggregateSample.C
@@ -2,7 +2,11 @@
 /*
  * 
  * $Log: aggregateSample.C,v $
- * Revision 1.5  1994/06/02 23:36:10  markc
+ * Revision 1.6  1994/06/14 15:36:49  markc
+ * Added support for different types of aggregation (max, min, sum, avg).
+ * Previously, only summation had been done.
+ *
+ * Revision 1.5  1994/06/02  23:36:10  markc
  * Added assertion to ensure values reported are positive.
  *
  * Revision 1.4  1994/05/17  00:14:44  hollings
@@ -53,6 +57,10 @@ struct sampleInterval sampleInfo::newValue(List<sampleInfo *> parts,
     timeStamp earlyestTime;
     struct sampleInterval ret;
     sampleValue aggregateVal;
+    int len=0;
+
+    assert((aggOp == Sum) || (aggOp == Avg) ||
+	   (aggOp == Min) || (aggOp == Max));
 
     if (parts.count() <= 1) {
        // not an aggregate.
@@ -60,6 +68,7 @@ struct sampleInterval sampleInfo::newValue(List<sampleInfo *> parts,
     } else {
 	earlyestTime = (*parts)->lastSampleEnd;
 	for (cp=parts; curr = *cp; cp++) {
+	    len++;
 	    if (curr->lastSampleEnd < earlyestTime) {
 		earlyestTime = curr->lastSampleEnd;
 	    }
@@ -87,6 +96,9 @@ struct sampleInterval sampleInfo::newValue(List<sampleInfo *> parts,
 	    }
 
 	    aggregateVal = 0.0;
+
+	    int first = 1;
+
 	    for (cp=parts; curr=*cp; cp++) {
 		// assert(earlyestTime >= curr->lastSampleStart);
 
@@ -99,10 +111,34 @@ struct sampleInterval sampleInfo::newValue(List<sampleInfo *> parts,
 		assert(component >= -0.01);
 
 		curr->lastSample -= component;
-		aggregateVal += component;
+
+		// each list entry comes from a separate reporter
+		switch (aggOp)
+		  {
+		  case Sum:
+		  case Avg:
+		    aggregateVal += component;
+		    break;
+		  case Min:
+		    if (first) {
+		      aggregateVal = component;
+		      first = 0;
+		    } else if (component < aggregateVal)
+		      aggregateVal = component;
+		    break;
+		  case Max:
+		    if (component > aggregateVal)
+		      aggregateVal = component;
+		    break;
+		  }
+
 		/* move forward our time of our earliest sample */
 		curr->lastSampleStart = earlyestTime;
-	    }
+	      }
+
+	    // len is the number of samples on the list
+	    if (aggOp == Avg)
+	      aggregateVal /= len;
 
 	    ret.valid = TRUE;
 	    ret.start = lastSampleEnd;
