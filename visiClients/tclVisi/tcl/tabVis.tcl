fa906ce85430f828685c6eb69e8c5f30dc3ee67f
rbi <rbi>
1994-08-06 22:01:02 +0000

    New status line.  Full path to logo. "Since" dataformat. Eliminated variable traces.

--- visiClients/tclVisi/tcl/tabVis.tcl
+++ visiClients/tclVisi/tcl/tabVis.tcl
@@ -2,9 +2,13 @@
 #  tabVis -- A tabular display visualization for Paradyn
 #
 #  $Log: tabVis.tcl,v $
-#  Revision 1.4  1994/07/20 21:52:27  rbi
-#  Better support for BW and standard color scheme
-#  Added "Actions" menu
+#  Revision 1.5  1994/08/06 22:01:02  rbi
+#  New status line.  Full path to logo. "Since" dataformat. Eliminated
+#  variable traces.
+#
+# Revision 1.4  1994/07/20  21:52:27  rbi
+# Better support for BW and standard color scheme
+# Added "Actions" menu
 #
 # Revision 1.3  1994/06/14  18:58:29  rbi
 # Updated layout and added curve validation callback.
@@ -24,8 +28,7 @@
 if {[string match [tk colormodel .] color] == 1} {
   # always defaults to bisque so reset it to grey
   . config -bg grey
-
-  option add *Background grey
+   option add *Background grey
   option add *activeBackground LightGrey
   option add *activeForeground black
   option add *Scale.activeForeground grey
@@ -38,10 +41,11 @@ option add *Visi*font *-New*Century*Schoolbook-Bold-R-*-18-*
 option add *Data*font *-Helvetica-*-r-*-12-* 
 option add *MyMenu*font *-New*Century*Schoolbook-Bold-R-*-14-*
 
+set W .table
+
 #
 #  Create the overall frame
 #
-set W .table
 frame $W -class Visi
 
 #
@@ -85,18 +89,20 @@ $W.top.left.menubar.acts.m add command -label "Delete Entry" -command DelEntry
 menubutton $W.top.left.menubar.opts -text "Options" -menu $W.top.left.menubar.opts.m
 menu $W.top.left.menubar.opts.m -selector black
 $W.top.left.menubar.opts.m add command -label "Signif. Digits" -command SetSignif
-$W.top.left.menubar.opts.m add check -label "Short Names" -variable ShortNames
+$W.top.left.menubar.opts.m add check -label "Short Names" -variable ShortNames -command UpdateResourceLabels
 $W.top.left.menubar.opts.m add separator
 $W.top.left.menubar.opts.m add radio -label "Current Value" \
-    -variable DataFormat \
-    -value Instantaneous
+  -variable DataFormat \
+  -value Instantaneous -command FormatChanged
 $W.top.left.menubar.opts.m add radio -label "Average Value" \
-    -variable DataFormat \
-    -value Average
+  -variable DataFormat \
+  -value Average -command FormatChanged
 $W.top.left.menubar.opts.m add radio -label "Total Value" \
-    -variable DataFormat \
-    -value Sum
-$W.top.left.menubar.opts.m invoke 3
+  -variable DataFormat \
+  -value Sum -command FormatChanged
+$W.top.left.menubar.opts.m add radio -label "Enabled Since" \
+  -variable DataFormat \
+  -value Since -command FormatChanged
 
 #
 #  Help menu
@@ -111,7 +117,7 @@ $W.top.left.menubar.help.m disable 1
 #
 #  Build the menu bar and add to display
 #
-pack $W.top.left.menubar.file $W.top.left.menubar.acts $W.top.left.menubar.opts -side left 
+pack $W.top.left.menubar.file $W.top.left.menubar.acts $W.top.left.menubar.opts -side left -padx 4
 pack $W.top.left.menubar.help -side right 
 
 #
@@ -122,8 +128,8 @@ tk_menuBar $W.top.left.menubar $W.top.left.menubar.file $W.top.left.menubar.acts
 #
 #  Build the logo 
 #
-label $W.top.logo -relief raised -bitmap @~paradyn/core/paradyn/tcl/logo.xbm \
-                  -foreground #b3331e1b53c7
+label $W.top.logo -relief raised -bitmap @/p/paradyn/core/paradyn/tcl/logo.xbm \
+                -foreground #b3331e1b53c7
 
 pack $W.top.logo -side right
 
@@ -139,13 +145,11 @@ pack $W.middle -side top -fill both -expand 1
 frame $W.bottom
 pack $W.bottom -side bottom -fill x -expand 1 -anchor s
 
-button $W.bottom.close -text "Close" -command Shutdown -padx 4 -pady 4
 label $W.bottom.status -text "No Data Yet" \
-      -font *-Helvetica-*-o-*-12-* \
-      -foreground blue -padx 6
+    -font *-Helvetica-*-o-*-12-* \
+    -foreground blue -padx 6
 
-pack $W.bottom.close -side left -fill both 
-pack $W.bottom.status -side left
+pack $W.bottom.status -side left -fill x -expand true
 
 #
 #  Finally display everything
@@ -209,8 +213,11 @@ proc NotImpl {} {
 #    we just update the status line and keep on going
 #
 proc DgFoldCallback {} {
+  global UpdateLimit
+
   UpdateStatus 
 }
+
 
 #
 #  Called by visi library when met/res space changes
@@ -230,8 +237,8 @@ proc DgConfigCallback {} {
     set row [expr $m+1]
     label $W.middle.data.rowlabel($m) -text [Dg metricname $m]
     label $W.middle.data.rowunits($m) -text [Dg metricunits $m]
-    blt_table $W.middle.data $W.middle.data.rowlabel($m) $row,0 -anchor e -pady 2 \
-                          $W.middle.data.rowunits($m) $row,99 -anchor w -pady 2
+    blt_table $W.middle.data $W.middle.data.rowlabel($m) $row,0 -anchor e \
+      -pady 2 $W.middle.data.rowunits($m) $row,[expr $nR+1] -anchor w -pady 2
     for {set r 0} {$r < $nR} {incr r} {
       if {$row == 1} {
         set col [expr $r+1]
@@ -239,7 +246,7 @@ proc DgConfigCallback {} {
         blt_table $W.middle.data $W.middle.data.collabel($r) 0,$col -anchor e
 	blt_table column $W.middle.data configure $col -padx 2
       }
-      label $W.middle.data.label($m,$r)
+      button $W.middle.data.label($m,$r) -relief flat
       blt_table $W.middle.data $W.middle.data.label($m,$r) $row,$col -anchor e
     }
   }
@@ -256,10 +263,9 @@ set DataFormat Average
 #
 #  Whenever DataFormat is changed, get new data and update status line
 #
-trace variable DataFormat w FormatChanged
 
-proc FormatChanged {name1 name2 how} {
-  DgDataCallback 0 0
+proc FormatChanged {} {
+  DataUpdate
   UpdateStatus
 }
 
@@ -268,12 +274,7 @@ proc FormatChanged {name1 name2 how} {
 #
 set ShortNames 0
 
-#
-#  Whenever ShortNames is changed, redo resource labels
-#  
-trace variable ShortNames w UpdateResourceLabels
-
-proc UpdateResourceLabels {name1 name2 how} {
+proc UpdateResourceLabels {} {
   global W
 
   set nR [Dg numresources]
@@ -297,8 +298,14 @@ proc GetResourceName {resid} {
 #
 proc UpdateStatus {} {
   global DataFormat W
+  global Callbacks UpdateLimit
 
-  $W.bottom.status configure -text "Interval = [Dg binwidth] s, DataFormat: $DataFormat"
+  set bw [Dg binwidth]
+
+  $W.bottom.status configure -text [format "Time: %-10s  Format: %s" [TimeLabel [expr int($bw*[Dg lastbucket 0 0])]] $DataFormat] 
+
+# Callbacks: $Callbacks
+
 }
 
 #
@@ -307,7 +314,7 @@ proc UpdateStatus {} {
 #
 proc GetValue {m n} {
   global DataFormat
-
+  
   if {[string match $DataFormat Average]} {
     return [Dg aggregate $m $n]
   }
@@ -324,23 +331,78 @@ proc DgValidCallback {m r} {
   puts stderr [format "Curve %d %d is now valid" $m $r]
 }
 
+set UpdateCounter 0
+set UpdateLimit 1
+
+proc TimeToUpdate {} {
+  global UpdateCounter UpdateLimit
+
+  if {$UpdateCounter <= 0} {
+    set UpdateCounter [expr $UpdateLimit / [Dg binwidth]]
+    return 1
+  } else {
+    set UpdateCounter [expr $UpdateCounter - 1]
+    return 0
+  }
+}
+
+set Callbacks 0
+
 #
 #  DgDataCallback -- visi calls this command when new data is available
 #    we fill in all of the data labels with the new data values
 #
 proc DgDataCallback {first last} {
+  global Callbacks
+
+  incr Callbacks
+  if {[TimeToUpdate]} {
+    DataUpdate
+  }
+}
+
+proc DataUpdate {} {
+  global DataFormat
   global SignificantDigits W
+
+  UpdateStatus
 
   set nM [Dg nummetrics]
   set nR [Dg numresources]
   for {set m 0} {$m < $nM} {incr m} {
     for {set r 0} {$r < $nR} {incr r} {
       if {[Dg valid $m $r]} {
-        set value [GetValue $m $r]
-        $W.middle.data.label($m,$r) configure \
-           -text [format "%.[set SignificantDigits]f" $value]
+	if {[string match $DataFormat Since]} {
+	  $W.middle.data.label($m,$r) configure \
+	    -text [TimeLabel [expr int ([Dg firstbucket $m $r] * [Dg binwidth])]]
+	} else {
+	  set value [GetValue $m $r]
+	    $W.middle.data.label($m,$r) configure \
+	      -text [format "%.[set SignificantDigits]f" $value]
+	}
       }
     }
   }
 }
 
+#
+#  TimeLabel -- given a time value in seconds, format a nice label
+#
+proc TimeLabel {val} {
+  if {($val > 60) && ($val < 3600)} {
+    set min [expr $val / 60]
+    set sec [expr $val - ($min * 60)]
+    return [format "%d m %d s" $min $sec]
+  }
+  if {$val < 60} {
+    return [format "%d s" $val]
+  }
+  if {$val > 3600} {
+    set hr [expr $val / 3600]
+    set left [expr $val - ($hr * 3600)]
+    set min [expr $left / 60]
+    set sec [expr $left - ($min * 60)]
+    return [format "%d h %d m %d s" $hr $min $sec]
+  }
+  return "$val s"
+}
