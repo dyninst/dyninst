a3abb28ebf1a2abac015b7d35d5c2db8fce37c72
markc <markc>
1994-09-02 19:42:46 +0000

    Added missing semicolon to end of typedef

--- pdutil/h/stringDecl.h
+++ pdutil/h/stringDecl.h
@@ -4,11 +4,14 @@
 
 /*
  * $Log: stringDecl.h,v $
- * Revision 1.1  1994/09/02 10:43:50  markc
+ * Revision 1.2  1994/09/02 19:42:46  markc
+ * Added missing semicolon to end of typedef
+ *
+ * Revision 1.1  1994/09/02  10:43:50  markc
  * Moved typedef for stringHandle outside of stringPool.h
  *
  */
 
-typedef void *stringHandle
+typedef void *stringHandle;
 
 #endif /* _STRING_DECL_H */
