38f11ab6b63458a7b826d20e853fb79cb1475b37
markc <markc>
1994-09-22 03:16:25 +0000

    added virtual destructor

--- common/h/rpcUtilPVM.h
+++ common/h/rpcUtilPVM.h
@@ -13,12 +13,13 @@ class PVMrpc {
     PVMrpc(char *where, char *program, char **argv, int flag);
     PVMrpc(int other);
     PVMrpc();
-    void setNonBlock() { ; }
+    virtual ~PVMrpc() { }
+    void setNonBlock() { }
     inline int get_error() { return pvm_error;}
     inline int get_other_tid() { return other_tid;}
     inline void set_other_tid(int set_to) {other_tid = set_to;}
     int readReady();
-  protected:
+  private:
     int my_tid;
     int other_tid;
     int pvm_error;
