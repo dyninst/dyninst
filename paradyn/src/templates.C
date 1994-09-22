1bc15e517f829a6e5a056f229e9f8f4ab8264172
markc <markc>
1994-09-22 00:49:12 +0000

    Paradyn now uses one template code generating file.  All threads should use this file to generate template code.

---
+++ paradyn/src/templates.C
@@ -0,0 +1,97 @@
+/*
+ * Put all the templates in one file
+ *
+ */
+
+/*
+ * $Log: templates.C,v $
+ * Revision 1.1  1994/09/22 00:49:12  markc
+ * Paradyn now uses one template code generating file.  All threads should use
+ * this file to generate template code.
+ *
+ * Revision 1.2  1994/08/22  15:59:31  markc
+ * Add List<daemonEntry*> which is the daemon definition dictionary.
+ *
+ * Revision 1.1  1994/05/19  00:02:18  hollings
+ * added templates.
+ *
+ *
+ */
+#pragma implementation "list.h"
+#include "util/h/list.h"
+
+
+/* *********************************   
+ * DMthread stuff
+ */
+
+#include "dataManager.h"
+#include "dyninstRPC.CLNT.h"
+#include "paradyn/src/DMthread/DMinternals.h"
+#include "util/h/tunableConst.h"
+class uniqueName;
+
+typedef List<uniqueName*> t0;
+typedef List<executable *> t1;
+typedef List<paradynDaemon *> t2;
+typedef List<performanceStream *> t3;
+typedef List<daemonEntry*> t4;
+
+/* ********************************
+ * PCthread stuff
+ */
+#include "paradyn/src/PCthread/PCshg.h"
+#include "paradyn/src/PCthread/PCevalTest.h"
+#include "paradyn/src/PCthread/PCglobals.h"
+#include "paradyn/src/PCthread/PCauto.h"
+#include "paradyn/src/PCthread/PCwhen.h"
+#include "paradyn/src/PCthread/PCwhere.h"
+#include "paradyn/src/PCthread/PCwhy.h"
+
+// typedef HTable<metricInstance *> p3;
+// typedef List<focus *> p5;
+// typedef List<focusList *> p6;
+// typedef List<metricInstance *> p9;
+
+typedef HTable<PCmetric *> p0;
+typedef HTable<datum *> p1;
+typedef HTable<focus *> p2;
+
+typedef List<datum *> p4;
+typedef List<hint *> p7;
+typedef List<hypothesis *> p8;
+typedef List<searchHistoryNode *> p10;
+typedef List<test *> p11;
+typedef List<testResult *> p12;
+typedef List<timeInterval *> p13;
+
+/* *************************************
+ * UIthread stuff
+ */
+class pRec;
+typedef HTable<pRec *> h1; 
+
+/* ************************************
+ * VMthread stuff
+ */
+#include "paradyn/src/VMthread/VMtypes.h"
+
+typedef List<VMactiveStruct *> v1;
+typedef List<VMvisisStruct *> v2;
+
+/* ***********************************
+ * met stuff
+ */
+#include "paradyn/src/met/metParse.h" 
+
+class stringList;
+class daemonMet;
+class processMet;
+class visiMet;
+class tunableMet;
+
+typedef List<stringList*> m1;
+typedef List<daemonMet*> m2;
+typedef List<processMet*> m3;
+typedef List<visiMet*> m4;
+typedef List<tunableMet*> m5; 
