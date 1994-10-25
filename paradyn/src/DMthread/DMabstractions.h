0ab367e7de651b96e600deeda12b22d17cbe81c0
karavan <karavan>
1994-10-25 19:01:37 +0000

    Protected this header file

--- paradyn/src/DMthread/DMabstractions.h
+++ paradyn/src/DMthread/DMabstractions.h
@@ -1,10 +1,16 @@
 /*
  * Classes and prototypes for Abstraction specific code
  * $Log: DMabstractions.h,v $
- * Revision 1.1  1994/09/30 19:17:42  rbi
+ * Revision 1.2  1994/10/25 19:01:37  karavan
+ * Protected this header file
+ *
+ * Revision 1.1  1994/09/30  19:17:42  rbi
  * Abstraction interface change.
  *
  */
+
+#ifndef _abstract_h
+#define _abstract_h
 
 class abstraction {
     friend abstraction *AMfind(char *aname);
@@ -23,3 +29,5 @@ abstraction *AMfind(char *aname);
 
 void AMnewMapping(char *abstraction, char *type, char *key, char *value);
 void AMnewResource(char *parent, char *name, char *abstraction);
+
+#endif
