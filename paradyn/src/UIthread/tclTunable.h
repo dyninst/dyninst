1f9bc40de585278e0dea98419cfcfe58a3c6a0dd
tamches <tamches>
1994-10-26 23:13:04 +0000

    first version

---
+++ paradyn/src/UIthread/tclTunable.h
@@ -0,0 +1,9 @@
+// tclTunable.h
+
+/* $Log: tclTunable.h,v $
+/* Revision 1.1  1994/10/26 23:13:04  tamches
+/* first version
+/* */
+
+int TclTunableCommand(ClientData cd, Tcl_Interp *interp,
+		      int argc, char **argv);
