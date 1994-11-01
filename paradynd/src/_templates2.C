af3f52cb23a01d68f6750a83df81cd1f5f97e281
markc <markc>
1994-11-01 16:52:06 +0000

    Templates for old c++ classes.

---
+++ paradynd/src/_templates2.C
@@ -0,0 +1,27 @@
+
+/*
+ * Part 2 of template code generation
+ * This is split up to because compiling this file thrashes on a 32M sparc
+ */
+
+#pragma implementation  "list.h"
+#include "util/h/list.h"
+
+#include "rtinst/h/rtinst.h"
+#include "rtinst/h/trace.h"
+#include "util/h/aggregateSample.h"
+#include "util/h/tunableConst.h"
+#include "symtab.h"
+#include "process.h"
+#include "inst.h"
+#include "instP.h"
+#include "dyninstP.h"
+#include "metric.h"
+#include "ast.h"
+#include "util.h"
+#include "internalMetrics.h"
+#include "util/h/Object.h"
+
+typedef List<internalMetric*> o1;
+typedef List<metricDefinitionNode*> o2;
+
