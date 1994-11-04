9bc26baafa2ba185d479b5d0f207e5632e76d120
markc <markc>
1994-11-04 12:59:53 +0000

    Set start of interval to time of first sample if that interval start is earlier.

--- paradyn/src/PCthread/PCmetric.C
+++ paradyn/src/PCthread/PCmetric.C
@@ -18,7 +18,10 @@
 /*
  * 
  * $Log: PCmetric.C,v $
- * Revision 1.22  1994/10/25 22:08:05  hollings
+ * Revision 1.23  1994/11/04 12:59:53  markc
+ * Set start of interval to time of first sample if that interval start is earlier.
+ *
+ * Revision 1.22  1994/10/25  22:08:05  hollings
  * changed print member functions to ostream operators.
  *
  * Fixed lots of small issues related to the cost model for the
@@ -150,7 +153,7 @@ static char Copyright[] = "@(#) Copyright (c) 1993, 1994 Barton P. Miller, \
   Jeff Hollingsworth, Jon Cargille, Krishna Kunchithapadam, Karen Karavanic,\
   Tia Newhall, Mark Callaghan.  All rights reserved.";
 
-static char rcsid[] = "@(#) $Header: /home/jaw/CVSROOT_20081103/CVSROOT/core/paradyn/src/PCthread/PCmetric.C,v 1.22 1994/10/25 22:08:05 hollings Exp $";
+static char rcsid[] = "@(#) $Header: /home/jaw/CVSROOT_20081103/CVSROOT/core/paradyn/src/PCthread/PCmetric.C,v 1.23 1994/11/04 12:59:53 markc Exp $";
 #endif
 
 #include <stdio.h>
@@ -253,7 +256,7 @@ sampleValue PCmetric::value(focus *f, timeStamp start, timeStamp end)
     if (!val) {
 	// This situation happens when an item that is requested to be enabled
 	// is not avaiable but other tests that are requested toogether are.
-	return(0.0);
+      return(0.0);
     }
 
     if (!val->enabled) {
@@ -263,9 +266,11 @@ sampleValue PCmetric::value(focus *f, timeStamp start, timeStamp end)
 	abort();
     }
 
+    // TODO mdc - added to support searching - use proper interval for evalTests
     // ask for time only from when it was enabled.
-    //start = val->firstSampleTime;
-    assert(start >= val->firstSampleTime);
+    if (val->firstSampleTime > start)
+      start = val->firstSampleTime;
+    // assert(start >= val->firstSampleTime);
 
     val->lastUsed = end;
 
