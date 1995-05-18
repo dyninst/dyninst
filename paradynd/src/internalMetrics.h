3d86f5f66a25742fa14fbb94415ce31a2fc09cee
markc <markc>
1995-05-18 10:37:28 +0000

    Removed metric class information from internalMetric

--- paradynd/src/internalMetrics.h
+++ paradynd/src/internalMetrics.h
@@ -2,50 +2,61 @@
 #ifndef INTERNAL_MET_HDR
 #define INTERNAL_MET_HDR
 
-#include "metric.h"
-#include "util/h/list.h"
+#include "paradynd/src/metric.h"
+#include "paradynd/src/im_preds.h"
 
 typedef float (*sampleValueFunc)();
 
 //
 // Metrics that are internal to a paradyn daemon.
 //
+
+// Why use classes if everything is public?
 class internalMetric {
-  public:
-    internalMetric(const string n, int style, int a, const string units,
-		   sampleValueFunc f, resourcePredicate *r, bool really=true);
+ public:
+  internalMetric(const string n, metricStyle style, int a, const string units,
+		 sampleValueFunc f, im_pred_struct& im_preds) 
+    : value(0.0), cumulativeValue(0.0), func(f), node(NULL), name_(n), agg_(a),
+      style_(style), units_(units), pred(im_preds) { }
 
-    float getValue() {
-	if (func) {
-	    return((func)());
-	} else {
-	    return(value);
-	}
-    }
-    void enable(metricDefinitionNode *n) {
-	node = n;
-	activeInternalMetrics[n] = this;
-    }
-    void disable() {
-	node = NULL;
-    }
-    bool enabled() {
-	return(node != NULL);
-    }
-    metric metRec;
-    float value;
-    float cumulativeValue;
-    sampleValueFunc func;
-    metricDefinitionNode *node;
-    static dictionary_hash<string, internalMetric*> allInternalMetrics;
-    static dictionary_hash<metricDefinitionNode*, internalMetric*>
-      activeInternalMetrics;
+  inline float getValue();
+  void enable(metricDefinitionNode *n) { node = n; }
+  void disable() { node = NULL; }
+  bool enabled() { return(node != NULL); }
+  metricStyle style() { return style_; }
+  string name() const { return name_;}
 
-    string getName() const { return name;}
-    bool reallyIsEventCounter;
+  static vector<internalMetric*> allInternalMetrics;
+  static internalMetric *newInternalMetric(const string n, metricStyle style, int a,
+					   const string units,
+					   sampleValueFunc f, im_pred_struct& preds);
+  float value;
+  float cumulativeValue;
+  sampleValueFunc func;
+  metricDefinitionNode *node;
+  int aggregate() const { return agg_; }
 
-  private:
-    string name;
+  T_dyninstRPC::metricInfo getInfo() {
+    T_dyninstRPC::metricInfo ret;
+    ret.name = name_; ret.style = style_;
+    ret.aggregate = agg_; ret.units = units_;
+    return ret;
+  }
+  bool legalToInst(vector< vector<string> >& focus);
+
+private:
+  string name_;
+  int agg_;
+  metricStyle style_;
+  string units_;
+  im_pred_struct pred;
 };
+
+float internalMetric::getValue() {
+  if (func)
+    return((func)());
+  else
+    return(value);
+}
 
 #endif
