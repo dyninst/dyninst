810c8d57d27eb835c77f57a1efdf6fe1997042a1
tamches <tamches>
1995-07-16 03:28:31 +0000

    Split string class into a .h and .C component; strings can now automatically convert numerical representations.

--- common/h/String.h
+++ common/h/String.h
@@ -2,27 +2,19 @@
  * String.h: a simple character string class.
 ************************************************************************/
 
-
-
-
-
 #if !defined(_String_h_)
 #define _String_h_
-
-
-
 
 
 /************************************************************************
  * header files.
 ************************************************************************/
 
+#include <stdio.h> // FILE *
 #include <iostream.h>
 #include "util/h/headers.h"
 
 
-
-
 /************************************************************************
  * class string
 ************************************************************************/
@@ -32,36 +24,42 @@ public:
      string ();
      string (const char *);
      string (const string &);
+     string (int);      // convert int to its string representation
+     string (unsigned); // convert unsigned to its string representation
+     string (float);    // convert float to its string representation
+     string (FILE *);   // load from a binary file
     ~string ();
 
-    string&       operator= (const char *);
-    string&       operator= (const string &);
-    string&      operator+= (const string &);
-    string        operator+ (const string &)         const;
+    int save(FILE *outfile) const; // complement of string(FILE *)
 
-    bool         operator== (const string &)         const;
-    bool         operator!= (const string &)         const;
-    bool          operator< (const string &)         const;
-    bool         operator<= (const string &)         const;
-    bool          operator> (const string &)         const;
-    bool         operator>= (const string &)         const;
+    string& operator= (const char *);
+    string& operator= (const string &);
+    string& operator+= (const string &);
+    string  operator+ (const string &) const;
 
-    bool          prefix_of (const char *, unsigned) const;
-    bool          prefix_of (const char *)           const;
-    bool          prefix_of (const string &)         const;
+    bool operator== (const string &) const;
+    bool operator!= (const string &) const;
+    bool operator<  (const string &s) const {return STRLT(str_, s.str_);}
+    bool operator<= (const string &) const;
+    bool operator>  (const string &s) const {return STRGT(str_, s.str_);}
+    bool operator>= (const string &) const;
 
-    bool        prefixed_by (const char *, unsigned) const;
-    bool        prefixed_by (const char *)           const;
-    bool        prefixed_by (const string &)         const;
+    bool prefix_of (const char *, unsigned) const;
+    bool prefix_of (const char *s)          const {return prefix_of(s, STRLEN(s));};
+    bool prefix_of (const string &)         const;
 
-    const char*   string_of ()                       const;
-    unsigned         length ()                       const;
+    bool prefixed_by (const char *, unsigned) const;
+    bool prefixed_by (const char *s)          const {return prefixed_by(s, STRLEN(s));};
+    bool prefixed_by (const string &)         const;
 
-    friend ostream& operator<< (ostream &os, const string &s) {
-      return os << s.str_;
-    }
+    const char*   string_of () const {return str_;}
+    unsigned         length () const {return len_;}
 
-    static unsigned       hash (const string &);
+    friend ostream& operator<< (ostream &os, const string &s);
+
+    static unsigned       hash (const string &s) {return s.key_;}
+
+    static string quote;
 
 private:
     static unsigned      hashs (const char *);
@@ -81,257 +79,5 @@ private:
     unsigned len_;
     unsigned key_;
 };
-
-inline
-string::string()
-    : str_(0), len_(0), key_(0) {
-}
-
-inline
-string::string(const char* str)
-    : str_(STRDUP(str)), len_(STRLEN(str)), key_(hashs(str)) {
-}
-
-inline
-string::string(const string& s)
-    : str_(STRDUP(s.str_)), len_(s.len_), key_(s.key_) {
-}
-
-inline
-string::~string() {
-    delete [] str_; str_ = 0;
-}
-
-inline
-string&
-string::operator=(const char* str) {
-    if (str_ == str) {
-        return *this;
-    }
-
-    delete [] str_; str_ = 0;
-
-    str_ = STRDUP(str);
-    len_ = STRLEN(str);
-    key_ = hashs(str);
-
-    return *this;
-}
-
-inline
-string&
-string::operator=(const string& s) {
-    if (this == &s) {
-        return *this;
-    }
-
-    delete [] str_; str_ = 0;
-
-    str_ = STRDUP(s.str_);
-    len_ = s.len_;
-    key_ = s.key_;
-
-    return *this;
-}
-
-inline
-string&
-string::operator+=(const string& s) {
-    unsigned nlen = len_ + s.len_;
-    char*    ptr  = new char[nlen+1];
-
-    memcpy(ptr, str_, len_);
-    memcpy(&ptr[len_], s.str_, s.len_);
-    ptr[nlen] = '\0';
-
-    delete[] str_; str_ = 0;
-    str_ = ptr;
-    len_ = nlen;
-    key_ = hashs(str_);
-
-    return *this;
-}
-
-inline
-string
-string::operator+(const string& s) const {
-    string ret = *this;
-    return ret += s;
-}
-
-inline
-bool
-string::operator==(const string& s) const {
-    return ((&s == this)
-        || ((key_ == s.key_)
-        && (len_ == s.len_)
-        && STREQ(str_, s.str_)));
-}
-
-inline
-bool
-string::operator!=(const string& s) const {
-    return ((!(&s == this)) && (len_ != s.len_)
-        || STRNE(str_, s.str_));
-}
-
-inline
-bool
-string::operator<(const string& s) const {
-    return STRLT(str_, s.str_);
-}
-
-inline
-bool
-string::operator<=(const string& s) const {
-    return ((&s == this) || STRLE(str_, s.str_));
-}
-
-inline
-bool
-string::operator>(const string& s) const {
-    return STRGT(str_, s.str_);
-}
-
-inline
-bool
-string::operator>=(const string& s) const {
-    return ((&s == this) || STRGE(str_, s.str_));
-}
-
-inline
-bool
-string::prefix_of(const char* s, unsigned sl) const {
-    return ((len_ > sl) ? false : STREQN(str_, s, len_));
-}
-
-inline
-bool
-string::prefix_of(const char* s) const {
-    return prefix_of(s, STRLEN(s));
-}
-
-inline
-bool
-string::prefix_of(const string& s) const {
-    return ((&s == this) || prefix_of(s.str_, s.len_));
-}
-
-inline
-bool
-string::prefixed_by(const char* s, unsigned sl) const {
-    return ((sl > len_) ? false : STREQN(str_, s, sl));
-}
-
-inline
-bool
-string::prefixed_by(const char* s) const {
-    return prefixed_by(s, STRLEN(s));
-}
-
-inline
-bool
-string::prefixed_by(const string& s) const {
-    return ((&s == this) || prefixed_by(s.str_, s.len_));
-}
-
-inline
-unsigned
-string::length() const {
-    return len_;
-}
-
-inline
-const char*
-string::string_of() const {
-    return str_;
-}
-
-inline
-unsigned
-string::hash(const string& s) {
-    return s.key_;
-}
-
-inline
-unsigned
-string::hashs(const char* str) {
-    if (!str) {
-        return 0;
-    }
-
-    unsigned h = 5381;
-    while (*str) {
-        h = (h << 5) + h + (unsigned) (*str);
-        str++;
-    }
-    return h;
-}
-
-inline
-unsigned
-string::STRLEN(const char* str) {
-    return ((str)?(P_strlen(str)):(0));
-}
-
-inline
-char*
-string::STRDUP(const char* str) {
-    if (!str) {
-        return 0;
-    }
-
-    unsigned size = P_strlen(str)+1;
-    char*    p    = new char[size];
-
-    (void) P_memcpy(p, str, size);
-    return p;
-}
-
-inline
-bool
-string::STREQ(const char* s1, const char* s2) {
-    return ((s1&&s2)?(P_strcmp(s1,s2)==0):(!(s1||s2)));
-}
-
-inline
-bool
-string::STREQN(const char* s1, const char* s2, unsigned len) {
-    return ((s1&&s2)?(P_strncmp(s1,s2,len)==0):(!(s1||s2)));
-}
-
-inline
-bool
-string::STRNE(const char* s1, const char* s2) {
-    return ((s1&&s2)?(P_strcmp(s1,s2)!=0):(false));
-}
-
-inline
-bool
-string::STRLT(const char* s1, const char* s2) {
-    return ((s1&&s2)?(P_strcmp(s1,s2)<0):(false));
-}
-
-inline
-bool
-string::STRLE(const char* s1, const char* s2) {
-    return ((s1&&s2)?(P_strcmp(s1,s2)<=0):(!(s1||s2)));
-}
-
-inline
-bool
-string::STRGT(const char* s1, const char* s2) {
-    return ((s1&&s2)?(P_strcmp(s1,s2)>0):(false));
-}
-
-inline
-bool
-string::STRGE(const char* s1, const char* s2) {
-    return ((s1&&s2)?(P_strcmp(s1,s2)>=0):(!(s1||s2)));
-}
-
-
-
-
 
 #endif /* !defined(_String_h_) */
---
+++ pdutil/src/String.C
@@ -0,0 +1,246 @@
+// String.C
+
+#include <assert.h>
+#include "util/h/String.h"
+
+string string::quote="\"";
+
+string::string()
+    : str_(0), len_(0), key_(0) {
+}
+
+string::string(const char* str)
+    : str_(STRDUP(str)), len_(STRLEN(str)), key_(hashs(str)) {
+}
+
+string::string(const string& s)
+    : str_(STRDUP(s.str_)), len_(s.len_), key_(s.key_) {
+}
+
+string::string(int i) {
+   char tempBuffer[40];
+   sprintf(tempBuffer, "%d", i);
+
+   str_ = STRDUP(tempBuffer);
+   len_ = STRLEN(tempBuffer);
+   key_ = hashs (tempBuffer);
+}
+
+string::string(unsigned u) {
+   char tempBuffer[40];
+   sprintf(tempBuffer, "%u", u);
+
+   str_ = STRDUP(tempBuffer);
+   len_ = STRLEN(tempBuffer);
+   key_ = hashs (tempBuffer);
+}
+
+string::string(float f) {
+   char tempBuffer[40];
+   sprintf(tempBuffer, "%f", f);
+
+   str_ = STRDUP(tempBuffer);
+   len_ = STRLEN(tempBuffer);
+   key_ = hashs (tempBuffer);
+}
+
+string::string(FILE *infile) {
+   assert(1 == fread(&len_, sizeof(len_), 1, infile));
+   char *temp = new char[len_ + 1]; assert(temp);
+   assert(len_ == fread(temp, 1, len_, infile));
+   temp[len_] = '\0';
+
+   str_ = STRDUP(temp);
+   key_ = hashs(temp);
+
+   delete [] temp;
+}
+
+string::~string() {
+    delete [] str_; str_ = 0;
+}
+
+int string::save(FILE *outfile) const {
+   if (1 != fwrite(&len_, sizeof(len_), 1, outfile))
+      return -1;
+
+   if (len_ != fwrite(str_, 1, len_, outfile))
+      return -1;
+
+   return 0; // success
+}
+
+string&
+string::operator=(const char* str) {
+    if (str_ == str) {
+        return *this;
+    }
+
+    delete [] str_; str_ = 0;
+
+    str_ = STRDUP(str);
+    len_ = STRLEN(str);
+    key_ = hashs(str);
+
+    return *this;
+}
+
+string&
+string::operator=(const string& s) {
+    if (this == &s) {
+        return *this;
+    }
+
+    delete [] str_; str_ = 0;
+
+    str_ = STRDUP(s.str_);
+    len_ = s.len_;
+    key_ = s.key_;
+
+    return *this;
+}
+
+string&
+string::operator+=(const string& s) {
+    unsigned nlen = len_ + s.len_;
+    char*    ptr  = new char[nlen+1];
+
+    memcpy(ptr, str_, len_);
+    memcpy(&ptr[len_], s.str_, s.len_);
+    ptr[nlen] = '\0';
+
+    delete[] str_; str_ = 0;
+    str_ = ptr;
+    len_ = nlen;
+    key_ = hashs(str_);
+
+    return *this;
+}
+
+string
+string::operator+(const string& s) const {
+    string ret = *this;
+    return ret += s;
+}
+
+bool
+string::operator==(const string& s) const {
+    return ((&s == this)
+        || ((key_ == s.key_)
+        && (len_ == s.len_)
+        && STREQ(str_, s.str_)));
+}
+
+bool
+string::operator!=(const string& s) const {
+    return ((!(&s == this)) && (len_ != s.len_)
+        || STRNE(str_, s.str_));
+}
+
+bool
+string::operator<=(const string& s) const {
+    return ((&s == this) || STRLE(str_, s.str_));
+}
+
+bool
+string::operator>=(const string& s) const {
+    return ((&s == this) || STRGE(str_, s.str_));
+}
+
+bool
+string::prefix_of(const char* s, unsigned sl) const {
+    return ((len_ > sl) ? false : STREQN(str_, s, len_));
+}
+
+bool
+string::prefix_of(const string& s) const {
+    return ((&s == this) || prefix_of(s.str_, s.len_));
+}
+
+bool
+string::prefixed_by(const char* s, unsigned sl) const {
+    return ((sl > len_) ? false : STREQN(str_, s, sl));
+}
+
+bool
+string::prefixed_by(const string& s) const {
+    return ((&s == this) || prefixed_by(s.str_, s.len_));
+}
+
+unsigned
+string::hashs(const char* str) {
+    if (!str) {
+        return 0;
+    }
+
+    unsigned h = 5381;
+    while (*str) {
+        h = (h << 5) + h + (unsigned) (*str);
+        str++;
+    }
+    return h;
+}
+
+//#define P_strlen strlen
+//#define P_memcpy memcpy
+
+unsigned
+string::STRLEN(const char* str) {
+    return ((str)?(P_strlen(str)):(0));
+}
+
+char*
+string::STRDUP(const char* str) {
+    if (!str) {
+        return 0;
+    }
+
+    unsigned size = P_strlen(str)+1;
+    char*    p    = new char[size];
+
+    (void) P_memcpy(p, str, size);
+    return p;
+}
+
+//#define P_strcmp strcmp
+
+bool
+string::STREQ(const char* s1, const char* s2) {
+    return ((s1&&s2)?(P_strcmp(s1,s2)==0):(!(s1||s2)));
+}
+
+//#define P_strncmp strncmp
+
+bool
+string::STREQN(const char* s1, const char* s2, unsigned len) {
+    return ((s1&&s2)?(P_strncmp(s1,s2,len)==0):(!(s1||s2)));
+}
+
+bool
+string::STRNE(const char* s1, const char* s2) {
+    return ((s1&&s2)?(P_strcmp(s1,s2)!=0):(false));
+}
+
+bool
+string::STRLT(const char* s1, const char* s2) {
+    return ((s1&&s2)?(P_strcmp(s1,s2)<0):(false));
+}
+
+bool
+string::STRLE(const char* s1, const char* s2) {
+    return ((s1&&s2)?(P_strcmp(s1,s2)<=0):(!(s1||s2)));
+}
+
+bool
+string::STRGT(const char* s1, const char* s2) {
+    return ((s1&&s2)?(P_strcmp(s1,s2)>0):(false));
+}
+
+bool
+string::STRGE(const char* s1, const char* s2) {
+    return ((s1&&s2)?(P_strcmp(s1,s2)>=0):(!(s1||s2)));
+}
+
+ostream& operator<< (ostream &os, const string &s) {
+   return os << s.str_;
+}
