979a9082c58bb961f6e5b32a97bb31997bff3346
tamches <tamches>
1995-07-17 04:53:21 +0000

    First version of new where axis

---
+++ paradyn/src/UIthread/minmax.h
@@ -0,0 +1,17 @@
+// minmax.h
+
+/* $Log: minmax.h,v $
+/* Revision 1.1  1995/07/17 04:53:21  tamches
+/* First version of new where axis
+/*
+ */
+#ifndef _MINMAX_H_
+#define _MINMAX_H_
+
+template <class T>
+T min(const T item1, const T item2);
+
+template <class T>
+T max(const T item1, const T item2);
+
+#endif
