0bb2af34abde48e66c4817a12925bf57410fe32b
newhall <newhall>
1995-08-01 02:04:43 +0000

    changes to menuing and window height

--- visiClients/tclVisi/tcl/phasetbl.tcl
+++ visiClients/tclVisi/tcl/phasetbl.tcl
@@ -57,7 +57,7 @@ set W .table
 #
 #  Create the overall frame
 #
-frame $W -class Visi -width 4i -height 3i
+frame $W -class Visi -width 4i -height 2i
 
 #
 #  Create the title bar, menu bar, and logo at the top
@@ -89,9 +89,14 @@ $W.top.left.menubar.file.m add command -label "Close" -command Shutdown
 #
 #  Actions menu
 #
-menubutton $W.top.left.menubar.acts -text "Actions" -menu $W.top.left.menubar.acts.m
-menu $W.top.left.menubar.acts.m
-$W.top.left.menubar.acts.m add command -label "Start Phase" -command PhaseDef 
+#menubutton $W.top.left.menubar.acts -text "Actions"  \
+#		-menu $W.top.left.menubar.acts.m
+#menu $W.top.left.menubar.acts.m
+#$W.top.left.menubar.acts.m add command -label "Start Phase" -command PhaseDef 
+
+button $W.top.left.menubar.acts -text "Start Phase"  \
+		-relief flat -borderwidth 0 -highlightbackground white -command PhaseDef  
+
 
 #
 #  Options menu
@@ -110,7 +115,7 @@ $W.top.left.menubar.help.m add command -label "Context" -command "NotImpl"
 #
 #  Build the menu bar and add to display
 #
-pack $W.top.left.menubar.file $W.top.left.menubar.acts -side left -padx 4
+pack $W.top.left.menubar.file $W.top.left.menubar.acts -side left -padx 2
 pack $W.top.left.menubar.help -side right 
 
 #
@@ -139,7 +144,7 @@ pack  $W.left.phaseName -side top -expand false
 # expand is false; if the window is made taller, we don't want the extra height
 
 canvas $W.left.dataCanvas -relief groove \
-        -width 1.3i
+        -width 1.3i -height 0.9i
 pack   $W.left.dataCanvas -side left -fill both -expand true
 
 
@@ -154,7 +159,7 @@ label $W.middle.phaseStart -text "Start Time" -foreground Blue
 pack   $W.middle.phaseStart -side top -fill x -expand false
 
 canvas $W.middle.dataCanvas -relief groove \
-        -width 1.3i
+        -width 1.3i -height 0.9i
 pack  $W.middle.dataCanvas -side top -fill both -expand true 
 
 #
@@ -167,8 +172,9 @@ pack  $W.right -side right -fill both -expand true
 label $W.right.phaseEnd -text "End Time" -foreground Blue 
 pack   $W.right.phaseEnd -side top -fill x -expand false
 
+
 canvas $W.right.dataCanvas -relief groove \
-        -width 1.3i
+        -width 1.3i -height 0.9i
 pack  $W.right.dataCanvas -side top -fill both -expand true
 
 
@@ -176,7 +182,7 @@ pack  $W.right.dataCanvas -side top -fill both -expand true
 # display everything
 #
 pack append . $W {fill expand frame center}
-wm minsize . 250 200
+wm minsize . 250 100
 wm title . "Phase Table"
 
 #
@@ -219,17 +225,20 @@ proc DgValidCallback {m} {
 proc DgDataCallback {bucket} {
 
 }
-set xOffset 5
+set xOffset 10 
 set yOffset 5
+set widthChange 20
 
 proc NameUpdate {phaseId} {
    global W
    global xOffset 
    global yOffset 
+   global widthChange
 
    set theCanvas $W.left.dataCanvas
 
    set theText [Dg phasename $phaseId] 
+   set yOffset [expr  5 + $phaseId * $widthChange]
    $theCanvas create text $xOffset $yOffset\
 	-anchor nw \
 	-fill black \
@@ -246,9 +255,11 @@ proc StartUpdate {phaseId} {
    global W
    global xOffset 
    global yOffset 
+   global widthChange
 
    set theCanvas $W.middle.dataCanvas
 
+   set yOffset [expr  5 + $phaseId * $widthChange]
    set theText [TimeLabel [expr int([Dg phasestartT $phaseId])]  ] 
    $theCanvas create text $xOffset $yOffset\
 	-anchor nw \
@@ -264,10 +275,17 @@ proc EndUpdate {phaseId} {
    global W
    global xOffset 
    global yOffset 
+   global widthChange
 
    set theCanvas $W.right.dataCanvas
-
+   
+   # puts stderr "in EndUpdate phaseId and phaseend"
+   # puts stderr $phaseId 
+   set result [Dg phaseendT $phaseId]
+   # puts stderr $result 
+   if {$result == -1} return
    set theText [TimeLabel [expr int([Dg phaseendT $phaseId])]  ]
+   set yOffset [expr  5 + $phaseId * $widthChange]
    $theCanvas create text $xOffset $yOffset\
 	-anchor nw \
 	-fill black \
@@ -276,7 +294,7 @@ proc EndUpdate {phaseId} {
 	-tag dataTag \
 	-text $theText 
 
-   set yOffset [expr $yOffset + 20]
+#   set yOffset [expr $yOffset + 20]
    return
 }
 
@@ -302,6 +320,16 @@ proc DgPhaseEndCallback {phaseId} {
 #  DgPhaseDataCallback -- visi calls this when a phase has started
 #
 proc DgPhaseDataCallback {} {
+   
+   set max [expr int([Dg numphases])]
+   for {set phasecount 0} {$phasecount < $max} {incr phasecount} {
+       NameUpdate $phasecount
+       StartUpdate $phasecount
+       if {$phasecount < [expr $max - 1]} {
+	   EndUpdate  $phasecount
+       }
+   }
+   
    return
 }
 
