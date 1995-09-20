11d27eba39b0fe1c916dff9b5081eb58698f1063
tamches <tamches>
1995-09-20 01:28:36 +0000

    Some stuff moved to tkTools

--- paradyn/src/UIthread/whereAxisMisc.h
+++ paradyn/src/UIthread/whereAxisMisc.h
@@ -4,18 +4,11 @@
 #ifndef _WHERE_AXIS_MISC_H_
 #define _WHERE_AXIS_MISC_H_
 
+#include <fstream.h>
 #include <tclclean.h>
+#include "simpSeq.h"
 #include "where4tree.h"
-
-void myTclEval(Tcl_Interp *interp, const char *buffer);
-void getScrollBarValues(Tcl_Interp *interp, const string &sbname,
-			float &first, float &last);
-float moveScrollBar(Tcl_Interp *interp, const string &sbname,
-		    float newFirst);
-   // returns _actual_ newFirst (usually newFirst, as passed in, but may be pinned)
-
-whereNodeRawPath graphical2RawPath(const whereNodeGraphicalPath &src);
-void printPath(const whereNodeRawPath &thePath);
+#include "tkTools.h"
 
 void nonSliderCallMeOnButtonRelease(ClientData cd, XEvent *theEvent);
 void nonSliderButtonAutoRepeatCallback(ClientData cd);
@@ -23,8 +16,11 @@ void nonSliderButtonAutoRepeatCallback(ClientData cd);
 void sliderCallMeOnMouseMotion(ClientData cd, XEvent *eventPtr);
 void sliderCallMeOnButtonRelease(ClientData cd, XEvent *eventPtr);
 
+#ifndef PARADYN
+// this is only for the where axis test program:
 string readUntilQuote(ifstream &is);
 string readUntilSpace(ifstream &is);
 string readItem(ifstream &is);
+#endif
 
 #endif
