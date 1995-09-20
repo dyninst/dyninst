16115d87ae2a82f8ca9c7520ce2e98c8003e2005
tamches <tamches>
1995-09-20 01:16:35 +0000

    added ipmin and ipmax

--- paradyn/src/UIthread/minmax.C
+++ paradyn/src/UIthread/minmax.C
@@ -1,9 +1,12 @@
 // minmax.C
 
 /* $Log: minmax.C,v $
-/* Revision 1.1  1995/07/17 04:58:55  tamches
-/* First version of the new where axis
+/* Revision 1.2  1995/09/20 01:16:36  tamches
+/* added ipmin and ipmax
 /*
+ * Revision 1.1  1995/07/17  04:58:55  tamches
+ * First version of the new where axis
+ *
  */
 
 #include "minmax.h"
@@ -20,3 +23,12 @@ T max(const T item1, const T item2) {
    return item2;
 }
 
+template <class T>
+void ipmin(T &item, const T otherItem) {
+   if (otherItem < item) item = otherItem;
+}
+
+template <class T>
+void ipmax(T &item, const T otherItem) {
+   if (otherItem > item) item = otherItem;
+}
--- paradyn/src/UIthread/minmax.h
+++ paradyn/src/UIthread/minmax.h
@@ -1,9 +1,12 @@
 // minmax.h
 
 /* $Log: minmax.h,v $
-/* Revision 1.1  1995/07/17 04:53:21  tamches
-/* First version of new where axis
+/* Revision 1.2  1995/09/20 01:16:35  tamches
+/* added ipmin and ipmax
 /*
+ * Revision 1.1  1995/07/17  04:53:21  tamches
+ * First version of new where axis
+ *
  */
 #ifndef _MINMAX_H_
 #define _MINMAX_H_
@@ -13,5 +16,13 @@ T min(const T item1, const T item2);
 
 template <class T>
 T max(const T item1, const T item2);
+
+// "ip" stands for in-place.
+// item = min(item, otherItem):
+template <class T>
+void ipmin(T &item, const T otherItem);
+
+template <class T>
+void ipmax(T &item, const T otherItem);
 
 #endif
