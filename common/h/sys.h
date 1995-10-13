c45530c6ab675d6fb357799820bcd471a2ea6ceb
newhall <newhall>
1995-10-13 22:04:53 +0000

    added BASESAMPLEINTERVAL BASEBUCKETWIDTH

--- common/h/sys.h
+++ common/h/sys.h
@@ -1,6 +1,10 @@
 #ifndef sys_H
 #define sys_H
 
+/* These must represent the same value */
+#define BASESAMPLEINTERVAL 200000  /* 200 msecs */
+#define BASEBUCKETWIDTH    0.2   /* .2 seconds  */
+
 typedef double timeStamp;
 typedef float sampleValue;
 
