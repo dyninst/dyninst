6e2a14e6be6f7988a4a08e5738eececea70a1d65
tamches <tamches>
1995-10-17 22:16:26 +0000

    Removed masterWindow.  Removed rootItemTextGC, rootItemRectGC, and their highlighted variants.  Removed listboxScrollbarBorderDragging. Removed several listbox gc's.

--- paradyn/src/UIthread/where4treeConstants.h
+++ paradyn/src/UIthread/where4treeConstants.h
@@ -2,9 +2,14 @@
 // Ariel Tamches
 
 /* $Log: where4treeConstants.h,v $
-/* Revision 1.1  1995/07/17 04:59:05  tamches
-/* First version of the new where axis
+/* Revision 1.2  1995/10/17 22:16:26  tamches
+/* Removed masterWindow.  Removed rootItemTextGC, rootItemRectGC,
+/* and their highlighted variants.  Removed listboxScrollbarBorderDragging.
+/* Removed several listbox gc's.
 /*
+ * Revision 1.1  1995/07/17 04:59:05  tamches
+ * First version of the new where axis
+ *
  */
 
 #ifndef _WHERE4TREECONSTANTS_H_
@@ -12,17 +17,12 @@
 
 #include <iostream.h>
 
-extern "C" {
-   #include <X11/Xlib.h>
-}
-
 #include "tclclean.h"
 #include "tkclean.h"
 
 struct where4TreeConstants {
    Display *display;
    Tk_Window theTkWindow;
-   Drawable masterWindow;
    Pixmap   offscreenPixmap;
 
    // Erasing:
@@ -31,23 +31,22 @@ struct where4TreeConstants {
    // Root Item:   
    XFontStruct *rootTextFontStruct;
    Tk_3DBorder rootNodeBorder;
-   GC rootItemTextGC, highlightedRootItemTextGC;
-   GC rootItemRectGC, highlightedRootItemRectGC;
+   GC rootItemTextGC;
 
    // Rays:
    GC listboxRayGC;
    GC subchildRayGC;
 
-   // Master listbox:
+   // Listbox:
+   int listboxBorderPix; // 3
+   int listboxScrollBarWidth; // 16
    int listboxHeightWhereSBappears;
       // varies as the window gets resize -- let's say for now that it's
       // always something like 80% of the height of the master canvas window.
-   XFontStruct *listboxFontStruct; // if there is to be a title, then there'd be another one of these...
+   XFontStruct *listboxFontStruct;
    Tk_3DBorder listboxBorder;
    Tk_3DBorder listboxScrollbarBorderNormal;
-   Tk_3DBorder listboxScrollbarBorderDragging;
-   GC listboxRectangleGC, listboxTextGC, listboxTriangleGC;
-   GC listboxBackgroundGC;
+   GC listboxTextGC, listboxTriangleGC;
    int listboxHorizPadBeforeText, listboxHorizPadBeforeTriangle;
    int listboxTriangleWidth, listboxTriangleHeight;
    int listboxHorizPadAfterTriangle;
@@ -64,16 +63,7 @@ struct where4TreeConstants {
    where4TreeConstants(Tcl_Interp *, Tk_Window theWindow);
   ~where4TreeConstants();  // needed to free some resources
   
-   void resize() {
-      XFreePixmap(display, offscreenPixmap);
-      offscreenPixmap = XCreatePixmap(display, masterWindow,
-				      Tk_Width(theTkWindow),
-				      Tk_Height(theTkWindow),
-				      Tk_Depth(theTkWindow));
-
-      listboxHeightWhereSBappears = Tk_Height(theTkWindow) * 8 / 10;
-            // 80%
-   }
+   void resize();
 };
 
 #endif
