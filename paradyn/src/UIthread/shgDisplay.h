78b67f097d10de6afce2ba5ff0c4ac16ff411a0b
tamches <tamches>
1995-10-17 22:08:10 +0000

    Commented everything out; this class has been obsoleted by the new search history graph.

--- paradyn/src/UIthread/shgDisplay.h
+++ paradyn/src/UIthread/shgDisplay.h
@@ -18,66 +18,70 @@
  * shgDisplay.h
  *
  * $Log: shgDisplay.h,v $
+ * Revision 1.2  1995/10/17 22:08:10  tamches
+ * Commented everything out; this class has been obsoleted by
+ * the new search history graph.
+ *
  * Revision 1.1  1995/10/05 04:32:01  karavan
  * Initial implementation.
  *
  */
 
-#ifndef _shgDisplay_h
-#define _shgDisplay_h
-
-#include <string.h>
-#include "dag.h"
-
-// hash function for dictionary members
-inline unsigned uihsh(const unsigned &ptr) {
-  return (ptr >> 2);
-}
-
-// 
-// shgDisplay
-// 
-// Contains everything related to GUI display for one Perf Consultant
-// search.  We assume there may be more than one, although only one 
-// per phase plus optionally one for global search.  Perf Consultant 
-// uses shgDisplay.id as shgToken for all update calls (this used to 
-// be a dag id), including text status update.
+//#ifndef _shgDisplay_h
+//#define _shgDisplay_h
 //
-class shgDisplay {
-  friend int shgShortExplainCmd (ClientData clientData, 
-                Tcl_Interp *interp, 
-                int argc, 
-                char *argv[]);
-
- public:
-  shgDisplay (const char *name, int phase);
-  int initDisplay();
-  // for token lookups
-  int getToken() {return (int)id;}
-  int getDagToken() {return (int)dagToken;}
-  int addNode (nodeIdType nodeID, int styleID, 
-	       char *label, char *shgname, int flags);
-  int addEdge (nodeIdType srcID, nodeIdType dstID, int styleID);
-  int configureNode (nodeIdType nodeID, char *label, int styleID);
-
-  void updateStatus (const char *newinfo);
-  const char *getWinTitle() {return winTitle.string_of();}
-  static dictionary_hash<unsigned, shgDisplay*> AllSearchDisplays;
- private:
-  // graph display
-  dag *shgdag;    
-  // token for graph display (needed to cross interfaces)
-  unsigned dagToken;
-  // window for entire PC display
-  string winName;
-  // token for PC display, used in PC/UI calls
-  unsigned id;
-  static unsigned nextID;
-  // What's displayed at the top of the Search Display window
-  string winTitle;
-  // unique identifer for phase, NOT same as DM id's 
-  int phaseID;
-  dictionary_hash<unsigned, string*> AllNodeFullNames;
-};
-
-#endif
+//#include <string.h>
+//#include "dag.h"
+//
+//// hash function for dictionary members
+//inline unsigned uihsh(const unsigned &ptr) {
+//  return (ptr >> 2);
+//}
+//
+//// 
+//// shgDisplay
+//// 
+//// Contains everything related to GUI display for one Perf Consultant
+//// search.  We assume there may be more than one, although only one 
+//// per phase plus optionally one for global search.  Perf Consultant 
+//// uses shgDisplay.id as shgToken for all update calls (this used to 
+//// be a dag id), including text status update.
+////
+//class shgDisplay {
+//  friend int shgShortExplainCmd (ClientData clientData, 
+//                Tcl_Interp *interp, 
+//                int argc, 
+//                char *argv[]);
+//
+// public:
+//  shgDisplay (const char *name, int phase);
+//  int initDisplay();
+//  // for token lookups
+//  int getToken() {return (int)id;}
+//  int getDagToken() {return (int)dagToken;}
+//  int addNode (nodeIdType nodeID, int styleID, 
+//	       char *label, char *shgname, int flags);
+//  int addEdge (nodeIdType srcID, nodeIdType dstID, int styleID);
+//  int configureNode (nodeIdType nodeID, char *label, int styleID);
+//
+//  void updateStatus (const char *newinfo);
+//  const char *getWinTitle() {return winTitle.string_of();}
+//  static dictionary_hash<unsigned, shgDisplay*> AllSearchDisplays;
+// private:
+//  // graph display
+//  dag *shgdag;    
+//  // token for graph display (needed to cross interfaces)
+//  unsigned dagToken;
+//  // window for entire PC display
+//  string winName;
+//  // token for PC display, used in PC/UI calls
+//  unsigned id;
+//  static unsigned nextID;
+//  // What's displayed at the top of the Search Display window
+//  string winTitle;
+//  // unique identifer for phase, NOT same as DM id's 
+//  int phaseID;
+//  dictionary_hash<unsigned, string*> AllNodeFullNames;
+//};
+//
+//#endif
