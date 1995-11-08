f0a1f8cc3293c3da8bf29cd02d7700b39c658ea9
tamches <tamches>
1995-11-08 21:53:57 +0000

    changed focus color from tomato to maroon3 cleaner initialization since dg2.C now strictly handles just the "Dg" tcl cmd

--- visiClients/tableVisi/src/main.C
+++ visiClients/tableVisi/src/main.C
@@ -3,6 +3,10 @@
 
 /*
  * $Log: main.C,v $
+ * Revision 1.3  1995/11/08 21:53:57  tamches
+ * changed focus color from tomato to maroon3
+ * cleaner initialization since dg2.C now strictly handles just the "Dg" tcl cmd
+ *
  * Revision 1.2  1995/11/08 21:17:07  naim
  * Adding matherr exception handler function to avoid error message when
  * computing the "not a number" (NaN) value - naim
@@ -19,9 +23,12 @@
 #include "tkclean.h"
 #include "tkTools.h"
 
+#include "visi/h/visualization.h"
 #include "dg2.h"
+
 #include "tableVisi.h"
 #include "tableVisiTcl.h"
+
 #include "util/h/matherr.h"
 
 Tcl_Interp *mainInterp;
@@ -34,6 +41,13 @@ void panic(const char *msg) {
 
 tableVisi *theTableVisi; // our main data structure
 bool xsynch_flag=false;
+
+void visiFdReadableHandler(ClientData, int) {
+   // Installed as a file-handler routine for whenever data arrives over
+   // the socket returned from VisiInit()
+   if (visi_callback() == -1)
+      exit(0);
+}
 
 int main(int argc, char **argv) {
    if (argc==2 && 0==strcmp(argv[1], "--debug")) {
@@ -73,10 +87,22 @@ int main(int argc, char **argv) {
    if (TCL_OK != Dg2_Init(mainInterp))
       tclpanic(mainInterp, "Could not Dg2_Init");
 
+   int fd = VisiInit();
+   if (fd < 0)
+      panic("failed to initialize w/ visi lib");
+
    Tcl_SetVar(mainInterp, "tcl_interactive", "0", TCL_GLOBAL_ONLY);
 
    // Install our new tcl commands here:
    installTableVisiCommands(mainInterp);
+
+   if (RegistrationCallback(ADDMETRICSRESOURCES, Dg2AddMetricsCallback) != 0)
+      panic("Dg2_Init() -- couldn't install ADDMETRICSRESOURCES callback");
+
+   if (RegistrationCallback(DATAVALUES, Dg2NewDataCallback) != 0)
+      panic("Dg2_Init() -- couldn't install DATAVALUES callback");
+
+   Tk_CreateFileHandler(fd, TK_READABLE, visiFdReadableHandler, 0);
 
    // Krishna's tcl2c stuff:
    extern int initialize_tcl_sources(Tcl_Interp *);
@@ -96,7 +122,7 @@ int main(int argc, char **argv) {
 				"lightBlue", // line color
 				"blue", // metric color
 				"black", // metric units color
-				"tomato", // focus color
+				"maroon3", // focus color
 				"black", // cell color
 				"gray", // background color
 				2 // initial # sig figs
@@ -106,7 +132,4 @@ int main(int argc, char **argv) {
    Tk_MainLoop(); // returns when all tk windows are closed
 
    delete theTableVisi;
-
-   // cleanup (uninstall tcl commands, etc.):
-   unInstallTableVisiCommands(mainInterp);
 }
