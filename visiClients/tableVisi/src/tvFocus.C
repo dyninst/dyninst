0434e1cfbdc430625b77a98bba863240a84b6fd1
tamches <tamches>
1995-11-08 21:48:12 +0000

    moved implementation of constructor to .C file

--- visiClients/tableVisi/src/tvFocus.C
+++ visiClients/tableVisi/src/tvFocus.C
@@ -3,10 +3,32 @@
 
 /*
  * $Log: tvFocus.C,v $
+ * Revision 1.2  1995/11/08 21:48:13  tamches
+ * moved implementation of constructor to .C file
+ *
  * Revision 1.1  1995/11/04 00:46:40  tamches
  * First version of new table visi
  *
  */
 
 #include "tvFocus.h"
+
+tvFocus::tvFocus(const string &iLongName, XFontStruct *nameFontStruct) :
+                                                   longName(iLongName) {
+   longNamePixWidth = XTextWidth(nameFontStruct, longName.string_of(),
+				 longName.length());
+      
+   // Calculate the short name now, by starting at the end of name, working
+   // backwards, and looking for a "/"
+   const char *ptr = strrchr(longName.string_of(), '/');
+   if (ptr == NULL)
+      shortName = longName;
+   else if (ptr + 1 == '\0')
+      shortName = longName;
+   else
+      shortName = ptr + 1;
+
+   shortNamePixWidth = XTextWidth(nameFontStruct, shortName.string_of(),
+				  shortName.length());
+}
 
--- visiClients/tableVisi/src/tvFocus.h
+++ visiClients/tableVisi/src/tvFocus.h
@@ -3,6 +3,9 @@
 
 /*
  * $Log: tvFocus.h,v $
+ * Revision 1.2  1995/11/08 21:48:12  tamches
+ * moved implementation of constructor to .C file
+ *
  * Revision 1.1  1995/11/04 00:46:39  tamches
  * First version of new table visi
  *
@@ -21,24 +24,8 @@ class tvFocus {
 
  public:
    tvFocus(){} // needed by class Vector (nuts)
-   tvFocus(const string &iLongName, XFontStruct *nameFontStruct) :
-            longName(iLongName) {
-      longNamePixWidth = XTextWidth(nameFontStruct, longName.string_of(),
-				    longName.length());
-      
-      // Calculate the short name now, by starting at the end of name, working
-      // backwards, and looking for a "/"
-      const char *ptr = strrchr(longName.string_of(), '/');
-      if (ptr == NULL)
-         shortName = longName;
-      else if (ptr + 1 == '\0')
-         shortName = longName;
-      else
-         shortName = ptr + 1;
+   tvFocus(const string &iLongName, XFontStruct *nameFontStruct);
 
-      shortNamePixWidth = XTextWidth(nameFontStruct, shortName.string_of(),
-				     shortName.length());
-   }
    tvFocus(const tvFocus &src) : longName(src.longName), shortName(src.shortName) {
       longNamePixWidth = src.longNamePixWidth;
       shortNamePixWidth = src.shortNamePixWidth;
