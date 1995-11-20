383549bd9ee1740d6d8987dd66cc873a90620613
tamches <tamches>
1995-11-20 20:20:44 +0000

    a new template to support changes to tableVisi.C

--- visiClients/tableVisi/src/templates.C
+++ visiClients/tableVisi/src/templates.C
@@ -3,6 +3,9 @@
 
 /*
  * $Log: templates.C,v $
+ * Revision 1.2  1995/11/20 20:20:44  tamches
+ * a new template to support changes to tableVisi.C
+ *
  * Revision 1.1  1995/11/04 00:47:44  tamches
  * First version of new table visi
  *
@@ -26,3 +29,4 @@ template class vector< vector<tvCell> >;
 template unsigned max(unsigned, unsigned);
 template float max(float, float);
 template bool ipmax(unsigned, unsigned);
+template int ipmin(int, int);
