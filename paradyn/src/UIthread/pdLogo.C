4115075cdfb2f67cb4885f39d793030482469db3
tamches <tamches>
1995-11-29 00:19:35 +0000

    first version of pdLogo

---
+++ paradyn/src/UIthread/pdLogo.C
@@ -0,0 +1,235 @@
+// pdLogo.C
+
+/*
+ * $Log: pdLogo.C,v $
+ * Revision 1.1  1995/11/29 00:19:46  tamches
+ * first version of pdLogo
+ *
+ */
+
+#include "pdLogo.h"
+
+dictionary_lite<string, pdLogo::logoStruct> pdLogo::all_installed_logos(string::hash);
+dictionary_lite<string, pdLogo *> pdLogo::all_logos(string::hash);
+
+bool pdLogo::tryFirst() {
+   if (theLogo != None)
+      return true;
+
+   if (Tk_WindowId(theTkWindow) == 0)
+      return false; // not yet ready
+
+   theLogo = XCreatePixmapFromBitmapData(Tk_Display(theTkWindow),
+					 Tk_WindowId(theTkWindow),
+					 (char *)theLogoData.rawData,
+					 theLogoData.width, theLogoData.height,
+					 foregroundColor->pixel,
+					 backgroundColor->pixel,
+					 Tk_Depth(theTkWindow)
+					 );
+   assert(theLogo);
+
+   XGCValues values;
+   copyGC = Tk_GetGC(theTkWindow,
+		     0, // (!)
+		     &values);
+
+   return true;
+}
+
+void pdLogo::draw() {
+   if (!tryFirst())
+      return;
+
+   installer.install(this); // calls real_draw() when idle
+}
+
+void pdLogo::real_draw(ClientData cd) {
+   pdLogo *pthis = (pdLogo *)cd;
+   assert(pthis);
+
+   XCopyArea(Tk_Display(pthis->theTkWindow),
+	     pthis->theLogo, // src drawable
+	     Tk_WindowId(pthis->theTkWindow), // dest drawable
+	     pthis->copyGC,
+	     0, 0, // src x, y
+	     pthis->theLogoData.width, pthis->theLogoData.height,
+	     pthis->borderPix, pthis->borderPix // dest x, y
+	     );
+}
+
+/* ********************************************************** */
+
+pdLogo::pdLogo(Tcl_Interp *iInterp, Tk_Window iTkWindow,
+	       void *iRawData, unsigned iWidth, unsigned iHeight) :
+		    installer(&pdLogo::real_draw) {
+   theLogoData.rawData = iRawData;
+   theLogoData.width = iWidth;
+   theLogoData.height = iHeight;
+   
+   interp = iInterp;
+   theTkWindow = iTkWindow;
+   theTkWindowName = string(Tk_PathName(theTkWindow));
+   theDisplay = Tk_Display(theTkWindow);
+
+   myTclEval(interp, theTkWindowName + " cget -borderwidth");
+   borderPix = atoi(interp->result);
+
+   myTclEval(interp, theTkWindowName + " cget -highlightcolor");
+   const string foregroundColorName = string(interp->result);
+
+   myTclEval(interp, theTkWindowName + " cget -background");
+   const string backgroundColorName = string(interp->result);
+
+   theLogo = None;
+
+   foregroundColor = Tk_GetColor(interp, theTkWindow,
+				 Tk_GetUid(foregroundColorName.string_of()));
+   assert(foregroundColor);
+
+   backgroundColor = Tk_GetColor(interp, theTkWindow,
+				 Tk_GetUid(backgroundColorName.string_of()));
+   assert(backgroundColor);
+
+   copyGC = None;
+}
+
+pdLogo::~pdLogo() {
+   if (copyGC)
+      Tk_FreeGC(theDisplay, copyGC);
+
+   if (theLogo)
+      XFreePixmap(theDisplay, theLogo);
+
+   Tk_FreeColor(foregroundColor);
+   Tk_FreeColor(backgroundColor);
+}
+
+/* ******************************************************************* */
+
+int pdLogo::install_fixed_logo(const string &str,
+			       void *rawData, unsigned width, unsigned height) {
+   // we have no uninstall_fixed_logo since there's not big need for it.
+   if (all_installed_logos.defines(str)) {
+      cout << "install_fixed_logo (" << str << "): already installed! (ignoring)" << endl;
+      return TCL_OK;
+   }
+   logoStruct l;
+   l.rawData = rawData;
+   l.width = width;
+   l.height = height;;
+   all_installed_logos[str] = l;
+
+   return TCL_OK;
+}
+
+int pdLogo::makeLogoCommand(ClientData cd, Tcl_Interp *interp,
+			    int argc, char **argv) {
+   // args: [0] (implicit) the command name
+   // 1) tk window name   2) logo name (previously installed via install_fixed_logo)
+   // 3) relief           4) border pix       5) color
+   // NOTE: ClientData must point to a Tk_Window created by Tk_CreateMainWindow()
+   if (argc != 6) {
+      cerr << argv[0] << " requires 5 args (found " << argc-1 << ")" << endl;
+      exit(5);
+   }
+   
+   assert(argc == 6);
+   const string theTkWindowName = argv[1];
+   const string theLogoName     = argv[2];
+   const string reliefString    = argv[3];
+   unsigned borderPix           = atoi(argv[4]);
+   const string colorString     = argv[5];
+
+   if (!all_installed_logos.defines(theLogoName)) {
+      cout << argv[0] << ": sorry, the logo " << theLogoName << " hasn't been installed" << endl;
+      exit(5);
+   }
+   if (all_logos.defines(theTkWindowName)) {
+      cout << argv[0] << " -- sorry, the tk window name " << theTkWindowName << " is already in the dictionary (didn't flush on window close?)" << endl;
+      exit(5);
+   }
+
+   Tk_Window rootTkWindow = (Tk_Window)cd;
+
+   // Obtain the logo from the installed-logos dictionary:
+   assert(all_installed_logos.defines(theLogoName));
+   logoStruct &l = all_installed_logos[theLogoName];
+
+   // create a frame widget:
+   myTclEval(interp, string("frame ") + theTkWindowName +
+	     " -width " + string(l.width + 2*borderPix) +
+	     " -height " + string(l.height + 2*borderPix) +
+	     " -relief " + reliefString +
+	     " -borderwidth " + string(borderPix) +
+	     " -highlightcolor " + colorString);
+
+   // At last, we can obtain the Tk_Window
+   Tk_Window theTkWindow = Tk_NameToWindow(interp, theTkWindowName.string_of(),
+					   rootTkWindow);
+   if (theTkWindow == NULL) {
+      cout << "pdLogo::createCommand() -- sorry, window " << theTkWindowName <<
+	      " does not exist (could not create properly?)" << endl;
+      exit(5);
+   }
+
+   pdLogo *theLogo = new pdLogo(interp, theTkWindow,
+				l.rawData, l.width, l.height);
+      // constructor does nothing terribly interesting
+   assert(theLogo);
+
+   all_logos[theTkWindowName] = theLogo;
+
+   // now create the bindings.  We call the tk "bind" command.  This requires
+   // us to do 2 Tcl_CreateCommand()'s.  We do this here if the commands
+   // don't already exist.
+   // the commands are: "pdLogoDrawCommand" and "pdLogoDestroyCommand"
+   Tcl_CmdInfo dummy;
+   if (0==Tcl_GetCommandInfo(interp, "pdLogoDrawCommand", &dummy))
+      Tcl_CreateCommand(interp, "pdLogoDrawCommand", &pdLogo::drawCallback,
+			NULL, NULL);
+   if (0==Tcl_GetCommandInfo(interp, "pdLogoDestroyCommand", &dummy))
+      Tcl_CreateCommand(interp, "pdLogoDestroyCommand", &pdLogo::destroyCallback,
+			NULL, NULL);
+
+   const string bindStringPrefix = string("bind ") + theTkWindowName + " ";
+   myTclEval(interp, bindStringPrefix + "<Configure> {pdLogoDrawCommand %W}");
+   myTclEval(interp, bindStringPrefix + "<Expose> {pdLogoDrawCommand %W}");
+   myTclEval(interp, bindStringPrefix + "<Destroy> {pdLogoDestroyCommand %W}");
+   return TCL_OK;      
+}
+
+int pdLogo::drawCallback(ClientData, Tcl_Interp *,
+			 int argc, char **argv) {
+   assert(argc == 2);
+
+   const string theTkWindowName = argv[1];
+
+   assert(all_logos.defines(theTkWindowName));
+   pdLogo *pthis = all_logos[theTkWindowName];
+   assert(pthis);
+
+   pthis->draw();
+
+   return TCL_OK;
+}
+
+int pdLogo::destroyCallback(ClientData, Tcl_Interp *,
+			    int argc, char **argv) {
+   // makeLogoCommand rigs things s.t. this is called when the window enclosing
+   // the logo is destroyed.  sent 1 arg: the tk window name
+   assert(argc == 2);
+
+   const string theTkWindowName = argv[1];
+   
+   assert(all_logos.defines(theTkWindowName));
+
+   pdLogo *pthis = all_logos[theTkWindowName];
+   assert(pthis);
+   delete pthis; // destructor does nothing interesting
+
+   all_logos.undef(theTkWindowName); // can't use pthis->theTkWindowName, which
+      // has just been deallocated!
+
+   return TCL_OK;
+}
---
+++ paradyn/src/UIthread/pdLogo.h
@@ -0,0 +1,94 @@
+// pdLogo.h
+
+/*
+ * $Log: pdLogo.h,v $
+ * Revision 1.1  1995/11/29 00:19:35  tamches
+ * first version of pdLogo
+ *
+ */
+
+#ifndef _PD_LOGO_H_
+#define _PD_LOGO_H_
+
+#include <assert.h>
+#include <iostream.h>
+
+#include "tclclean.h"
+#include "tkclean.h"
+#include "tkTools.h"
+
+#include "String.h"
+
+#ifdef PARADYN
+#include "util/h/DictionaryLite.h"
+#else
+#include "DictionaryLite.h"
+#endif
+
+class pdLogo {
+ private:
+   struct logoStruct {
+      void *rawData; unsigned width; unsigned height;
+      bool operator==(const logoStruct &other) const {
+         return rawData == other.rawData;
+      }
+      logoStruct() {} // required by Pair.h
+      logoStruct(int) {} // required by Pair.h
+   };
+   logoStruct theLogoData;
+
+   Tcl_Interp *interp;
+   Tk_Window theTkWindow;
+   string theTkWindowName;
+      // needed since in the destructor, Tk_PathName() may return junk
+      // since the window may (probably?) have been deleted already.
+   Display *theDisplay;
+      // needed in destructor, when Tk_Display(theTkWindow)
+      // may no longer be defined, since the tk window will have been deleted.
+   Pixmap theLogo; // "None" until tryFirst() returns true
+   XColor *foregroundColor, *backgroundColor;
+   GC copyGC; // used only in XCopyArea()
+   unsigned borderPix;
+
+   tkInstallIdle installer;
+
+   static dictionary_lite<string, logoStruct> all_installed_logos;
+   static dictionary_lite<string, pdLogo *> all_logos;
+   static tcl_cmd_installer createPdLogo; // tkTools.C
+
+ private:
+   bool tryFirst();
+   void draw();
+   static void real_draw(ClientData cd);
+
+   // constructor and destructor do nothing terribly interesting
+   pdLogo(Tcl_Interp *, Tk_Window,
+	  void *rawData, unsigned width, unsigned height);
+  ~pdLogo();
+
+   static int drawCallback(ClientData pthis, Tcl_Interp *interp,
+			   int argc, char **argv);
+      // makeLogoCommand rigs things s.t. this is called when the window enclosing
+      // the logo needs redrawing.  sent 1 arg: the tk window name
+
+   static int destroyCallback(ClientData pthis, Tcl_Interp *interp,
+                              int argc, char **argv);
+      // makeLogoCommand rigs things s.t. this is called when the window enclosing
+      // the logo is destroyed.  sent 1 arg: the tk window name
+
+ public:
+   static int install_fixed_logo(const string &str,
+				 void *rawData, unsigned width, unsigned height);
+      // we have no uninstall_fixed_logo since there's not big need for it.
+      // creates the tcl command "createPdLogo" (rigged to makeLogoCommand, below)
+      // if necessary.
+
+   static int makeLogoCommand(ClientData cd, Tcl_Interp *interp,
+			       int argc, char **argv);
+      // args: [0] (implicit) the command name
+      // 1) tk window name   2) logo name (previously installed via install_fixed_logo)
+      // 3) relief           4) border pix       5) color
+      // NOTE: ClientData must point to a Tk_Window created by Tk_CreateMainWindow()
+};
+
+#endif
