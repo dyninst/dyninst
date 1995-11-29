1e1bcdb4094de309027de734e906b37426cc40fa
tamches <tamches>
1995-11-29 00:41:42 +0000

    added lots of new template instantiations needed for pdLogo stuff

--- visiClients/barchart/templates.C
+++ visiClients/barchart/templates.C
@@ -11,3 +11,25 @@ template class dynamic1dArray<bool>;
 template class dynamic2dArray<double>;
 template class dynamic2dArray<int>;
 template class dynamic2dArray<bool>;
+
+#include "minmax.C"
+template float max(float, float);
+
+#include "Vector.h"
+template class vector<unsigned>;
+
+#include "pdLogo.h"
+#include "String.h"
+#include "util/src/DictionaryLite.C"
+template class dictionary_lite<string, pdLogo *>;
+template class vector<string>;
+template class vector<pdLogo *>;
+template class pair<string, pdLogo *>;
+template class vector<dictionary_lite<string, pdLogo *>::hash_pair>;
+template class vector< vector<dictionary_lite<string, pdLogo *>::hash_pair> >;
+
+template class dictionary_lite<string, pdLogo::logoStruct>;
+template class vector<pdLogo::logoStruct>;
+template class vector<dictionary_lite<string, pdLogo::logoStruct>::hash_pair>;
+template class vector< vector<dictionary_lite<string, pdLogo::logoStruct>::hash_pair> >;
+template class pair<string, pdLogo::logoStruct>;
