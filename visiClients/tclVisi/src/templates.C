254c64b6d7473be4c2d40c35879a2e76314411b7
tamches <tamches>
1995-12-01 06:43:24 +0000

    added templates for the new hard-coded logo

--- visiClients/tclVisi/src/templates.C
+++ visiClients/tclVisi/src/templates.C
@@ -6,3 +6,13 @@ class PhaseInfo;
 
 template class vector<PhaseInfo *>;
 
+// logo stuff:
+#include "paradyn/src/UIthread/minmax.C"
+template float max(float, float);
+
+#include "util/h/String.h"
+#include "util/src/DictionaryLite.C"
+#include "pdLogo.h"
+template class dictionary_lite<string, pdLogo *>;
+
+template class dictionary_lite<string, pdLogo::logoStruct>;
