51d9a95232a31886975f82c429079c7be76ce343
tamches <tamches>
1995-12-09 18:35:30 +0000

    put implementation of Status routines into Status.C

--- paradyn/src/UIthread/Status.C
+++ paradyn/src/UIthread/Status.C
@@ -1,20 +1,4 @@
-/************************************************************************
- * Status.c: static data structures for status lines.
-************************************************************************/
-
-
-
-
-
-/************************************************************************
- * header files.
-************************************************************************/
-
 #include "Status.h"
-
-
-
-
 
 /************************************************************************
  * static data structures.
@@ -22,3 +6,78 @@
 
 unsigned    status_line::n_lines_ = 0;
 Tcl_Interp* status_line::interp_  = 0;
+
+/************************************************************************
+ * implementations of member functions
+************************************************************************/
+
+status_line::status_line(const char* title)
+    : id_(n_lines_++) {
+    create(title);
+}
+
+status_line::~status_line() {
+    assert(interp_);
+    char buf[BUF_LENGTH];
+    (void) sprintf(buf, "status_destroy %u", id_);
+    int ret = Tcl_VarEval(interp_, buf, 0);
+    if (ret != TCL_OK) {
+        fprintf(stderr, "status_line::~status_line: command `%s' -> `%s'\n",
+            buf, interp_->result);
+    }
+    // assert(ret == TCL_OK);
+}
+
+void
+status_line::message(const char* msg) {
+    assert(interp_);
+    char buf[BUF_LENGTH];
+    (void) sprintf(buf, "status_message %u {%-*.*s}",
+        id_, (int) MESG_LENGTH, (int) MESG_LENGTH, msg);
+    int ret = Tcl_VarEval(interp_, buf, 0);
+    if (ret != TCL_OK) {
+        fprintf(stderr, "status_line::message: command `%s' -> `%s'\n",
+            buf, interp_->result);
+    }
+    // assert(ret == TCL_OK);
+}
+
+status_line&
+status_line::operator<<(const char* msg) {
+    message(msg);
+    return *this;
+}
+
+void
+status_line::state(State st) {
+    assert(interp_);
+    char buf[BUF_LENGTH];
+    (void) sprintf(buf, "status_state %u %u", id_, (st != NORMAL));
+    int ret = Tcl_VarEval(interp_, buf, 0);
+    if (ret != TCL_OK) {
+        fprintf(stderr, "status_line::state: command `%s' -> `%s'\n",
+            buf, interp_->result);
+    }
+    // assert(ret == TCL_OK);
+}
+
+void
+status_line::status_init(Tcl_Interp* interp) {
+    interp_ = interp;
+}
+
+void
+status_line::create(const char* title) {
+    assert(interp_);
+    char buf[BUF_LENGTH];
+    (void) sprintf(buf, "status_create %u {%-*.*s}",
+        id_, (int) TITLE_LENGTH, (int) TITLE_LENGTH, title);
+    int ret = Tcl_VarEval(interp_, buf, 0);
+    if (ret != TCL_OK) {
+        fprintf(stderr, "status_line::create: command `%s' -> `%s'\n",
+            buf, interp_->result);
+    }
+    // assert(ret == TCL_OK);
+}
+
+
--- paradyn/src/UIthread/Status.h
+++ paradyn/src/UIthread/Status.h
@@ -3,27 +3,14 @@
 ************************************************************************/
 
 
-
-
-
 #if !defined(_Status_h_)
 #define _Status_h_
 
-
-
-
-
-/************************************************************************
- * header files.
-************************************************************************/
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "tclclean.h"
-
-
-
 
 
 /************************************************************************
@@ -35,13 +22,6 @@ static const unsigned BUF_LENGTH     = 256;
 static const unsigned TITLE_LENGTH   = 20;
 static const unsigned MESG_LENGTH    = 200;
 
-
-
-
-
-/************************************************************************
- * class status_line
-************************************************************************/
 
 class status_line {
 public:
@@ -70,85 +50,5 @@ private:
     status_line            (const status_line &); // explicitly private
     status_line& operator= (const status_line &); // explicitly private
 };
-
-inline
-status_line::status_line(const char* title)
-    : id_(n_lines_++) {
-    create(title);
-}
-
-inline
-status_line::~status_line() {
-    assert(interp_);
-    char buf[BUF_LENGTH];
-    (void) sprintf(buf, "status_destroy %u", id_);
-    int ret = Tcl_VarEval(interp_, buf, 0);
-    if (ret != TCL_OK) {
-        fprintf(stderr, "status_line::~status_line: command `%s' -> `%s'\n",
-            buf, interp_->result);
-    }
-    // assert(ret == TCL_OK);
-}
-
-inline
-void
-status_line::message(const char* msg) {
-    assert(interp_);
-    char buf[BUF_LENGTH];
-    (void) sprintf(buf, "status_message %u {%-*.*s}",
-        id_, (int) MESG_LENGTH, (int) MESG_LENGTH, msg);
-    int ret = Tcl_VarEval(interp_, buf, 0);
-    if (ret != TCL_OK) {
-        fprintf(stderr, "status_line::message: command `%s' -> `%s'\n",
-            buf, interp_->result);
-    }
-    // assert(ret == TCL_OK);
-}
-
-inline
-status_line&
-status_line::operator<<(const char* msg) {
-    message(msg);
-    return *this;
-}
-
-inline
-void
-status_line::state(State st) {
-    assert(interp_);
-    char buf[BUF_LENGTH];
-    (void) sprintf(buf, "status_state %u %u", id_, (st != NORMAL));
-    int ret = Tcl_VarEval(interp_, buf, 0);
-    if (ret != TCL_OK) {
-        fprintf(stderr, "status_line::state: command `%s' -> `%s'\n",
-            buf, interp_->result);
-    }
-    // assert(ret == TCL_OK);
-}
-
-inline
-void
-status_line::status_init(Tcl_Interp* interp) {
-    interp_ = interp;
-}
-
-inline
-void
-status_line::create(const char* title) {
-    assert(interp_);
-    char buf[BUF_LENGTH];
-    (void) sprintf(buf, "status_create %u {%-*.*s}",
-        id_, (int) TITLE_LENGTH, (int) TITLE_LENGTH, title);
-    int ret = Tcl_VarEval(interp_, buf, 0);
-    if (ret != TCL_OK) {
-        fprintf(stderr, "status_line::create: command `%s' -> `%s'\n",
-            buf, interp_->result);
-    }
-    // assert(ret == TCL_OK);
-}
-
-
-
-
 
 #endif /* !defined(_Status_h_) */
