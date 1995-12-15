9f6786ce72faedd435ef17ac06194e6cc5ab2991
mjrg <mjrg>
1995-12-15 22:31:08 +0000

    Get module name for functions from symbol table in solaris

--- common/h/Object-elf.h
+++ common/h/Object-elf.h
@@ -35,6 +35,52 @@ extern "C" {
 #include <sys/stat.h>
 }
 
+/***
+   The standard symbol table in an elf file is the .symtab section. This section does
+   not have information to find the module to which a global symbol belongs, so we must
+   also read the .stab section to get this info.
+***/
+
+// Declarations for the .stab section.
+// These are not declared in any system header files, so we must provide our own
+// declarations. The declarations below were taken from:
+//       SPARCWorks 3.0.x Debugger Interface, July 1994/
+
+struct stab_entry { // an entry in the stab section
+  unsigned long name;  // stabstr table index for this symbol
+  unsigned char type; // type of this symbol
+  unsigned char other; 
+  unsigned short desc; 
+  unsigned long val; // value of this symbol -- usually zero. The real value must
+                     // be obtained from the symtab section
+};
+
+// Types 
+#define N_UNDF  0x00 /* start of object file */
+#define N_GSYM  0x20 /* global symbol */
+#define N_FUN   0x24 /* function or procedure */
+#define N_STSYM 0x26 /* initialized static symbol */
+#define N_LCSYM 0x28 /* unitialized static symbol */
+#define N_ROSYM 0x2c /* read-only static symbol */
+#define N_ENDM  0x62 /* end module */
+#define N_SO    0x64 /* source directory and file */
+
+// Language code -- the desc field in a N_SO entry is a language code
+#define N_SO_AS      1 /* assembler source */
+#define N_SO_C       2 /* K & R C source */
+#define N_SO_ANSI_C  3 /* ANSI C source */
+#define N_SO_CC      4 /* C++ source */
+#define N_SO_FORTRAN 5 /* fortran source */
+#define N_SO_PASCAL  6 /* Pascal source */
+
+// Symbol descriptors
+// The format of a name is "<name>:<symbol descriptor><rest of name>
+// The following are the descriptors of interest
+#define SD_GLOBAL_FUN 'F' /* global function or procedure */
+#define SD_PROTOTYPE 'P'  /* function prototypes */
+#define SD_GLOBAL_VAR 'G' /* global variable */
+
+// end of stab declarations
 
 
 
@@ -56,21 +102,10 @@ private:
     void    log_elferror (void (*)(const char *), const char *);
 
     bool      loaded_elf (int, bool &, Elf* &, Elf32_Ehdr* &, Elf32_Phdr* &,
-                          unsigned &, unsigned &, Elf_Scn* &, Elf_Scn* &);
+                          unsigned &, unsigned &, Elf_Scn* &, Elf_Scn* &,
+                          Elf_Scn* &, Elf_Scn* &);
     void     load_object ();
 };
-
-inline
-Object::Object(const string file, void (*err_func)(const char *))
-    : AObject(file, err_func) {
-    load_object();
-}
-
-inline
-Object::Object(const Object& obj)
-    : AObject(obj) {
-    load_object();
-}
 
 inline
 Object::~Object() {
@@ -94,7 +129,7 @@ inline
 bool
 Object::loaded_elf(int fd, bool& did_elf, Elf*& elfp, Elf32_Ehdr*& ehdrp,
     Elf32_Phdr*& phdrp, unsigned& txtaddr, unsigned& bssaddr,
-    Elf_Scn*& symscnp, Elf_Scn*& strscnp) {
+    Elf_Scn*& symscnp, Elf_Scn*& strscnp, Elf_Scn*& stabscnp, Elf_Scn*& stabstrscnp) {
 
     elf_version(EV_CURRENT);
     elf_errno();
@@ -145,7 +180,10 @@ Object::loaded_elf(int fd, bool& did_elf, Elf*& elfp, Elf32_Ehdr*& ehdrp,
         const char* BSS_NAME    = ".bss";
         const char* SYMTAB_NAME = ".symtab";
         const char* STRTAB_NAME = ".strtab";
+        const char* STAB_NAME   = ".stab";
+        const char* STABSTR_NAME= ".stabstr";
         const char* name        = (const char *) &shnames[shdrp->sh_name];
+
         if (strcmp(name, TEXT_NAME) == 0) {
             txtaddr = shdrp->sh_addr;
         }
@@ -157,6 +195,12 @@ Object::loaded_elf(int fd, bool& did_elf, Elf*& elfp, Elf32_Ehdr*& ehdrp,
         }
         else if (strcmp(name, STRTAB_NAME) == 0) {
             strscnp = scnp;
+        }
+        else if (strcmp(name, STAB_NAME) == 0) {
+            stabscnp = scnp;
+        }
+        else if (strcmp(name, STABSTR_NAME) == 0) {
+            stabstrscnp = scnp;
         }
     }
     if (!txtaddr || !bssaddr || !symscnp || !strscnp) {
@@ -196,10 +240,12 @@ Object::load_object() {
         Elf32_Phdr* phdrp   = 0;
         Elf_Scn*    symscnp = 0;
         Elf_Scn*    strscnp = 0;
+        Elf_Scn*    stabscnp = 0;
+        Elf_Scn*    stabstrscnp = 0;
         unsigned    txtaddr = 0;
         unsigned    bssaddr = 0;
         if (!loaded_elf(fd, did_elf, elfp, ehdrp, phdrp, txtaddr,
-            bssaddr, symscnp, strscnp)) {
+            bssaddr, symscnp, strscnp, stabscnp, stabstrscnp)) {
             /* throw exception */ goto cleanup;
         }
 
@@ -238,17 +284,17 @@ Object::load_object() {
         const char* strs   = (const char *) strdatap->d_buf;
         string      module = "DEFAULT_MODULE";
         string      name   = "DEFAULT_NAME";
+
+        // global symbols are put in global_symbols. Later we read the
+        // stab section to find the module to where they belong.
+        dictionary_hash<string, Symbol> global_symbols(string::hash);
+
         for (unsigned i1 = 0; i1 < nsyms; i1++) {
 	  // First, we must check st_shndx. 
 	  // if st_shndx == SHN_UNDEF, this is an undefined symbol,
 	  // probably defined in a shared object that will be dynamically
 	  // linked with the executable. We just ignore it for now.
 	  if (syms[i1].st_shndx != SHN_UNDEF) {
-            Symbol::SymbolLinkage linkage =
-                ((ELF32_ST_BIND(syms[i1].st_info) == STB_LOCAL)
-                ? Symbol::SL_LOCAL
-                : Symbol::SL_GLOBAL);
-
             bool st_kludge = false;
             Symbol::SymbolType type = Symbol::PDST_UNKNOWN;
             switch (ELF32_ST_TYPE(syms[i1].st_info)) {
@@ -270,10 +316,117 @@ Object::load_object() {
             }
 
             name = string(&strs[syms[i1].st_name]);
-            symbols_[name] = Symbol(name, module, type, linkage,
+
+            // We are done with the local symbols. We save the global so that
+            // we can get their module from the .stab section.
+            if (ELF32_ST_BIND(syms[i1].st_info) == STB_LOCAL) {
+	       symbols_[name] = Symbol(name, module, type, Symbol::SL_LOCAL,
                                     syms[i1].st_value, st_kludge);
+	    }
+            else {
+               assert(!(global_symbols.defines(name))); // globals should be unique
+	       global_symbols[name] = Symbol(name, string(""), type, Symbol::SL_GLOBAL,
+                                    syms[i1].st_value, st_kludge);
+	    }
 	  }
         }
+
+        // Read the stab section to find the module of global symbols.
+        // The symbols appear in the stab section by module. A module begins
+        // with a symbol of type N_UNDF and ends with a symbol of type N_ENDM.
+        // All the symbols in between those two symbols belong to the module.
+
+        Elf_Data* stabdatap = elf_getdata(stabscnp, 0);
+        Elf_Data* stabstrdatap = elf_getdata(stabstrscnp, 0);
+        struct stab_entry * stabsyms = (struct stab_entry *) stabdatap->d_buf;
+        unsigned stab_nsyms = stabdatap->d_size / sizeof(struct stab_entry);
+        const char* stabstrs = (const char *) stabstrdatap->d_buf;
+        
+        // the stabstr contains one string table for each module.
+        // stabstr_offset gives the offset from the begining of stabstr of the
+        // string table for the current module.
+        // stabstr_nextoffset gives the offset for the next module.
+        unsigned stabstr_offset = 0;
+        unsigned stabstr_nextoffset = 0;
+
+        bool is_fortran = false;  // is the current module fortran code?
+           /* we must know if we are reading a fortran module because fortran
+              symbol names are different in the .stab and .symtab sections.
+              A symbol that appears as 'foo' in the .stab section, appears
+              as 'foo_' in .symtab.
+           */
+        module = "";
+  
+        for (unsigned i = 0; i < stab_nsyms; i++) {
+	  switch(stabsyms[i].type) {
+	  case N_UNDF: /* start of object file */
+	    assert(stabsyms[i].name == 1);
+	    stabstr_offset = stabstr_nextoffset;
+	    // stabsyms[i].val has the size of the string table of this module.
+	    // We use this value to compute the offset of the next string table.
+	    stabstr_nextoffset = stabstr_offset + stabsyms[i].val;
+	    module = string(&stabstrs[stabstr_offset+stabsyms[i].name]);
+	    break;
+
+	  case N_ENDM: /* end of object file */
+	    is_fortran = false;
+	    module = "";
+	    break;
+
+	  case N_SO: /* compilation source or file name */
+	    if (stabsyms[i].desc == N_SO_FORTRAN)
+	      is_fortran = true;
+	    break;
+
+	  case N_FUN: /* function */
+	  case N_GSYM: /* global symbol */
+	    // the name string of a function or object appears in the stab string table
+	    // as <symbol name>:<symbol descriptor><other stuff>
+	    // where <symbol descriptor> is a one char code.
+	    // we must extract the name and descriptor from the string
+          {
+	    const char *p = &stabstrs[stabstr_offset+stabsyms[i].name];
+	    const char *q = strchr(p,':');
+	    assert(q);
+	    unsigned len = q - p;
+	    assert(len > 0);
+	    char *sname = new char[len+1];
+	    strncpy(sname, p, len);
+	    sname[len] = 0;
+	    
+	    // q points to the ':' in the name string, so 
+	    // q[1] is the symbol descriptor. We must check the symbol descriptor
+	    // here to skip things we are not interested in, such as local functions
+	    // and prototypes.
+	    if (q[1] == SD_GLOBAL_FUN || q[1] == SD_GLOBAL_VAR) { 
+	      string SymName = string(sname);
+	      bool res = global_symbols.defines(SymName);
+	      if (!res && is_fortran) {
+		SymName += "_";
+		res = global_symbols.defines(SymName);
+	      }
+              assert(res); // All globals .stab should be defined in .symtab
+	      Symbol sym = global_symbols[SymName];
+	      global_symbols.undef(SymName);
+	      symbols_[SymName] = Symbol(sym.name(), module, sym.type(), sym.linkage(), 
+				  sym.addr(), sym.kludge());
+	    }
+          }
+	    break;
+
+	  default:
+	    /* ignore other entries */
+	    break;
+	  }
+	}
+
+        /* The remaing symbols go without module */
+        vector<string> k = global_symbols.keys();
+        for (unsigned i1 = 0; i1 < k.size(); i1++) {
+	  Symbol sym = global_symbols[k[i1]];
+	  symbols_[sym.name()] = sym;
+	}
+
     }
 
     /* catch */
@@ -285,6 +438,18 @@ cleanup: {
             log_perror(err_func_, "close");
         }
     }
+}
+
+inline
+Object::Object(const string file, void (*err_func)(const char *))
+    : AObject(file, err_func) {
+    load_object();
+}
+
+inline
+Object::Object(const Object& obj)
+    : AObject(obj) {
+    load_object();
 }
 
 
