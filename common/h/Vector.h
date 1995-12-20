443426947ee5ae18eaceb5474a649a3602455a65
tamches <tamches>
1995-12-20 02:13:06 +0000

    leaner and meaner; use VectorDeluxe.h if necessary

--- common/h/Vector.h
+++ common/h/Vector.h
@@ -46,11 +46,8 @@ public:
     DO_INLINE_F ~vector ();
 
     DO_INLINE_F vector<T>&  operator= (const vector<T> &);
-    DO_INLINE_F vector<T>&  operator= (const T &);
     DO_INLINE_F vector<T>& operator+= (const vector<T> &);
     DO_INLINE_F vector<T>& operator+= (const T &);
-    DO_INLINE_F vector<T>   operator+ (const vector<T> &)                      const;
-    DO_INLINE_F vector<T>   operator- ()                                       const;
 
     DO_INLINE_F T&         operator[] (unsigned)                               const;
     DO_INLINE_F bool       operator== (const vector<T> &)                      const;
@@ -59,11 +56,6 @@ public:
 
 
     DO_INLINE_F void             sort (int (*)(const void *, const void *));
-    DO_INLINE_F bool           sorted (int (*)(const void *, const void *))    const;
-    DO_INLINE_F void              app (void (*)(T &));
-    DO_INLINE_F void           revapp (void (*)(T &));
-    DO_INLINE_F T                fold (T (*)(const T &, const T &), const T &) const;
-    DO_INLINE_F T             revfold (T (*)(const T &, const T &), const T &) const;
 
 private:
     DO_INLINE_P void  create (unsigned);
@@ -122,14 +114,6 @@ vector<T>::operator=(const vector<T>& v) {
 template<class T>
 DO_INLINE_F
 vector<T>&
-vector<T>::operator=(const T& v0) {
-    init(v0);
-    return *this;
-}
-
-template<class T>
-DO_INLINE_F
-vector<T>&
 vector<T>::operator+=(const vector<T>& v) {
     unsigned osz = sz_;
     resize(osz + v.sz_);
@@ -146,25 +130,6 @@ vector<T>::operator+=(const T& v0) {
     resize(sz_+1);
     data_[sz_-1] = v0;
     return *this;
-}
-
-template<class T>
-DO_INLINE_F
-vector<T>
-vector<T>::operator+(const vector<T>& v) const {
-    vector<T> ret = *this;
-    return ret += v;
-}
-
-template<class T>
-DO_INLINE_F
-vector<T>
-vector<T>::operator-() const {
-    vector<T> ret(sz_);
-    for (unsigned i = 0; i < sz_; ++i) {
-        ret.data_[sz_-i-1] = data_[i];
-    }
-    return ret;
 }
 
 template<class T>
@@ -236,61 +201,6 @@ vector<T>::sort(int (*cmpfunc)(const void *, const void *)) {
 }
 
 template<class T>
-DO_INLINE_F
-bool
-vector<T>::sorted(int (*cmpfunc)(const void *, const void *)) const {
-    if (sz_ <= 1) {
-        return true;
-    }
-    for (unsigned i = 0; i < (sz_-1); ++i) {
-        if (cmpfunc(&data_[i], &data_[i+1]) > 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-template<class T>
-DO_INLINE_F
-void
-vector<T>::app(void (*f)(T &)) {
-    for (unsigned i = 0; i < sz_; ++i) {
-        f(data_[i]);
-    }
-}
-
-template<class T>
-DO_INLINE_F
-void
-vector<T>::revapp(void (*f)(T &)) {
-    for (int i = sz_-1; i >= 0; --i) {
-        f(data_[i]);
-    }
-}
-
-template<class T>
-DO_INLINE_F
-T
-vector<T>::fold(T (*f)(const T &, const T &), const T& arg0) const {
-    T ret = arg0;
-    for (int i = sz_-1; i >= 0; --i) {
-        ret = f(data_[i], ret);
-    }
-    return ret;
-}
-
-template<class T>
-DO_INLINE_F
-T
-vector<T>::revfold(T (*f)(const T &, const T &), const T& arg0) const {
-    T ret = arg0;
-    for (unsigned i = 0; i < sz_; ++i) {
-        ret = f(ret, data_[i]);
-    }
-    return ret;
-}
-
-template<class T>
 DO_INLINE_P
 void
 vector<T>::create(unsigned sz) {
@@ -325,42 +235,5 @@ vector<T>::destroy() {
     delete[] data_; data_ = 0;
     sz_ = tsz_ = 0;
 }
-
-template<class T, class U>
-DO_INLINE_F
-vector<U>
-map(U (*f)(const T &), const vector<T>& v) {
-    vector<U> ret(sz_);
-    for (unsigned i = 0; i < sz_; ++i) {
-        ret[i] = f(data_[i]);
-    }
-    return ret;
-}
-
-template<class T, class U>
-DO_INLINE_F
-U
-fold(U (*f)(const T &, const U &), const vector<T>& v, const U& arg0) {
-    U ret = arg0;
-    for (int i = sz_-1; i >= 0; --i) {
-        ret = f(data_[i], ret);
-    }
-    return ret;
-}
-
-template<class T, class U>
-DO_INLINE_F
-U
-revfold(U (*f)(const U &, const T &), const vector<T>& v, const U& arg0) {
-    U ret = arg0;
-    for (unsigned i = 0; i < sz_; ++i) {
-        ret = f(ret, data_[i]);
-    }
-    return ret;
-}
-
-
-
-
 
 #endif /* !defined(_Vector_h_) */
