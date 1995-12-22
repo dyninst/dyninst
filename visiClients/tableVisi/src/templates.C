2e8d24b9f8a11228803035ba2c2292942488a345
tamches <tamches>
1995-12-22 22:37:43 +0000

    2 new instantiations

--- visiClients/tableVisi/src/templates.C
+++ visiClients/tableVisi/src/templates.C
@@ -3,6 +3,9 @@
 
 /*
  * $Log: templates.C,v $
+ * Revision 1.4  1995/12/22 22:37:43  tamches
+ * 2 new instantiations
+ *
  * Revision 1.3  1995/11/29 00:43:56  tamches
  * added lots of templates needed for new pdLogo stuff
  *
@@ -33,6 +36,8 @@ template unsigned max(unsigned, unsigned);
 template float max(float, float);
 template bool ipmax(unsigned, unsigned);
 template int ipmin(int, int);
+template int min(int, int);
+template int max(int, int);
 
 #include "util/src/DictionaryLite.C"
 #include "paradyn/src/UIthread/pdLogo.h"
