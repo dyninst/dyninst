bc7ce57ad37e6553bf15e6b37320d76727b63f6a
tamches <tamches>
1995-12-29 08:17:58 +0000

    a cleanup commit: moved body of some member functions from tvCell.h to tvCell.C

--- visiClients/tableVisi/src/tvCell.C
+++ visiClients/tableVisi/src/tvCell.C
@@ -3,9 +3,67 @@
 
 /*
  * $Log: tvCell.C,v $
+ * Revision 1.2  1995/12/29 08:17:59  tamches
+ * a cleanup commit: moved body of some member functions from tvCell.h to tvCell.C
+ *
  * Revision 1.1  1995/11/04 00:47:22  tamches
  * First version of new table visi
  *
  */
 
 #include "tvCell.h"
+
+bool tvCell::operator<(const tvCell &src) const {
+   // let's work out a sorting order for valid v. invalid data right now:
+   // let's say that valid data is always "greater than" any invalid data,
+   // and that all invalid data are equal.
+   if (validData)
+      if (src.validData)
+         // yea, a sane comparison is possible
+         return data < src.data;
+      else
+         // we are sane but "src" is nuts.  valid data is always > invalid data, so:
+         return false;
+   else
+      if (src.validData)
+         // we are nuts; "src" is sane.  valid data > invalid data, so:
+         return true;
+      else
+         // everyone's nuts; all invalid data are equal, so:
+         return false;
+}
+
+bool tvCell::operator==(const tvCell &src) const {
+   if (validData)
+      if (src.validData)
+         return data == src.data;
+      else
+         // valid data > invalid data, so:
+         return false;
+   else
+      if (src.validData)
+         // valid data > invalid data, so:
+         return false;
+      else
+         // invalid data are equal, so:
+         return true;
+}
+
+bool tvCell::operator>(const tvCell &src) const {
+   if (validData)
+      if (src.validData)
+         return data > src.data;
+      else
+         return true; // valid data > invalid data
+   else
+      if (src.validData)
+         return false; // valid data > invalid data
+      else
+         return false; // invalid == invalid
+}
+
+tvCell &tvCell::operator=(const tvCell &src) {
+   if (validData = src.validData) // yes, single = on purpose
+      data = src.data;
+   return *this;
+}
--- visiClients/tableVisi/src/tvCell.h
+++ visiClients/tableVisi/src/tvCell.h
@@ -3,6 +3,9 @@
 
 /*
  * $Log: tvCell.h,v $
+ * Revision 1.3  1995/12/29 08:17:58  tamches
+ * a cleanup commit: moved body of some member functions from tvCell.h to tvCell.C
+ *
  * Revision 1.2  1995/12/22 22:38:30  tamches
  * operator<, operator>, operator== are new; chose a rule that an invalid
  * cell is less than any valid cell, and all invalid cells are equal.
@@ -31,58 +34,11 @@ class tvCell {
    }
   ~tvCell() {}
 
-   bool operator<(const tvCell &src) {
-      // let's work out a sorting order for valid v. invalid data right now:
-      // let's say that valid data is always "greater than" any invalid data,
-      // and that all invalid data are equal.
-      if (validData)
-         if (src.validData)
-            // yea, a sane comparison is possible
-            return data < src.data;
-         else
-            // we are sane but "src" is nuts.  valid data is always > invalid data, so:
-            return false;
-      else
-         if (src.validData)
-            // we are nuts; "src" is sane.  valid data > invalid data, so:
-            return true;
-         else
-            // everyone's nuts; all invalid data are equal, so:
-            return false;
-   }
-   bool operator==(const tvCell &src) {
-      if (validData)
-         if (src.validData)
-            return data == src.data;
-         else
-            // valid data > invalid data, so:
-            return false;
-      else
-         if (src.validData)
-            // valid data > invalid data, so:
-            return false;
-         else
-            // invalid data are equal, so:
-            return true;
-   }
-   bool operator>(const tvCell &src) {
-      if (validData)
-         if (src.validData)
-            return data > src.data;
-         else
-            return true; // valid data > invalid data
-      else
-         if (src.validData)
-            return false; // valid data > invalid data
-         else
-            return false; // invalid == invalid
-   }
+   bool operator<(const tvCell &src) const;
+   bool operator==(const tvCell &src) const;
+   bool operator>(const tvCell &src) const;
 
-   tvCell &operator=(const tvCell &src) {
-      if (validData = src.validData) // yes, single = on purpose
-         data = src.data;
-      return *this;
-   }
+   tvCell &operator=(const tvCell &src);
 
    bool isValid() const {return validData;}
    double getData() const {assert(validData); return data;}
