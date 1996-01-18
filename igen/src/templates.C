58368afd95d2eaa6f4b9a657d35dd6a50639028f
hollings <hollings>
1996-01-18 16:35:54 +0000

    Added extra  items for the AIX linker.

--- igen/src/templates.C
+++ igen/src/templates.C
@@ -1,4 +1,11 @@
 
+/*
+ * $Log: templates.C,v $
+ * Revision 1.5  1996/01/18 16:35:54  hollings
+ * Added extra  items for the AIX linker.
+ *
+ *
+ */
 #include "util/h/headers.h"
 
 #pragma implementation "Vector.h"
@@ -15,6 +22,11 @@ template class vector<string>;
 template class vector<Options::el_data>;
 template class vector<Options::stl_data>;
 template class vector<type_defn *>;
+
+template class dictionary_iter<string, remote_func *>;
+template class dictionary_iter<string, type_defn *>;
+template class dictionary<string, type_defn *>;
+template class dictionary<string, remote_func *>;
 
 template class dictionary_hash<string, remote_func*>;
 template class dictionary_hash<string, type_defn*>;
