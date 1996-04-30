28fe93a80e8cddc2250661b5e0218e82abea1579
lzheng <lzheng>
1996-04-30 16:27:24 +0000

    Used #define for the timer function to be instrumented.

--- dyninstAPI/src/hpux.h
+++ dyninstAPI/src/hpux.h
@@ -9,4 +9,10 @@
 #define EXIT_NAME "_exitcu"
 //#define EXIT_NAME "_exit"
 
+
+#define START_WALL_TIMER "DYNINSTstartWallTimer_hpux"
+#define STOP_WALL_TIMER  "DYNINSTstopWallTimer_hpux"
+#define START_PROC_TIMER "DYNINSTstartProcessTimer_hpux"
+#define STOP_PROC_TIMER  "DYNINSTstopProcessTimer_hpux" 
+
 #endif
