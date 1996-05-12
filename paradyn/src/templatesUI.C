e4762bdae9e30d65aa5a73dccc50514e8fdb1e15
tamches <tamches>
1996-05-12 04:42:35 +0000

    split off some contents into the new templatesUIWASHG.C file

--- paradyn/src/templatesUI.C
+++ paradyn/src/templatesUI.C
@@ -5,13 +5,18 @@
 
 #pragma implementation "list.h"
 #include "util/h/list.h"
+
 #include "util/h/String.h"
+
 #pragma implementation "Vector.h"
 #include "util/h/Vector.h"
-#pragma implementation "Queue.h"
-#include "util/h/Queue.h"
+
+//#pragma implementation "Queue.h"
+//#include "util/h/Queue.h"
+
 #pragma implementation "Dictionary.h"
 #include "util/h/Dictionary.h"
+
 #pragma implementation "DictionaryLite.h"
 #include "util/src/DictionaryLite.C"
 
@@ -51,59 +56,6 @@ template class vector< dictionary_hash<unsigned, string *>::hash_pair >;
 template class dictionary_lite<unsigned, string>;
 template class vector< dictionary_lite<unsigned, string>::hash_pair >;
 template class vector< vector< dictionary_lite<unsigned, string>::hash_pair > >;
-
-/* *************************************
- * UIthread WhereAxis stuff
- */
-
-#include "paradyn/src/UIthread/where4tree.C"
-#include "paradyn/src/UIthread/rootNode.h"
-template class where4tree<whereAxisRootNode>;
-template class vector<const whereAxisRootNode *>;
-template class vector<where4tree<whereAxisRootNode>::childstruct>;
-template class dictionary_lite< resourceHandle, where4tree<whereAxisRootNode> * >;
-template class vector<where4tree<whereAxisRootNode> *>;
-template class vector<dictionary_lite<resourceHandle,where4tree<whereAxisRootNode>*>::hash_pair>;
-template class vector< vector< dictionary_lite<resourceHandle,where4tree<whereAxisRootNode>*>::hash_pair> >;
-
-#include "paradyn/src/UIthread/graphicalPath.C"
-template class whereNodeGraphicalPath<whereAxisRootNode>;
-
-#include "paradyn/src/UIthread/simpSeq.C"
-template class simpSeq<unsigned>;
-
-#include "paradyn/src/UIthread/abstractions.h"
-template class vector<abstractions::whereAxisStruct>;
-
-/* *************************************
- * UIthread Search History Graph stuff
- */
-
-#include "paradyn/src/UIthread/shgRootNode.h"
-template class where4tree<shgRootNode>;
-template class vector<where4tree<shgRootNode>::childstruct>;
-template class whereNodeGraphicalPath<shgRootNode>;
-template class vector<const shgRootNode *>;
-
-#include "paradyn/src/UIthread/shgPhases.h"
-template class vector<shgPhases::shgStruct>;
-
-template class dictionary_lite<unsigned, where4tree<shgRootNode> *>;
-template class vector<where4tree<shgRootNode> *>;
-template class vector<dictionary_lite<unsigned, where4tree<shgRootNode> *>::hash_pair>;
-template class vector< vector< dictionary_lite<unsigned, where4tree<shgRootNode> *>::hash_pair> >;
-
-template class dictionary_lite<where4tree<shgRootNode> *, where4tree<shgRootNode> *>;
-template class vector<dictionary_lite<where4tree<shgRootNode> *, where4tree<shgRootNode> *>::hash_pair>;
-template class vector< vector< dictionary_lite<where4tree<shgRootNode> *, where4tree<shgRootNode> *>::hash_pair> >;
-
-template class dictionary_lite< unsigned, vector<where4tree<shgRootNode>*> >;
-template class vector< vector<dictionary_lite< unsigned, vector<where4tree<shgRootNode>*> > :: hash_pair > >;
-template class vector<dictionary_lite< unsigned, vector<where4tree<shgRootNode>*> > :: hash_pair >;
-template class vector< vector<where4tree<shgRootNode>*> >;
-
-
-template class vector<Tk_3DBorder>; // shg consts
 
 /* *************************************
  * UIthread Logo Stuff
