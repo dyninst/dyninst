d3b956db41aff3bafd89f0fa014b03900a5ee7b7
mjrg <mjrg>
1996-08-12 16:31:55 +0000

    Removed cm5 files

--- paradynd/src/primitives.h
+++
@@ -1,7 +0,0 @@
-
-/*
- * process a sample ariving from an inferrior
- *
- */
-void processSample(traceHeader*, traceSample *);
-void processCM5Sample(traceHeader *h, traceSample *s,int num_processes);
