2d1a2bbf0223abab29ea70c55f8daa99be11549d
sec <sec>
1997-02-18 21:24:09 +0000

    Added a field to instPoint, which identifies what the instrumentation point is for (an enum saying ipFuncEntry, ipFuncReturn, ipFuncCallPoint).

--- dyninstAPI/src/inst-power.h
+++ dyninstAPI/src/inst-power.h
@@ -45,6 +45,10 @@
 
 /*
  * $Log: inst-power.h,v $
+ * Revision 1.4  1997/02/18 21:24:09  sec
+ * Added a field to instPoint, which identifies what the instrumentation
+ * point is for (an enum saying ipFuncEntry, ipFuncReturn, ipFuncCallPoint).
+ *
  * Revision 1.3  1997/01/27 19:40:48  naim
  * Part of the base instrumentation for supporting multithreaded applications
  * (vectors of counter/timers) implemented for all current platforms +
@@ -92,5 +96,43 @@ extern trampTemplate withArgsTemplate;
                                   /* for each thread.                      */
 #define NUM_INSN_MT_PREAMBLE 21   /* number of instructions required for   */
                                   /* the MT preamble.                      */ 
+
+enum ipFuncLoc { ipFuncEntry, ipFuncReturn, ipFuncCallPoint };
+
+class instPoint {
+public:
+  instPoint(pdFunction *f, const instruction &instr, const image *owner,
+	    const Address adr, const bool delayOK, const ipFuncLoc);
+
+  ~instPoint() {  /* TODO */ }
+
+  // can't set this in the constructor because call points can't be classified until
+  // all functions have been seen -- this might be cleaned up
+  void set_callee(pdFunction *to) { callee = to; }
+
+
+  Address addr;                   /* address of inst point */
+  instruction originalInstruction;    /* original instruction */
+
+//  instruction delaySlotInsn;  /* original instruction */
+//  instruction aggregateInsn;  /* aggregate insn */
+//  bool inDelaySlot;            /* Is the instruction in a delay slot */
+//  bool isDelayed;		/* is the instruction a delayed instruction */
+
+  bool callIndirect;		/* is it a call whose target is rt computed ? */
+
+//  bool callAggregate;		/* calling a func that returns an aggregate
+//				   we need to reolcate three insns in this case
+//				   */
+
+  pdFunction *callee;		/* what function is called */
+  pdFunction *func;		/* what function we are inst */
+  
+  ipFuncLoc ipLoc;
+};
+
+
+bool isCallInsn(const instruction);
+bool isReturnInsn(const image *, Address);
 
 #endif
