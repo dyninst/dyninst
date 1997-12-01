b6e2e2d50542d0b61b39d4053fef55976db5d365
tung <tung>
1997-12-01 02:27:52 +0000

    For Linux/X86 platform

--- dyninstAPI/src/arch-x86.h
+++ dyninstAPI/src/arch-x86.h
@@ -43,6 +43,9 @@
  * arch-x86.h - x86 instruction declarations
  *
  * $Log: arch-x86.h,v $
+ * Revision 1.4  1997/12/01 02:27:52  tung
+ * For Linux/X86 platform
+ *
  * Revision 1.3  1997/02/26 23:42:46  mjrg
  * First part on WindowsNT port: changes for compiling with Visual C++;
  * moved unix specific code to unix.C
@@ -59,7 +62,7 @@
  *
  */
 
-#if !(defined(i386_unknown_solaris2_5) || defined(i386_unknown_nt4_0))
+#if !(defined(i386_unknown_solaris2_5) || defined(i386_unknown_nt4_0) || defined(i386_unknown_linux2_0))
 #error "invalid architecture-os inclusion"
 #endif
 
