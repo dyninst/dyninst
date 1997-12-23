e606780bb2fcab7482cfcb4d354d1340d1b0f489
wylie <wylie>
1997-12-23 19:18:33 +0000

    Incorporation of dynInstAPI within NT build

--- make-nt.bat
+++ make-nt.bat
@@ -1,9 +1,9 @@
-rem
 rem script for building paradyn on NT platform
-rem
 
 cmd /c "cd util\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd igen\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd rtinst\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd paradynd\%PLATFORM% && nmake clean && nmake install"
+cmd /c "cd dyninstAPI\%PLATFORM% && nmake clean && nmake install"
+cmd /c "cd dyninstAPI_RT\%PLATFORM% && nmake clean && nmake install"
 
