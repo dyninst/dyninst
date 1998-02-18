07e275abc93a5a1a3c86da52a278977f4b366d36
ssuen <ssuen>
1998-02-18 02:37:17 +0000

    Library calls are not call sites.

--- dyninstAPI/src/inst-power.C
+++ dyninstAPI/src/inst-power.C
@@ -264,7 +264,7 @@ void pd_Function::checkCallPoints() {
     if(isCallInsn(p->originalInstruction)) {
       loc_addr = p->addr + (p->originalInstruction.iform.li << 2);
       pd_Function *pdf = (file_->exec())->findFunction(loc_addr);
-      if (pdf) {
+      if (pdf && !pdf->isLibTag()) {
         p->callee = pdf;
         non_lib += p;
       } else {
