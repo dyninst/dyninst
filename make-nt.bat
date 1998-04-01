ac66810571f4eb3e8bb07ace0eed6877286e98be
wylie <wylie>
1998-04-01 01:30:08 +0000

    Added dyninstAPI tests to build

--- make-nt.bat
+++ make-nt.bat
@@ -1,4 +1,4 @@
-rem script for building paradyn on NT platform
+rem script for building Paradyn & DynInstAPI on WinNT platform
 
 cmd /c "cd util\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd igen\%PLATFORM% && nmake clean && nmake install"
@@ -6,4 +6,5 @@ cmd /c "cd rtinst\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd paradynd\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd dyninstAPI\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd dyninstAPI_RT\%PLATFORM% && nmake clean && nmake install"
+cmd /c "cd dyninstAPI\tests\%PLATFORM% && nmake clean && nmake install"
 
