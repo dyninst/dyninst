5d48e5a22992c26e3b6d4a11268157a3b1d43eac
wylie <wylie>
1998-09-02 21:12:34 +0000

    Tidy

--- pdutil/src/templates.C
+++ pdutil/src/templates.C
@@ -41,16 +41,7 @@
 
 /*
  * Generate code for template classes used by libutil
- *
- * templates.C,v
- * Revision 1.2  1995/06/02  21:00:09  newhall
- * added a NaN value generator
- * fixed memory leaks in Histogram class
- * added newValue member with a vector<sampleInfo *> to class sampleInfo
- *
- * Revision 1.1  1994/09/22  03:21:14  markc
- * generate libutil template code
- *
+ * $Id: templates.C,v 1.7 1998/09/02 21:12:34 wylie Exp $
  */
 
 #pragma implementation "Vector.h"
