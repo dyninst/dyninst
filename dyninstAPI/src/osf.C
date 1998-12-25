2a3f019816a0f1c19251ad80f44bd0c8f336db28
wylie <wylie>
1998-12-25 22:29:02 +0000

    ptrace() fix-up and Address and Register typing.

--- dyninstAPI/src/osf.C
+++ dyninstAPI/src/osf.C
@@ -1,3 +1,45 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: osf.C,v 1.3 1998/12/25 22:29:02 wylie Exp $
 
 #include "util/h/headers.h"
 #include "os.h"
@@ -22,7 +64,6 @@
 #define PC_REGNUM 31
 extern bool exists_executable(const string &fullpathname);
 
-int P_ptrace(int, int, int, int);
 
 int getNumberOfCPUs()
 {
@@ -30,16 +71,16 @@ int getNumberOfCPUs()
 }
 
 
-bool process::emitInferiorRPCheader(void *insnPtr,Address &baseBytes) {
+bool process::emitInferiorRPCheader(void *insnPtr, unsigned &baseBytes) {
 
   return true;
 }
 
-bool process::emitInferiorRPCtrailer(void *insnPtr,Address &baseBytes,
-				     Address &breakOffset,
+bool process::emitInferiorRPCtrailer(void *insnPtr, unsigned &baseBytes,
+				     unsigned &breakOffset,
 				     bool stopForResult,
-				     Address &stopForResultOffset,
-				     Address &justAfter_stopForResultOffset) {
+				     unsigned &stopForResultOffset,
+				     unsigned &justAfter_stopForResultOffset) {
   instruction *insn = (instruction *)insnPtr;
   unsigned baseInstruc = baseBytes / sizeof(instruction);
 
@@ -68,7 +109,7 @@ bool process::emitInferiorRPCtrailer(void *insnPtr,Address &baseBytes,
   return true;
 }
 
-unsigned process::read_inferiorRPC_result_register(reg) {
+Address process::read_inferiorRPC_result_register(Register) {
   
   gregset_t theIntRegs;
   if (-1 == ioctl(proc_fd, PIOCGREG, &theIntRegs)) {
@@ -85,7 +126,7 @@ unsigned process::read_inferiorRPC_result_register(reg) {
 //void OS::osTraceMe(void) { P_ptrace(PT_TRACE_ME, 0, 0, 0); }
 
 
-bool process::needToAddALeafFrame(Frame current_frame,Address &leaf_pc){
+bool process::needToAddALeafFrame(Frame current_frame, Address &leaf_pc){
     return false;
 }
 
@@ -93,7 +134,7 @@ bool process::needToAddALeafFrame(Frame current_frame,Address &leaf_pc){
 // return the current frame pointer (fp) and program counter (pc). 
 //    returns true if we are able to read the regsiters.
 //
-bool process::getActiveFrame(int *fp, int *pc)
+bool process::getActiveFrame(Address *fp, Address *pc)
 {
   gregset_t theIntRegs;
   bool ok=false;
@@ -312,7 +353,8 @@ int process::waitProcs(int *status) {
 //     (2) the return address of the function for that frame (rtn).
 //     (3) return true if we are able to read the frame.
 //
-bool process::readDataFromFrame(int currentFP, int *fp, int *rtn,bool uppermost = False)
+bool process::readDataFromFrame(Address currentFP, Address *fp, Address *rtn,
+                                bool uppermost = False)
 {
 	return false;
 }
@@ -323,12 +365,12 @@ bool process::dumpCore_(const string coreFile) {
   ptraceOps++; ptraceOtherOps++;
   char temp[255];
   char name[] = "./test1.mutatee";
-  sprintf(temp,"file = %s",coreFile);
+  sprintf(temp,"file = %s",coreFile.string_of());
   logLine(temp);
   assert(osDumpImage(name, pid, NULL));
   //  assert(OS::osDumpImage(symbols->file(), pid, symbols->codeOffset()));
   errno = 0;
-  (void) ptrace(PT_CONTINUE, pid, (int*)1, SIGBUS, NULL);
+  (void) ptrace(PT_CONTINUE, pid, (Address*)1, SIGBUS);
   assert(errno == 0);
   return true;
 }
@@ -490,7 +532,7 @@ bool process::osDumpImage(const string &imageFileName,  pid_t pid, Address codeO
 
     
     if (needsCont) {
-	ptrace(PT_CONTINUE, pid, (int*) 1, SIGCONT, 0);
+	ptrace(PT_CONTINUE, pid, (Address*) 1, SIGCONT);
     }
 
     close(ofd);
