d6ae28484bee43e90755e5cf6b9105ae3946d4ae
wylie <wylie>
1998-12-25 22:13:18 +0000

    Consistent use of Address type.

--- dyninstAPI/src/sharedobject.h
+++ dyninstAPI/src/sharedobject.h
@@ -1,7 +1,51 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: sharedobject.h,v 1.16 1998/12/25 22:13:18 wylie Exp $
+
 #if !defined(_shared_object_h)
 #define _shared_object_h
 
 #include "util/h/String.h"
+#include "util/h/Types.h"
 #include "dyninstAPI/src/symtab.h"
 #include "dyninstAPI/src/symtab.h"
 #ifndef BPATCH_LIBRARY
@@ -21,7 +65,7 @@ class shared_object {
 public:
     shared_object():name(0),short_name(0),base_addr(0),processed(false),
 	 mapped(false),include_funcs(true), objs_image(0),some_funcs(0){}
-    shared_object(string &n,u_int b, bool p,bool m, bool i, image *d):
+    shared_object(string &n, Address b, bool p,bool m, bool i, image *d):
 	name(n), base_addr(b),processed(p),mapped(m),
 	include_funcs(i), objs_image(d),some_funcs(0){ 
         set_short_name();
@@ -40,7 +84,7 @@ public:
 
     const string &getName(){ return(name); }
     const string &getShortName() { return short_name; }
-    u_int getBaseAddress() { return(base_addr); }
+    Address getBaseAddress() { return(base_addr); }
     bool  isProcessed() { return(processed); }
     bool  isMapped() { return(mapped); }
     const image  *getImage() { return(objs_image); }
@@ -49,7 +93,7 @@ public:
     void changeIncludeFuncs(bool flag){ include_funcs = flag; } 
 
     void  unMapped(){ mapped = false; }
-    void  setBaseAddress(u_int new_ba){ base_addr = new_ba; }
+    void  setBaseAddress(Address new_ba){ base_addr = new_ba; }
 
     bool  getSymbolInfo(const string &n,Symbol &info) {
         if(objs_image) {
@@ -97,7 +141,11 @@ public:
     }
     bool removeImage(){ return true;}
 
-    pd_Function *findOneFunction(string f_name,bool check_excluded){
+    pd_Function *findOneFunction(string f_name, bool 
+#ifndef BPATCH_LIBRARY
+				 check_excluded
+#endif
+				 ){
 	if (f_name.string_of() == 0) return 0;
         if(objs_image) {
 #ifndef BPATCH_LIBRARY
@@ -119,7 +167,11 @@ public:
 	return 0;
     }
 
-    pd_Function *findOneFunctionFromAll(string f_name,bool check_excluded){
+    pd_Function *findOneFunctionFromAll(string f_name, bool 
+#ifndef BPATCH_LIBRARY
+					check_excluded
+#endif
+					){
 	if (f_name.string_of() == 0) return 0;
         if(objs_image) {
 #ifndef BPATCH_LIBRARY
@@ -174,7 +226,7 @@ private:
     string  name;	// full file name of the shared object
     string  short_name; // name of shared object as it should be identified
 			//  in mdl, e.g. as used for "exclude"....
-    u_int   base_addr;  // base address of where the shared object is mapped
+    Address   base_addr;// base address of where the shared object is mapped
     bool    processed;  // if true, daemon has processed the shared obj. file
     bool    mapped;     // if true, the application has the shared obj. mapped
 			// shared objects can be unmapped as the appl. runs
