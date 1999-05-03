b7a8199eeb258432a256df0b18715f858420b098
zandy <zandy>
1999-05-03 20:04:57 +0000

    Sources for shared libraries used in test1.

---
+++ dyninstAPI/tests/src/libtestA.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// These are copied in test1.mutatee.c and libtestB.c
+#define MAGIC22_1   2200100
+#define MAGIC22_2   2200200
+#define MAGIC22_3   2200300
+#define MAGIC22_4   2200400
+#define MAGIC22_5A  2200510
+#define MAGIC22_5B  2200520
+#define MAGIC22_6   2200600
+#define MAGIC22_7   2200700
+
+extern int globalVariable22_1;
+extern int globalVariable22_2;
+extern int globalVariable22_3;
+extern int globalVariable22_4;
+
+// Keep this function at the start of this file to kludgily ensure
+// that its base address differs from its counterpart in libtestB.c
+void call21_1()
+{
+     printf("This function was not meant to be called!\n");
+}
+
+void call22_4(int x)
+{
+     globalVariable22_2 += x;
+     globalVariable22_2 += MAGIC22_4;
+}
+
+void call22_5(int x)
+{
+     globalVariable22_3 += x;
+     globalVariable22_3 += MAGIC22_5A;
+}
+
+void call22_6(int x)
+{
+     globalVariable22_4 += x;
+     globalVariable22_4 += MAGIC22_6;
+}
