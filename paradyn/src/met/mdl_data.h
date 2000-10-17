83d9ff28ab15633d9365d1499f6f70ef948e1109
schendel <schendel>
2000-10-17 17:26:34 +0000

    Restructuring of declararations required because of include conflicts when declarations were included in the paradyn daemon.

--- paradyn/src/met/mdl_data.h
+++ paradyn/src/met/mdl_data.h
@@ -39,14 +39,45 @@
  * incur to third parties resulting from your use of Paradyn.
  */
 
-// $Id: globals.h,v 1.13 2000/10/17 17:26:34 schendel Exp $
+#ifndef _MDL_DATA_H
+#define _MDL_DATA_H
 
-#ifndef _MDL_GLOBALS_H
-#define _MDL_GLOBALS_H
+// The daemon needs to include this file at times.
+
 
 #include "dyninstRPC.xdr.h"
-#include "../DMthread/DMinclude.h"
-#include "mdl_data.h"
+
+class mdl_data {
+public:
+  static dictionary_hash<unsigned, vector<mdl_type_desc> > fields;
+  static vector<mdl_focus_element> foci;
+  static vector<T_dyninstRPC::mdl_stmt*> stmts;
+  static vector<T_dyninstRPC::mdl_constraint*> all_constraints;
+  static vector<string> lib_constraints;
+  static vector<unsigned> lib_constraint_flags;
+
+  static T_dyninstRPC::mdl_constraint *new_constraint(string id, vector<string> *path,
+					       vector<T_dyninstRPC::mdl_stmt*> *stmts,
+					       bool replace, u_int data_type);
+
+  static vector<T_dyninstRPC::mdl_metric*> all_metrics;
+  static bool new_metric(string id, string name, string units,
+			 u_int agg, u_int style, u_int type,
+			 vector<T_dyninstRPC::mdl_stmt*> *stmts, 
+			 vector<string> *flavs,
+			 vector<T_dyninstRPC::mdl_constraint*> *cons,
+		 vector<string> *temp_counters,
+			 bool developerMode,
+			 int normalized);
+
+
+  // unique_name: prepares for the declaration of a new mdl object,
+  // by deleting all objects previously declared that have the same
+  // name.
+  static void unique_name(string name);
+
+
+};
 
 
 #endif
