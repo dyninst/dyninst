adf74ceae0da07f76685b3ae91a78bf497863496
schendel <schendel>
2001-06-20 20:28:49 +0000

    Added initialization flag to timeStamp class.  Added relTimeStamp class.   Fixed some bugs in printing of timeStamp.

--- common/src/Time.C
+++ common/src/Time.C
@@ -111,6 +111,12 @@ int64_t timeParent::getRolloverTime(double dt) {
 const timeStamp *timeStamp::_ts1970 = NULL;
 const timeStamp *timeStamp::_tsStd  = NULL;
 const timeStamp *timeStamp::_ts2200  = NULL;
+// This 88 quintillion number is some 280 years.  Since a timeStamp is
+// internally based at the year of timeBase::StdBaseMark, ie. year 2000, the
+// uninitialized value represents a timeStamp around year 2280, plenty safe
+// out of range for a long time.  We want the same number, eg. 8, for every
+// digit of this number so it can be easily recognized from a debugger.
+const int64_t    timeStamp::uninitializedValue = I64_C(8888888888888888888);
 
 const timeStamp *timeStamp::ts1970Help() {
   return new timeStamp(0, timeUnit::sec(), timeBase::b1970());
@@ -153,6 +159,21 @@ void timeLength::initI(int64_t iTime, const timeUnit &u) {
   */
 }
 
+void relTimeStamp::initI(int64_t iTime, const timeUnit &u) {
+  assign( u.cvtTo_ns(iTime));
+  /*  // if we allow exceptions in the future, we will be able to handle
+      // nicely cases of overflows in the fraction class  
+      } catch(finalMultOverflowExc &) {
+      double dRes= static_cast<double>(u.cvtTo_ns(static_cast<double>(iTime)));
+      assign( getRolloverTime(dRes));
+      }
+  */
+}
+
+timeStamp::timeStamp(const timeLength &tl, timeBase b) : timeParent() {
+  initI(tl.getI(timeUnit::ns()), timeUnit::ns(), b);
+}
+
 timeStamp::timeStamp(double dTime, const timeUnit &u, timeBase b): timeParent()
 {
   double dRes = static_cast<double>(b.cvtTo_bStd( u.cvtTo_ns(dTime)));
@@ -162,10 +183,38 @@ timeStamp::timeStamp(double dTime, const timeUnit &u, timeBase b): timeParent()
     assign( getRolloverTime(dRes));
 }
 
+const relTimeStamp *relTimeStamp::_Zero      = NULL;
+// This 88 quintillion number is some 280 years.  For this sentinal value to
+// be incorrectly used as a relTimeStamp, a relTimeStamp of 280 years from
+// the user defined zero mark would have to be defined.  We decided the
+// benefit of having this sentinal value outweighs the risk of this error
+// occuring.  We want the same number, eg. 8, for every digit of this number
+// so it can be easily recognized from a debugger.
+const int64_t relTimeStamp::uninitializedValue = I64_C(8888888888888888888);
+
+const relTimeStamp *relTimeStamp::ZeroHelp() {
+  return new relTimeStamp(0, timeUnit::sec());
+}
+
+
 const timeLength *timeLength::_zero = NULL;
 const timeLength *timeLength::ZeroHelp() {
   return new timeLength(0, timeUnit::sec());
 }
+
+relTimeStamp::relTimeStamp(const timeLength &tl) : timeParent() {
+  initI(tl.getI(timeUnit::ns()), timeUnit::ns());
+}
+
+relTimeStamp::relTimeStamp(double dTime, const timeUnit &u): timeParent()
+{
+  double dRes = static_cast<double>( u.cvtTo_ns(dTime));
+  if(dRes <= static_cast<double>(I64_MAX)) 
+    assign( static_cast<int64_t>(dRes));
+  else
+    assign( getRolloverTime(dRes));
+}
+
 const timeLength *timeLength::_ns = NULL;
 const timeLength *timeLength::nsHelp() {
   return new timeLength(1, timeUnit::ns());
@@ -291,7 +340,7 @@ ostream& operator<<(ostream&s, timeLength z) {
   if(s.flags() & ostream::oct) {  compact=true;  s.flags(ostream::dec);  }
   if(compact == true) strcpy(cSt,",");
   s << "[";
-  if(tUser.get_ns() == 0) {  s << "0 time"; }
+  if(z.get_ns() == 0) {  s << "0 time"; }
   else {
     if(years!=0) {  s << (prev? cSt:"") << years << " years";   prev=true; }
     if(days!=0)  {  s << (prev? cSt:"") << days  << " days";    prev=true; }
@@ -310,8 +359,10 @@ ostream& operator<<(ostream&s, timeStamp z) {
   time_t s1970 = static_cast<time_t>(z.getI(timeUnit::sec(), timeBase::b1970()));
   int64_t nsI1970 = static_cast<int64_t>(s1970) * I64_C(1000000000);
   int64_t max_time_t = (I64_C(1)<<((sizeof(time_t)*8)-1))-1;  // eg. (2^(32-1)) -1
-  if(z < timeStamp(0, timeUnit::year(), timeBase::b1970()) ||
-     z > timeStamp(max_time_t, timeUnit::sec(), timeBase::b1970())) {
+  if(! z.isInitialized()) {  
+    s << "[uninitialized]";
+  } else if(z < timeStamp(0, timeUnit::year(), timeBase::b1970()) ||
+	    z > timeStamp(max_time_t, timeUnit::sec(), timeBase::b1970())) {
     timeLength tl(z - timeStamp(0, timeUnit::year(), timeBase::b1970()));
     // setting the oct flag is just a hack to tell the timeLength ostream<<
     // to print the time compressed
@@ -327,7 +378,7 @@ ostream& operator<<(ostream&s, timeStamp z) {
     int64_t nstot = z.getI(timeUnit::ns(), timeBase::b1970());
     int64_t nsrem = nstot - nsI1970;
     char dateStr[50];
-    s1970 = mktime(gmtime(&s1970));
+    s1970 = mktime(localtime(&s1970));
     strcpy(dateStr, ctime(&s1970));
     char *p = strstr(dateStr, "\n");  // erase the nl
     if(p != NULL) {  *p++ = 0;   *p = 0; }
@@ -338,6 +389,15 @@ ostream& operator<<(ostream&s, timeStamp z) {
   return s;
 }
 
+ostream& operator<<(ostream&s, relTimeStamp z) {
+  if(! z.isInitialized()) {  
+    s << "[uninitialized]";
+  } else {
+    timeLength t(z.getI(timeUnit::ns()), timeUnit::ns());
+    s << t;
+  }
+  return s;
+}
 
 
 
