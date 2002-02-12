7d32bb0ae926bf7cbff339c52f93f7972975aee2
tlmiller <tlmiller>
2002-02-12 16:41:04 +0000

    For the IA-64 build; duplicate 'warning prevention' from i386-unknown-linux2.0

--- visiClients/terrain/src/FormatBox.c
+++ visiClients/terrain/src/FormatBox.c
@@ -15,10 +15,10 @@
 /* 
  * FormatBox.c - Formated box widget
  * 
- * $Id: FormatBox.c,v 1.6 2001/06/12 19:56:11 schendel Exp $
+ * $Id: FormatBox.c,v 1.7 2002/02/12 16:41:04 tlmiller Exp $
  */
 
-#ifdef i386_unknown_linux2_0
+#ifdef i386_unknown_linux2_0 || defined(ia64_unknown_linux2_4)
 #define _HAVE_STRING_ARCH_strcpy  /* gets rid of warnings */
 #define _HAVE_STRING_ARCH_strsep
 #endif
