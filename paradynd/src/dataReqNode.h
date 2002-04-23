57828d498e05b513e3da4503812d391b34630c7d
schendel <schendel>
2002-04-23 18:58:48 +0000

    changed the name of heapStates.h to superTableTypes.h to reflect wider   use of this file; now sharing common code in dataReqNode base class; change the interface for the creation of a instrumentation variable from   "create" and "reuse", to "allocateInstVarForAllThreads"   and "createThrInstVar" (easier to understand); changed semantics so instrumentation variables aren't by default sampled   but created a separate path for a variable to be marked as to be   sampled or unsampled; pushed back the awareness & concept of level of instrumentation variable; added type for index of instrumentation variable (inst_var_index) and   type for type of instrumentation variable (inst_var_type);

--- paradynd/src/dataReqNode.h
+++ paradynd/src/dataReqNode.h
@@ -46,6 +46,7 @@
 #include "common/h/Vector.h"
 #include "common/h/Dictionary.h"
 #include "rtinst/h/rtinst.h"
+#include "superTableTypes.h"
 
 class process;
 class threadMetFocusNode_Val;
@@ -56,261 +57,131 @@ class dataReqNode {
  private:
   // Since we don't define these, make sure they're not used:
   dataReqNode &operator=(const dataReqNode &src);
-  dataReqNode (const dataReqNode &src);
+
+  process *proc;
+
+  inst_var_type varType;
+  inst_var_index varIndex;
+  unsigned threadPos;
+  int theSampleId; 
+  bool dontInsertData;
 
  protected:
-  dataReqNode() { }
-
+  dataReqNode(process *proc_, inst_var_type varType_, inst_var_index varIndex_,
+	      unsigned thrPos, int sampleId, bool dontInsertData_) 
+     : proc(proc_), varType(varType_), varIndex(varIndex_), threadPos(thrPos),
+     theSampleId(sampleId), dontInsertData(dontInsertData_)
+  { }
+    
  public:
   virtual ~dataReqNode() {};
 
-  virtual Address getInferiorPtr(process *proc=NULL) const = 0;
-  virtual unsigned getAllocatedIndex() const = 0;
-  virtual unsigned getAllocatedLevel() const = 0;
+  static inst_var_index allocateForInstVar(process *proc, 
+					   inst_var_type varType);
+  
+  unsigned getLevel() const { return int(varType); }
+  inst_var_index getVarIndex()  const { return varIndex;  }
+  unsigned getThreadPos() const { return threadPos; }
 
-  virtual int getSampleId() const = 0;
+  int getSampleId() const { return theSampleId; }
+  bool getDontInsertData() const { return dontInsertData; }
 
-  virtual int getThreadId() const = 0;
+  Address getInferiorPtr() const;
 
-     // Allocates stuff from inferior heap, instrumenting DYNINSTreportCounter
-     // as appropriate.  
-     // Returns true iff successful.
+  void markAsSampled();
+  void markAsNotSampled();
 
-  virtual void disable(process *,
-		       const vector< vector<Address> > &pointsToCheck) = 0;
-     // the opposite of insertShmVar.  Deinstruments, deallocates
-     // from inferior heap, etc.
+  // the opposite of insertShmVar.  Deinstruments, deallocates
+  // from inferior heap, etc.
+  void disable(const vector< vector<Address> > &pointsToCheck);
 
-  virtual void setThrNodeClient(threadMetFocusNode_Val *thrObj,
-				process *proc) = 0;
+  void setThrNodeClient(threadMetFocusNode_Val *thrObj);
 
-  virtual dataReqNode *dup(process *childProc, int iCounterId,
-			   const dictionary_hash<instInstance*,instInstance*> &map
-			   ) const = 0;
+  //  virtual dataReqNode *dup(process *childProc, int iCounterId,
+  //			   const dictionary_hash<instInstance*,instInstance*> &map
+  //			   ) const = 0;
      // duplicate 'this' (allocate w/ new) and return.  Call after a fork().
 
      // "map" provides a dictionary that maps instInstance's of the parent process to
      // those in the child process; dup() may find it useful. (for now, the shm
      // dataReqNodes have no need for it; the alarm sampled ones need it).
 
-  virtual bool unFork(dictionary_hash<instInstance*,instInstance*> &map) = 0;
+//  virtual bool unFork(dictionary_hash<instInstance*,instInstance*> &map) = 0;
      // the fork syscall duplicates all instrumentation (except on AIX),
      // which is a problem when we determine that a certain mi shouldn't be
      // propagated to the child process.  Hence this routine.
 };
 
-
-
-/*
- * Classes derived from dataReqNode
- *
- * There are a lot of classes derived from dataReqNode, so things can get a
- * little confusing.  Most of them are used in either shm_sampling mode or
- * not; few are used in both.  Here's a bit of documentation that should sort
- * things out:
- *
- * A) classes used in _both_ shm_sampling and non-shm-sampling
- *
- *    nonSampledIntCounterReqNode --
- *       provides an intCounter value that is never sampled.  Currently
- *       useful for predicates (constraint booleans).  The intCounter is
- *       allocated from the "conventional" heap (where tramps are allocated)
- *       using good old inferiorMalloc(), even when shm_sampling.  (In the
- *       future, we may provide a separate shm_sampling version, to make
- *       allocation faster [allocation in the shm seg heap is always faster])
- *
- * B) classes used only when shm_sampling
- *
- *    sampledShmIntCounterReqNode --
- *       provides an intCounter value that is sampled with shm sampling.  The
- *       intCounter is allocated from the shm segment (i.e., not with
- *       inferiorMalloc).
- *
- *    sampledShmWallTimerReqNode --
- *       provides a wall timer value that is sampled with shm sampling.  The
- *       tTimer is allocated from the shm segment (i.e., not with
- *       inferiorMalloc)
- *    
- *    sampledShmProcTimerReqNode --
- *       provides a process timer value that is sampled with shm sampling.
- *       The tTimer is allocated from the shm segment (i.e., not with
- *       inferiorMalloc)
- *    
- * C) classes used only when _not_ shm-sampling
- *    
- *    sampledIntCounterReqNode --
- *       provides an intCounter value that is sampled.  The intCounter is
- *       allocated in the conventional heap with inferiorMalloc().  Sampling
- *       is done the old-fasioned SIGALRM way: by instrumenting
- *       DYNINSTsampleValues to call DYNINSTreportTimer
- *
- *    sampledTimerReqNode --
- *       provides a tTimer value (can be wall or process timer) that is
- *       sampled.  The tTimer is allocated in the conventional heap with
- *       inferiorMalloc().  Sampling is done the old-fasioned SIGALRM way: by
- *       instrumenting DYNINSTsampleValues to call DYNINSTreportTimer.
- *
- */
-
-class sampledShmIntCounterReqNode : public dataReqNode {
- // intCounter for use when shm-sampling.  Allocated in the shm segment heap.
- // Sampled using shm sampling.
+class sampledIntCounterReqNode : public dataReqNode {
  private:
-   // The following fields are always properly initialized in ctor:
-   int theSampleId; // obsolete with shm sampling; can be removed.
-   pdThread *thr_;
-
    rawTime64 initialValue; // needed when dup()'ing
 
-   // The following fields are NULL until insertShmVar() called:
-   unsigned allocatedIndex;
-   unsigned allocatedLevel;
-
-   unsigned position_;
-
    // Since we don't use these, making them privates ensures they're not used.
-   sampledShmIntCounterReqNode &operator=(const sampledShmIntCounterReqNode &);
-   sampledShmIntCounterReqNode(const sampledShmIntCounterReqNode &);
+   sampledIntCounterReqNode &operator=(const sampledIntCounterReqNode &);
+   sampledIntCounterReqNode(const sampledIntCounterReqNode &);
 
    // private fork-ctor called by dup():
-   sampledShmIntCounterReqNode(const sampledShmIntCounterReqNode &src,
-                               process *childProc,
-			       int iCounterId, const process *parentProc);
-   // allocates from inferior heap; initializes it, etc.
-   bool insertShmVar(pdThread *, process *, bool);
+   sampledIntCounterReqNode(const sampledIntCounterReqNode &src,
+			    process *childProc,
+			    int iCounterId, const process *parentProc);
 
  public:
-   sampledShmIntCounterReqNode(pdThread *thr, rawTime64 iValue, int iCounterId,
-			       process *proc, bool dontInsertData,
-                               bool doNotSample, unsigned, unsigned);
-  ~sampledShmIntCounterReqNode() {}
+   sampledIntCounterReqNode(process *proc, inst_var_index varIndex,
+			    pdThread *thr, rawTime64 iValue,
+			    int iCounterId, bool dontInsertData_);
+  ~sampledIntCounterReqNode() {}
       // Hopefully, disable() has already been called.
       // A bit of a complication since disable() needs an
       // arg passed, which we can't do here.  Too bad.
-
+  
    dataReqNode *dup(process *, int iCounterId,
-                   const dictionary_hash<instInstance*,instInstance*> &) const;
-
-   void disable(process *, const vector< vector<Address> > &);
-
-   Address getInferiorPtr(process *) const;
-   void setThrNodeClient(threadMetFocusNode_Val *thrObj, process *proc);
-
-   unsigned getAllocatedIndex() const {return allocatedIndex;}
-   unsigned getAllocatedLevel() const {return allocatedLevel;}
-
-   unsigned getPosition() const {return position_;}
-   int getSampleId() const {return theSampleId;}
-   int getThreadId() const;
-   pdThread *getThread() {return thr_;}
+                  const dictionary_hash<instInstance*,instInstance*> &) const;
 
    bool unFork(dictionary_hash<instInstance*,instInstance*> &) {return true;}
 };
 
 
-
-class sampledShmWallTimerReqNode : public dataReqNode {
- // wall tTimer for use when shm sampling.  Allocated in the shm segment heap.
- // Sampled using shm-sampling.
+class sampledWallTimerReqNode : public dataReqNode {
  private:
-   // The following fields are always initialized in the ctor:   
-   int theSampleId;
-   pdThread *thr_;
-
-   // The following fields are NULL until insertShmVar():
-   unsigned allocatedIndex;
-   unsigned allocatedLevel;
-
-   unsigned position_;
-
-   // since we don't use these, disallow:
-   sampledShmWallTimerReqNode(const sampledShmWallTimerReqNode &);
-   sampledShmWallTimerReqNode &operator=(const sampledShmWallTimerReqNode &);
-
    // fork ctor:
-   sampledShmWallTimerReqNode(const sampledShmWallTimerReqNode &src, 
-			      process *childProc, int iCounterId,
-			      const process *parentProc);
-   bool insertShmVar(pdThread *, process *, bool doNotSample = false);
+   sampledWallTimerReqNode(const sampledWallTimerReqNode &src, 
+			   process *childProc, int iCounterId,
+			   const process *parentProc);
 
  public:
-   sampledShmWallTimerReqNode(pdThread *thr, int iCounterId,
-                              process *proc, bool dontInsertData,
-			      unsigned, unsigned);
-  ~sampledShmWallTimerReqNode() {}
+   sampledWallTimerReqNode(process *proc, inst_var_index varIndex,
+			   pdThread *thr, int iCounterId, 
+			   bool dontInsertData_);
+  ~sampledWallTimerReqNode() {}
       // hopefully, freeInInferior() has already been called
       // a bit of a complication since freeInInferior() needs an
       // arg passed, which we can't do here.  Too bad.
 
    dataReqNode *dup(process *childProc, int iCounterId,
-                   const dictionary_hash<instInstance*,instInstance*> &) const;
-   void disable(process *, const vector< vector<Address> > &);
-
-   Address getInferiorPtr(process *) const;
-   void setThrNodeClient(threadMetFocusNode_Val *thrObj, process *proc);
-
-   unsigned getAllocatedIndex() const {return allocatedIndex;}
-   unsigned getAllocatedLevel() const {return allocatedLevel;}
-
-   unsigned getPosition() const {return position_;}
-
-   int getSampleId() const {return theSampleId;}
-   int getThreadId() const;
-   pdThread *getThread() {return thr_;}
-
+                  const dictionary_hash<instInstance*,instInstance*> &) const;
    bool unFork(dictionary_hash<instInstance*,instInstance*> &) {return true;}
 };
 
 
 
-class sampledShmProcTimerReqNode : public dataReqNode {
- // process tTimer for use when shm sampling.  Allocated in the shm segment heap.
- // Sampled using shm-sampling.
+class sampledProcTimerReqNode : public dataReqNode {
  private:
-   // The following fields are always initialized in the ctor:   
-   int theSampleId;
-   pdThread *thr_;
-
-   // The following fields are NULL until insertShmVar():
-   unsigned allocatedIndex;
-   unsigned allocatedLevel;
-
-   unsigned position_;
-
-   // since we don't use these, disallow:
-   sampledShmProcTimerReqNode(const sampledShmProcTimerReqNode &);
-   sampledShmProcTimerReqNode &operator=(const sampledShmProcTimerReqNode &);
-
    // fork ctor:
-   sampledShmProcTimerReqNode(const sampledShmProcTimerReqNode &src, 
-			      process *proc, int iCounterId,
-			      const process *parentProc);
-   bool insertShmVar(pdThread *, process *, bool doNotSample = false);
+   sampledProcTimerReqNode(const sampledProcTimerReqNode &src, 
+			   process *proc, int iCounterId,
+			   const process *parentProc);
 
  public:
-   sampledShmProcTimerReqNode(pdThread *thr, int iCounterId,
-			      process *proc, bool dontInsertData,
-			      unsigned, unsigned);
-  ~sampledShmProcTimerReqNode() {}
+   sampledProcTimerReqNode(process *proc, inst_var_index varIndex,
+			   pdThread *thr, 
+			   int iCounterId, bool dontInsertData_);
+  ~sampledProcTimerReqNode() {}
       // hopefully, freeInInferior() has already been called
       // a bit of a complication since freeInInferior() needs an
       // arg passed, which we can't do here.  Too bad.
 
    dataReqNode *dup(process *childProc, int iCounterId,
-		   const dictionary_hash<instInstance*,instInstance*> &) const;
-   void disable(process *, const vector< vector<Address> > &);
-
-   void setThrNodeClient(threadMetFocusNode_Val *thrObj, process *proc);
-   Address getInferiorPtr(process *) const;
-
-   unsigned getAllocatedIndex() const {return allocatedIndex;}
-   unsigned getAllocatedLevel() const {return allocatedLevel;}
-
-   unsigned getPosition() const {return position_;}
-
-   int getSampleId() const {return theSampleId;}
-   int getThreadId() const;
-   pdThread *getThread() {return thr_;}
+		  const dictionary_hash<instInstance*,instInstance*> &) const;
 
    bool unFork(dictionary_hash<instInstance*,instInstance*> &) {return true;}
 };
