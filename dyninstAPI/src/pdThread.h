8bc4847373d2a6c08a541b51dd6fbe6a8a9ffcd2
bernat <bernat>
2002-07-03 22:20:46 +0000

    Removed pd_pos

--- dyninstAPI/src/pdThread.h
+++ dyninstAPI/src/pdThread.h
@@ -39,7 +39,7 @@
  * incur to third parties resulting from your use of Paradyn.
  */
 
-// $Id: pdThread.h,v 1.15 2002/06/10 21:30:56 bernat Exp $
+// $Id: pdThread.h,v 1.16 2002/07/03 22:20:46 bernat Exp $
 
 #ifndef _PDTHREAD_H_
 #define _PDTHREAD_H_
@@ -49,140 +49,125 @@
 class Frame;
 
 class pdThread {
-  public:
-    // This definition must be completed later when we get the result of a call
-    // to thr_self in the application. We are assuming that 
-    // 0 <= thr_self,tid < MAX_NUMBER_OF_THREADS - naim
-    // We are also assuming that the position in the paradynd super table
-    // for this thread is initially 0, until it gets updated with the record
-    // sent in DYNINSTinit - naim 4/15/97
-    pdThread(process *pproc) : 
-      tid(0), 
-      fd(-1), 
-      pos(0),
-      pd_pos(0),
-      stack_addr(0),
-      start_pc(0),
-      start_func(NULL),
-      rid(NULL),
+ public:
+  //
+  pdThread(process *pproc) : 
+    tid(0), 
+    fd(-1), 
+    pos(0),
+    stack_addr(0),
+    start_pc(0),
+    start_func(NULL),
+    rid(NULL),
 #ifndef BPATCH_LIBRARY
-      previous(0),
+    previous(0),
 #endif
-      pending_tramp_addr( ADDR_NULL )
+    pending_tramp_addr( ADDR_NULL )
     { 
       proc = pproc; 
       ppid = pproc->getPid();
     }
-    pdThread(process *proc_, int tid_, unsigned pos_) :
-      tid(tid_),
-      fd(-1),
-      pos(pos_),
-      pd_pos(0),
-      stack_addr(0),
-      start_pc(0),
-      start_func(NULL),
-      rid(NULL),
+  pdThread(process *proc_, int tid_, unsigned pos_) :
+    tid(tid_),
+    fd(-1),
+    pos(pos_),
+    stack_addr(0),
+    start_pc(0),
+    start_func(NULL),
+    rid(NULL),
 #ifndef BPATCH_LIBRARY
-      previous(0),
+    previous(0),
 #endif
-      pending_tramp_addr( ADDR_NULL )
+    pending_tramp_addr( ADDR_NULL )
     {
       proc = proc_;
       ppid = proc_->getPid();
     }
-    pdThread(process *pproc, int tid_, handleT handle_) :
-      tid(tid_),
-      fd(-1),
-      pos(0),
-      pd_pos(0),
-      stack_addr(0),
-      start_pc(0),
-      start_func(NULL),
-      rid(NULL),
+  pdThread(process *pproc, int tid_, handleT handle_) :
+    tid(tid_),
+    fd(-1),
+    pos(0),
+    stack_addr(0),
+    start_pc(0),
+    start_func(NULL),
+    rid(NULL),
 #ifndef BPATCH_LIBRARY
-      previous(0),
+    previous(0),
 #endif
-      pending_tramp_addr( ADDR_NULL )
+    pending_tramp_addr( ADDR_NULL )
     {
       assert(pproc);
       proc = pproc;
       ppid = pproc->getPid();
       handle = handle_ ;
     }
-    pdThread(process *parent, pdThread *src) {
-      assert(src && parent);
-      tid = src->tid;
-      ppid = parent->getPid();
-      pos = src->pos;
-      pd_pos = src->pd_pos;
-      stack_addr = src->stack_addr;
-      start_pc = src->start_pc;
-      start_func = src->start_func;
-      rid = src->rid;
-      proc = parent;
+  pdThread(process *parent, pdThread *src) {
+    assert(src && parent);
+    tid = src->tid;
+    ppid = parent->getPid();
+    pos = src->pos;
+    stack_addr = src->stack_addr;
+    start_pc = src->start_pc;
+    start_func = src->start_func;
+    rid = src->rid;
+    proc = parent;
 #ifndef BPATCH_LIBRARY
-      previous = 0;
+    previous = 0;
 #endif
-      pending_tramp_addr = ADDR_NULL;
-    }
-    ~pdThread() {
+    pending_tramp_addr = ADDR_NULL;
+  }
+  ~pdThread() {
+    //delete rid; //deletion of resources is not yet implemented! - naim 1/21/98
+    close(fd);
+  }
+  
+  Frame getActiveFrame();
+  
+  unsigned       get_tid()           const { return(tid); }
+  unsigned       get_pos()           const { return(pos); }
+  unsigned       get_stack_addr()    const { return(stack_addr); }
+  int            get_fd()            const { return(fd); }
+  int            get_ppid()          const { return(ppid); }
+  resource*      get_rid()                 { return(rid); }
+  process*       get_proc()                { return(proc); }
+  handleT        get_handle()        const { return(handle); }
+  function_base* get_start_func()          { return(start_func); }
+  unsigned       get_start_pc()      const { return(start_pc); }
+  void*          get_resumestate_p()       { return resumestate_p; }
 #if defined(MT_THREAD)
-      proc->threadMap->remove(tid);
+  static rawTime64  getInferiorVtime(tTimer* , process*, bool&);
 #endif
-      //delete rid; //deletion of resources is not yet implemented! - naim 1/21/98
-      close(fd);
-    }
-
-    Frame getActiveFrame();
-
-    unsigned       get_tid()           const { return(tid); }
-    unsigned       get_pos()           const { return(pos); }
-    unsigned       get_pd_pos()        const { return(pd_pos); }
-    unsigned       get_stack_addr()    const { return(stack_addr); }
-    int            get_fd()            const { return(fd); }
-    int            get_ppid()          const { return(ppid); }
-    resource*      get_rid()                 { return(rid); }
-    process*       get_proc()                { return(proc); }
-    handleT        get_handle()        const { return(handle); }
-    function_base* get_start_func()          { return(start_func); }
-    unsigned       get_start_pc()      const { return(start_pc); }
-    void*          get_resumestate_p()       { return resumestate_p; }
-#if defined(MT_THREAD)
-    static rawTime64  getInferiorVtime(tTimer* , process*, bool&);
-#endif
-    void update_tid          (int id, unsigned p)   { tid = id; pos = p; }
-    void update_handle       (int id, handleT h)    { tid = id; handle = h; }
-    void update_rid          (resource *rid_)       { rid = rid_; } 
-    void update_pd_pos       (unsigned p)           { pd_pos = p; }
-    void update_fd           (int fd_)              { fd = fd_; }
-    void update_stack_addr   (unsigned stack_addr_) { stack_addr=stack_addr_; }
-    void update_start_pc     (unsigned start_pc_)   { start_pc=start_pc_; }
-    void update_start_func   (function_base *pdf)   { start_func=pdf; }
-    void update_resumestate_p(void* resumestate_p_) { resumestate_p=resumestate_p_; }
-
-	Address get_pending_tramp_addr( void ) const	{ return pending_tramp_addr; }
-	void set_pending_tramp_addr( Address a )	{ pending_tramp_addr = a; }
-
-///
-  private:
-    int tid;
-    int ppid;
-    int fd;
-    unsigned pos;
-    unsigned pd_pos;
-    unsigned stack_addr;
-    unsigned start_pc ;
-    void*    resumestate_p; //platform specific
-    function_base *start_func ;
-    resource *rid;
-    process *proc;
-    handleT handle; // the thread handle (/proc file descriptor or NT handle)
+  void update_tid          (int id, unsigned p)   { tid = id; pos = p; }
+  void update_handle       (int id, handleT h)    { tid = id; handle = h; }
+  void update_rid          (resource *rid_)       { rid = rid_; } 
+  void update_fd           (int fd_)              { fd = fd_; }
+  void update_stack_addr   (unsigned stack_addr_) { stack_addr=stack_addr_; }
+  void update_start_pc     (unsigned start_pc_)   { start_pc=start_pc_; }
+  void update_start_func   (function_base *pdf)   { start_func=pdf; }
+  void update_resumestate_p(void* resumestate_p_) { resumestate_p=resumestate_p_; }
+  
+  Address get_pending_tramp_addr( void ) const	{ return pending_tramp_addr; }
+  void set_pending_tramp_addr( Address a )	{ pending_tramp_addr = a; }
+  
+  ///
+ private:
+  int tid;
+  int ppid;
+  int fd;
+  unsigned pos;
+  unsigned stack_addr;
+  unsigned start_pc ;
+  void*    resumestate_p; //platform specific
+  function_base *start_func ;
+  resource *rid;
+  process *proc;
+  handleT handle; // the thread handle (/proc file descriptor or NT handle)
 #ifndef BPATCH_LIBRARY
-    rawTime64 previous;
+  rawTime64 previous;
 #endif
-    Address pending_tramp_addr;	// address of pending instrumentation
-								// currently used on NT only
-
+  Address pending_tramp_addr;	// address of pending instrumentation
+  // currently used on NT only
+  
 };
 
 #endif
