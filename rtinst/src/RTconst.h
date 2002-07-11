9eb709926b1bd76f5a6ebc830710bb066a580bde
bernat <bernat>
2002-07-11 19:45:49 +0000

    More thread updates.   - - - - - - - - - - - - - - - - - - - - - - - - - removed MAX_NUMBER_OF_THREADS as a constant

--- rtinst/src/RTconst.h
+++ rtinst/src/RTconst.h
@@ -1,8 +1,9 @@
 #ifndef _RT_CONST_H_
 #define _RT_CONST_H_
 
-#define MAX_NUMBER_OF_THREADS  (512)
-#define MAX_NUMBER_OF_LEVELS   (30)
+/* Thread control constants */
+#define THREAD_AWAITING_DELETION ((unsigned) -2)
+
 #define DYNINST_DEAD_LOCK      -2
 #define DYNINST_LIVE_LOCK      -1
 #define DYNINST_UNLOCK_NONE    -3
