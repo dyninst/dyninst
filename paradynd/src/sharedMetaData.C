044d8372644672988b29f303336fcfd7845f2677
schendel <schendel>
2002-08-12 04:22:01 +0000

    add ability for paradyn to deal with processes which fork  - - - - - - - implement the meta data in shared memory as an object, which can be easily   duplicated upon forks;

---
+++ paradynd/src/sharedMetaData.C
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notics of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: sharedMetaData.C,v
+
+#include <iostream.h>
+#include "paradynd/src/sharedMetaData.h"
+#include "paradynd/src/shmMgr.h"
+
+
+void sharedMetaData::mallocInShm(shmMgr &theShmMgr) {
+  cookie = reinterpret_cast<unsigned *>(theShmMgr.malloc(sizeof(unsigned)));
+  inferior_pid = reinterpret_cast<unsigned *>(
+				    theShmMgr.malloc(sizeof(unsigned)));
+  daemon_pid   = reinterpret_cast<unsigned *>(
+				    theShmMgr.malloc(sizeof(unsigned)));
+  observed_cost= reinterpret_cast<unsigned *>(
+				    theShmMgr.malloc(sizeof(unsigned)));
+  trampGuards  = reinterpret_cast<unsigned *>(
+			    theShmMgr.malloc(sizeof(unsigned) * maxThreads));
+  /* MT */
+  virtualTimers= reinterpret_cast<tTimer *>(
+			    theShmMgr.malloc(sizeof(unsigned) * maxThreads));
+  posToThread  = reinterpret_cast<unsigned *>(
+			    theShmMgr.malloc(sizeof(unsigned) * maxThreads));
+  for(unsigned i = 0; i < maxThreads; i++) {
+    pendingIRPCs[i] = reinterpret_cast<rpcToDo *>(
+				    theShmMgr.malloc(perThrRpcBufferSize));
+  }
+  
+  curBaseAddr  = reinterpret_cast<Address>(theShmMgr.getBaseAddrInDaemon());
+  malloced     = true;
+}
+
+void sharedMetaData::initialize(unsigned cookie_a, int dmnPid, int appPid) {
+  assert(malloced == true);
+  *cookie = cookie_a;
+  *inferior_pid = appPid;
+  *daemon_pid = dmnPid;
+  *observed_cost = 0;
+  for(unsigned i=0; i < maxThreads; i++) {
+    trampGuards[i] = 1;
+  }
+}
+
+void sharedMetaData::initializeForkedProc(unsigned cookie_a, int appPid) {
+  *cookie = cookie_a;
+  *inferior_pid = appPid;
+  // *daemon_pid ... value copied from parent is fine
+  *observed_cost = 0;
+  ///we want to inherit the current values of the trampGuards in the parent
+  //for(unsigned i=0; i < maxThreads; i++) {
+  //  trampGuards[i] = 1;
+  //}  
+}				  
+
+void sharedMetaData::adjustToNewBaseAddr(Address newBaseAddr) {
+  assert(malloced == true);
+  cookie = reinterpret_cast<unsigned *>(
+               (reinterpret_cast<Address>(cookie) - curBaseAddr) + 
+                newBaseAddr);
+  inferior_pid = reinterpret_cast<unsigned *>(
+	         (reinterpret_cast<Address>(inferior_pid) - curBaseAddr) + 
+                  newBaseAddr);
+  daemon_pid = reinterpret_cast<unsigned *>(
+	         (reinterpret_cast<Address>(daemon_pid) - curBaseAddr) + 
+                  newBaseAddr);
+  observed_cost = reinterpret_cast<unsigned *>(
+	         (reinterpret_cast<Address>(observed_cost) - curBaseAddr) + 
+                  newBaseAddr);
+  trampGuards = reinterpret_cast<unsigned *>(
+	         (reinterpret_cast<Address>(trampGuards) - curBaseAddr) + 
+                  newBaseAddr);
+  virtualTimers = reinterpret_cast<tTimer *>(
+	         (reinterpret_cast<Address>(virtualTimers) - curBaseAddr) + 
+                  newBaseAddr);
+  posToThread = reinterpret_cast<unsigned *>(
+	         (reinterpret_cast<Address>(posToThread) - curBaseAddr) + 
+                  newBaseAddr);
+  for (unsigned i = 0; i < maxThreads; i++) {
+    pendingIRPCs[i] = reinterpret_cast<rpcToDo *>(
+		 (reinterpret_cast<Address>(pendingIRPCs[i]) - curBaseAddr) +
+                  newBaseAddr);      
+  }
+  curBaseAddr = newBaseAddr;
+}
+
+void sharedMetaData::saveOffsetsIntoRTstructure(RTsharedData_t *RTdata) {
+  assert(malloced == true);
+  RTdata->cookie = reinterpret_cast<unsigned *>(
+	             reinterpret_cast<Address>(cookie) - curBaseAddr);
+  RTdata->inferior_pid = reinterpret_cast<unsigned *>(
+                     reinterpret_cast<Address>(inferior_pid) - curBaseAddr);
+  RTdata->daemon_pid = reinterpret_cast<unsigned *>(
+		     reinterpret_cast<Address>(daemon_pid) - curBaseAddr);
+  RTdata->observed_cost = reinterpret_cast<unsigned *>(
+                     reinterpret_cast<Address>(observed_cost) - curBaseAddr);
+  cerr << "saving obsCost offset = " << RTdata->observed_cost << "\n";
+  RTdata->trampGuards = reinterpret_cast<unsigned *>(
+		     reinterpret_cast<Address>(trampGuards) - curBaseAddr);
+  RTdata->virtualTimers = reinterpret_cast<tTimer *>(
+                     reinterpret_cast<Address>(virtualTimers) - curBaseAddr);
+  cerr << "saving vTimer offset = " << RTdata->virtualTimers << "\n";
+  RTdata->posToThread = reinterpret_cast<unsigned *>(
+		     reinterpret_cast<Address>(posToThread) - curBaseAddr);
+  for (unsigned i = 0; i < maxThreads; i++) {
+    RTdata->pendingIRPCs[i] = reinterpret_cast<rpcToDo *>(
+                  reinterpret_cast<Address>(pendingIRPCs[i]) - curBaseAddr);
+  }
+  // We want this to be an offset from within the shared memory segment
+  RTdata->pendingIRPCs = reinterpret_cast<rpcToDo **>(
+		  (unsigned) RTdata->pendingIRPCs - (unsigned)curBaseAddr);
+
+}
+
+
+
+
---
+++ paradynd/src/sharedMetaData.h
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: sharedMetaData.h,v
+
+#ifndef SHARED_META_DATA
+#define SHARED_META_DATA
+
+#include <stdlib.h>
+#include <string.h>
+#include "rtinst/h/rtinst.h"
+
+class shmMgr;
+
+class sharedMetaData {
+ private:
+  unsigned maxThreads;
+  unsigned *cookie;
+  unsigned *inferior_pid;
+  unsigned *daemon_pid;
+  unsigned *observed_cost;
+  unsigned *trampGuards;
+  /* MT */
+  tTimer *virtualTimers;
+  unsigned *posToThread;
+
+  enum { rpcSize = sizeof(rpcToDo), rpcPtrSize = sizeof(rpcToDo*),
+         numRPCs = MAX_PENDING_RPC, 
+	 // the size of the (per thread) buffer that contains the rpcs
+	 perThrRpcBufferSize = rpcSize * numRPCs };
+  // the size of the array that contains all of the pointers to the
+  // buffers, where there is a buffer for each thread that stores the
+  // rpcs
+  const unsigned rpcPtrBufferSize;  
+  rpcToDo **pendingIRPCs; /* By POS, then by index */
+
+  bool malloced;
+  Address curBaseAddr;
+  sharedMetaData& operator=(const sharedMetaData &from);
+
+ public:
+
+  sharedMetaData(unsigned maxNumberOfThreads) : 
+    maxThreads(maxNumberOfThreads),
+    rpcPtrBufferSize(rpcPtrSize * maxThreads), malloced(false), 
+    curBaseAddr(0)
+  { 
+    pendingIRPCs = reinterpret_cast<rpcToDo **>(malloc(rpcPtrBufferSize));
+  }
+  sharedMetaData(const sharedMetaData &par) : 
+    maxThreads(par.maxThreads), cookie(par.cookie),
+    inferior_pid(par.inferior_pid), daemon_pid(par.daemon_pid),
+    observed_cost(par.observed_cost), trampGuards(par.trampGuards),
+    virtualTimers(par.virtualTimers), posToThread(par.posToThread),
+    rpcPtrBufferSize(par.rpcPtrBufferSize), malloced(par.malloced), 
+    curBaseAddr(par.curBaseAddr)
+  {
+    pendingIRPCs = reinterpret_cast<rpcToDo **>(malloc(rpcPtrBufferSize));
+    memcpy(pendingIRPCs, par.pendingIRPCs, rpcPtrBufferSize);
+  }
+  ~sharedMetaData() {
+    free(pendingIRPCs);
+  }
+
+  unsigned *getObservedCost() { return observed_cost; }
+  unsigned *getTrampGuards() { return trampGuards; }
+  tTimer *getVirtualTimers() { return virtualTimers; }
+  tTimer &getVirtualTimer(int pos) { return virtualTimers[pos]; }
+  unsigned getPosToThread(int pos) const { return posToThread[pos]; }
+  void setPosToThread(int pos, unsigned v) { posToThread[pos] = v; }
+
+  // malloc some memory for the sharedMetaData in shared memory
+  void mallocInShm(shmMgr &theShmMgr);
+  void initialize(unsigned cookie_a, int dmnPid, int appPid);
+  void initializeForkedProc(unsigned cookie_a, int appPid);
+
+  // adjust the addresses of shmMem malloced memory to be relative to
+  // a new, given base address
+  void adjustToNewBaseAddr(Address newBaseAddr);
+
+  // save the offsets of the shmMem malloced memory into a structure
+  // which can be given to the run time library
+  void saveOffsetsIntoRTstructure(RTsharedData_t *RTdata);
+};
+
+
+
+#endif   // SHARED_META_DATA
+
+
