b82cf360a8489b07a4390ae5d7bd7f15ddb731db
schendel <schendel>
2002-10-15 17:12:15 +0000

    create Paradyn specific pd_process and pd_thread classes  - - - - - - - - created threadMgr to manage the threads for a pd_process;

---
+++ paradynd/src/threadMgr.C
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+
+#include "paradynd/src/threadMgr.h"
+#include "paradynd/src/pd_thread.h"
+
+
+void threadMgr::removeThread(unsigned tid) {
+   for(unsigned i=0; i<thrBuf.size(); i++) {
+      if(thrBuf[i] && thrBuf[i]->get_tid() == tid) {
+	 thrBuf[i] = NULL;
+      }
+   }
+}
+
+pd_thread *threadMgr::find_pd_thread(dyn_thread *dyn_thr) {
+   cerr << "in threadMgr::find_pd_thread, " << thrBuf.size() << " threads\n"
+	<< flush;
+   for(unsigned i=0; thrBuf.size(); i++) {
+      pd_thread *curThr = thrBuf[i];
+      cerr << "   i: " << i << ", pd_thr = " << curThr << ", dyn_thr = "
+	   << curThr->get_dyn_thread() << "\n" << flush;
+      if(curThr->get_dyn_thread() == dyn_thr) {
+	 cerr << "returning " << curThr << "\n" << flush;
+	 return curThr;
+      }
+   }
+   cerr << "returning NULL\n" << flush;
+   return NULL;
+}
+
+pd_thread *threadMgr::find_pd_thread(int tid) {
+   for(unsigned i=0; thrBuf.size(); i++) {
+      pd_thread *curThr = thrBuf[i];
+      if(curThr->get_tid() == tid) {
+	 return curThr;
+      }
+   }
+   return NULL;
+}
+
