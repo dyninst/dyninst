b65293e9ace53ff631011793f2662a237a30e946
pcroth <pcroth>
2002-12-19 16:38:33 +0000

    Added methods for building the network with no backends and later connecting them.

--- mrnet/mrnet/h/MR_Network.h
+++ mrnet/mrnet/h/MR_Network.h
@@ -1,30 +1,52 @@
 #if !defined(mc_network_h)
 #define mc_network_h 1
 
+#ifndef NULL
+#  define NULL (0)
+#endif // NULL
+
 class MC_NetworkImpl;
 class MC_BackEndNode;
+
+
+
+
 class MC_Network{
  public:
-  static MC_NetworkImpl * network;
-  static MC_BackEndNode * back_end;
+    class LeafInfo
+    {
+    public:
+        virtual const char* get_Host( void ) const = NULL;
+        virtual unsigned short  get_Port( void ) const = NULL;
+    };
+
   static int new_Network(const char * _filename, 
                             const char * _commnode,
                             const char * _application);
+  static int new_Network(const char * _filename,
+                            const char * _commnode,
+                            MC_Network::LeafInfo*** leaves,
+                            unsigned int* numLeaves);
+  static int connect_Backends( void );
   static void delete_Network();
+
   static int init_Backend(const char *hostname, const char *port,
                           const char *phostname, const char *pport,
                           const char *pid);
   static void error_str(const char *);
+
+  static MC_NetworkImpl * network;
+  static MC_BackEndNode * back_end;
 };
 
 class MC_EndPoint{
  public:
   static MC_EndPoint * new_EndPoint(int _id, const char * _hostname,
                                     unsigned short _port);
-  virtual bool compare(const char * _hostname, unsigned short _port) =0;
-  virtual const char * get_HostName() =0;
-  virtual unsigned short get_Port() =0;
-  virtual unsigned int get_Id() =0;
+  virtual bool compare(const char * _hostname, unsigned short _port) =NULL;
+  virtual const char * get_HostName() =NULL;
+  virtual unsigned short get_Port() =NULL;
+  virtual unsigned int get_Id() =NULL;
 };
 
 class MC_Communicator{
@@ -33,12 +55,12 @@ class MC_Communicator{
   static MC_Communicator * new_Communicator(MC_Communicator &);
   static MC_Communicator * get_BroadcastCommunicator();
 
-  virtual int add_EndPoint(const char * hostname, unsigned short port)=0;
-  virtual int add_EndPoint(MC_EndPoint *)=0;
-  virtual int size()=0;
-  virtual const char * get_HostName(int)=0; 
-  virtual unsigned short get_Port(int)=0;
-  virtual unsigned int get_Id(int)=0;
+  virtual int add_EndPoint(const char * hostname, unsigned short port)=NULL;
+  virtual int add_EndPoint(MC_EndPoint *)=NULL;
+  virtual int size()=NULL;
+  virtual const char * get_HostName(int)=NULL; 
+  virtual unsigned short get_Port(int)=NULL;
+  virtual unsigned int get_Id(int)=NULL;
 };
 
 class MC_Stream{
@@ -47,13 +69,13 @@ class MC_Stream{
   static int recv(int *tag, void **buf, MC_Stream ** stream);
   static int unpack(char * buf, const char * format_str, ...);
 
-  virtual int send(int tag, const char * format_str, ...)=0;
-  virtual int flush()=0;
-  virtual int recv(int *tag, void **buf)=0;
+  virtual int send(int tag, const char * format_str, ...)=NULL;
+  virtual int flush()=NULL;
+  virtual int recv(int *tag, void **buf)=NULL;
 
   //static MC_Stream * get_Stream(int stream_id);
-  //static MC_Stream * new_Stream(int stream_id, int * backends=0,
-  // int num_backends=-1, int filter_id=-1)=0;
+  //static MC_Stream * new_Stream(int stream_id, int * backends=NULL,
+  // int num_backends=-1, int filter_id=-1)=NULL;
 };
 
 #endif /* mrnet_H */
