11c781e05dc66df27e3199432a70c4cb2ff602c0
schendel <schendel>
2003-07-31 19:01:44 +0000

    remove cvs log info from file;

--- paradynd/src/comm.C
+++ paradynd/src/comm.C
@@ -39,74 +39,7 @@
  * incur to third parties resulting from your use of Paradyn.
  */
 
-/*
- * Implements virtual function called during an igen error.
- *
- * $Log: comm.C,v $
- * Revision 1.17  2003/07/15 22:46:41  schendel
- * rename string to pdstring
- *
- * Revision 1.16  2002/10/15 17:11:34  schendel
- * create Paradyn specific pd_process and pd_thread classes  - - - - - - - -
- * fixed include problem
- *
- * Revision 1.15  2000/07/28 17:22:10  pcroth
- * Updated #includes to reflect util library split
- *
- * Revision 1.14  1999/03/03 18:01:08  pcroth
- * Updated to support changes in igen output,
- * Updated to support automatic startup from Paradyn front end
- *
- * Revision 1.13  1998/04/22 02:37:24  buck
- * Moved showerror.h from paradynd directory to dyninstAPI directory.
- *
- * Revision 1.12  1997/11/01 05:02:07  tamches
- * changed an #include<> to #include""
- *
- * Revision 1.11  1997/03/18 19:45:54  buck
- * first commit of dyninst library.  Also includes:
- * 	moving templates from paradynd to dyninstAPI
- * 	converting showError into a function (in showerror.C)
- * 	many ifdefs for BPATCH_LIBRARY in dyinstAPI/src.
- *
- * Revision 1.10  1997/02/21 20:15:35  naim
- * Moving files from paradynd to dyninstAPI + eliminating references to
- * dataReqNode from the ast class. This is the first pre-dyninstAPI commit! - naim
- *
- * Revision 1.9  1996/08/16 21:18:19  tamches
- * updated copyright for release 1.1
- *
- * Revision 1.8  1995/11/22 00:02:13  mjrg
- * Updates for paradyndPVM on solaris
- * Fixed problem with wrong daemon getting connection to paradyn
- * Removed -f and -t arguments to paradyn
- * Added cleanUpAndExit to clean up and exit from pvm before we exit paradynd
- * Fixed bug in my previous commit
- *
- * Revision 1.7  1995/09/26  20:17:41  naim
- * Adding error messages using showErrorCallback function for paradynd
- *
- * Revision 1.6  1995/02/16  08:53:00  markc
- * Corrected error in comments -- I put a "star slash" in the comment.
- *
- * Revision 1.5  1995/02/16  08:32:54  markc
- * Changed igen interfaces to use strings/vectors rather than char igen-arrays
- * Changed igen interfaces to use bool, not Boolean.
- * Cleaned up symbol table parsing - favor properly labeled symbol table objects
- * Updated binary search for modules
- * Moved machine dependnent ptrace code to architecture specific files.
- * Moved machine dependent code out of class process.
- * Removed almost all compiler warnings.
- * Use "posix" like library to remove compiler warnings
- *
- * Revision 1.4  1994/11/02  11:01:57  markc
- * Replace printf's with logLine calls.
- *
- * Revision 1.3  1994/09/22  01:46:42  markc
- * Made system includes extern "C"
- * Access igen members via methods
- *
- */
+// $Id: comm.C,v
 
 #include "common/h/headers.h"
 #include "paradynd/src/comm.h"
