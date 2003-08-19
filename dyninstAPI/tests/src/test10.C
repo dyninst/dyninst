5032d6a3889840d8767d4cda7a1c09ca4d6f2b0a
hollings <hollings>
2003-08-19 18:36:32 +0000

    Added test case #10 - platform specific code generation issues.
    The initial version is sparc only but will compile on all UNIX
    systems we support.

---
+++ dyninstAPI/tests/src/test10.C
@@ -0,0 +1,391 @@
+// $Id: test10.C,v 1.1 2003/08/19 18:36:32 hollings Exp $
+//
+// libdyninst validation suite test #10
+//    Author: Jeff Hollingsworth Williams (14 aug 2003) 
+//
+
+//  This program tests the platform specific code modifications by
+//	of DyninstAPI.
+//	The mutatee that goes with this file is test10.mutatee.c
+//	
+//  Naming conventions:
+//      All functions, variables, etc are name funcXX_YY, exprXX_YY, etc.
+//          XX is the test number
+//          YY is the instance withing the test
+//	    func1_2 is the second function used in test case #1.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <unistd.h>
+
+#if defined(i386_unknown_linux2_0)
+#include <sys/types.h>
+#include <sys/wait.h>
+#endif
+
+#include "BPatch.h"
+#include "BPatch_Vector.h"
+#include "BPatch_thread.h"
+#include "BPatch_snippet.h"
+#include "test_util.h"
+#include "test1.h"
+
+#define TEST1 "1"
+#define TEST2 "2"
+#define TEST3 "3"
+#define TEST4 "4"
+  
+int debugPrint = 0; // internal "mutator" tracing
+int errorPrint = 0; // external "dyninst" tracing (via errorFunc)
+
+bool forceRelocation = true; // force relocation of functions
+
+int mutateeCplusplus = 0;
+int mutateeFortran = 0;
+int mutateeF77 = 0;
+bool runAllTests = true;
+const unsigned int MAX_TEST = 6;
+bool runTest[MAX_TEST+1];
+bool passedTest[MAX_TEST+1];
+
+template class BPatch_Vector<BPatch_variableExpr*>;
+template class BPatch_Set<int>;
+
+BPatch *bpatch;
+
+static const char *mutateeNameRoot = "test10.mutatee";
+
+// control debug printf statements
+#define dprintf	if (debugPrint) printf
+
+/**************************************************************************
+ * Error callback
+ **************************************************************************/
+
+#define DYNINST_NO_ERROR -1
+
+int expectError = DYNINST_NO_ERROR;
+
+void errorFunc(BPatchErrorLevel level, int num, const char **params)
+{
+    if (num == 0) {
+        // conditional reporting of warnings and informational messages
+        if (errorPrint) {
+            if (level == BPatchInfo)
+              { if (errorPrint > 1) printf("%s\n", params[0]); }
+            else
+                printf("%s", params[0]);
+        }
+    } else {
+        // reporting of actual errors
+        char line[256];
+        const char *msg = bpatch->getEnglishErrorString(num);
+        bpatch->formatErrorString(line, sizeof(line), msg, params);
+        
+        if (num != expectError) {
+	  if(num != 112)
+	    printf("Error #%d (level %d): %s\n", num, level, line);
+        
+            // We consider some errors fatal.
+            if (num == 101) {
+               exit(-1);
+            }
+        }
+    }
+}
+
+void createInstPointError(BPatchErrorLevel level, int num, const char **params)
+{
+    if (num != 117 && num != 118)
+	errorFunc(level, num, params);
+}
+
+/**************************************************************************
+ * Utility functions
+ **************************************************************************/
+// Wrapper function to find variables
+// For Fortran, will look for lowercase variable, if mixed case not found
+BPatch_variableExpr *findVariable(BPatch_image *appImage, const char* var,
+                                  BPatch_Vector <BPatch_point *> *point = NULL)
+{
+  //BPatch_variableExpr *FortVar = NULL;
+    BPatch_variableExpr *ret = NULL;
+    int i, numchars = strlen (var);
+    char *lowercase = new char [numchars];
+    int temp = expectError;
+
+    if (mutateeFortran && point) {
+            strcpy (lowercase, var);
+            expectError = 100;
+            for (i = 0; i < numchars; i++)
+                lowercase [i] = tolower (lowercase [i]);
+            ret = appImage->findVariable (*(*point) [0], lowercase);
+        if (!ret) {
+            expectError = temp;
+            ret = appImage->findVariable (*(*point) [0], var);
+        }
+    } else {
+        ret = appImage->findVariable (var);
+    }
+
+    expectError = temp;
+    delete [] lowercase;
+    return ret;
+}
+
+
+void instrumentToCallZeroArg(BPatch_thread *appThread, BPatch_image *appImage, char *instrumentee, char*patch, int testNo, char *testName){
+
+  BPatch_Vector<BPatch_function *> found_funcs;
+  if ((NULL == appImage->findFunction(instrumentee, found_funcs)) || !found_funcs.size()) {
+    fprintf(stderr, "    Unable to find function %s\n","instrumentee");
+    exit(1);
+  }
+  
+  if (1 < found_funcs.size()) {
+    fprintf(stderr, "%s[%d]:  WARNING  : found %d functions named %s.  Using the first.\n", 
+	    __FILE__, __LINE__, found_funcs.size(), instrumentee);
+  }
+  
+  BPatch_Vector<BPatch_point *> *point1_1 = found_funcs[0]->findPoint(BPatch_entry);
+
+
+  if (!point1_1 || ((*point1_1).size() == 0)) {
+    fprintf(stderr, "**Failed** test #%d (%s)\n", testNo,testName);
+    fprintf(stderr, "    Unable to find entry point to \"%s.\"\n",instrumentee);
+    exit(1);
+  }
+
+  BPatch_Vector<BPatch_function *> bpfv;
+  if (NULL == appImage->findFunction(patch, bpfv) || !bpfv.size()
+      || NULL == bpfv[0]){
+    fprintf(stderr, "**Failed** test #%d (%s)\n", testNo, testName);
+    fprintf(stderr, "    Unable to find function %s\n", patch);
+    exit(1);
+  }
+  BPatch_function *call1_func = bpfv[0];
+  
+  BPatch_Vector<BPatch_snippet *> call1_args;
+  BPatch_funcCallExpr call1Expr(*call1_func, call1_args);
+  
+  dprintf("Inserted snippet2\n");
+  appThread->insertSnippet(call1Expr, *point1_1);
+
+}
+
+
+//
+// Start Test Case #1 - ()
+//
+void mutatorTest1(BPatch_thread *appThread, BPatch_image *appImage)
+{
+    instrumentToCallZeroArg(appThread, appImage, "func1", "call1", 1, "test10.1");
+}
+
+//
+// Start Test Case #2 
+//
+void mutatorTest2(BPatch_thread *appThread, BPatch_image *appImage)
+{
+    instrumentToCallZeroArg(appThread, appImage, "func2", "call2", 2, "test10.2");
+}
+
+
+//
+// Start Test Case #3 
+//
+void mutatorTest3(BPatch_thread *appThread, BPatch_image *appImage)
+{
+    instrumentToCallZeroArg(appThread, appImage, "func3", "call3", 3, "test10.3");
+}
+
+//
+// Start Test Case #4 
+//
+void mutatorTest4(BPatch_thread *appThread, BPatch_image *appImage)
+{
+    instrumentToCallZeroArg(appThread, appImage, "func4", "call4", 4, "test10.4");
+}
+
+int mutatorMAIN(char *pathname)
+{
+
+    BPatch_thread *appThread;
+    BPatch_image *appImage;
+
+    // Create an instance of the BPatch library
+    bpatch = new BPatch;
+
+    // Force functions to be relocated
+    bpatch->setForcedRelocation_NP(true);
+
+    // Register a callback function that prints any error messages
+    bpatch->registerErrorCallback(errorFunc);
+
+    // Start the mutatee
+    printf("Starting \"%s\"\n", pathname);
+
+    char* child_argv[MAX_TEST+5];
+
+    int n = 0;
+    child_argv[n++] = pathname;
+    if (debugPrint) child_argv[n++] = const_cast<char*>("-verbose");
+
+    if (runAllTests) {
+        child_argv[n++] = const_cast<char*>("-runall"); // signifies all tests
+    } else {
+        child_argv[n++] = const_cast<char*>("-run");
+        for (unsigned int j=1; j <= MAX_TEST; j++) {
+            if (runTest[j]) {
+        	char str[5];
+        	sprintf(str, "%d", j);
+        	child_argv[n++] = strdup(str);
+            }
+        }
+    }
+
+    child_argv[n] = NULL;
+
+    appThread = bpatch->createProcess(pathname, child_argv,NULL);
+    appImage = appThread->getImage();
+
+    if (runTest[1]) mutatorTest1(appThread, appImage);
+    if (runTest[2]) mutatorTest2(appThread, appImage);
+    if (runTest[3]) mutatorTest3(appThread, appImage);
+    if (runTest[4]) mutatorTest4(appThread, appImage);
+
+    appThread->continueExecution();
+
+    while (!appThread->isTerminated())
+        bpatch->waitForStatusChange();
+
+    int exitCode = appThread->terminationStatus();
+    if (exitCode || debugPrint) printf("Mutatee exit code 0x%x\n", exitCode);
+
+    dprintf("Done.\n");
+
+    return exitCode;
+}
+
+//
+// main - decide our role and call the correct "main"
+//
+int
+main(unsigned int argc, char *argv[])
+{
+    char mutateeName[128];
+    char libRTname[256];
+
+
+    strcpy(mutateeName,mutateeNameRoot);
+    libRTname[0]='\0';
+
+    if (!getenv("DYNINSTAPI_RT_LIB")) {
+	 fprintf(stderr,"Environment variable DYNINSTAPI_RT_LIB undefined:\n"
+	         "    set it to the full pathname of libdyninstAPI_RT\n");
+         exit(-1);
+    } else
+         strcpy((char *)libRTname, (char *)getenv("DYNINSTAPI_RT_LIB"));
+
+    unsigned int i;
+    // by default run all tests
+    for (i=1; i <= MAX_TEST; i++) {
+        runTest[i] = true;
+        passedTest[i] = false;
+    }
+
+    for (i=1; i < argc; i++) {
+        if (strncmp(argv[i], "-v+", 3) == 0)    errorPrint++;
+        if (strncmp(argv[i], "-v++", 4) == 0)   errorPrint++;
+	if (strncmp(argv[i], "-verbose", 2) == 0) {
+	    debugPrint = 1;
+	} else if (!strcmp(argv[i], "-V")) {
+            fprintf (stdout, "%s\n", V_libdyninstAPI);
+            if (libRTname[0])
+                fprintf (stdout, "DYNINSTAPI_RT_LIB=%s\n", libRTname);
+            fflush(stdout);
+	} else if (!strcmp(argv[i], "-skip")) {
+	    unsigned int j;
+	    runAllTests = false;
+            for (j=i+1; j < argc; j++) {
+                unsigned int testId;
+                if ((testId = atoi(argv[j]))) {
+                    if ((testId > 0) && (testId <= MAX_TEST)) {
+                        runTest[testId] = false;
+                    } else {
+                        printf("invalid test %d requested\n", testId);
+                        exit(-1);
+                    }
+                } else {
+                    // end of test list
+		    break;
+                }
+            }
+            i=j-1;
+	} else if (!strcmp(argv[i], "-run")) {
+	    unsigned int j;
+	    runAllTests = false;
+            for (j=0; j <= MAX_TEST; j++) runTest[j] = false;
+            for (j=i+1; j < argc; j++) {
+                unsigned int testId;
+                if ((testId = atoi(argv[j]))) {
+                    if ((testId > 0) && (testId <= MAX_TEST)) {
+                        runTest[testId] = true;
+                    } else {
+                        printf("invalid test %d requested\n", testId);
+                        exit(-1);
+                    }
+                } else {
+                    // end of test list
+		    break;
+                }
+            }
+            i=j-1;
+	} else if (!strcmp(argv[i], "-mutatee")) {
+	    i++;
+            if (*argv[i]=='_')
+                strcat(mutateeName,argv[i]);
+            else
+                strcpy(mutateeName,argv[i]);
+#if defined(i386_unknown_nt4_0) || defined(i386_unknown_linux2_0) || defined(sparc_sun_solaris2_4)
+	} else if (!strcmp(argv[i], "-relocate")) {
+            forceRelocation = true;
+#endif
+	} else {
+	    fprintf(stderr, "Usage: test10 "
+		    "[-V] [-verbose]  "
+#if defined(sparc_sun_solaris2_4) || defined(i386_unknown_linux2_0) || defined(rs6000_ibm_aix4_1)
+		    "[-saveworld] "
+#endif 
+                    "[-mutatee <test1.mutatee>] "
+		    "[-run <test#> <test#> ...] "
+		    "[-skip <test#> <test#> ...]\n");
+            fprintf(stderr, "%d subtests\n", MAX_TEST);
+	    exit(-1);
+	}
+    }
+
+    if (!runAllTests) {
+        printf("Running Tests: ");
+	for (unsigned int j=1; j <= MAX_TEST; j++) {
+	    if (runTest[j]) printf("%d ", j);
+	}
+	printf("\n");
+    }
+
+    // patch up the default compiler in mutatee name (if necessary)
+    if (!strstr(mutateeName, "_"))
+#if defined(i386_unknown_nt4_0)
+        strcat(mutateeName,"_VC");
+#else
+        strcat(mutateeName,"_gcc");
+#endif
+    int exitCode = mutatorMAIN(mutateeName);
+
+	int testsFailed=0;
+
+    return exitCode;
+}
---
+++ dyninstAPI/tests/src/test10.mutatee.c
@@ -0,0 +1,280 @@
+int dummy = 0;
+
+#include <stdio.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define USAGE "Usage: test10.mutatee [-attach] [-verbose] -run <num> .."
+
+#define MAX_TEST 4
+#define TRUE 1
+#define FALSE 0
+int debugPrint = 0;
+#define dprintf if (debugPrint) printf
+int runTest[MAX_TEST+1];
+int passedTest[MAX_TEST+1];
+const char Builder_id[]=COMPILER; /* defined on compile line */
+
+
+#if defined(sparc_sun_solaris2_4) 
+
+extern void func1();
+extern void func2();
+extern void func3();
+extern void func4();
+
+//
+// force jmp %reg; nop tail code function
+//
+asm("		.align 8");
+asm("		.stabs  \"func1:F(0,1)\",36,0,16,func1");
+asm("		.global func1");
+asm("		.type   func1,#function");
+asm("func1:");
+asm("		sethi   %hi(call0),%g1");
+asm("		or      %g1,%lo(call0),%g1");
+asm("		jmp     %g1");
+asm("		nop	");
+
+
+//
+// force call; move to %07 tail code function
+//
+asm("		.stabs  \"func2:F(0,1)\",36,0,16,func2");
+asm("		.global func2");
+asm("		.type   func2,#function");
+asm("func2:");
+asm("		sethi %hi(call0),%l0");
+asm("		or %l0,%lo(call0),%l0");
+asm("		mov %o7, %l0");
+asm("		call call0");
+asm("		mov %l0, %o7");
+
+
+//
+// use call; load %07 with current PC 
+//
+asm("		.stabs  \"func3:F(0,1)\",36,0,16,func3");
+asm("		.global func3");
+asm("		.type   func3,#function");
+asm("func3:");
+asm("		save %sp, -112, %sp");
+asm("		nop");
+asm("		nop");
+asm("		call label3");		// external call to set pc into o7
+asm("		nop");
+asm("		call call0");
+asm("		nop");
+asm("		ret");
+asm("		restore");
+
+asm("		.stabs  \"func4:F(0,1)\",36,0,16,func4");
+asm("		.global func4");
+asm("		.type   func4,#function");
+asm("func4:");
+asm("		save %sp, -112, %sp");
+asm("		nop");
+asm("		call .+8");		// call to set pc into o7
+asm("		restore");
+asm("		nop");
+asm("		ret");
+asm("		restore");
+
+// moved after func4 to make sure it appears to be "outside" func3
+asm("label3:");
+asm("		retl");
+asm("		nop");
+asm("		call abort");
+asm("		nop");
+
+void call1()
+{
+    passedTest[1] = TRUE;
+    printf("\nPassed test #1 \n");
+}
+
+void call2()
+{
+    passedTest[2] = TRUE;
+    printf("\nPassed test #2 \n");
+}
+
+void call3()
+{
+    passedTest[3] = TRUE;
+    printf("\nPassed test #3 \n");
+}
+
+void call4()
+{
+    passedTest[4] = TRUE;
+    printf("\nPassed test #4 \n");
+}
+
+#else
+
+void func1() 
+{ 
+    passedTest[1] = TRUE; 
+}
+
+void func2() 
+{ 
+    passedTest[2] = TRUE; 
+}
+
+void func3() 
+{ 
+    passedTest[3] = TRUE; 
+}
+
+void func4() 
+{ 
+    passedTest[4] = TRUE; 
+}
+
+
+void call0()
+{
+}
+
+void call1()
+{
+    passedTest[1] = TRUE;
+    printf("\Skipped test #1 N/A\n");
+}
+
+void call2()
+{
+    passedTest[2] = TRUE;
+    printf("\nSkipped test #2 N/A\n");
+}
+
+void call3()
+{
+    passedTest[3] = TRUE;
+    printf("\nSkipped test #3 N/A\n");
+}
+
+void call4()
+{
+    passedTest[4] = TRUE;
+    printf("\nSkipped test #4 N/A\n");
+}
+
+#endif
+
+void runTests()
+{
+    int j;
+
+    for (j=0; j <= MAX_TEST; j++) {
+        passedTest [j] = FALSE;
+    }
+
+    if (runTest[1]) {
+	func1();
+	if (!passedTest[1]) printf("\n **Failed** test #1\n");
+    }
+
+    if (runTest[2]) {
+	func2();
+	if (!passedTest[1]) printf("\n **Failed** test #2\n");
+    }
+
+    if (runTest[3]) {
+	func3();
+	if (!passedTest[1]) printf("\n **Failed** test #3\n");
+    }
+
+    if (runTest[4]) {
+	func4();
+	if (!passedTest[1]) printf("\n **Failed** test #4\n");
+    }
+}
+
+
+int main(int iargc, char *argv[])
+{                                       /* despite different conventions */
+    unsigned argc=(unsigned)iargc;      /* make argc consistently unsigned */
+    unsigned int i, j;
+    unsigned int testsFailed = 0;
+    int useAttach = FALSE;
+#ifndef i386_unknown_nt4_0
+    int pfd;
+#endif
+
+    for (j=0; j <= MAX_TEST; j++) {
+	runTest [j] = FALSE;
+    }
+
+    for (i=1; i < argc; i++) {
+        if (!strcmp(argv[i], "-verbose")) {
+            debugPrint = TRUE;
+        } else if (!strcmp(argv[i], "-attach")) {
+            useAttach = TRUE;
+#ifndef i386_unknown_nt4_0
+	    if (++i >= argc) {
+		fprintf(stderr, "%s\n", USAGE);
+		exit(-1);
+	    }
+	    pfd = atoi(argv[i]);
+#endif
+        } else if (!strcmp(argv[i], "-runall")) {
+            dprintf("selecting all tests\n");
+            for (j=1; j <= MAX_TEST; j++) runTest[j] = TRUE;
+        } else if (!strcmp(argv[i], "-run")) {
+            for (j=i+1; j < argc; j++) {
+                unsigned int testId;
+                if ((testId = atoi(argv[j]))) {
+                    if ((testId > 0) && (testId <= MAX_TEST)) {
+                        dprintf("selecting test %d\n", testId);
+                        runTest[testId] = TRUE;
+                    } else {
+                        printf("invalid test %d requested\n", testId);
+                        exit(-1);
+                    }
+                } else {
+                    /* end of test list */
+		    break;
+                }
+            }
+            i=j-1;
+		} else {
+            fprintf(stderr, "%s\n", USAGE);
+            exit(-1);
+        }
+    }
+
+    if ((argc==1) || debugPrint)
+        printf("Mutatee %s [C]:\"%s\"\n", argv[0], Builder_id);
+    if (argc==1) exit(0);
+
+    /* actually invoke the tests.  This function is implemented in two
+     *   different locations (one for C and one for Fortran). Look in
+     *     test1.mutatee.c and test1.mutateeFortC.c for the code.
+     */
+    runTests();
+
+    /* See how we did running the tests. */
+    for (i=1; i <= MAX_TEST; i++) {
+        if (runTest[i] && !passedTest[i]) {
+	    testsFailed++;
+	}
+    }
+
+    if (!testsFailed) {
+	printf("All tests passed\n");
+    } else {
+	printf("**Failed** %d test%c\n",testsFailed,(testsFailed>1)?'s':' ');
+    }
+ 
+
+    fflush(stdout);
+    dprintf("Mutatee %s terminating.\n", argv[0]);
+    exit(testsFailed ? 127 :1); /* 1 is success! 127 is failure*/ 
+}
