75d9038921283f34fb6f38e06ec65051d118bcbe
tlmiller <tlmiller>
2003-09-23 17:28:56 +0000

    With this commit, the IA-64 port passes its first DyninstAPI test, `test2'. Nightly builds will therefore begin shortly.  - - - - - - - - - - - - - - Redefined dyn_saved_regs for mutatee-side state preservation.

--- dyninstAPI/src/linux-ia64.h
+++ dyninstAPI/src/linux-ia64.h
@@ -9,10 +9,17 @@
 
 IA64_bundle generateTrapBundle();
 
+#if defined( MUTATOR_SAVES_REGISTERS )
 struct dyn_saved_regs {
     struct pt_regs pt;
     struct switch_stack ss;
 };
+#else
+struct dyn_saved_regs {
+	Address pc;
+	bool restorePredicateRegistersFromStack;
+	};
+#endif
 
 /* Removes a #ifdef in linux.C to include this here. */
 #include <asm/ptrace_offsets.h>
