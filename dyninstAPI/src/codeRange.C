89228a9756cab180729733cf8631bb359ca65a99
bernat <bernat>
2003-10-21 17:22:50 +0000

    codeRange data structures

---
+++ dyninstAPI/src/codeRange.C
@@ -0,0 +1,398 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: codeRange.C,v 1.1 2003/10/21 17:22:50 bernat Exp $
+
+#include <stdio.h>
+#include "codeRange.h"
+
+
+void codeRangeTree::leftRotate(entry* pivot){
+	if(!pivot || (pivot == nil))
+		return;
+	entry* y = pivot->right;
+	if(y == nil)
+		return;
+	pivot->right = y->left;
+	if(y->left != nil)
+		y->left->parent = pivot;
+	y->parent = pivot->parent;
+	if(!pivot->parent)
+		setData = y;
+	else if(pivot == pivot->parent->left)
+		pivot->parent->left = y;
+	else
+		pivot->parent->right = y;
+	y->left = pivot;
+	pivot->parent = y;
+}
+
+
+void codeRangeTree::rightRotate(entry* pivot){
+	if(!pivot || (pivot == nil))
+		return;
+	entry* x = pivot->left;
+	if(x == nil)
+		return;
+	pivot->left = x->right;
+	if(x->right != nil)
+		x->right->parent = pivot;
+	x->parent = pivot->parent;
+	if(!pivot->parent)
+		setData = x;
+	else if(pivot == pivot->parent->left)
+		pivot->parent->left = x;
+	else
+		pivot->parent->right = x;
+	x->right = pivot;
+	pivot->parent = x;
+}
+
+
+void codeRangeTree::deleteFixup(entry* x){
+	while((x != setData) && 
+	      (x->color == TREE_BLACK))
+	{
+		if(x == x->parent->left){
+			entry* w = x->parent->right;
+			if(w->color == TREE_RED){
+				w->color = TREE_BLACK;
+				x->parent->color = TREE_RED;
+				leftRotate(x->parent);
+				w = x->parent->right;
+			}
+			if((w->left->color == TREE_BLACK) &&
+			   (w->right->color == TREE_BLACK)){
+				w->color = TREE_RED;
+				x = x->parent;
+			}
+			else{
+				if(w->right->color == TREE_BLACK){
+					w->left->color = TREE_BLACK;
+					w->color = TREE_RED;
+					rightRotate(w);
+					w = x->parent->right;
+				}
+				w->color = x->parent->color;
+				x->parent->color = TREE_BLACK;
+				w->right->color = TREE_BLACK;
+				leftRotate(x->parent);
+				x = setData;
+			}
+		}
+		else{
+			entry* w = x->parent->left;
+			if(w->color == TREE_RED){
+				w->color = TREE_BLACK;
+				x->parent->color = TREE_RED;
+				rightRotate(x->parent);
+				w = x->parent->left;
+			}
+			if((w->right->color == TREE_BLACK) &&
+			   (w->left->color == TREE_BLACK)){
+				w->color = TREE_RED;
+				x = x->parent;
+			}
+			else{
+				if(w->left->color == TREE_BLACK){
+					w->right->color = TREE_BLACK;
+					w->color = TREE_RED;
+					leftRotate(w);
+					w = x->parent->left;
+				}
+				w->color = x->parent->color;
+				x->parent->color = TREE_BLACK;
+				w->left->color = TREE_BLACK;
+				rightRotate(x->parent);
+				x = setData;
+			}
+		}
+	}
+	x->color = TREE_BLACK;
+}
+
+
+codeRangeTree::entry *codeRangeTree::treeInsert(Address& key, codeRange *& value) {
+	entry* y = NULL;
+	entry* x = setData;
+	while(x != nil){
+		y = x;
+        if (key < x->key) 
+			x = x->left;
+		else if(key > x->key)
+			x = x->right;
+		else
+			return NULL;
+	}	
+	entry* z = new entry(key, value, nil);
+	z->parent = y;
+	if(!y)
+		setData = z;
+	else {
+        if (key < y->key)
+            y->left = z;
+		else if (key > y->key)
+			y->right = z;
+	}
+	setSize++;
+	return z;
+}
+
+/** finds the minimum value node when x is being deleted */
+
+codeRangeTree::entry *codeRangeTree::treeSuccessor(entry* x) const{
+	if(!x || (x == nil))
+		return NULL;
+	if(x->right != nil){
+		entry* z = x->right;
+		while(z->left != nil) z = z->left;
+		return z;
+	}
+	entry* y = x->parent;
+	while(y && (x == y->right)){
+		x = y;
+		y = y->parent;
+	}
+	return y;
+}
+
+
+codeRangeTree::entry *codeRangeTree::find_internal(const Address& element) const{
+	entry* x = setData;
+	while(x != nil){
+        if (element < x->key)
+            x = x->left;
+        else if (element > x->key)
+            x = x->right;
+        else
+            return x;
+	}	
+	return NULL;
+}
+
+
+void codeRangeTree::traverse(codeRange ** all,entry* node,int& n) const{
+	if(node == nil)
+		return;
+	if(node->left != nil)
+		traverse(all,node->left,n);
+	if(all)
+		all[n++] = node->value;
+	if(node->right != nil)
+		traverse(all,node->right,n);
+}
+
+//////////////////////////// PUBLIC FUNCTIONS ////////////////////////////////
+
+ void codeRangeTree::insert(Address& key, codeRange *& value){
+	entry* x = treeInsert(key, value);
+	if(!x) return;
+	x->color = TREE_RED;
+	while((x != setData) && (x->parent->color == TREE_RED)){
+		if(x->parent == x->parent->parent->left){
+			entry* y = x->parent->parent->right;
+			if(y->color == TREE_RED){
+				x->parent->color = TREE_BLACK;
+				y->color = TREE_BLACK;
+				x->parent->parent->color = TREE_RED;
+				x = x->parent->parent;
+			}
+			else{
+				if(x == x->parent->right){
+					x = x->parent;
+					leftRotate(x);
+				}
+				x->parent->color = TREE_BLACK;
+				x->parent->parent->color = TREE_RED;
+				rightRotate(x->parent->parent);
+			}
+		}
+		else{
+			entry* y = x->parent->parent->left;
+			if(y->color == TREE_RED){
+				x->parent->color = TREE_BLACK;
+				y->color = TREE_BLACK;
+				x->parent->parent->color = TREE_RED;
+				x = x->parent->parent;
+			}
+			else{
+				if(x == x->parent->left){
+					x = x->parent;
+					rightRotate(x);
+				}
+				x->parent->color = TREE_BLACK;
+				x->parent->parent->color = TREE_RED;
+				leftRotate(x->parent->parent);
+			}
+		}
+	}
+	setData->color = TREE_BLACK;
+}
+
+ void codeRangeTree::remove(const Address& key){
+	entry* z = find_internal(key);
+	if(!z)
+		return;
+	entry* y=((z->left == nil)||(z->right == nil)) ? z : treeSuccessor(z);
+	entry* x=(y->left != nil) ? y->left : y->right;
+	x->parent = y->parent;
+	if(!y->parent)
+		setData = x;
+	else if(y == y->parent->left)
+		y->parent->left = x;
+	else
+		y->parent->right = x;
+	if(y != z) {
+		z->value = y->value;
+        z->key = y->key;
+    }
+	if(y->color == TREE_BLACK)
+		deleteFixup(x);
+	setSize--;
+	delete y;
+}
+
+
+
+
+void codeRangeTree::destroy(entry* node){
+	if(!node || (node == nil))
+		return;
+	if(node->left != nil)
+		destroy(node->left);
+	if(node->right != nil)
+		destroy(node->right);
+	delete node;
+}
+
+ bool codeRangeTree::find(const Address& key, codeRange *& value) const{
+	entry* x = find_internal(key);
+    if (!x) return false;
+    value = x->value;
+    return true;
+}
+
+ bool codeRangeTree::precessor(const Address& key, codeRange * &value) const{
+    entry *x = setData;
+    entry *last = nil;
+    while (x != nil) {
+        assert(x != NULL);
+        if (x->key == key) {
+            value = x->value;
+            return true;
+        }
+        else if (key < x->key) {
+            x = x->left;
+        }
+        else { // key > x->key
+            last = x;
+            x = x->right;
+        }
+    }
+    if (x == nil) {
+        // Ran out of tree to search... get the parent
+        assert(last != NULL);
+        
+        if (last != nil) {
+            value = last->value;
+            return true;
+        }
+        else return false;
+    }
+    // Should never hit here
+    assert(0);
+    return false;
+}
+
+ bool codeRangeTree::successor(const Address& key, codeRange * &value) const{
+    entry *x = setData;
+    entry *last = nil;
+    while (x != nil) {
+        if (x->key == key) {
+            value = x->value;
+            return true;
+        }
+        else if (key > x->key) {
+            x = x->right;
+        }
+        else { // key < x->key
+            last = x;
+            x = x->left;
+        }
+    }
+    if (x == nil) {
+        // Ran out of tree to search... get the parent
+        if (last != nil) {
+            value = last->value;
+            return true;
+        }
+        else return false;
+    }
+    // Should never reach this point
+    assert(0);
+    return false;
+}
+
+ codeRange ** codeRangeTree::elements(codeRange ** buffer) const{
+	if(setData == nil) return NULL;
+	if(!buffer) return NULL;
+	int tmp = 0;
+	traverse(buffer,setData,tmp);	
+	return buffer;
+}
+
+
+codeRangeTree::entry *codeRangeTree::replicateTree(entry* node,entry* parent,
+                                                   entry* oldNil,entry* newNil)
+{
+	if(node == oldNil)
+		return newNil;	
+
+	entry* newNode = new entry(*node);
+	newNode->parent = parent; 
+	newNode->left = replicateTree(node->left,newNode,oldNil,newNil);
+	newNode->right = replicateTree(node->right,newNode,oldNil,newNil);
+	return newNode; 
+}
+
+
+
+
---
+++ dyninstAPI/src/codeRange.h
@@ -0,0 +1,230 @@
+/*
+ * Copyright (c) 1996 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: codeRange.h,v 1.1 2003/10/21 17:22:51 bernat Exp $
+
+
+#ifndef _codeRangeTree_h_
+#define _codeRangeTree_h_
+
+/*******************************************************/
+/*		header files 			       */
+/*******************************************************/
+
+#include <assert.h>
+#include <stdlib.h>
+#include "common/h/Types.h"
+
+/** template class for codeRangeTree. The implementation is based on red black
+  * tree implementation for efficiency concerns and for getting sorted
+  * elements easier.
+  * There are two template types, K (key) and V (value).
+  */
+
+/* Note: this is a near copy of BPatch_Set. That class didn't do what I needed,
+   so... -- bernat, 10OCT03 */
+
+typedef enum { TREE_RED, TREE_BLACK } color_t;
+
+class pd_Function;
+class image;
+class shared_object;
+class trampTemplate;
+class miniTrampHandle;
+class relocatedFuncInfo;
+
+struct codeRange {
+    pd_Function *function_ptr;
+    image *image_ptr;
+    shared_object *sharedobject_ptr;
+    trampTemplate *basetramp_ptr;
+    miniTrampHandle *minitramp_ptr;
+    relocatedFuncInfo *reloc_ptr;
+};
+
+
+class codeRangeTree {
+
+	/** tree implementation structure. Used to implement the RB tree */
+	typedef struct entry {
+        Address key;
+        codeRange *value;
+        color_t color;	/* color of the node */
+		struct entry* left; /* left child */
+		struct entry* right; /* right child */
+		struct entry* parent; /* parent of the node */
+
+		/** constructor for structure */
+		entry() 
+			: color(TREE_BLACK),left(NULL),right(NULL),parent(NULL) {}
+
+		/** constructor used for non-nil elements 
+		  * @param e nil entry
+		  */	  
+		entry(entry* e) //constructor with nil entry 
+			: color(TREE_RED),left(e),right(e),parent(NULL) {}
+
+		/** constructor
+		  * @param d data element
+		  * @param e nill entry 
+		  */
+		entry(const Address& key_, codeRange *& value_,entry* e) 
+		    	: key(key_), value(value_) ,color(TREE_RED),left(e),right(e),parent(NULL){}
+
+		/** constructor 
+		  * @param e the entry structure that will be copied 
+		  */
+		entry(const entry& e) : key(e.key),value(e.value),color(e.color),
+			left(NULL),right(NULL),parent(NULL) {}
+	} entry;
+
+	/** pointer to define the nil element of the tree NULL is not used
+	  * since some operations need sentinel nil which may have non-nil
+	  * parent.
+	  */
+	entry* nil;
+
+	/** size of the tree */
+	int setSize;
+	
+	/** pointer to the tree structure */
+	entry* setData;
+
+	// method that implements left rotation used by RB tree for balanced
+	// tree construction and keeps the RBtree properties.
+	 void leftRotate(entry*);
+
+	// method that implements right rotattion used by RB tree for balanced
+	// tree construction and keeps the RBtree properties.
+	 void rightRotate(entry*);
+
+	// method that modifies the tree structure after deletion for keeping
+	// the RBtree properties.
+	 void deleteFixup(entry*);
+
+	// insertion to a binary search tree. It returns the new element pointer
+	// that is inserted. If element is already there it returns NULL
+	 entry* treeInsert(Address&, codeRange *&);
+
+	// finds the elemnts in the tree that will be replaced with the element
+	// being deleted in the  deletion. That is the element with the largest
+	// smallest value than the element being deleted. 
+	 entry* treeSuccessor(entry* ) const;
+
+	// method that returns the entry pointer for the element that is searched
+	//for. If the entry is not found then it retuns NULL
+	 entry* find_internal(const Address&) const;
+
+	// infix traverse of the RB tree. It traverses the tree in ascending order
+	 void traverse(codeRange **,entry*,int&) const;
+
+	// deletes the tree structure for deconstructor.
+	 void destroy(entry*);
+
+public:
+
+	/** constructor. The default comparison structure is used */
+	 codeRangeTree() : setSize(0) 
+	{ 
+		nil = new entry;
+		setData = nil;
+	}
+
+    /** copy constructor */
+     codeRangeTree(const codeRangeTree &y) {
+         fprintf(stderr, "Copying code range tree: this ==  0x%x\n", this);
+         nil = new entry;
+        setSize = y.setSize;
+        setData = replicateTree(y.setData, NULL, y.nil, nil);
+    }
+    
+	/** destructor which deletes all tree structure and allocated entries */
+	 ~codeRangeTree() 
+	{
+		destroy(setData);
+		delete nil;
+	}
+
+	/** returns the cardinality of the tree , number of elements */
+	 int size() const { return setSize; }
+
+	/** returns true if tree is empty */
+	 bool empty() const { return (setData == nil); }
+
+	/** inserts the element in the tree 
+	  * @param 1 element that will be inserted
+	  */
+	 void insert(Address&, codeRange *&);
+
+	/** removes the element in the tree 
+	  * @param 1 element that will be removed  
+	  */
+	 void remove(const Address&);
+
+	/** returns true if the argument is member of the codeRangeTree
+	  * @param e the element that will be searched for
+	  */
+	 bool find(const Address&, codeRange *&) const;
+
+    /** Returns the largest value less than or equal to the
+      * key given
+      */
+     bool precessor(const Address&, codeRange *&) const;
+
+    /** Returns the smallest value greater than or equal to the
+      * key given
+      */
+     bool successor(const Address&, codeRange *&) const;
+
+	/** fill an buffer array with the sorted
+	  * elements of the codeRangeTree in ascending order according to comparison function
+	  * if the codeRangeTree is empty it retuns NULL, other wise it returns 
+ 	  * the input argument.
+	  */
+	 codeRange ** elements(codeRange **) const;
+
+    // method that replicates the tree structure of this tree
+	 entry* replicateTree(entry*,entry*,entry*,entry*);
+};
+
+
+#endif /* _codeRangeTree_h_ */
+
