8a11c66e16e957b41d904299f2daf81649e0b754
bernat <bernat>
2003-10-21 17:22:08 +0000

    Moved some prototypes to inst.h

--- dyninstAPI/src/instP.h
+++ dyninstAPI/src/instP.h
@@ -39,7 +39,7 @@
  * incur to third parties resulting from your use of Paradyn.
  */
 
-// $Id: instP.h,v 1.41 2003/06/20 22:07:48 schendel Exp $
+// $Id: instP.h,v 1.42 2003/10/21 17:22:08 bernat Exp $
 
 #if !defined(instP_h)
 #define instP_h
@@ -52,95 +52,12 @@
 #include "dyninstAPI/src/arch.h"  // for type, instruction
 #include "dyninstAPI/src/frame.h"
 
-
-class trampTemplate {
- public:
-    int size;
-    void *trampTemp;		/* template of code to execute */
-    Address baseAddr;           /* the base address of this tramp */
-    /* used only in base tramp */
-    int globalPreOffset;
-    int globalPostOffset;
-    int localPreOffset;
-    int localPostOffset;
-
-    int localPreReturnOffset;   /* return offset for local pre tramps */
-    int localPostReturnOffset;  /* offset of the return instruction */
-
-    int returnInsOffset;
-    int skipPreInsOffset;
-    int skipPostInsOffset;
-    int emulateInsOffset;
-    int updateCostOffset;
-
-    int savePreInsOffset;
-    int restorePreInsOffset;
-    int savePostInsOffset;
-    int restorePostInsOffset;
-
-    int recursiveGuardPreJumpOffset;
-    int recursiveGuardPostJumpOffset;
-
-    int MTpreBranch;
-    int MTpostBranch;
-
-    int cost;			/* cost in cycles for this basetramp. */
-    Address costAddr;           /* address of cost in this tramp      */
-    bool prevInstru;
-    bool postInstru;
-    int  prevBaseCost;
-    int  postBaseCost;
-
-    void updateTrampCost(process *proc, int c);
-
-    bool inBasetramp( Address addr );
-    bool inSavedRegion( Address addr );
-};
-
-extern trampTemplate baseTemplate;
-
-class miniTrampHandle {
- public:
-  instInstance *inst;
-  // when and location are used to get the other mini-tramps at the point
-  callWhen when;	     /* call before or after instruction */
-  instPoint *location;       /* where we put the code */
-};
-
-// Callback func for deletion of a minitramp
-typedef void (*instInstanceFreeCallback)(void *, instInstance *);
-
-class instInstance {
-  static int _id;
- public:
-  enum { uninitialized_id = -1 };
-  static int get_new_id() { return _id++; }
-
-  instInstance() : ID(uninitialized_id), trampBase(0), returnAddr(0), 
-    baseInstance(NULL), cost(0), callback(NULL), callbackData(NULL) {
-  }
-
-  ~instInstance() {
-    if (callback)
-      (*callback)(callbackData, this);  
-  }
-
-  void registerCallback(instInstanceFreeCallback cb, void *data) {
-    callback = cb;
-    callbackData = data;
-  };
-
-  int ID;                    // used to match up instInstances in forked procs
-  Address trampBase;         // base of code
-  Address returnAddr;        // address of the return from tramp insn
-  trampTemplate *baseInstance;  // the base trampoline instance
-  int cost;		     // cost in cycles of this inst req.
-
-  // Material to check when deleting this instInstance
-  instInstanceFreeCallback callback; /* Callback to be made when
-					instance is deleted */
-  void *callbackData;                /* Associated data */
-};
+// base tramp template
+#include "dyninstAPI/src/trampTemplate.h"
+// minitramp data structure
+#include "dyninstAPI/src/miniTrampHandle.h"
+// minitramp list structure
+#include "dyninstAPI/src/installed_miniTramps_list.h"
 
 // class returnInstance: describes how to jmp to the base tramp
 class returnInstance {
@@ -210,14 +127,14 @@ class instWaitingList {
 
 extern pdvector<instWaitingList*> instWList;
 
-extern trampTemplate *findAndInstallBaseTramp(process *proc, 
-				 instPoint *&location,
-				 returnInstance *&retInstance,
-				 bool trampRecursiveDesired,
-				 bool noCost,
-                                 bool &deferred);
-extern void installTramp(instInstance *inst, process *proc, char *code, 
-			 int codeSize, instPoint *location, callWhen when);
+extern trampTemplate *findOrInstallBaseTramp(process *proc, 
+                                             instPoint *&location,
+                                             returnInstance *&retInstance,
+                                             bool trampRecursiveDesired,
+                                             bool noCost,
+                                             bool &deferred);
+extern void installTramp(miniTrampHandle *mtHandle, process *proc, char *code, 
+                         int codeSize);
 extern void modifyTrampReturn(process*, Address returnAddr, Address newReturnTo);
 extern void generateReturn(process *proc, Address currAddr, instPoint *location);
 extern void generateEmulationInsn(process *proc, Address addr, instPoint *location);
@@ -225,7 +142,6 @@ extern void generateNoOp(process *proc, Address addr);
 extern void initTramps(bool is_multithreaded);
 extern void generateBranch(process *proc, Address fromAddr, Address newAddr);
 extern void removeTramp(process *proc, instPoint *location);
-
 extern int flushPtrace();
-
+extern bool deleteBaseTramp(process *, trampTemplate *);
 #endif
