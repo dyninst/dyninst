7db8d3908722b2cca29d6147371c749a1a8b0c7c
bernat <bernat>
2005-07-29 19:23:01 +0000

    parsing/instrumentation new files  - - - - - - - - - - - - - - - - - - - miniTramp and miniTrampInstance

---
+++ dyninstAPI/src/miniTramp.C
@@ -0,0 +1,587 @@
+/*
+ * Copyright (c) 1996-2004 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as "Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: miniTramp.C,v 1.1 2005/07/29 19:23:01 bernat Exp $
+// Code to install and remove instrumentation from a running process.
+
+#include "miniTramp.h"
+#include "baseTramp.h"
+#include "instP.h"
+#include "instPoint.h"
+#include "process.h"
+
+int miniTramp::_id = 1;
+
+#if defined(os_aix)
+  extern void resetBRL(process *p, Address loc, unsigned val); //inst-power.C
+  extern void resetBR( process *p, Address loc);               //inst-power.C
+#endif
+
+/*
+ * The tramps are chained together left to right, so we need to find the
+ *    tramps to the left anf right of the one to delete, and then patch the
+ *    call from the left one to the old to call the right one.
+ *    Also we need to patch the return from the right one to go to the left
+ *    one.
+ *
+ * New logic: this routine gaps the minitramp out of the execution
+ * sequence, but leaves deletion for later. There is a routine in 
+ * the process object which maintains a list of elements to be deleted,
+ * and the associated data to ensure that deletion is safe. I've
+ * added a callback function to the instInstance class which is 
+ * called when deletion actually takes place. This allows recordkeeping
+ * for any data which may rely on the minitramp (i.e. Paradyn variables)
+ *
+ */
+
+// returns true if deleted, false if not deleted (because non-existant
+// or already deleted
+bool miniTramp::uninstrument() {
+
+  // First check: have we started to delete this guy already?
+  // This happens when we try to delete an instInstance and GC it
+  // We then pause the process, but if the process is exited Paradyn
+  // tries to disable all instrumentation... thus causing this one
+  // to be deleted again. Sigh. 
+  
+  // Better fix: figure out why we're double-deleting instrCodeNodes.
+
+  if (deleteInProgress)
+    return false;
+
+  deleteInProgress = true;
+
+  // We do next so that it's definitely fixed before we call
+  // correctBTJumps below.
+  if (next) {
+      next->prev = prev;
+  }
+  else {
+      // Like above, except last
+      baseT->lastMini = prev;
+  }
+
+  if (prev) {
+      prev->next = next; 
+  }
+  else {
+      // We're first, so clean up the base tramp
+      baseT->firstMini = next;
+      // Correcting of jumps will be handled by removeCode calls
+  }
+  
+  // DON'T delete the miniTramp. When it is deleted, the callback
+  // is made... which should only happen when the memory is freed.
+  // Place it on the list to be deleted.
+  for (unsigned i = 0; i < instances.size(); i++)
+      instances[i]->removeCode(NULL);
+  // When all instances are successfully deleted, the miniTramp
+  // will be deleted as well.
+  
+  if(BPatch::bpatch->baseTrampDeletion())
+      {
+          baseT->deleteIfEmpty();
+      }
+
+  return true;
+}
+
+void miniTramp::deleteMTI(miniTrampInstance *mti) {
+    for (unsigned i = 0; i < instances.size(); i++)
+        if (instances[i] == mti) {
+            instances[i] = instances.back();
+            instances.pop_back();               
+        }
+    if (deleteInProgress &&
+        (instances.size() == 0))
+        delete this;
+}
+
+process *miniTramp::proc() const { return instP->proc(); }
+
+// Defined in multiTramp.C, dinky "get the debugger to stop here" function.
+extern void debugBreakpoint();
+
+bool miniTramp::generateMT() {
+    inst_printf("AST pointer is %p\n", ast_);
+
+    // This can be called multiple times
+    if (miniTrampCode_()) return true;
+
+    miniTrampCode_.allocate(MAX_MINITRAMP_SIZE);
+    Register blank[] = {1};
+    registerSpace * regS = new registerSpace(0, blank, 0, blank, false);
+    
+    /* VG(11/06/01): Added location, needed by effective address AST node */
+    returnOffset = ast_->generateTramp(proc(), instP,
+                                       miniTrampCode_, &cost, 
+                                       false, // noCost -- we can always ignore it later
+                                       regS);
+
+    size_ = miniTrampCode_.used();
+    miniTrampCode_.finalize();
+
+    debugBreakpoint();
+    
+    return true;
+}
+
+#if 0
+bool instPoint::allocateAndCopy(miniTrampInstance *mti, unsigned size) {
+    // TODO: figure out where this miniTramp is near
+    // (platform-specific depending on jump style); allocate memory
+    // near that point; copy from the global buffer into allocated
+    // memory.
+
+    miniTramp *mini = mti->mini;
+    Address near = 0;
+    
+    // Okay, we want to allocate near the miniTramp before us (or
+    // after, if there is no before). So we get the appropriate miniT,
+    // use the baseTrampInstance to get the miniTrampInstance that
+    // corresponds to that, and go from there
+
+    miniTramp *nearby = mini->next;
+    if (!nearby) nearby = mini->prev;
+
+    if (nearby) {
+        miniTrampInstance *nearI = nearby->getMTInstanceByBTI(mti->baseTI);
+        assert(nearI);
+        near = nearI->trampBase;
+    }
+
+
+}
+#endif
+
+bool miniTramp::correctMTJumps() {
+    for (unsigned i = 0; i < instances.size(); i++) {
+        instances[i]->linkCode();
+    }
+    return true;
+}
+
+miniTrampInstance *miniTramp::getMTInstanceByBTI(baseTrampInstance *bti) {
+    for (unsigned i = 0; i < instances.size(); i++) {
+        if (instances[i]->baseTI == bti)
+            return instances[i];
+    }
+    // Didn't find it... add it if the miniTramp->baseTramp mapping
+    // is correct
+    assert(baseT == bti->baseT);
+    
+    miniTrampInstance *mtInst = new miniTrampInstance(this, bti);
+
+    instances.push_back(mtInst);
+    return mtInst;
+}
+
+miniTrampInstance::~miniTrampInstance() {
+    mini->deleteMTI(this);
+
+    proc()->deleteCodeRange(get_address_cr());
+    proc()->inferiorFree(trampBase);
+}
+
+
+unsigned miniTrampInstance::maxSizeRequired() {
+    // If in-line...
+
+    if (mini->baseT->firstMini == mini) {
+        inst_printf("Size request for first mini\n");
+        return miniTramp::interJumpSize();
+    }
+    return 0;
+}
+
+
+// This must write the "top-level" code for the minitramp;
+// that is, for inlined minitramps the entire body, and
+// for out-of-line minitramps a set of code that will
+// reach the (allocated and installed) minitramp.
+
+// Right now we only do out-of-line, so the first miniTramp
+// reserves space for a jump (filled with noops for now),
+// and the rest just return.
+
+bool miniTrampInstance::generateCode(codeGen &gen,
+                                     Address baseInMutatee) {
+    inst_printf("miniTrampInstance::generateCode(%p, 0x%x, %d)\n",
+                gen.start_ptr(), baseInMutatee, gen.used());
+    
+    assert(mini);
+    // if (in-line...)
+    // Are we first?
+    if (mini->baseT->firstMini == mini) {
+        if (!generated_) { 
+            gen.fill(miniTramp::interJumpSize(),
+                     codeGen::cgNOP);
+            mini->baseT->instSize = miniTramp::interJumpSize();
+        }
+        else {
+            gen.moveIndex(miniTramp::interJumpSize());
+        }
+    }
+    // And make-y the code
+    inst_printf("Generating MT code\n");
+    if (!mini->generateMT())
+        return false;
+    generated_ = true;
+    inst_printf("Done with MT code generation\n");
+    return true;
+}
+
+bool miniTrampInstance::installCode() {
+    // If in-line...
+
+    // Write, I say _write_ into the addr space
+    if (installed_) {
+        assert(trampBase);
+        return true;
+    }
+    
+    // See if there's anyone nearby.
+    Address nearAddr = 0;
+
+    miniTramp *nearby = (mini->next);
+    if (nearby) {
+        miniTrampInstance *nearMTI = nearby->getMTInstanceByBTI(baseTI);
+        assert(nearMTI);
+        nearAddr = nearMTI->trampBase;
+    }
+    if (!nearAddr) {
+        // Either no next, or hasn't been allocated... try again with prev
+        nearby = mini->prev;
+        if (nearby) {
+            miniTrampInstance *nearMTI = nearby->getMTInstanceByBTI(baseTI);
+            assert(nearMTI);
+            nearAddr = nearMTI->trampBase;
+        }
+    }
+    // NearAddr might be 0 if there's nobody nearby or if the neighbor hasn't been
+    // allocated yet.
+
+#if defined(arch_alpha)
+    // Short branch range...
+    if (!nearAddr)
+        nearAddr = baseTI->trampPreAddr();
+#endif
+
+
+    inferiorHeapType htype;
+#if defined(os_aix)
+    // We use the data heap on AIX because it is unlimited, unlike
+    // the textHeap. The text heap is allocated from spare parts of 
+    // pages, and as such can run out. Since minitramps can be arbitrarily
+    // far from the base tramp (link register branching), but only a 
+    // single jump from each other, we cluster them in the dataHeap.
+    htype = dataHeap;
+#else
+    htype = (mini->proc()->splitHeaps) ? ((inferiorHeapType) (textHeap | uncopiedHeap)) : (anyHeap);
+#endif
+    
+#if defined(os_aix)
+    if (proc()->requestTextMiniTramp ||
+        ((nearAddr < 0x20000000)  && (nearAddr > 0x0))) {
+        htype = anyHeap;
+        nearAddr = 0x10000000;
+    }
+    else {
+#if defined(bug_aix_proc_broken_fork)
+        if (mini->instP->func()->prettyName() == pdstring("__fork")) {
+            nearAddr = 0;
+            htype = dataHeap;
+        }
+#endif
+    }
+#endif
+    bool err = false;
+    trampBase = mini->proc()->inferiorMalloc(mini->size_, htype, nearAddr, &err);
+
+    if (err) {
+        cerr << "inst.C: failed allocation" << endl;
+        return false;
+    }
+    if (!proc()->writeTextSpace((void *)trampBase,
+                                mini->miniTrampCode_.used(),
+                                (void *)mini->miniTrampCode_.start_ptr()))
+        return false;
+
+    // TODO in-line
+    proc()->addCodeRange(this);
+    
+    installed_ = true;
+    return true;
+}
+
+bool miniTrampInstance::safeToFree(codeRange *range) {
+    // TODO: in-line...
+
+    if (dynamic_cast<miniTrampInstance *>(range) == this) {
+        return false;
+    }
+    else {
+        // Out-of-line miniTramps are independent; if we're not
+        // inside one, we can safely nuke.
+        return true;
+    }
+}
+
+void miniTrampInstance::removeCode(generatedCodeObject *subObject) {
+    // TODO: in-line
+
+    baseTrampInstance *delBTI = dynamic_cast<baseTrampInstance *>(subObject);
+
+    assert((subObject == NULL) || delBTI);
+
+    // removeCode can be called in one of two directions: from a child
+    // (with themselves as the object) or a parent. We differ in 
+    // behavior depending on the type.
+
+    if (subObject == NULL) {
+        baseTI->removeCode(this);
+        proc()->deleteGeneratedCode(this);
+        // Make sure our previous guy jumps to the next guy
+        if (mini->prev) {
+            miniTrampInstance *prevI = mini->prev->getMTInstanceByBTI(baseTI);
+            assert(prevI);
+            prevI->linkCode();
+        }
+    }
+    else {
+        assert(delBTI);
+        // Base tramp went away; but me may have been reattached to
+        // a different instance. If so, we're cool. If not, clean 
+        // up and go away.
+        if (delBTI == baseTI) {
+            proc()->deleteGeneratedCode(this);
+            // Don't call baseTI->removeCode...
+        }
+    }
+}
+
+void miniTrampInstance::freeCode() {
+    // TODO: in-line
+
+    // baseTrampInstance is deleted by the multiTramp...
+    // baseTI->deleteMTI(this);
+
+    mini->deleteMTI(this);
+    proc()->inferiorFree(trampBase);
+    delete this;
+}
+
+process *miniTrampInstance::proc() {
+    return mini->proc();
+}
+
+bool miniTrampInstance::linkCode() {
+    assert(baseTI);
+    assert(baseTI->trampPreAddr());
+
+    // if in-line...
+    if (mini->next) {
+        assert(baseTI);
+        miniTrampInstance *nextI = mini->next->getMTInstanceByBTI(baseTI);
+        assert(nextI);
+        inst_printf("Writing branch from 0x%x (0x%x,0x%x) to 0x%x, miniT -> miniT\n",
+                    trampBase + mini->returnOffset,
+                    trampBase,
+                    mini->returnOffset,
+                    nextI->trampBase);
+        generateAndWriteBranch(mini->proc(), 
+                               trampBase + mini->returnOffset,
+                               nextI->trampBase,
+                               miniTramp::interJumpSize());
+    }
+    else {
+        // Last one; go to the base tramp
+        inst_printf("Writing branch from 0x%x to 0x%x, miniT -> baseT\n",
+                    trampBase + mini->returnOffset,
+                    baseTI->miniTrampReturnAddr());
+#if defined(os_aix)
+        // TODO: miniTramp methods to wrap this.
+        resetBR(mini->proc(),
+                trampBase + mini->returnOffset);
+#else
+        generateAndWriteBranch(mini->proc(),
+                               (trampBase + mini->returnOffset),
+                               baseTI->miniTrampReturnAddr(),
+                               miniTramp::interJumpSize());
+#endif
+    }
+
+    linked_ = true;
+    return true;
+}
+
+void miniTrampInstance::invalidateCode() {
+    assert(!linked_);
+
+    if (trampBase)
+        proc()->inferiorFree(trampBase);
+    trampBase = 0;
+    
+    generated_ = false;
+    installed_ = false;
+}
+
+unsigned miniTrampInstance::cost() {
+    if (!mini->noCost_)
+        return mini->cost;
+    return 0;
+}
+
+generatedCodeObject *miniTrampInstance::replaceCode(generatedCodeObject *newParent) {
+    // We out-of-line, and so this just shifts allegiance.
+    baseTrampInstance *newBTI = dynamic_cast<baseTrampInstance *>(newParent);
+    assert(newBTI);
+
+    baseTI->deleteMTI(this);
+    baseTI = newBTI;
+
+    // Not linked yet...
+    linked_ = false;
+    return this;
+}
+ 
+unsigned miniTrampInstance::get_size_cr() const { 
+     return mini->size_;
+}
+
+miniTrampInstance::miniTrampInstance(const miniTrampInstance *parMTI,
+                                     baseTrampInstance *cBTI,
+                                     miniTramp *cMT,
+                                     process *child) :
+    generatedCodeObject(parMTI, child),
+    baseTI(cBTI),
+    mini(cMT),
+    trampBase(parMTI->trampBase),
+    deleted(parMTI->deleted) 
+{
+    mini->instances.push_back(this);
+}
+   
+                      
+
+miniTramp::miniTramp(callWhen when_,
+                     AstNode *ast,
+                     baseTramp *base,
+                     instPoint *inst,
+                     bool noCost) :
+    miniTrampCode_(),
+    ID(_id++), 
+    returnOffset(0), 
+    size_(0),
+    baseT(base),
+    instP(inst), when(when_),
+    cost(0), 
+    noCost_(noCost),
+    prev(NULL), next(NULL),
+    callback(NULL), callbackData(NULL),
+    deleteInProgress(false) {
+    ast_ = assignAst(ast);
+}
+
+miniTramp::miniTramp(const miniTramp *parMini,
+                     baseTramp *childT,
+                     process *proc) :
+    miniTrampCode_(parMini->miniTrampCode_),
+    ID(parMini->ID),
+    returnOffset(parMini->returnOffset),
+    size_(parMini->size_),
+    baseT(childT),
+    when(parMini->when),
+    cost(parMini->cost),
+    noCost_(parMini->noCost_),
+    prev(NULL),
+    next(NULL),
+    callback(NULL),
+    callbackData(NULL),
+    deleteInProgress(parMini->deleteInProgress)
+{
+    assert(parMini->ast_);
+    ast_ = assignAst(parMini->ast_);
+
+    // Fix up instP
+    instP = proc->findInstPByAddr(parMini->instP->addr());
+    assert(instP);
+    // Uhh... what about callbacks?
+    // Can either set them to null or have them returning 
+    // the same void * as their parent...
+
+}    
+
+miniTramp::~miniTramp() {
+    if (callback)
+        (*callback)(callbackData, this);  
+    removeAst(ast_);
+}
+
+// Given a miniTramp parentMT, find the equivalent in the child
+// process (matching by the ID member). Fill in childMT.
+  
+bool getInheritedMiniTramp(const miniTramp *parentMT, 
+                             miniTramp * &childMT, 
+                             process *childProc) {
+    pdvector<instPoint *> instPs;
+    if (parentMT->baseT->preInstP)
+        instPs.push_back(parentMT->baseT->preInstP);
+    if (parentMT->baseT->postInstP)
+        instPs.push_back(parentMT->baseT->postInstP);
+    
+    for (unsigned i = 0; i < instPs.size(); i++) {
+        
+        instPoint *childP = childProc->findInstPByAddr(instPs[i]->addr());
+        baseTramp *childB = childP->getBaseTramp(parentMT->when);
+        miniTramp *mt = childB->firstMini;
+        
+        while (mt) {
+            if (mt->ID == parentMT->ID) {
+                childMT = mt;
+                return true;
+            }
+            mt = mt->next;
+        }
+    }
+    return false;
+}
+
---
+++ dyninstAPI/src/miniTramp.h
@@ -0,0 +1,216 @@
+/*
+ * Copyright (c) 1996-2004 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as "Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+// $Id: miniTramp.h,v 1.1 2005/07/29 19:23:02 bernat Exp $
+
+#ifndef MINI_TRAMP_H
+#define MINI_TRAMP_H
+
+#include "common/h/Types.h"
+#include "dyninstAPI/src/codeRange.h"
+#include "dyninstAPI/src/inst.h" // callOrder and callWhen
+#include "dyninstAPI/src/ast.h" // assignAST
+#include "dyninstAPI/src/instPoint.h" // generatedCodeObject
+#include "dyninstAPI/src/multiTramp.h" // generatedCodeObject
+
+// This is a serious problem: our code generation has no way to check
+// the size of the buffer it's emitting to.
+// 1 megabyte buffer
+#define MAX_MINITRAMP_SIZE (0x100000)
+
+// Callback func for deletion of a minitramp
+class miniTramp;
+typedef void (*miniTrampFreeCallback)(void *, miniTramp *);
+
+// The new miniTramp class -- description of a particular minitramp.
+// mini tramps are kind of annoying... there can be multiple copies of
+// a single minitramp depending on whether a function has been cloned
+// or not. So we need a single miniTramp structure that can handle
+// multiple instantiations of actual code.
+
+class miniTrampInstance : public generatedCodeObject {
+    friend class miniTramp;
+ public:
+
+  miniTrampInstance(miniTramp *mini,
+                    baseTrampInstance *bti) :
+      generatedCodeObject(),
+      baseTI(bti),
+      mini(mini),
+      trampBase(0),
+      deleted(false) {
+  }
+
+  // FORK!
+  miniTrampInstance(const miniTrampInstance *parMini,
+                    baseTrampInstance *cBTI,
+                    miniTramp *cMT,
+                    process *child);
+  
+  // Need to have a destructor, so we can track when each instance goes away. 
+  ~miniTrampInstance();
+
+  baseTrampInstance *baseTI;
+  miniTramp *mini;
+  Address trampBase;
+  bool deleted;
+
+  Address get_address_cr() const { return trampBase; }
+  unsigned get_size_cr() const; // needs miniTramp and is so defined
+                                // in .C file
+
+  unsigned maxSizeRequired();
+
+  bool generateCode(codeGen &gen,
+                    Address baseInMutatee);
+  bool installCode();
+  
+  void invalidateCode();
+
+  bool linkCode();
+
+  void removeCode(generatedCodeObject *subObject);
+
+  generatedCodeObject *replaceCode(generatedCodeObject *newParent);
+
+  bool safeToFree(codeRange *range);
+
+  void freeCode();
+
+  unsigned cost();
+
+  process *proc();
+};
+
+class miniTramp {
+    friend class miniTrampInstance;
+  friend class instPoint;
+    // Global numbering of minitramps
+  static int _id;
+
+  miniTramp();
+
+  public:
+
+  miniTramp(callWhen when_,
+            AstNode *ast,
+	    baseTramp *base,
+	    instPoint *inst,
+            bool noCost);
+  
+  // Fork constructor
+  miniTramp(const miniTramp *parMini, baseTramp *childT, process *proc);
+  
+  ~miniTramp();
+
+  // From base tramp to mini tramp (or the reverse)
+  // Platform specific
+  static unsigned interJumpSize();
+  // Between mini tramps
+  static unsigned intraJumpSize();
+
+  // Generate the code necessary
+  // We use a single global image of a minitramp.
+  bool generateMT();
+  codeGen miniTrampCode_;
+  
+  // The delete operator, just without delete. Uninstruments minis.
+  bool uninstrument();
+
+  unsigned get_size_cr() const {
+    assert(returnOffset);
+    return returnOffset;
+  }
+  
+  // Register a callback for when the mini is finally deleted...
+  void registerCallback(miniTrampFreeCallback cb, void *data) {
+    callback = cb;
+    callbackData = data;
+  };
+
+
+  // Returns true if all's well.
+  bool checkMTStatus();
+
+  miniTrampInstance *getMTInstanceByBTI(baseTrampInstance *instance);
+
+  int ID;                    // used to match up miniTramps in forked procs
+  Address returnOffset;      // Offset from base to the return addr
+  unsigned size_;
+
+  // Base tramp we're installed at
+  baseTramp *baseT;
+
+  // And the instPoint we belong to.
+  instPoint *instP;
+
+  process *proc() const;
+
+  void deleteMTI(miniTrampInstance *);
+
+  // This can go away when base tramps become singular...
+  callWhen when; /* Pre or post */
+
+  int cost;		     // cost in cycles of this inst req.
+  bool noCost_;
+
+  pdvector<miniTrampInstance *> instances;
+  pdvector<miniTrampInstance *> deletedMTIs;
+
+  // Make sure all jumps are consistent...
+  bool correctMTJumps();
+
+  // Previous and next minitramps
+  miniTramp *prev;
+  miniTramp *next;
+
+  // Material to check when deleting this miniTramp
+  miniTrampFreeCallback callback; /* Callback to be made when
+					instance is deleted */
+  void *callbackData;                /* Associated data */
+
+  bool deleteInProgress; // Don't double-delete
+
+  AstNode *ast_; // For regenerating miniTs
+};
+
+
+#endif
