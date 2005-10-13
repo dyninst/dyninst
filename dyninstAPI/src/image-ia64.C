f8313625c2c7421916261199e9aa3e7004035d27
tlmiller <tlmiller>
2005-10-13 21:12:30 +0000

    Fix CFG creation so that allocs always open bundles.

--- dyninstAPI/src/image-ia64.C
+++ dyninstAPI/src/image-ia64.C
@@ -38,7 +38,7 @@
  * incur to third parties resulting from your use of Paradyn.
  */
 
-// $Id: image-ia64.C,v 1.4 2005/09/01 22:18:18 bernat Exp $
+// $Id: image-ia64.C,v 1.5 2005/10/13 21:12:30 tlmiller Exp $
 
 #include "common/h/Vector.h"
 #include "common/h/Dictionary.h"
@@ -51,124 +51,104 @@
 #include "showerror.h"
 #include "arch.h"
 
-
-bool image_func::findInstPoints(pdvector<Address> & /*callTargets*/)
-{
-    if( parsed_ ) 
-        {
-            parsing_printf("Error: multiple call of findInstPoints\n");
-            return false;
-        } 
-    parsed_ = true;
+bool image_func::findInstPoints( pdvector<Address> & /* callTargets */ ) {
+	if( parsed_ ) {
+		parsing_printf("Error: multiple call of findInstPoints\n");
+		return false;
+		} 
+	parsed_ = true;
     
-    parsing_printf("findInstPoints for func %s, at 0x%lx, end 0x%lx, size %d\n",
-		   symTabName().c_str(), getOffset(), getEndOffset(), getSymTabSize());
+	parsing_printf( "findInstPoints for func %s, at 0x%lx, end 0x%lx, size %d\n", symTabName().c_str(), getOffset(), getEndOffset(), getSymTabSize() );
     
-    BPatch_Set< Address > leaders;
-    dictionary_hash< Address, image_basicBlock* > leadersToBlock( addrHash );
+	BPatch_Set< Address > leaders;
+	dictionary_hash< Address, image_basicBlock* > leadersToBlock( addrHash );
     
-    // FIXME: laune's parser: this is wrong
-    Address funcBegin = getOffset();
-    Address funcEnd = getEndOffset();
+	// FIXME: laune's parser: this is wrong
+	Address funcBegin = getOffset();
+	Address funcEnd = getEndOffset();
+	InstrucIter ah0( funcBegin, this );	
+
+	// define entry instpoint 
+	image_instPoint * p = new image_instPoint( funcBegin, ah0.getInstruction(), this, functionEntry );
+	funcEntries_.push_back( p );
+
+	/* Iterate forward over instructions from all jump targets in the function,
+	   creating basic blocks and inst points as we go. */
+	Address currAddr = funcBegin;
+	BPatch_Set< Address > visited;
+	pdvector< Address > jmpTargets;
     
-    InstrucIter ah (funcBegin, this);
-
-    image_instPoint *p;
-
-    //define entry instpoint 
-    p = new image_instPoint(funcBegin,
-                            ah.getInstruction(),
-                            this,
-                            functionEntry);
-    funcEntries_.push_back(p);
+	/* Create the entry basic block */
+	leaders += funcBegin;
+	leadersToBlock[ funcBegin ] = new image_basicBlock( this, funcBegin );
+	leadersToBlock[ funcBegin ]->isEntryBlock_ = true;
+	blockList.push_back( leadersToBlock[ funcBegin ] );
+	jmpTargets.push_back( funcBegin );
     
-    Address currAddr = funcBegin;
-    
-    //find all the basic blocks and define the instpoints for this function
-    BPatch_Set< Address > visited;
-    pdvector< Address > jmpTargets;
+	for( unsigned i = 0; i < jmpTargets.size(); i++ ) {
+		InstrucIter ah( jmpTargets[ i ], this ); 
+		image_basicBlock * currBlk = leadersToBlock[ jmpTargets[ i ] ];
+		assert( currBlk );
+		assert( currBlk->firstInsnOffset() == jmpTargets[i] );
 
-    //entry basic block
-    leaders += funcBegin;
-    leadersToBlock[ funcBegin ] = new image_basicBlock(this, funcBegin);
-    leadersToBlock[ funcBegin ]->isEntryBlock_ = true;
-    blockList.push_back( leadersToBlock[ funcBegin ] );
-    jmpTargets.push_back( funcBegin );
-    
-    for( unsigned i = 0; i < jmpTargets.size(); i++ )
-    {
-        //parsing_printf("Making block iter for target 0x%x\n", jmpTargets[i]);
-        InstrucIter ah( jmpTargets[ i ], this ); 
-        image_basicBlock* currBlk = leadersToBlock[ jmpTargets[ i ] ];
+		/* Iteratate forward. */
+		while( true ) {
+			currAddr = * ah;
 
-	assert(currBlk);
-        assert(currBlk->firstInsnOffset() == jmpTargets[i]);
-
-        while( true ) {
-            currAddr = *ah;
-
-            if ((currAddr < funcBegin) ||
-                (currAddr >= funcEnd)) {
-                // Must not have seen a return....
-                currBlk->lastInsnOffset_ = ah.peekPrev();
-                currBlk->blockEndOffset_ = currAddr;
-                break;
-            }
-
-            if( visited.contains( currAddr ) )
-                break;
-            else
-                visited += currAddr;
-
-            //parsing_printf("checking insn at 0x%llx, %s + %d\n", *ah,
-            //symTabName().c_str(), (*ah) - funcBegin);
-
-            if( ah.isACondBranchInstruction() )
-                {
-                    parsing_printf("cond branch\n");
-                    currBlk->lastInsnOffset_ = currAddr; 
-                    currBlk->blockEndOffset_ = ah.peekNext();
+			/* Have we seen this address before? */
+			if( visited.contains( currAddr ) ) {
+				break;
+				}
+			visited += currAddr;
+			
+			/* Have we exited the function? */
+			if( (currAddr < funcBegin) || (currAddr >= funcEnd ) ) {
+				// /* DEBUG */ fprintf( stderr, "%s[%d]: exited function boundaries, truncating basic block.\n", __FILE__, __LINE__ );
+				currBlk->lastInsnOffset_ = ah.peekPrev();
+				currBlk->blockEndOffset_ = currAddr;
+				break;
+				}
+			
+			/* Begin considering the different types of instruction. */
+			if( ah.isACondBranchInstruction() ) {
+				/* Close off the current block. */
+				currBlk->lastInsnOffset_ = currAddr; 
+				currBlk->blockEndOffset_ = ah.peekNext();
                     
-                    Address target = ah.getBranchTargetAddress();
-                    // img()->addJumpTarget(target);
+				Address target = ah.getBranchTargetAddress();
 
-                    bool exit = false;
-
-                    if( (target < funcBegin) ||
-			(target >= funcEnd)) {
-                        exit = true;
+				/* Add the branch target, if it isn't headed outside the function. */
+				bool exit = false;
+				if( (target < funcBegin) || (target >= funcEnd) ) {
+					exit = true;
                     }
-                    else {
-                        addBasicBlock(target,
-                                      currBlk,
-                                      leaders,
-                                      leadersToBlock,
-                                      jmpTargets);
+				else {
+					addBasicBlock( target, currBlk, leaders, leadersToBlock, jmpTargets );
                     }                        
-                    
-                    Address t2 = ah.peekNext();
-                    if (t2 < funcEnd) {
-                        addBasicBlock(t2,
-                                      currBlk,
-                                      leaders,
-                                      leadersToBlock,
-                                      jmpTargets);
+                
+                /* Consider the fall-through case. */
+				Address t2 = ah.peekNext();
+				if( t2 < funcEnd ) {
+					addBasicBlock( t2, currBlk, leaders, leadersToBlock, jmpTargets );
                     }
-                    else {
-                        exit = true;
+				else {
+					exit = true;
+					}
+
+				if( exit ) {
+					currBlk->isExitBlock_ = true;
+					
+					p = new image_instPoint( currAddr, ah.getInstruction(), this, functionExit );
+					parsing_printf("Function exit at 0x%x\n", *ah);
+					funcReturns.push_back(p);
                     }
-                    if (exit) {
-                        currBlk->isExitBlock_ = true;
-                        // And make an inst point
-                        p = new image_instPoint(currAddr, 
-                                                ah.getInstruction(),
-                                                this,
-                                                functionExit);
-                        parsing_printf("Function exit at 0x%x\n", *ah);
-                        funcReturns.push_back(p);
-                    }
-                    break;
-                }
+                
+				/* We've found the end of this basic block; go look at another jump target. */
+				break;
+                } /* end if we found a conditional branch */
+
+
+                
              else if( ah.isAIndirectJumpInstruction() )
              {
                  parsing_printf("ind branch\n");
@@ -286,103 +266,43 @@ bool image_func::findInstPoints(pdvector<Address> & /*callTargets*/)
                  }
                  calls.push_back(p);
              }
-            else if( ah.getInstruction().getType() == instruction::ALLOC ) {
-                allocs.push_back(currAddr);
-                parsing_printf("Alloc insn (0x%llx)\n", currAddr);
-                // Alloc's don't actually start basic blocks, but our analysis
-                // has an easier time if they do. -- todd
-                if (currAddr == currBlk->firstInsnOffset()) {
-                    // Cool, we're done
-                }
-                else {
-                    addBasicBlock(currAddr,
-                                  currBlk,
-                                  leaders,
-                                  leadersToBlock,
-                                  jmpTargets);
-                }
-            } /* end if an alloc instruction */
-            else {
-                parsing_printf("Non-control-flow insn (0x%lx)\n", currAddr);
-            }
-            ah++;
-        }                   
-    }
+
+             
+            
+            /* An alloc instruction doesn't actually start a basic block, but our analysis is simpler if they do. */
+			else if( ah.getInstruction().getType() == instruction::ALLOC ) {
+				allocs.push_back( currAddr );
+				if( currAddr == currBlk->firstInsnOffset() ) {
+					/* If an alloc is the first instruction in its block, keep parsing the block. */
+					}
+				else {
+					/* Re-visit this alloc. */
+					visited.remove( currAddr );
+				
+					/* Close off the current block. */
+					currBlk->lastInsnOffset_ = ah.peekPrev(); 
+					currBlk->blockEndOffset_ = currAddr;
+					
+					// /* DEBUG */ fprintf( stderr, "%s[%d]: alloc block for function '%s': offset 0x%lx - 0x%lx\n",
+					//						__FILE__, __LINE__, symTabName().c_str(), currBlk->firstInsnOffset_ - getOffset(), currBlk->blockEndOffset_ - getOffset() );
+					
+					/* Start the next one; */
+					addBasicBlock( currAddr, currBlk, leaders, leadersToBlock, jmpTargets );
+        	        break;
+        	        }
+                } /* end if we found an alloc */
+                
+			else {
+				parsing_printf( "Non-control-flow insn (0x%lx)\n", currAddr );
+				} /* end the default case */
+            
+            /* Iterate over instructions. */
+			ah++;
+			} /* end while() loop over instructions */
+		} /* end for() loop over jump targets. */
 
     cleanBlockList();
 
     isInstrumentable_ = true;
     return true;
-}
-
-
-
-
-#if 0
-/* Required by symtab.C:
-   Locate the entry point (funcEntry_, an InstPoint), the call sites
-   (calls, vector<instPoint *>), and the return(s) (funcReturns, vector<instPoint *>).
- */
-bool int_function::findInstPoints( const image * i_owner ) {
-
-    parsed_ = true;
-    
-    /* We assume the function boundaries are correct.  [Check jump tables, etc.] */
-    Address addr = (Address)i_owner->getPtrToInstruction( getAddress( 0 ) );
-    
-    IA64_iterator iAddr( addr );
-    Address lastI = addr + get_size();
-    Address addressDelta = getAddress( 0 ) - addr;
-    
-    /* Generate an instPoint for the function entry. */
-    funcEntry_ = new instPoint( getAddress( 0 ), this, * iAddr, functionEntry );
-    
-    instruction * currInsn;
-    for( ; iAddr < lastI; iAddr++ ) {	
-        currInsn = * iAddr;
-        switch( currInsn->getType() ) {
-        case instruction::DIRECT_BRANCH: {
-            /* Some system call -wrapping functions (write(), read()) jump to common code
-               (at __libc_start_main+560) to handle system calls which return errors,
-               but this common code returns directly, instead of jumping back to its
-               callee, which means our exit instrumentation may not be run.
-               
-               Laune's new parsing code will handle cases like this, so for now,
-               use the same wrong assumption as the rest of the code (that symbol
-               table sizes are correct) to determine the boundaries of the function,
-               and claim that jumps outside these boundaries are exits. */
-            Address targetAddress = currInsn->getTargetAddress() + iAddr.getEncodedAddress() + addressDelta;
-            targetAddress =  targetAddress - (targetAddress % 16);
-            Address lowAddress = addr + addressDelta;
-            Address highAddress = addr + get_size() + addressDelta;
-            
-            if( ! ( lowAddress <= targetAddress && targetAddress < highAddress ) ) { 
-                // /* DEBUG */ fprintf( stderr, "%s[%d]: 0x%lx targets 0x%lx, outside of 0x%lx to 0x%lx\n", __FILE__, __LINE__, iAddr.getEncodedAddress() + addressDelta, targetAddress, lowAddress, highAddress );
-                funcReturns.push_back( new instPoint( iAddr.getEncodedAddress() + addressDelta, this, currInsn, functionExit ) );
-            }			
-        } break;				
-        
-        case instruction::RETURN:
-            funcReturns.push_back( new instPoint( iAddr.getEncodedAddress() + addressDelta, this, currInsn, functionExit ) );
-            break;
-            
-        case instruction::DIRECT_CALL:
-        case instruction::INDIRECT_CALL:
-            calls.push_back( new instPoint( iAddr.getEncodedAddress() + addressDelta, this, currInsn, callSite) );
-            break;
-            
-        case instruction::ALLOC:
-            allocs.push_back( iAddr.getEncodedAddress() - addr );
-            break;
-            
-        default:
-            break;
-        } /* end instruction type switch */
-    } /* end instruction iteration */
-    
-    isInstrumentable_ = 1;
-    return true; // if this function can ever return false, make sure the is_instrumentable flag
-    // is set properly in this function.
-} /* end findInstPoints() */
-
-#endif
+	} /* end image_func::findInstPoints() */
