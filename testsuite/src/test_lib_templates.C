ef6698fcc09d3f78b19c7377db2b615a46b1eb94
bpellin <bpellin>
2005-10-22 22:11:40 +0000

    Start building explicit templates for the testsuite

---
+++ testsuite/src/test_lib_templates.C
@@ -0,0 +1,11 @@
+
+#include "ParameterDict.h"
+#include <map>
+#include <string>
+
+template class std::map<std::string, Parameter*>;
+
+#include "Process_data.h"
+#include <vector>
+
+template class std::vector<Process_data>;
