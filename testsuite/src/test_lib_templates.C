b02a47bfcd34483929ad7ad948afa482c2d70c3f
bpellin <bpellin>
2005-11-22 19:41:19 +0000

    Fixes windows support in the new testsuite.  - - - - - - - - - - - - - - Changed templates for new ParameterDict representation

--- testsuite/src/test_lib_templates.C
+++ testsuite/src/test_lib_templates.C
@@ -1,9 +1,8 @@
-
-#include "ParameterDict.h"
-#include <map>
-#include <string>
-
-template class std::map<std::string, Parameter*>;
+#if defined(__XLC__) || defined(__xlC__)
+#pragma implementation("Dictionary.h")
+#else
+#pragma implementation "Dictionary.h"
+#endif
 
 #include "Process_data.h"
 #include <vector>
@@ -13,3 +12,9 @@ template class std::vector<Process_data>;
 // Templates used in test1_30.C
 template class std::vector<std::pair< unsigned long, unsigned long > >;
 template class std::vector<std::pair< const char *, unsigned int > >;
+
+#include "ParameterDict.h"
+#include "common/src/Dictionary.C"
+#include "common/h/String.h"
+template class dictionary_hash<pdstring, Parameter*>;
+template class pdvector<dictionary_hash<pdstring, Parameter*>::entry>;
