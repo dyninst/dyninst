d7c74c0865653d8fbfce809a1c6255300170bdc1
bernat <bernat>
2006-06-19 21:30:49 +0000

    Miscellaneous fixes  - - - - - - - - - - - - - - - - - - - - - - - - - - Remove tc_lock_trylock (unused function)

--- dyninstAPI_RT/src/RTthread.h
+++ dyninstAPI_RT/src/RTthread.h
@@ -68,7 +68,6 @@ typedef dyninst_lock_t tc_lock_t;
 
 int tc_lock_init(tc_lock_t*);
 int tc_lock_lock(tc_lock_t*);
-int tc_lock_trylock(tc_lock_t*);
 int tc_lock_unlock(tc_lock_t*);
 int tc_lock_destroy(tc_lock_t*);
 
