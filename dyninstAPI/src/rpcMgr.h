88ba1fa889742ae2975f78900d71010cba710d75
bernat <bernat>
2006-11-09 17:16:25 +0000

    Improve support for memory modifications common/h/Types.h  - - - - - - - - - - - - - - - - - - - - - - - - - - - - Add code to track register locations and pull 'em back out

--- dyninstAPI/src/rpcMgr.h
+++ dyninstAPI/src/rpcMgr.h
@@ -98,10 +98,26 @@ struct inferiorRPCtoDo {
 #include "codeRange.h"
 class inferiorRPCinProgress : public codeRange {
 	public:
-		virtual Address get_address_cr() const { return rpcStartAddr; }
-		virtual unsigned get_size_cr() const { return (rpcCompletionAddr - rpcStartAddr) + 1; }
-		virtual	void * getPtrToInstruction( Address /*addr*/ ) const { assert( 0 ); return NULL; }
-		        
+    inferiorRPCinProgress() : 
+        rpc(NULL),
+        savedRegs(NULL),
+        origPC(0),
+        runProcWhenDone(false),
+        rpcStartAddr(0),
+        rpcResultAddr(0),
+        rpcContPostResultAddr(0),
+        rpcCompletionAddr(0),
+        resultRegister(REG_NULL),
+        resultValue(NULL),
+        rpcthr(NULL),
+        rpclwp(NULL),
+        isProcessRPC(false),
+        state(irpcNotValid) {};
+
+    virtual Address get_address_cr() const { return rpcStartAddr; }
+    virtual unsigned get_size_cr() const { return (rpcCompletionAddr - rpcStartAddr) + 1; }
+    virtual	void * getPtrToInstruction( Address /*addr*/ ) const { assert( 0 ); return NULL; }
+    
     struct inferiorRPCtoDo *rpc;
     // Saved state about the RPC
     struct dyn_saved_regs *savedRegs;
