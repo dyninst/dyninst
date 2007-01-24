688b5b7aaf74a49e51995e5a4f34516ccad48a43
darnold <darnold>
2007-01-24 19:32:28 +0000

    Bug fixes and prep for MRNet minor release  - - - - - - - - - - - - - - - killed warning message

--- mrnet/mrnet/src/Tree.C
+++ mrnet/mrnet/src/Tree.C
@@ -1,3 +1,8 @@
+/****************************************************************************
+ * Copyright © 2003-2007 Dorian C. Arnold, Philip C. Roth, Barton P. Miller *
+ *                  Detailed MRNet usage rights in "LICENSE" file.          *
+ ****************************************************************************/
+
 #include <stdio.h>
 #include <assert.h>
 #include <ctype.h>
@@ -99,7 +104,7 @@ void Tree::create_TopologyFile( FILE * f )
     //}
 }
 
-void Tree::get_TopologyBuffer( char ** buf )
+void Tree::get_TopologyBuffer( char ** /* buf */ )
 {
 }
 
