772379843bc5552089eea2df31b34049f1c7746d
giri <giri>
2007-09-19 21:55:15 +0000

    Move debug information parsing to SymtabAPI  - - - - - - - - - - - - - - Add a new dynutil dir which includes declares utilities used by all current & future components of dyninst under the namespace Dyninst

---
+++ dynutil/h/dyntypes.h
@@ -0,0 +1,34 @@
+
+#if !defined(DYNTYPES_H)
+#define DYNTYPES_H
+
+#if defined(_MSC_VER)
+#include <windows.h>
+#endif
+
+namespace Dyninst
+{
+  //   typedef unsigned long Address;   
+   typedef unsigned long Offset;
+   
+#if defined(_MSC_VER)
+   typedef HANDLE PID;
+   typedef HANDLE LWP;
+   typedef HANDLE TID;
+   
+#define NULL_PID     INVALID_HANDLE_VALUE
+#define NULL_LWP     INVALID_HANDLE_VALUE
+#define NULL_TID     INVALID_HANDLE_VALUE
+
+#else
+   typedef int PID;
+   typedef int LWP;
+   typedef int TRID;
+
+#define NULL_PID     -1
+#define NULL_LWP     -1
+#define NULL_TRID     -1
+#endif
+}
+
+#endif
