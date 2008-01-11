0f09674bed2dd69dff0603d5f8e68b0f851cf1fa
giri <giri>
2008-01-11 21:51:35 +0000

    fix solaris9 build error

--- symtabAPI/src/emitElf.C
+++ symtabAPI/src/emitElf.C
@@ -29,7 +29,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef cap_libelf_so_0
+#if !defined(cap_libelf_so_0) && defined(os_linux)
 #define _FILE_OFFSET_BITS 64
 #endif
 
