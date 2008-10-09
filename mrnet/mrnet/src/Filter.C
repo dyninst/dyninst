da20cf5c26bb5a1c305fae6e0c86cf723ee2f7e9
mjbrim <mjbrim>
2008-10-09 19:52:55 +0000

    merge mrnet-2-0-development branch  - - - - - - - - - - - - - - - - - - - unified transformation and synchronization filter class

--- mrnet/mrnet/src/Filter.C
+++ mrnet/mrnet/src/Filter.C
@@ -6,12 +6,16 @@
 #include <vector>
 
 #include "mrnet/MRNet.h"
-#include "xplat/SharedObject.h"
-#include "Filter.h"
+
 #include "CommunicationNode.h"
+#include "Filter.h"
 #include "ParentNode.h"
+#include "PeerNode.h"
 #include "utils.h"
 
+#include "xplat/SharedObject.h"
+
+using namespace std;
 
 namespace MRN
 {
@@ -19,161 +23,142 @@ namespace MRN
 /*======================================*
  *    Filter Class Definition        *
  *======================================*/
-std::map < unsigned short, void (*)() >Filter::FilterFuncById;
-std::map < unsigned short, std::string > Filter::FilterFmtById;
+map< unsigned short, void (*)() > Filter::FilterFuncs;
+map< unsigned short, void (*)() > Filter::GetStateFuncs;
+map< unsigned short, string > Filter::FilterFmts;
 int FilterCounter::count=0;
 static FilterCounter fc;
 
-
-Filter::Filter(unsigned short _filter_id)
-  :filter_id(_filter_id), local_storage(NULL)
+Filter::Filter(unsigned short iid)
+    : _id(iid), _filter_state(NULL), _params(Packet::NullPacket)
 {
+    _filter_func =
+        (void (*)(const vector<PacketPtr>&, vector<PacketPtr>&, vector<PacketPtr>&, void **, PacketPtr& ))
+        FilterFuncs[iid];
+
+    _get_state_func = ( PacketPtr (*)( void **, int ) ) GetStateFuncs[iid];
+
+    _fmt_str = FilterFmts[iid];
 }
 
 Filter::~Filter(  )
 {
 }
 
-//TODO: use trans_filter and in_fid? Quiet warnings for now
-int Filter::load_FilterFunc( const char *so_file, const char *func,
-                             bool /* transformation_filter */,
-                             unsigned short /* in_fid */ )
+int Filter::push_Packets( vector< PacketPtr >& ipackets,
+                          vector< PacketPtr >& opackets,
+                          vector< PacketPtr >& opackets_reverse )
+{
+    mrn_dbg_func_begin();
+
+    _mutex.Lock();
+    
+    if( _filter_func == NULL ) {  //do nothing
+        opackets = ipackets;
+        ipackets.clear( );
+        mrn_dbg( 3, mrn_printf(FLF, stderr, "NULL FILTER: returning %d packets\n",
+                               opackets.size( ) ));
+        _mutex.Unlock();
+        return 0;
+    }
+
+    //TODO: put exception block to catch user error
+    _filter_func( ipackets, opackets, opackets_reverse, &_filter_state, _params );
+    ipackets.clear( );
+    
+    _mutex.Unlock();
+
+    mrn_dbg_func_end();
+    return 0;
+}
+
+PacketPtr Filter::get_FilterState( int istream_id )
+{
+    mrn_dbg_func_begin();
+
+    if( _get_state_func == NULL ){
+        return Packet::NullPacket;
+    }
+
+    PacketPtr packet( _get_state_func( &_filter_state, istream_id ) );
+
+    mrn_dbg_func_end();
+    return packet;
+}
+
+void Filter::set_FilterParams( PacketPtr iparams )
+{
+   mrn_dbg_func_begin();
+   _params = iparams;
+   mrn_dbg_func_end();
+}
+
+int Filter::load_FilterFunc( const char *iso_file, const char *ifunc_name )
 {
     XPlat::SharedObject* so_handle = NULL;
-    void (*func_ptr)()=NULL;
-    const char *fmt_str;
-    std::string func_fmt_str = func;
+    void (*filter_func_ptr)()=NULL;
+    void (*state_func_ptr)()=NULL;
+    const char **fmt_str_ptr=NULL;
+    string func_fmt_str = ifunc_name;
     func_fmt_str += "_format_string";
+    string state_func_name = ifunc_name;
+    state_func_name += "_get_state";
 
-    so_handle = XPlat::SharedObject::Load( so_file );
+    mrn_dbg_func_begin();
+
+    so_handle = XPlat::SharedObject::Load( iso_file );
     if( so_handle == NULL ) {
         mrn_dbg( 1, mrn_printf(FLF, stderr, "XPlat::SharedObject::Load(\"%s\"): %s\n",
-                 so_file, XPlat::SharedObject::GetErrorString() ));
-        char buf[1024];
-        sprintf( buf, "XPlat::SharedObject::Load(\"%s\"): %s\n",
-                 so_file, XPlat::SharedObject::GetErrorString() );
-        Event * event = Event::new_Event( ESYSTEM, buf );
-        Event::add_Event( *event );
-        delete event;
+                 iso_file, XPlat::SharedObject::GetErrorString() ));
         return -1;
     }
 
-    func_ptr = (void(*)())so_handle->GetSymbol( func );
-    if( func_ptr == NULL ) {
+    //find where the filter function is loaded
+    filter_func_ptr = (void(*)())so_handle->GetSymbol( ifunc_name );
+    if( filter_func_ptr == NULL ) {
         mrn_dbg( 1, mrn_printf(FLF, stderr,
                                "XPlat::SharedObject::GetSymbol(\"%s\"): %s\n",
-                               func, XPlat::SharedObject::GetErrorString() ));
+                               ifunc_name, XPlat::SharedObject::GetErrorString() ));
 
-        char buf[1024];
-        sprintf( buf, "XPlat::SharedObject::GetSymbol(\"%s\"): %s\n",
-                 so_file, XPlat::SharedObject::GetErrorString() );
-        Event *event = Event::new_Event( ESYSTEM, buf );
-        Event::add_Event( *event );
-        delete event;
         delete so_handle;
         return -1;
     }
 
-    fmt_str = ( const char * )so_handle->GetSymbol( func_fmt_str.c_str() );
-    if( fmt_str == NULL ) {
+    //find where the filter state function is loaded
+    //we don't test filter state function ptr because it doesn't have to exist
+    state_func_ptr = (void(*)())so_handle->GetSymbol( state_func_name.c_str() );
+
+    //find where the filter state format string is loaded
+    fmt_str_ptr = ( const char ** )so_handle->GetSymbol( func_fmt_str.c_str() );
+    if( fmt_str_ptr == NULL ) {
         mrn_dbg( 1, mrn_printf(FLF, stderr,
                                "XPlat::SharedObject::GetSymbol(\"%s\"): %s\n",
-                               func_fmt_str.c_str(), XPlat::SharedObject::GetErrorString()));
-        char buf[1024];
-        sprintf( buf, "XPlat::SharedObject::GetSymbol(\"%s\"): %s\n",
-                 so_file, XPlat::SharedObject::GetErrorString() );
-        Event * event = Event::new_Event( ESYSTEM, buf );
-        Event::add_Event( *event );
-        delete event;
+                               func_fmt_str.c_str(),
+                               XPlat::SharedObject::GetErrorString()));
         delete so_handle;
         return -1;
     }
 
-    return (int) register_Filter( func_ptr, fmt_str );
+    mrn_dbg_func_end();
+    return  (int)register_Filter( filter_func_ptr, state_func_ptr, *fmt_str_ptr );
 }
 
-/*==========================================*
- *    Aggregator Class Definition        *
- *==========================================*/
-TransFilter::TransFilter( unsigned short _filter_id )
-    : Filter( _filter_id )
+unsigned short Filter::register_Filter( void (*ifilter_func)( ),
+                                        void (*istate_func)( ),
+                                        const char *ifmt )
 {
-    trans_filter = ( void ( * )( const std::vector < Packet >&,
-                                 std::vector < Packet >&,
-                                 void ** ) )
-        FilterFuncById[_filter_id];
-    fmt_str = FilterFmtById[_filter_id];
-}
+    static unsigned short next_filter_id=0; 
+    unsigned short cur_filter_id=next_filter_id;
+    next_filter_id++;
 
-TransFilter::~TransFilter(  )
-{
-}
+    //FilterFuncs[cur_filter_id] = (void(*)())ifilter_func;
+    //StateFuncs[cur_filter_id] = (void(*)())istate_func;
 
-int TransFilter::push_packets( std::vector < Packet >&packets_in,
-                               std::vector < Packet >&packets_out )
-{
-    mrn_dbg( 3, mrn_printf(FLF, stderr, "In aggr.push_packets()\n" ));
-    
-    if( trans_filter == NULL ) {  //do nothing
-        packets_out = packets_in;
-        packets_in.clear(  );
-        mrn_dbg( 3, mrn_printf(FLF, stderr, "NULL FILTER: returning %d packets\n",
-                    packets_out.size(  ) ));
-        return 0;
-    }
+    FilterFuncs[cur_filter_id] = ifilter_func;
+    GetStateFuncs[cur_filter_id] = istate_func;
+    FilterFmts[cur_filter_id] = ifmt;
 
-    //TODO: put exception block to catch user error
-    trans_filter( packets_in, packets_out, &local_storage );
-    packets_in.clear(  );
-    
-    mrn_dbg( 3, mrn_printf(FLF, stderr, "trans_filter() returned %u packets\n",
-                packets_out.size() ));
-    mrn_dbg( 3, mrn_printf(FLF, stderr, "Leaving aggr.push_packets()\n" ));
-    return 0;
-}
-
-/*============================================*
- *    SyncFilter Class Definition        *
- *============================================*/
-SyncFilter::SyncFilter( unsigned short _filter_id,
-                        const std::list < const RemoteNode * >&nodes )
-    :    Filter( _filter_id ), downstream_nodes( nodes )
-{
-    sync_filter = ( void ( * )
-             ( std::vector < Packet >&,
-               std::vector < Packet >&,
-               const std::list < const RemoteNode * >&,
-               void ** ) )
-        FilterFuncById[_filter_id];
-}
-
-SyncFilter::~SyncFilter(  )
-{
-}
-
-int SyncFilter::push_packets( std::vector < Packet >&packets_in,
-                              std::vector < Packet >&packets_out )
-{
-    mrn_dbg( 3, mrn_printf(FLF, stderr,
-                "In sync.push_packets(). Pushing %d packets\n",
-                packets_in.size(  ) ));
-
-    if( sync_filter == NULL ) {  //do nothing
-        packets_out = packets_in;
-        packets_in.clear(  );
-        mrn_dbg( 3, mrn_printf(FLF, stderr, "NULL FILTER: returning %d packets\n",
-                    packets_out.size(  ) ));
-        return 0;
-    }
-
-    fsync.Lock(  );
-    //TODO: put exception block to catch user error
-    sync_filter( packets_in, packets_out, downstream_nodes, &local_storage );
-    fsync.Unlock(  );
-    mrn_dbg( 3, mrn_printf(FLF, stderr,
-                    "Leaving sync.push_packets(). Returning %d packets\n",
-                packets_out.size(  ) ));
-    return 0;
+    return cur_filter_id;
 }
 
 } // namespace MRN
--- mrnet/mrnet/src/Filter.h
+++ mrnet/mrnet/src/Filter.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright © 2003-2007 Dorian C. Arnold, Philip C. Roth, Barton P. Miller *
+ * Copyright © 2003-2008 Dorian C. Arnold, Philip C. Roth, Barton P. Miller *
  *                  Detailed MRNet usage rights in "LICENSE" file.          *
  ****************************************************************************/
 
@@ -7,123 +7,98 @@
 #define __filter_h 1
 
 #include <string>
-#include <list>
+#include <vector>
 #include <map>
 #include <vector>
+#include <set>
+#include "Error.h"
 #include "Message.h"
 #include "FilterDefinitions.h"
 #include "xplat/Mutex.h"
 
 #include "ParadynFilterDefinitions.h"
+#include "PeerNode.h"
 
 namespace MRN
 {
 
 class Filter: public Error {
-
- protected:
-    unsigned short filter_id;
-    void * local_storage; //TODO: deallocate when filter is destructed
+    friend class FilterCounter;
 
  public:
+    Filter( unsigned short iid );
+    ~Filter( void );
+    int push_Packets( std::vector < PacketPtr > &ipackets,
+                      std::vector < PacketPtr > &opackets, 
+                      std::vector < PacketPtr > &opackets_reverse
+                    );
+    PacketPtr get_FilterState( int istream_id );
+    void set_FilterParams( PacketPtr iparams );
+
+    static int load_FilterFunc( const char *so_file, const char *func );
+    static unsigned short register_Filter( void (*ifilter_func)( ),
+                                           void (*istate_func)( ),
+                                           const char * ifmt );
+
+ private:
     static void initialize_static_stuff( );
     static void free_static_stuff( );
-    static std::map < unsigned short, void(*)() >FilterFuncById;
-    static std::map < unsigned short, std::string > FilterFmtById;
-    static int load_FilterFunc( const char *so_file, const char *func,
-                                bool transformation_filter = true,
-                                unsigned short in_fid = 0 );
-    static unsigned short register_Filter( 
-       void (*)( ),
-       const char * fmt );
-    Filter( unsigned short _filter_id );
-    virtual ~ Filter(  );
-    virtual int push_packets( std::vector < Packet >&packets_in,
-                              std::vector < Packet >&packets_out ) = 0;
+
+    static std::map < unsigned short, void(*)() > FilterFuncs;
+    static std::map < unsigned short, void(*)() > GetStateFuncs;
+    static std::map < unsigned short, std::string > FilterFmts;
+
+    unsigned short _id;
+    void * _filter_state;  //TODO: deallocate when filter is destructed
+    XPlat::Mutex _mutex;
+    PacketPtr _params;
+    std::string _fmt_str;
+    void ( *_filter_func )( const std::vector < PacketPtr >&,
+                            std::vector < PacketPtr >&, 
+                            std::vector < PacketPtr >&, 
+                            void **, PacketPtr& );
+    PacketPtr ( *_get_state_func )( void ** ifilter_state, int istream_id );
 };
-
-class TransFilter:public Filter {
- private:
-    std::string fmt_str;
-    void ( *trans_filter ) ( const std::vector < Packet >&packets_in,
-                             std::vector < Packet >&packets_out,
-                             void ** );
- public:
-    TransFilter( unsigned short _fid );
-    virtual ~ TransFilter(  );
-    virtual int push_packets( std::vector < Packet >&packets_in,
-                              std::vector < Packet >&packets_out );
-};
-
-class RemoteNode;
-class SyncFilter:public Filter {
- private:
-    std::map < const RemoteNode *, std::vector < Packet >*>PacketsByNode;
-    void ( *sync_filter ) ( std::vector < Packet >&,
-                            std::vector < Packet >&,
-                            const std::list < const RemoteNode * >&, void ** );
-    const std::list < const RemoteNode * >downstream_nodes;
-    XPlat::Mutex fsync;
-    
- public:
-    SyncFilter( unsigned short _filter_id,
-                const std::list < const RemoteNode * >& );
-    virtual ~ SyncFilter(  );
-    virtual int push_packets( std::vector < Packet >&packets_in,
-                              std::vector < Packet >&packets_out );
-};
-
-inline unsigned short Filter::register_Filter( 
-    void (*func)( ),
-    const char * fmt )
-{
-    static unsigned short next_filter_id=0; 
-
-    next_filter_id++;
-    FilterFuncById[next_filter_id] = (void(*)())func;
-    FilterFmtById[next_filter_id] = fmt;
-
-    return next_filter_id;
-}
 
 inline void Filter::initialize_static_stuff( )
 {
-    TFILTER_NULL = register_Filter( (void(*)())NULL, TFILTER_NULL_FORMATSTR);
+    TFILTER_NULL = register_Filter( NULL, NULL, TFILTER_NULL_FORMATSTR);
 
-    TFILTER_SUM = register_Filter( (void(*)())tfilter_Sum, TFILTER_SUM_FORMATSTR);
+    TFILTER_SUM = register_Filter( (void(*)())tfilter_Sum, NULL, TFILTER_SUM_FORMATSTR);
 
-    TFILTER_AVG = register_Filter( (void(*)())tfilter_Avg, TFILTER_AVG_FORMATSTR);
+    TFILTER_AVG = register_Filter( (void(*)())tfilter_Avg, NULL, TFILTER_AVG_FORMATSTR);
 
-    TFILTER_MAX = register_Filter( (void(*)())tfilter_Max, TFILTER_MAX_FORMATSTR);
+    TFILTER_MAX = register_Filter( (void(*)())tfilter_Max, NULL, TFILTER_MAX_FORMATSTR);
 
-    TFILTER_MIN = register_Filter( (void(*)())tfilter_Min, TFILTER_MIN_FORMATSTR);
+    TFILTER_MIN = register_Filter( (void(*)())tfilter_Min, NULL, TFILTER_MIN_FORMATSTR);
 
-    TFILTER_ARRAY_CONCAT = register_Filter( (void(*)())tfilter_ArrayConcat,
+    TFILTER_ARRAY_CONCAT = register_Filter( (void(*)())tfilter_ArrayConcat, NULL,
                                             TFILTER_ARRAY_CONCAT_FORMATSTR);
 
-    TFILTER_INT_EQ_CLASS = register_Filter( (void(*)())tfilter_IntEqClass,
+    TFILTER_INT_EQ_CLASS = register_Filter( (void(*)())tfilter_IntEqClass, NULL,
                                             TFILTER_INT_EQ_CLASS_FORMATSTR);
 
     TFILTER_SAVE_LOCAL_CLOCK_SKEW_UPSTREAM =
-        register_Filter( (void(*)())save_LocalClockSkewUpstream,
+        register_Filter( (void(*)())save_LocalClockSkewUpstream, NULL,
                          TFILTER_SAVE_LOCAL_CLOCK_SKEW_UPSTREAM_FORMATSTR );
 
     TFILTER_SAVE_LOCAL_CLOCK_SKEW_DOWNSTREAM =
-        register_Filter( (void(*)())save_LocalClockSkewDownstream,
+        register_Filter( (void(*)())save_LocalClockSkewDownstream, NULL,
                          TFILTER_SAVE_LOCAL_CLOCK_SKEW_DOWNSTREAM_FORMATSTR );
 
     TFILTER_GET_CLOCK_SKEW =
-        register_Filter( (void(*)())get_ClockSkew,
+        register_Filter( (void(*)())get_ClockSkew, NULL,
                          TFILTER_GET_CLOCK_SKEW_FORMATSTR );
 
     TFILTER_PD_UINT_EQ_CLASS = register_Filter( (void(*)())tfilter_PDUIntEqClass,
+                                                NULL,
                                                 TFILTER_PD_UINT_EQ_CLASS_FORMATSTR);
 
-    SFILTER_DONTWAIT = register_Filter( (void(*)())NULL, "");
+    SFILTER_DONTWAIT = register_Filter( (void(*)())NULL, NULL, "");
 
-    SFILTER_WAITFORALL = register_Filter( (void(*)())sfilter_WaitForAll, "");
+    SFILTER_WAITFORALL = register_Filter( (void(*)())sfilter_WaitForAll, NULL, "");
 
-    SFILTER_TIMEOUT = register_Filter( (void(*)())sfilter_TimeOut, "");
+    SFILTER_TIMEOUT = register_Filter( (void(*)())sfilter_TimeOut, NULL, "");
 }
 
 inline void Filter::free_static_stuff( )
