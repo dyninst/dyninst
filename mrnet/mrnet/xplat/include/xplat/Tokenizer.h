9e3587486ebeef9985ce5658d5fa9c9552cd1a56
mjbrim <mjbrim>
2008-10-09 19:54:14 +0000

    merge mrnet-2-0-development branch  - - - - - - - - - - - - - - - - - - - move implementation to own file

--- mrnet/mrnet/xplat/include/xplat/Tokenizer.h
+++ mrnet/mrnet/xplat/include/xplat/Tokenizer.h
@@ -3,7 +3,7 @@
  *                  Detailed MRNet usage rights in "LICENSE" file.          *
  ****************************************************************************/
 
-// $Id: Tokenizer.h,v 1.3 2007/01/24 19:33:54 darnold Exp $
+// $Id: Tokenizer.h,v 1.4 2008/10/09 19:54:14 mjbrim Exp $
 #ifndef XPLAT_TOKENIZER_H
 #define XPLAT_TOKENIZER_H
 
@@ -15,62 +15,17 @@ namespace XPlat
 class Tokenizer
 {
 private:
-    const std::string& s;
+    const std::string &s;
     std::string::size_type curPos;
 
 public:
-    Tokenizer( const std::string& _s )
-      : s( _s )
-    {
-        Reset();
-    }
+    Tokenizer( const std::string& _s );
 
-    void Reset( void )
-    {
-        curPos = 0;
-    }
+    void Reset( void );
 
     std::string::size_type
-    GetNextToken( std::string::size_type& len, const char* delim = " \t" )
-    {
-        std::string::size_type ret = curPos;
+        GetNextToken( std::string::size_type& len, const char* delim = " \t" );
 
-        if( curPos != std::string::npos )
-        {
-            // we're not yet at the end of the string
-            // skip any whitespace starting at curPos
-            curPos = s.find_first_not_of( delim, curPos );
-
-            // curPos now points at start of next token, if there is one
-            ret = curPos;
-
-            // check if there actually is another token
-            if( curPos != std::string::npos )
-            {
-                // there is at least one more token
-                std::string::size_type curEnd = s.find_first_of( delim, curPos );
-                if( curEnd == std::string::npos )
-                {
-                    // this is definitely the last token
-                    curEnd = s.length();
-                }
-                len = curEnd - curPos;
-
-                // advance curPos past the current token
-                curPos += len;
-                if( curPos == s.length() )
-                {
-                    curPos = std::string::npos;
-                }
-            }
-        }
-
-        if( ret == std::string::npos )
-        {
-            len = 0;
-        }
-        return ret;
-    }
 };
 
 } // namespace XPlat
