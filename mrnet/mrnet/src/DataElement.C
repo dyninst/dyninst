7dbbd99076bd7537668345c95b2ff92e235c6965
mjbrim <mjbrim>
2008-10-09 19:52:46 +0000

    merge mrnet-2-0-development branch  - - - - - - - - - - - - - - - - - - - move implementation out of header

--- mrnet/mrnet/src/DataElement.C
+++ mrnet/mrnet/src/DataElement.C
@@ -12,6 +12,11 @@
 namespace MRN
 {
 
+DataElement::DataElement( ) : type(UNKNOWN_T), array_len(0), destroy_data(false)
+{ 
+    val.uld=0;
+}
+
 DataElement::~DataElement()
 {
     if( destroy_data == false ){
@@ -22,7 +27,6 @@ DataElement::~DataElement()
     case CHAR_ARRAY_T:
     case UCHAR_ARRAY_T:
     case STRING_T:
-    case STRING_ARRAY_T:
     case INT16_ARRAY_T:
     case UINT16_ARRAY_T:
     case INT32_ARRAY_T:
@@ -31,8 +35,18 @@ DataElement::~DataElement()
     case UINT64_ARRAY_T:
     case FLOAT_ARRAY_T:
     case DOUBLE_ARRAY_T:
-        if( val.p != NULL){
-            free( const_cast< void *>( val.p ) );
+        if( val.p != NULL ){
+            free( val.p );
+        }
+        break;
+    case STRING_ARRAY_T:
+        if( val.p != NULL ){
+            char** arr = ( char** ) val.p;
+            for(unsigned i=0; i < array_len; i++) {
+               if( arr[i] != NULL )
+                  free( arr[i] );
+            }
+            free( arr );
         }
         break;
     case CHAR_T:
@@ -48,6 +62,126 @@ DataElement::~DataElement()
     case UNKNOWN_T:
         break;
     }
+}
+
+void DataElement::set_DestroyData( bool d )
+{
+    destroy_data = d;
+}
+
+DataType DataElement::get_Type( void ) const
+{
+    return type;
+}
+
+char DataElement::get_char( void ) const
+{
+    return val.c;
+}
+void DataElement::set_char( char c )
+{
+    val.c = c; type = CHAR_T;
+}
+unsigned char DataElement::get_uchar( void ) const
+{
+    return val.uc;
+}
+void DataElement::set_uchar( unsigned char uc )
+{
+    val.uc = uc; type = UCHAR_T;
+}
+int16_t DataElement::get_int16_t( void ) const
+{
+    return val.hd;
+}
+void DataElement::set_int16_t( int16_t hd )
+{
+    val.hd = hd; type = INT16_T;
+}
+
+uint16_t DataElement::get_uint16_t( void ) const
+{
+    return val.uhd;
+}
+void DataElement::set_uint16_t( uint16_t uhd )
+{
+    val.uhd = uhd; type = UINT16_T;
+}
+
+int32_t DataElement::get_int32_t( void ) const
+{
+    return val.d;
+}
+void DataElement::set_int32_t( int32_t d )
+{
+    val.d = d; type = INT32_T;
+}
+
+uint32_t DataElement::get_uint32_t( void ) const
+{
+    return val.ud;
+}
+void DataElement::set_uint32_t( uint32_t ud )
+{
+    val.ud = ud; type = UINT32_T;
+}
+
+int64_t DataElement::get_int64_t( void ) const
+{
+    return val.ld;
+}
+void DataElement::set_int64_t( int64_t ld )
+{
+    val.ld = ld; type = INT64_T;
+}
+
+uint64_t DataElement::get_uint64_t( void ) const
+{
+    return val.uld;
+}
+void DataElement::set_uint64_t( uint64_t uld )
+{
+    val.uld = uld; type = UINT64_T;
+}
+
+float DataElement::get_float( void ) const
+{
+    return val.f;
+}
+void DataElement::set_float( float f )
+{
+    val.f = f; type = FLOAT_T;
+}
+
+double DataElement::get_double( void ) const
+{
+    return val.lf;
+}
+void DataElement::set_double( double lf )
+{
+    val.lf = lf; type = DOUBLE_T;
+}
+
+const char * DataElement::get_string( void ) const 
+{ 
+    return (const char *)val.p;
+}
+void DataElement::set_string( const char *p )
+{ 
+    val.p = const_cast<void*>((const void*)p); type = STRING_T;
+}
+
+const void * DataElement::get_array( DataType *t, uint32_t *len ) const
+{ 
+    *t=type; 
+    *len=array_len; 
+    return val.p;
+}
+void DataElement::set_array( const void *p, DataType t, uint32_t len )
+{ 
+    val.p = const_cast<void*>(p); 
+    type = t; 
+    array_len = len;
 }
 
 DataType Fmt2Type(const char * cur_fmt)
--- mrnet/mrnet/src/DataElement.h
+++ mrnet/mrnet/src/DataElement.h
@@ -6,7 +6,7 @@
 #if !defined(dataelement_h)
 #define dataelement_h 1
 
-#include "Types.h"
+#include "mrnet/Types.h"
 
 namespace MRN {
 
@@ -21,7 +21,7 @@ typedef union{
     uint64_t uld;
     float f;
     double lf;
-    const void * p; // May need to be allocated by pdr routine
+    void * p; // May need to be allocated by pdr routine
 } DataValue;
 
 typedef enum {
@@ -39,59 +39,55 @@ typedef enum {
 } DataType;
 
 class DataElement{
+    friend class Packet;
  public:
     DataValue val;
+    DataElement( void );
+    ~DataElement();
+
+    void set_DestroyData( bool d );
+    DataType get_Type( void ) const;
+
+    char get_char( void ) const;
+    void set_char( char c );
+
+    unsigned char get_uchar( void ) const;
+    void set_uchar( unsigned char uc );
+
+    int16_t get_int16_t( void ) const;
+    void set_int16_t( int16_t hd );
+
+    uint16_t get_uint16_t( void ) const;
+    void set_uint16_t( uint16_t uhd );
+
+    int32_t get_int32_t( void ) const;
+    void set_int32_t( int32_t d );
+
+    uint32_t get_uint32_t( void ) const;
+    void set_uint32_t( uint32_t ud );
+
+    int64_t get_int64_t( void ) const;
+    void set_int64_t( int64_t ld );
+
+    uint64_t get_uint64_t( void ) const;
+    void set_uint64_t( uint64_t uld );
+
+    float get_float( void ) const;
+    void set_float( float f );
+
+    double get_double( void ) const;
+    void set_double( double lf );
+
+    const char * get_string( void ) const;
+    void set_string( const char *p );
+
+    const void * get_array( DataType *t, uint32_t *len ) const;
+    void set_array( const void *p, DataType t, uint32_t len );
+
+ private:
     DataType type;
     uint32_t array_len;
     bool destroy_data;
-
-    DataElement( ) : type(UNKNOWN_T), array_len(0), destroy_data(false) { 
-        val.uld=0;
-    }
-    ~DataElement();
-
-    void set_DestroyData( bool d ) { destroy_data = d; }
-    DataType get_Type( void ) const { return type; }
-
-    char get_char( void ) const { return val.c; }
-    void set_char( char c ) { val.c = c; type = CHAR_T; }
-
-    unsigned char get_uchar( void ) const { return val.uc; }
-    void set_uchar( unsigned char uc ) { val.uc = uc; type = UCHAR_T; }
-
-    int16_t get_int16_t( void ) const { return val.hd; }
-    void set_int16_t( int16_t hd ) { val.hd = hd; type = INT16_T; }
-
-    uint16_t get_uint16_t( void ) const { return val.uhd; }
-    void set_uint16_t( uint16_t uhd ) { val.uhd = uhd; type = UINT16_T; };
-
-    int32_t get_int32_t( void ) const { return val.d; }
-    void set_int32_t( int32_t d ) { val.d = d; type = INT32_T; };
-
-    uint32_t get_uint32_t( void ) const { return val.ud; }
-    void set_uint32_t( uint32_t ud ) { val.ud = ud; type = UINT32_T; };
-
-    int64_t get_int64_t( void ) const { return val.ld; }
-    void set_int64_t( int64_t ld ) { val.ld = ld; type = INT64_T; };
-
-    uint64_t get_uint64_t( void ) const { return val.uld; }
-    void set_uint64_t( uint64_t uld ) { val.uld = uld; type = UINT64_T; };
-
-    float get_float( void ) const { return val.f; }
-    void set_float( float f ) { val.f = f; type = FLOAT_T; };
-
-    double get_double( void ) const { return val.lf; }
-    void set_double( double lf ) { val.lf = lf; type = DOUBLE_T; };
-
-    const char * get_string( void ) const { return (const char *)val.p; }
-    void set_string( const char *p ) { val.p = p; type = STRING_T; };
-
-    const void * get_array( DataType *t, uint32_t *len ) const
-        { *t=type; *len=array_len; return val.p; }
-    void set_array( const void *p, DataType t, uint32_t len )
-        { val.p = p; type = t; array_len = len; }
-
-		void print()const;
 };
 
 DataType Fmt2Type(const char * cur_fmt);
