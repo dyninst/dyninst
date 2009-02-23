add6a464cf60dc45263360b3c1b85e326c2ed31e
Andrew Bernat <bernat@brie.cs.wisc.edu>
2009-02-23 16:58:10 -0600

    Add the "Auxiliary carry" flag to the set of ID->name mappings for IA32/AMD64. Also reorder register IDs to make converting between 8/16/32/64-bit registers easier.

--- instructionAPI/h/RegisterIDs-x86.h
+++ instructionAPI/h/RegisterIDs-x86.h
@@ -48,31 +48,38 @@ namespace Dyninst
 		/// \enum Dyninst::InstructionAPI::IA32Regs
 		/// \brief Registers for IA32 and AMD64 processors.
 		///
-		enum IA32Regs { r_AH=0, r_BH, r_CH, r_DH, r_AL, r_BL, r_CL, r_DL, //107
-			r_AX, r_DX, //109
-			r_eAX, r_eBX, r_eCX, r_eDX, //113
-			r_EAX, r_EBX, r_ECX, r_EDX, //117
-			r_CS, r_DS, r_ES, r_FS, r_GS, r_SS, //123
-			r_eSP, r_eBP, r_eSI, r_eDI, //127
-			r_ESP, r_EBP, r_ESI, r_EDI, //131
-			r_EDXEAX, r_ECXEBX, //133
-			r_OF, r_SF, r_ZF, r_AF, r_PF, r_CF, r_TF, r_IF, r_DF, r_NT, r_RF, r_EFLAGS,
-			// flags need to be separate registers for proper liveness analysis
-			r_DummyFPR, r_Reserved,
-			// and we have a dummy register to make liveness consistent since floating point saves are all/none at present
-			r_R8, r_R9, r_R10, r_R11, r_R12, r_R13, r_R14, r_R15,
-			// AMD64 GPRs
-			r_EIP, r_RIP,
-			r_RSP, r_RBP,
-			r_RAX, r_RBX, r_RCX, r_RDX,r_RSI, r_RDI,
-			r_SI, r_DI,
-			r_XMM0, r_XMM1, r_XMM2, r_XMM3, r_XMM4, r_XMM5, r_XMM6, r_XMM7,
-			r_MM0, r_MM1, r_MM2, r_MM3, r_MM4, r_MM5, r_MM6, r_MM7,
-			r_CR0, r_CR1, r_CR2, r_CR3, r_CR4, r_CR5, r_CR6, r_CR7,
-			r_DR0, r_DR1, r_DR2, r_DR3, r_DR4, r_DR5, r_DR6, r_DR7,
-			r_rAX, r_rCX, r_rDX, r_rBX, r_rSP, r_rBP, r_rSI, r_rDI,
-			r_ALLGPRS
+            // We REALLY REALLY NEED these definitions to stay aligned as given,
+            // so (e.g.) (r_EAX - r_AH) == (r_EDX - r_DH). We use that later
+            // for upconverting overlapping/aliased registers.
+		enum IA32Regs { r_AH=0, r_BH, r_CH, r_DH, 
+                                r_AL=10, r_BL, r_CL, r_DL,
+                                r_AX=20, r_BX, r_CX, r_DX, r_SI, r_DI,
+                                r_eAX=30, r_eBX, r_eCX, r_eDX, r_eSI, r_eDI,
+                                r_EAX=40, r_EBX, r_ECX, r_EDX, r_ESI, r_EDI,
+                                r_eSP=50, r_eBP,
+                                r_ESP=60, r_EBP,
+                                r_EDXEAX=70, r_ECXEBX, //133
+                                r_OF=80, r_SF, r_ZF, r_AF, r_PF, r_CF, r_TF, r_IF, r_DF, r_NT, r_RF, // NOTE: 11 of these!
+                                r_EFLAGS=100,
+                                r_CS=110, r_DS, r_ES, r_FS, r_GS, r_SS,
+                                r_EIP=120,
+                                // flags need to be separate registers for proper liveness analysis
+                                r_DummyFPR=130, r_Reserved,
+                                // and we have a dummy register to make liveness consistent since floating point saves are all/none at present
+                                r_rAX=140, r_rBX, r_rCX, r_rDX, r_rSI, r_rDI,
+                                r_RAX=150, r_RBX, r_RCX, r_RDX, r_RSI, r_RDI,
+                                r_rSP=160, r_rBP,
+                                r_RSP=170, r_RBP,
+                                r_R8=180, r_R9, r_R10, r_R11, r_R12, r_R13, r_R14, r_R15,
+                                // AMD64 GPRs
+                                r_RIP=190,
+                                r_XMM0=200, r_XMM1, r_XMM2, r_XMM3, r_XMM4, r_XMM5, r_XMM6, r_XMM7,
+                                r_MM0, r_MM1, r_MM2, r_MM3, r_MM4, r_MM5, r_MM6, r_MM7,
+                                r_CR0, r_CR1, r_CR2, r_CR3, r_CR4, r_CR5, r_CR6, r_CR7,
+                                r_DR0, r_DR1, r_DR2, r_DR3, r_DR4, r_DR5, r_DR6, r_DR7,
+                                r_ALLGPRS
 		};
+
 		/// The %RegInfo struct associates a register ID with its size and name.
 		struct RegInfo
 		{
@@ -118,7 +125,7 @@ namespace Dyninst
 					(r_ESP, RegInfo(u32,"ESP"))(r_EBP, RegInfo(u32,"EBP"))(r_ESI, RegInfo(u32,"ESI"))(r_EDI, RegInfo(u32,"EDI"))
 					(r_CS, RegInfo(u32,"CS"))(r_DS, RegInfo(u32,"DS"))(r_ES, RegInfo(u32,"ES"))
 					(r_FS, RegInfo(u32,"FS"))(r_GS, RegInfo(u32,"GS"))(r_SS, RegInfo(u32,"SS"))
-					(r_EIP, RegInfo(u32,"EIP"))(r_CF, RegInfo(bit_flag,"Carry"))(r_ZF,  RegInfo(bit_flag,"Zero"))
+                                    (r_EIP, RegInfo(u32,"EIP"))(r_CF, RegInfo(bit_flag,"Carry"))(r_ZF,  RegInfo(bit_flag,"Zero"))(r_AF, RegInfo(bit_flag,"Carry Auxiliary"))
 					(r_PF,  RegInfo(bit_flag,"Parity"))(r_OF,  RegInfo(bit_flag,"Overflow"))(r_SF,  RegInfo(bit_flag,"Sign"))
 					(r_RSP, RegInfo(u64, "RSP"))(r_RBP, RegInfo(u64, "RBP"))(r_RAX, RegInfo(u64, "RAX"))(r_RBX, RegInfo(u64, "RBX"))
 					(r_RCX, RegInfo(u64, "RCX"))(r_RDX, RegInfo(u64, "RDX"))(r_RSI, RegInfo(u64, "RSI"))(r_RDI, RegInfo(u64, "RDI"))
