6915463f239f49a5fb03767810f94a2afe51746b
Michael Lam <lam@maxfli.cs.umd.edu>
2009-03-27 18:51:46 -0400

    Fixed build errors

--- dyninstAPI/src/emit-x86.h
+++ dyninstAPI/src/emit-x86.h
@@ -192,7 +192,7 @@ public:
     void emitLoadIndir(Register dest, Register addr_reg, codeGen &gen);
     bool emitLoadRelative(Register dest, Address offset, Register base, codeGen &gen);
     bool emitLoadRelative(registerSlot *dest, Address offset, registerSlot *base, codeGen &gen);
-    void emitLoadShared(Register dest, const image_variable *var, int size, codeGen &gen) { assert(0); return true; }
+    void emitLoadShared(Register dest, const image_variable *var, int size, codeGen &gen) { assert(0); }
     void emitLoadFrameAddr(Register dest, Address offset, codeGen &gen);
 
     void emitLoadOrigFrameRelative(Register dest, Address offset, codeGen &gen);
@@ -208,7 +208,7 @@ public:
 
     void emitStoreRelative(registerSlot *source, Address offset, registerSlot *base, codeGen &gen);
 
-    bool emitStoreShared(Register source, const image_variable *var, int size, codeGen &gen) { assert(0); return true; }
+    void emitStoreShared(Register source, const image_variable *var, int size, codeGen &gen) { assert(0); }
 
     bool clobberAllFuncCall(registerSpace *rs, int_function *callee);
     void setFPSaveOrNot(const int * liveFPReg,bool saveOrNot);
