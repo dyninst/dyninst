29a0621004bb1307a5962825ebb92879e9468ab7
Madhavi Krishnan <madhavi@stx.cs.umd.edu>
2009-06-16 00:25:04 -0400

    This commit implements new approach to creating hashsection in rewritten binary. We add only symbols that are present in the original binary's hash table and the new symbols created as part of instrumention to the rewritten binary's hash table.

--- symtabAPI/src/emitElf.C
+++ symtabAPI/src/emitElf.C
@@ -1513,7 +1513,7 @@ bool emitElf::createSymbolTables(Symtab *obj, vector<Symbol *>&allSymbols, std::
   // build new .hash section
   Elf32_Word *hashsecData;
   unsigned hashsecSize = 0;
-  createHashSection(hashsecData, hashsecSize, dynsymVector);
+  createHashSection(obj, hashsecData, hashsecSize, dynsymVector);
   if(hashsecSize) {
     string name; 
     if (secTagRegionMapping.find(DT_HASH) != secTagRegionMapping.end()) {
@@ -1818,8 +1818,45 @@ void emitElf::createSymbolVersions(Symtab *obj, Elf32_Half *&symVers, char*&vern
   return;
 }
 
-void emitElf::createHashSection(Elf32_Word *&hashsecData, unsigned &hashsecSize, vector<Symbol *>&dynSymbols)
+void emitElf::createHashSection(Symtab *obj, Elf32_Word *&hashsecData, unsigned &hashsecSize, vector<Symbol *>&dynSymbols)
 {
+
+  /* Save the original hash table entries */
+  std::vector<unsigned> originalHashEntries;
+  unsigned dynsymSize = obj->getObject()->getDynsymSize();
+  
+  Elf_Scn *scn = NULL;
+  Elf32_Shdr *shdr = NULL;
+  for (unsigned scncount = 0; (scn = elf_nextscn(oldElf, scn)); scncount++) {
+    shdr = elf32_getshdr(scn);
+    if(obj->getObject()->getElfHashAddr() != 0 &&
+       obj->getObject()->getElfHashAddr() == shdr->sh_addr){
+      Elf_Data *hashData = elf_getdata(scn,NULL);
+      Elf32_Word *oldHashSec = (Elf32_Word *) hashData->d_buf;
+      unsigned original_nbuckets, original_nchains;
+      original_nbuckets =  oldHashSec[0];
+      original_nchains = oldHashSec[1];
+      for (unsigned i = 0; i < original_nbuckets+original_nchains; i++) {
+	if ( oldHashSec[2+i] != 0) {
+		originalHashEntries.push_back( oldHashSec[2+i]);
+		//printf(" ELF HASH pushing hash entry %d \n", oldHashSec[2+i] );
+	}
+      }
+    }
+
+    if(obj->getObject()->getGnuHashAddr() != 0 &&
+       obj->getObject()->getGnuHashAddr() == shdr->sh_addr){
+      Elf_Data *hashData = elf_getdata(scn,NULL);
+      Elf32_Word *oldHashSec = (Elf32_Word *) hashData->d_buf;
+      unsigned symndx = oldHashSec[1];
+      if (dynsymSize != 0)
+      	for (unsigned i = symndx; i < dynsymSize ; i++) {
+		originalHashEntries.push_back(i);
+		//printf(" GNU HASH pushing hash entry %d \n", i);
+	}
+      }
+  }
+
   vector<Symbol *>::iterator iter;
   dyn_hash_map<unsigned, unsigned> lastHash; // bucket number to symbol index
   unsigned nbuckets = (unsigned)dynSymbols.size()*2/3;
@@ -1839,8 +1876,11 @@ void emitElf::createHashSection(Elf32_Word *&hashsecData, unsigned &hashsecSize,
   i = 0;
   for (iter = dynSymbols.begin(); iter != dynSymbols.end(); iter++, i++) {
     if((*iter)->getName().empty()) continue;
-    if((*iter)->getRegion() == NULL) continue;
-    
+    unsigned index = (*iter)->getIndex();
+    if ((find(originalHashEntries.begin(),originalHashEntries.end(),index) == originalHashEntries.end()) && 
+	(index < obj->getObject()->getDynsymSize())) {
+	continue;
+    }
     key = elfHash((*iter)->getName().c_str()) % nbuckets;
     if (lastHash.find(key) != lastHash.end()) {
       hashsecData[2+nbuckets+lastHash[key]] = i;
