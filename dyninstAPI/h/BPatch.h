5e59bdcade3031eb44f6b675ff991c86a99af16e
Matthew Legendre <legendre@wasabi.cs.wisc.edu>
2009-06-30 12:02:56 -0500

    Update Dyninst version numbers from 5.2 to 6.0

--- dyninstAPI/h/BPatch.h
+++ dyninstAPI/h/BPatch.h
@@ -62,9 +62,10 @@ class process;
 // DYNINST_MINOR, and DYNINST_SUBMINOR
 #define DYNINST_5_1
 #define DYNINST_5_2
+#define DYNINST_6_0
 
-#define DYNINST_MAJOR 5
-#define DYNINST_MINOR 2
+#define DYNINST_MAJOR 6
+#define DYNINST_MINOR 0
 #define DYNINST_SUBMINOR 0
 
 #ifdef IBM_BPATCH_COMPAT
