1b938338e9547031bc3ede5d00992b8de5c127ee
Madhavi Krishnan <madhavi@cs.wisc.edu>
2009-10-12 11:04:23 -0500

    Fixed typo in previous commit

--- dyninstAPI_RT/src/RTheap.c
+++ dyninstAPI_RT/src/RTheap.c
@@ -154,7 +154,7 @@ static Address constrained_mmap(size_t len, Address lo, Address hi,
 {
    const dyninstmm_t *mlo, *mhi, *p;
    Address beg, end, try;
-#if defined(os_linux && arch_power)
+#if defined (os_linux)  && defined(arch_power)
 // DYNINSTheap_loAddr should already be defined in DYNINSTos_malloc. 
 // Redefining here, just in case constrained_mmap is called from a different call path.
    DYNINSTheap_loAddr = getpagesize();
@@ -309,7 +309,7 @@ void *DYNINSTos_malloc(size_t nbytes, void *lo_addr, void *hi_addr)
     dyninstmm_t *maps;
 
     /* What if we need to allocate memory not in the area we can mmap? */
-#if defined(os_linux && arch_power)
+#if defined (os_linux)  && defined(arch_power)
    DYNINSTheap_loAddr = getpagesize();
 #endif
     if ((hi < DYNINSTheap_loAddr) || (lo > DYNINSTheap_hiAddr)) {
