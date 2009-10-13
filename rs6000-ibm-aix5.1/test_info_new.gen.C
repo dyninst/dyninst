d246cdb75c64de14284cdf3eaa3df5109fe90698
Madhavi Krishnan <madhavi@cs.wisc.edu>
2009-10-13 13:02:18 -0500

    Added a new test case to test deletion of multiple snippets at a single instrumentation point. Mutatee: test_snip_remove_mutatee.c Mutator: test_snip_remove.C
    Updated spec-grouped.pl and regenerated the Makefiles.

--- rs6000-ibm-aix5.1/test_info_new.gen.C
+++ rs6000-ibm-aix5.1/test_info_new.gen.C
@@ -4439,6 +4439,198 @@ void initialize_mutatees(std::vector<RunGroup *> &tests) {
   tests.push_back(rg);
 #endif // defined(XLC)
   test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_none", STOPPED, CREATE, false, "dyninst", "gcc", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: gcc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_none", STOPPED, USEATTACH, false, "dyninst", "gcc", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: gcc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_low", STOPPED, CREATE, false, "dyninst", "gcc", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: gcc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_low", STOPPED, USEATTACH, false, "dyninst", "gcc", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: gcc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_high", STOPPED, CREATE, false, "dyninst", "gcc", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: gcc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_high", STOPPED, USEATTACH, false, "dyninst", "gcc", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: gcc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_max", STOPPED, CREATE, false, "dyninst", "gcc", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: gcc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_gcc_32_max", STOPPED, USEATTACH, false, "dyninst", "gcc", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: gcc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_none", STOPPED, CREATE, false, "dyninst", "xlc", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: xlc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_none", STOPPED, USEATTACH, false, "dyninst", "xlc", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: xlc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_low", STOPPED, CREATE, false, "dyninst", "xlc", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: xlc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_low", STOPPED, USEATTACH, false, "dyninst", "xlc", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: xlc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_high", STOPPED, CREATE, false, "dyninst", "xlc", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: xlc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_high", STOPPED, USEATTACH, false, "dyninst", "xlc", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: xlc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_max", STOPPED, CREATE, false, "dyninst", "xlc", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: xlc, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlc_32_max", STOPPED, USEATTACH, false, "dyninst", "xlc", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: xlc, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_none", STOPPED, CREATE, false, "dyninst", "g++", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: g++, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_none", STOPPED, USEATTACH, false, "dyninst", "g++", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: g++, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_low", STOPPED, CREATE, false, "dyninst", "g++", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: g++, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_low", STOPPED, USEATTACH, false, "dyninst", "g++", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: g++, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_high", STOPPED, CREATE, false, "dyninst", "g++", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: g++, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_high", STOPPED, USEATTACH, false, "dyninst", "g++", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: g++, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_max", STOPPED, CREATE, false, "dyninst", "g++", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: g++, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_g++_32_max", STOPPED, USEATTACH, false, "dyninst", "g++", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: g++, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_none", STOPPED, CREATE, false, "dyninst", "xlC", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: xlC, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_none", STOPPED, USEATTACH, false, "dyninst", "xlC", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: none, compiler: xlC, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_low", STOPPED, CREATE, false, "dyninst", "xlC", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: xlC, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_low", STOPPED, USEATTACH, false, "dyninst", "xlC", "low", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: low, compiler: xlC, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_high", STOPPED, CREATE, false, "dyninst", "xlC", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: xlC, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_high", STOPPED, USEATTACH, false, "dyninst", "xlC", "high", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: high, compiler: xlC, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_max", STOPPED, CREATE, false, "dyninst", "xlC", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: xlC, run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+#ifdef XLC
+  test_count = 0;
+  rg = new RunGroup("test_snip_remove.mutatee_solo_xlC_32_max", STOPPED, USEATTACH, false, "dyninst", "xlC", "max", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "test_snip_remove", "test_snip_remove", "test_snip_remove.so", false, "{test: test_snip_remove, mutator: test_snip_remove, grouped: false, start_state: stopped, abi: 32, mutatee: test_snip_remove, optimization: max, compiler: xlC, run_mode: useAttach}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+#endif // defined(XLC)
+  test_count = 0;
   rg = new RunGroup("test1_29.mutatee_solo_gcc_32_none", STOPPED, CREATE, false, "dyninst", "gcc", "none", "32");
   rg->tests.push_back(new TestInfo(test_count++, "test1_29", "test1_29", "test1_29.so", false, "{test: test1_29, mutator: test1_29, grouped: false, start_state: stopped, abi: 32, mutatee: test1_29, optimization: none, compiler: gcc, run_mode: createProcess}"));
   rg->index = group_count++;
