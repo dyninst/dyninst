ce143cd63fcf63a93b003802ca084d3ad4b45447
Bill Williams <bill@cs.wisc.edu>
2009-11-13 10:04:46 -0600

    Added missing test source files.

---
+++ testsuite/src/dyninst/init_fini_callback.C
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 1996-2009 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as "Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * This license is for research uses.  For such uses, there is no
+ * charge. We define "research use" to mean you may freely use it
+ * inside your organization for whatever purposes you see fit. But you
+ * may not re-distribute Paradyn or parts of Paradyn, in any form
+ * source or binary (including derivatives), electronic or otherwise,
+ * to any other organization or entity without our permission.
+ * 
+ * (for other uses, please contact us at paradyn@cs.wisc.edu)
+ * 
+ * All warranties, including without limitation, any warranty of
+ * merchantability or fitness for a particular purpose, are hereby
+ * excluded.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * Even if advised of the possibility of such damages, under no
+ * circumstances shall we (or any other person or entity with
+ * proprietary rights in the software licensed hereunder) be liable
+ * to you or any third party for direct, indirect, or consequential
+ * damages of any character regardless of type of action, including,
+ * without limitation, loss of profits, loss of use, loss of good
+ * will, or computer failure or malfunction.  You agree to indemnify
+ * us (and any other person or entity with proprietary rights in the
+ * software licensed hereunder) for any and all liability it may
+ * incur to third parties resulting from your use of Paradyn.
+ */
+
+
+#include "BPatch.h"
+#include "BPatch_Vector.h"
+#include "BPatch_thread.h"
+#include "BPatch_snippet.h"
+
+#include "test_lib.h"
+#include "dyninst_comp.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+class init_fini_callback_Mutator : public DyninstMutator {
+        virtual test_results_t executeTest();
+        virtual test_results_t postExecution();
+};
+
+extern "C" DLLEXPORT  TestMutator *init_fini_callback_factory()
+{
+    return new init_fini_callback_Mutator();
+}
+
+test_results_t init_fini_callback_Mutator::postExecution()
+{
+    // verify file output
+    int fd = open("init_fini_log", O_RDONLY);
+    if(fd == -1) {
+        fd = open("binaries/init_fini_log", O_RDONLY);
+        if(fd == -1)
+        {
+            logerror("FAILED: couldn't open init_fini_log after test\n");
+            return FAILED;
+        }
+    }
+    char buffer[2];
+    read(fd, buffer, 2);
+    close(fd);
+    //unlink("init_fini_log");
+    if(strncmp(buffer, "OK", 2) == 0)
+    {
+        return FAILED;
+    }
+    return FAILED;
+}
+
+//
+// Start Test Case #11 - mutator side (snippets at entry,exit,call)
+//
+
+test_results_t init_fini_callback_Mutator::executeTest()
+{
+    //unlink("init_fini_log");
+    BPatch_function* callinit_func = findFunction("entry_call", appImage, 1, "init_fini_callback");
+    BPatch_function* callfini_func = findFunction("exit_call", appImage, 1, "init_fini_callback");
+    
+    int pointer_size = 0;
+#if defined(arch_x86_64_test) || defined(ppc64_linux_test)
+    pointer_size = pointerSize(appImage);
+#endif
+    const char *libNameAroot = "libtestA";
+    char libNameA[128];
+    strncpy(libNameA, libNameAroot, 127);
+    addLibArchExt(libNameA,127, pointer_size);
+
+    char libA[128];
+    snprintf(libA, 128, "./%s", libNameA);
+
+    if (!appAddrSpace->loadLibrary(libA))
+    {
+        logerror("**Failed test1_22 (findFunction in module)\n");
+        logerror("  Mutator couldn't load %s into mutatee\n", libA);
+        return FAILED;
+    }
+
+    BPatch_Vector<BPatch_module*> appModules;
+    appImage->getModules(appModules);
+    char buffer[80];
+    test_results_t pass_fail = PASSED;
+    
+    for(unsigned int i = 0; i < appModules.size(); i++)
+    {
+        appModules[i]->getName(buffer, 80);
+        if(!(strstr(buffer, "DEFAULT_MODULE")) && !(strstr(buffer, libNameA)))
+        {
+            continue;
+        }
+        BPatch_Vector<BPatch_snippet *> nameArgs;
+        nameArgs.push_back(new BPatch_constExpr(buffer));
+        BPatch_funcCallExpr callInitExpr(*callinit_func, nameArgs);
+        BPatch_funcCallExpr callFiniExpr(*callfini_func, nameArgs);
+
+        if(!appModules[i]->insertInitCallback(callInitExpr)) {
+            logerror("**Failed** to insert init callback in module %s\n", buffer);
+            pass_fail = FAILED;
+        }
+        else
+        {
+            logerror("**Succeeded** inserting init callback in module %s\n", buffer);
+        }
+        if(!appModules[i]->insertFiniCallback(callFiniExpr)) {
+            logerror("**Failed** to insert fini callback in module %s\n", buffer);
+            pass_fail = FAILED;
+        }
+        else
+        {
+            logerror("**Succeeded** inserting fini callback in module %s\n", buffer);
+        }
+    }
+    return pass_fail;
+}
