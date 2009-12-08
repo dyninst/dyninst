41c13014727d5dcc612ad82548e322a9ae390266
Bill Williams <bill@cs.wisc.edu>
2009-12-08 11:22:38 -0600

    Build fix: don't use non-standard hash extensions for GCC 4.3 and up.

--- depGraphAPI/src/analyzeDDG.h
+++ depGraphAPI/src/analyzeDDG.h
@@ -57,7 +57,9 @@
 class BPatch_basicBlock;
 class BPatch_function;
 
-
+#ifdef __GNUC__
+#if (__GNUC__ < 4) || \
+	((__GNUC__ == 4) && (__GNUC_MINOR__ < 3))
 using namespace __gnu_cxx;
 namespace __gnu_cxx {
     template<typename T> struct hash<dyn_detail::boost::shared_ptr<T> > {
@@ -73,6 +75,8 @@ namespace __gnu_cxx {
         }
     };
 };
+#endif // GNUC < 4.3.0
+#endif // GNUC
 
 namespace Dyninst {
 namespace DepGraphAPI {
