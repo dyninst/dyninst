2eb3032971548207f02512a69fe6d2d6eb51979c
Dan McNulty <mcnulty@cs.wisc.edu>
2010-05-17 09:58:16 -0500

    More work to port ProcControlAPI to FreeBSD.
    Getting binary rewriter working on FreeBSD 7.2. This includes
    creating platform specific directories for FreeBSD 7.2. Also includes
    some bug fixes for bugs exposed by switching to FreeBSD 7.2.

--- src/specification/spec-grouped.pl
+++ src/specification/spec-grouped.pl
@@ -561,6 +561,7 @@ test_platform('test1_35', 'x86_64-unknown-linux2.4').
 test_platform('test1_35', 'sparc-sun-solaris2.8').
 test_platform('test1_35', 'sparc-sun-solaris2.9').
 test_platform('test1_35', 'i386-unknown-freebsd8.0').
+test_platform('test1_35', 'i386-unknown-freebsd7.2').
 groupable_test('test1_35').
 mutator('test1_35', ['test1_35.C']).
 mutatee('test1_35', ['test1_35_mutatee.c'], Sources) :-
@@ -2540,6 +2541,7 @@ platform('x86_64', 'linux', 'linux2.4', 'x86_64-unknown-linux2.4').
 platform('power', 'linux', 'linux2.6', 'ppc64_linux').
 platform('power', 'linux', 'linux2.6', 'ppc32_linux').
 platform('i386', 'freebsd', 'freebsd8.0', 'i386-unknown-freebsd8.0').
+platform('i386', 'freebsd', 'freebsd7.2', 'i386-unknown-freebsd7.2').
 
 % Platform Defns
 % platform/1
