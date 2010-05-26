c58175e0d372a97dff2c0cec6690f2f04de1f9f5
Dan McNulty <mcnulty@cs.wisc.edu>
2010-05-26 14:30:11 -0500

    Mutatee traps implementation copied from Linux.
    However, mutatee traps currently do not work when enabled so they are
    disabled for now.

--- dyninstAPI_RT/src/RTfreebsd.c
+++ dyninstAPI_RT/src/RTfreebsd.c
@@ -63,7 +63,7 @@ extern double DYNINSTstaticHeap_16M_anyHeap_1[];
 extern unsigned long sizeOfLowMemHeap1;
 extern unsigned long sizeOfAnyHeap1;
 
-//static struct trap_mapping_header *getStaticTrapMap(unsigned long addr);
+static struct trap_mapping_header *getStaticTrapMap(unsigned long addr);
 
 /** RT lib initialization **/
 
@@ -269,6 +269,312 @@ int DYNINSTthreadInfo(BPatch_newThreadEventRecord *ev) {
     return 1;
 }
 
+/** trap based instrumentation **/
+
+#if defined(cap_mutatee_traps)
+
+#include <ucontext.h>
+
+/* XXX This will compile, but it does not work yet */
+
+/* XXX This current only works for amd64 FreeBSD -- needs some ifdefs for i386 */
+
+extern void dyninstSetupContext(ucontext_t *context, unsigned long flags, void *retPoint);
+extern unsigned long dyninstTrapTableUsed;
+extern unsigned long dyninstTrapTableVersion;
+extern trapMapping_t *dyninstTrapTable;
+extern unsigned long dyninstTrapTableIsSorted;
+
+/**
+ * Called by the SIGTRAP handler, dyninstTrapHandler.  This function is 
+ * closly intwined with dyninstTrapHandler, don't modify one without 
+ * understanding the other.
+ *
+ * This function sets up the calling context that was passed to the
+ * SIGTRAP handler so that control will be redirected to our instrumentation
+ * when we do the setcontext call.
+ * 
+ * There are a couple things that make this more difficult than it should be:
+ *   1. The OS provided calling context is similar to the GLIBC calling context,
+ *      but not compatible.  We'll create a new GLIBC compatible context and
+ *      copy the possibly stomped registers from the OS context into it.  The
+ *      incompatiblities seem to deal with FP and other special purpose registers.
+ *   2. setcontext doesn't restore the flags register.  Thus dyninstTrapHandler
+ *      will save the flags register first thing and pass us its value in the
+ *      flags parameter.  We'll then push the instrumentation entry and flags
+ *      onto the context's stack.  Instead of transfering control straight to the
+ *      instrumentation, we'll actually go back to dyninstTrapHandler, which will
+ *      do a popf/ret to restore flags and go to instrumentation.  The 'retPoint'
+ *      parameter is the address in dyninstTrapHandler the popf/ret can be found.
+ **/
+void dyninstSetupContext(ucontext_t *context, unsigned long flags, void *retPoint)
+{
+   ucontext_t newcontext;
+   unsigned long *orig_sp;
+   void *orig_ip;
+   void *trap_to;
+
+   getcontext(&newcontext);
+   
+   //Set up the 'context' parameter so that when we restore 'context' control
+   // will get transfered to our instrumentation.
+   newcontext.uc_mcontext = context->uc_mcontext;
+
+   orig_sp = (unsigned long *) context->uc_mcontext.mc_rsp;
+   orig_ip = (void *) context->uc_mcontext.mc_rip;
+
+   assert(orig_ip);
+
+   //Set up the PC to go to the 'ret_point' in RTsignal-x86.s
+   newcontext.uc_mcontext.mc_rip = (unsigned long) retPoint;
+
+   //simulate a "push" of the flags and instrumentation entry points onto
+   // the stack.
+   if (DYNINSTstaticMode) {
+      unsigned long zero = 0;
+      unsigned long one = 1;
+      struct trap_mapping_header *hdr = getStaticTrapMap((unsigned long) orig_ip);
+      assert(hdr);
+      trapMapping_t *mapping = &(hdr->traps[0]);
+      trap_to = dyninstTrapTranslate(orig_ip, 
+                                     (unsigned long *) &hdr->num_entries, 
+                                     &zero, 
+                                     (volatile trapMapping_t **) &mapping,
+                                     &one);
+   }
+   else {
+      trap_to = dyninstTrapTranslate(orig_ip, 
+                                     &dyninstTrapTableUsed,
+                                     &dyninstTrapTableVersion,
+                                     (volatile trapMapping_t **) &dyninstTrapTable,
+                                     &dyninstTrapTableIsSorted);
+                                     
+   }
+   *(orig_sp - 1) = (unsigned long) trap_to;
+   *(orig_sp - 2) = flags;
+   unsigned shift = 2;
+#if defined(arch_x86_64) && !defined(MUTATEE_32)
+   *(orig_sp - 3) = context->uc_mcontext.mc_r11;
+   *(orig_sp - 4) = context->uc_mcontext.mc_r10;
+   *(orig_sp - 5) = context->uc_mcontext.mc_rax;
+   shift = 5;
+#else
+#error mutatee traps unavailable on this architecture
+#endif
+   newcontext.uc_mcontext.mc_rsp = (unsigned long) (orig_sp - shift);
+
+   //Restore the context.  This will move all the register values of 
+   // context into the actual registers and transfer control away from
+   // this function.  This function shouldn't actually return.
+   setcontext(&newcontext);
+   assert(0);
+}
+
+#if defined(cap_binary_rewriter)
+
+extern struct r_debug _r_debug;
+struct r_debug _r_debug __attribute__ ((weak));
+
+#define NUM_LIBRARIES 512 //Important, max number of rewritten libraries
+
+#define WORD_SIZE (8 * sizeof(unsigned))
+#define NUM_LIBRARIES_BITMASK_SIZE (1 + NUM_LIBRARIES / WORD_SIZE)
+struct trap_mapping_header *all_headers[NUM_LIBRARIES];
+
+static unsigned all_headers_current[NUM_LIBRARIES_BITMASK_SIZE];
+static unsigned all_headers_last[NUM_LIBRARIES_BITMASK_SIZE];
+
+#if !defined(arch_x86_64) || defined(MUTATEE_32)
+typedef Elf32_Dyn ElfX_Dyn;
+#else
+typedef Elf64_Dyn ElfX_Dyn;
+#endif
+
+static int parse_libs();
+static int parse_link_map(struct link_map *l);
+static void clear_unloaded_libs();
+
+static void set_bit(unsigned *bit_mask, int bit, char value);
+static void clear_bitmask(unsigned *bit_mask);
+static unsigned get_next_free_bitmask(unsigned *bit_mask, int last_pos);
+static unsigned get_next_set_bitmask(unsigned *bit_mask, int last_pos);
+
+static tc_lock_t trap_mapping_lock;
+
+static struct trap_mapping_header *getStaticTrapMap(unsigned long addr)
+{
+   struct trap_mapping_header *header;
+   int i;
+   
+   tc_lock_lock(&trap_mapping_lock);
+   parse_libs();
+
+   i = -1;
+   for (;;) {
+      i = get_next_set_bitmask(all_headers_current, i);
+      assert(i >= 0 && i <= NUM_LIBRARIES);
+      if (i == NUM_LIBRARIES) {
+         header = NULL;
+         goto done;
+      }
+      header = all_headers[i];
+      if (addr >= header->low_entry && addr <= header->high_entry) {
+         goto done;
+      }
+   }  
+ done:
+   tc_lock_unlock(&trap_mapping_lock);
+   return header;
+}
+
+static int parse_libs()
+{
+   struct link_map *l_current;
+
+   l_current = _r_debug.r_map;
+   if (!l_current)
+      return -1;
+
+   clear_bitmask(all_headers_current);
+   while (l_current) {
+      parse_link_map(l_current);
+      l_current = l_current->l_next;
+   }
+   clear_unloaded_libs();
+
+   return 0;
+}
+
+//parse_link_map return values
+#define PARSED 0
+#define NOT_REWRITTEN 1
+#define ALREADY_PARSED 2
+#define ERROR_INTERNAL -1
+#define ERROR_FULL -2
+static int parse_link_map(struct link_map *l) 
+{
+   ElfX_Dyn *dynamic_ptr;
+   struct trap_mapping_header *header;
+   unsigned int i, new_pos;
+
+   dynamic_ptr = (ElfX_Dyn *) l->l_ld;
+   if (!dynamic_ptr)
+      return -1;
+
+   assert(sizeof(dynamic_ptr->d_un.d_ptr) == sizeof(void *));
+   for (; dynamic_ptr->d_tag != DT_NULL && dynamic_ptr->d_tag != DT_DYNINST; dynamic_ptr++);
+   if (dynamic_ptr->d_tag == DT_NULL) {
+      return NOT_REWRITTEN;
+   }
+
+   header = (struct trap_mapping_header *) (dynamic_ptr->d_un.d_val + l->l_addr);
+   
+   if (header->signature != TRAP_HEADER_SIG)
+      return ERROR_INTERNAL;
+   if (header->pos != -1) {
+      set_bit(all_headers_current, header->pos, 1);
+      assert(all_headers[header->pos] == header);
+      return ALREADY_PARSED;
+   }
+ 
+   for (i = 0; i < header->num_entries; i++)
+   {
+      header->traps[i].source = (void *) (((unsigned long) header->traps[i].source) + l->l_addr);
+      header->traps[i].target = (void *) (((unsigned long) header->traps[i].target) + l->l_addr);
+      if (!header->low_entry || header->low_entry > (unsigned long) header->traps[i].source)
+         header->low_entry = (unsigned long) header->traps[i].source;
+      if (!header->high_entry || header->high_entry < (unsigned long) header->traps[i].source)
+         header->high_entry = (unsigned long) header->traps[i].source;
+   }
+
+   new_pos = get_next_free_bitmask(all_headers_last, -1);
+   assert(new_pos >= 0 && new_pos < NUM_LIBRARIES);
+   if (new_pos == NUM_LIBRARIES)
+      return ERROR_FULL;
+
+   header->pos = new_pos;
+   all_headers[new_pos] = header;
+   set_bit(all_headers_current, new_pos, 1);
+   set_bit(all_headers_last, new_pos, 1);
+
+   return PARSED;
+}
+
+static void clear_unloaded_libs()
+{
+   unsigned i;
+   for (i = 0; i<NUM_LIBRARIES_BITMASK_SIZE; i++)
+   {
+      all_headers_last[i] = all_headers_current[i];
+   }
+}
+
+static void set_bit(unsigned *bit_mask, int bit, char value) {
+   assert(bit < NUM_LIBRARIES);
+   unsigned *word = bit_mask + bit / WORD_SIZE;
+   unsigned shift = bit % WORD_SIZE;
+   if (value) {
+      *word |= (1 << shift);
+   }
+   else {
+      *word &= ~(1 << shift);
+   }
+}
+
+static void clear_bitmask(unsigned *bit_mask) {
+   unsigned i;
+   for (i = 0; i < NUM_LIBRARIES_BITMASK_SIZE; i++) {
+      bit_mask[i] = 0;
+   }
+}
+
+static unsigned get_next_free_bitmask(unsigned *bit_mask, int last_pos) {
+   unsigned i, j;
+   j = last_pos+1;
+   i = j / WORD_SIZE;
+   for (; j < NUM_LIBRARIES; i++) {
+      if (bit_mask[i] == (unsigned) -1) {
+         j += WORD_SIZE;
+         continue;
+      }
+      for (;;) {
+         if (!((1 << (j % WORD_SIZE) & bit_mask[i]))) {
+            return j;
+         }
+         j++;
+         if (j % WORD_SIZE == 0) {
+            break;
+         }
+      }
+   }
+   return NUM_LIBRARIES;
+}
+
+static unsigned get_next_set_bitmask(unsigned *bit_mask, int last_pos) {
+   unsigned i, j;
+   j = last_pos+1;
+   i = j / WORD_SIZE;
+   for (; j < NUM_LIBRARIES; i++) {
+      if (bit_mask[i] == (unsigned) 0) {
+         j += WORD_SIZE;
+         continue;
+      }
+      for (;;) {
+         if ((1 << (j % WORD_SIZE) & bit_mask[i])) {
+            return j;
+         }
+         j++;
+         if (j % WORD_SIZE == 0) {
+            break;
+         }
+      }
+   }
+   return NUM_LIBRARIES;
+}
+
+#endif
+
+#endif /* cap_mutatee_traps */
 
 /*
  * A program to determine the offsets of certain thread structures on FreeBSD
