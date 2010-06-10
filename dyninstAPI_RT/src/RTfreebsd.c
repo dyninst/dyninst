d1c6da575ead401813b2dca6261b27b73b8351d4
Dan McNulty <mcnulty@cs.wisc.edu>
2010-06-10 09:46:38 -0500

    Fixes for RT library on FreeBSD x86

--- dyninstAPI_RT/src/RTfreebsd.c
+++ dyninstAPI_RT/src/RTfreebsd.c
@@ -247,6 +247,8 @@ typedef struct pthread_offset_t {
 
 #if defined(amd64_unknown_freebsd7_0)
 static pthread_offset_t offsets = { 0, 112, 152, 160 };
+#elif defined(i386_unknown_freebsd7_0)
+static pthread_offset_t offsets = { 0, 80, 108, 112};
 #else
 #error pthread_t offsets undefined for this architecture
 #endif
