346132a0d0ae08bde9f16b3a95cda697d1cb30af
Ray Chen <rchen@cs.umd.edu>
2010-11-12 08:12:54 -0500

    Force stack alignment on x86/x86_64 platforms.
    Both x86 and x86_64 platforms require an aligned stack pointer at certain
    points of execution.  This commit introduces a preamble in our state saving
    code to forcably align the stack pointer.
    
    Care is also taken to ensure the stack is aligned before we generate a
    function call on x86_64.  Similar functionality may be needed for x86
    in the future.

--- common/h/arch-x86.h
+++ common/h/arch-x86.h
@@ -766,6 +766,9 @@ COMMON_EXPORT Address get_target(const unsigned char *instr, unsigned type, unsi
 #define EXTENDED_0x81_XOR 6
 #define EXTENDED_0x81_CMP 7
 
+#define EXTENDED_0x83_ADD 0
+#define EXTENDED_0x83_AND 4
+#define EXTENDED_0x83_SUB 5
 
 
 class instruction {
