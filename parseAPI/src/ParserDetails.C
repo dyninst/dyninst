daf27bf2f034a190a2725b8cdcdc1d77ec2d4612
Andrew Bernat <bernat@brie.cs.wisc.edu>
2010-12-21 14:44:19 -0600

    Fix corruption bug copying IA_IAPI instruction adapters
    Conflicts:
    
    	parseAPI/src/ParserDetails.C

--- parseAPI/src/ParserDetails.C
+++ parseAPI/src/ParserDetails.C
@@ -98,6 +98,30 @@ verbose_log(Address currAddr, Edges_t::iterator & curEdge)
 }
 } // anonymous namespace
 
+static void 
+getBlockInsns(Block &blk, std::set<Address> &addrs)
+{
+    unsigned bufSize = blk.size();
+	if (blk.start() == 0x1256) {
+		int i = 3;
+	}
+#if defined(cap_instruction_api)
+    using namespace InstructionAPI;
+    const unsigned char* bufferBegin = (const unsigned char *)
+        (blk.obj()->cs()->getPtrToInstruction(blk.start()));
+    InstructionDecoder dec = InstructionDecoder
+        (bufferBegin, bufSize, blk.region()->getArch());
+    InstructionAdapter_t ah(dec, blk.start(), blk.obj(), blk.region(), blk.obj()->cs(), &blk);
+#else        
+    InstrucIter iter(blk.start(), bufSize, blk.obj()->cs());
+    InstructionAdapter_t ah(iter, 
+        blk.obj(), blk.region(), obj()->cs());
+#endif
+
+	for (; ah.getAddr() < blk.end(); ah.advance()) {
+        addrs.insert(ah.getAddr());
+    } 
+}
 /*
  * Extra handling of return instructions
  */
