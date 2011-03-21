fdb4ad2ad538159b675da6fccf5de5cbeb6a6a08
Bill Williams <bill@cs.wisc.edu>
2011-03-21 16:32:07 -0500

    Better debug output in test 5-8 (expected vs. actual).

--- testsuite/src/dyninst/test5_8_mutatee.C
+++ testsuite/src/dyninst/test5_8_mutatee.C
@@ -65,7 +65,7 @@ void decl_test::func_cpp()
    if ( 1024 != ::CPP_DEFLT_ARG )
      logerror("::CPP_DEFLT_ARG init value wrong\n");
    if ( 0 != cpp_test_util::CPP_TEST_UTIL_VAR )
-     logerror("cpp_test_util::CPP_TEST_UTIL_VAR int value wrong\n");
+       logerror("cpp_test_util::CPP_TEST_UTIL_VAR int value wrong, expected %d, got %d\n", 0, cpp_test_util::CPP_TEST_UTIL_VAR);
 }
 
 // A call to this is inserted by the mutator when its analysis succeeds (test
