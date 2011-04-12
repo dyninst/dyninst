75d2b5ec108eeaa86697a3d7d519760c87b05219
Andrew Bernat <bernat@cumin.cs.wisc.edu>
2011-04-12 13:22:49 -0500

    Disable test1_35 as it no longer has meaning; replace getSize in test1_30 with a safe call to getAddressRange

--- testsuite/src/dyninst/test1_30.C
+++ testsuite/src/dyninst/test1_30.C
@@ -133,9 +133,7 @@ test_results_t test1_30_Mutator::executeTest() {
 	expr30_7->readValue(&n);
 	call30_1_line_no = (unsigned)(n+1);
 
-	//get the base addr and last addr of the function call30_1
-	baseAddr = (unsigned long)(call30_1func->getBaseAddr());
-	lastAddr = baseAddr + call30_1func->getSize();
+        call30_1func->getAddressRange(baseAddr, lastAddr);
 
 	//now write the base address and last address of the function
 	BPatch_variableExpr *expr30_8 = 
