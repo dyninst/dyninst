3ddbd268c09d1d97334d42e2099394479a65c93c
Emily Jacobson <jacobson@gouda.cs.wisc.edu>
2011-04-21 10:41:49 -0500

    Segment registers were 32-bit and should have been 16-bit.
    An associated test has been added, also.

--- i386-unknown-freebsd7.2/test_info_new.gen.C
+++ i386-unknown-freebsd7.2/test_info_new.gen.C
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 1996-2011 Barton P. Miller
- * 
- * We provide the Paradyn Parallel Performance Tools (below
- * described as "Paradyn") on an AS IS basis, and do not warrant its
- * validity or performance.  We reserve the right to update, modify,
- * or discontinue this software at any time.  We shall have no
- * obligation to supply such updates or modifications or any other
- * form of support to you.
- * 
- * By your use of Paradyn, you understand and agree that we (or any
- * other person or entity with proprietary rights in Paradyn) are
- * under no obligation to provide either maintenance services,
- * update services, notices of latent defects, or correction of
- * defects for Paradyn.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
 /* This file automatically generated from test specifications.  See
  * specification/spec.pl and specification/makemake.py
  */
@@ -2531,6 +2501,11 @@ void initialize_mutatees(std::vector<RunGroup *> &tests) {
   test_count = 0;
   rg = new RunGroup("", STOPPED, CREATE, TNone, PNone, DynamicLink, true, nonPIC, "instruction", "", "none", "32");
   rg->tests.push_back(new TestInfo(test_count++, "fucompp", "fucompp", "fucompp.so", false, "{test: fucompp, mutator: fucompp, grouped: false, start_state: stopped, format: dynamicMutatee, process_mode: None, abi: 32, thread_mode: None, pic: none, mutatee: none, optimization: none, compiler: , run_mode: createProcess}"));
+  rg->index = group_count++;
+  tests.push_back(rg);
+  test_count = 0;
+  rg = new RunGroup("", STOPPED, CREATE, TNone, PNone, DynamicLink, true, nonPIC, "instruction", "", "none", "32");
+  rg->tests.push_back(new TestInfo(test_count++, "mov_size_details", "mov_size_details", "mov_size_details.so", false, "{test: mov_size_details, mutator: mov_size_details, grouped: false, start_state: stopped, format: dynamicMutatee, process_mode: None, abi: 32, thread_mode: None, pic: none, mutatee: none, optimization: none, compiler: , run_mode: createProcess}"));
   rg->index = group_count++;
   tests.push_back(rg);
   test_count = 0;
