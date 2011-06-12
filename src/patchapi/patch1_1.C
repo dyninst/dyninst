cfce903ca570979cbf48a345dbbb95f58e749276
Wenbin Fang <wenbin@cs.wisc.edu>
2011-06-12 18:04:05 -0500

    Finish patch4_1 test case for PatchAPi, to test transactional semantics.

---
+++ src/patchapi/patch1_1.C
@@ -0,0 +1,15 @@
+#include "test_lib.h"
+#include "patchapi_comp.h"
+
+class patch1_1_Mutator : public PatchApiMutator {
+  virtual test_results_t executeTest();
+};
+
+extern "C" DLLEXPORT TestMutator* patch1_1_factory() {
+  return new patch1_1_Mutator();
+}
+
+test_results_t patch1_1_Mutator::executeTest() {
+  //std::cerr << "before pssed\n";
+  return PASSED;
+}
