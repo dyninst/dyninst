a636c69415d8440cfe53df4227e8392588f9542d
Wenbin Fang <wenbin@cs.wisc.edu>
2011-06-12 18:04:05 -0500

    Finish patch4_1 test case for PatchAPi, to test transactional semantics.

--- src/patchapi/patchapi_comp.C
+++ src/patchapi/patchapi_comp.C
@@ -21,40 +21,40 @@ class PatchApiComponent : public ComponentTester {
 };
 
 PatchApiComponent::PatchApiComponent() {
-  test_cerr << "PatchApiComponent\n";
+
 }
 
 test_results_t PatchApiComponent::program_setup(ParameterDict &params) {
-  test_cerr << "program_setup\n";
+
   return PASSED;
 }
 
 test_results_t PatchApiComponent::program_teardown(ParameterDict &params) {
-  test_cerr << "program_teardown\n";
+
   return PASSED;
 }
 
 test_results_t PatchApiComponent::group_setup(RunGroup *group,
                                              ParameterDict &params) {
-  test_cerr << "group_setup\n";
+
   return PASSED;
 }
 
 test_results_t PatchApiComponent::group_teardown(RunGroup *group,
                                                 ParameterDict &params) {
-  test_cerr << "group_teardown\n";
+
   return PASSED;
 }
 
 test_results_t PatchApiComponent::test_setup(TestInfo *test,
                                          ParameterDict &parms) {
-  test_cerr << "test_setup\n";
+
   return PASSED;
 }
 
 test_results_t PatchApiComponent::test_teardown(TestInfo *test,
                                          ParameterDict &parms) {
-  test_cerr << "test_teardown\n";
+
   return PASSED;
 }
 
@@ -74,14 +74,14 @@ PatchApiComponent::~PatchApiComponent() {
 }
 
 PatchApiMutator::PatchApiMutator() {
-  test_cerr<< "PatchApiMutator\n";
+
 }
 
 PatchApiMutator::~PatchApiMutator() {
 }
 
 test_results_t PatchApiMutator::setup(ParameterDict &param) {
-  test_cerr << "PatchApiMutator::setup\n";
+
   return PASSED;
 }
 
