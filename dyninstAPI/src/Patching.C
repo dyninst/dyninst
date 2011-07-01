ff6a8dba98ed8e3dce90314ea2a36e0f6165e1a2
Kevin Roundy <roundy@cs.wisc.edu>
2011-07-01 14:45:43 -0500

    Adds missing files

---
+++ dyninstAPI/src/Patching.C
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 1996-2011 Barton P. Miller
+ * 
+ * We provide the Paradyn Parallel Performance Tools (below
+ * described as "Paradyn") on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ * 
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "Patching.h"
+#include "block.h"
+#include "mapped_object.h"
+
+using namespace Dyninst;
+using namespace PatchAPI;
+
+void DynPatchCallback::split_block_cb(PatchBlock *first, PatchBlock *second)
+{
+    mapped_object *obj = SCAST_MO(first->object());
+    obj->splitBlock(SCAST_BI(first),SCAST_BI(second));
+    //KEVINTODO: clean up BPatch-level items from here instead of top-down
+}
+
