39f2ab1c12845b55b6c748046099517d82004b05
Andrew Bernat <bernat@cumin.cs.wisc.edu>
2011-07-13 13:30:16 -0500

    Added functionality: inserting a binary buffer into a preexisting object, now with test.

--- src/patchapi/patch_modifier_mutatee.c
+++ src/patchapi/patch_modifier_mutatee.c
@@ -22,11 +22,19 @@ int patchMod_redirect(int foo) {
    return bar;
 }
 
+int patchMod_insert(int foo) {
+   int bar;
+   /* We want a single entry block */
+   bar = foo * 3;
+   return bar + 10;
+}
+
 
 int patch_modifier_mutatee() {
    /* No execution necessary... yet */
    patch_modifier_var = patchMod_split(patch_modifier_var);
    patch_modifier_var = patchMod_redirect(patch_modifier_var);
+   patch_modifier_var = patchMod_insert(patch_modifier_var);
    test_passes(testname);
    return 0;
 }
