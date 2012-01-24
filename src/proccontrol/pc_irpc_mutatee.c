3a8fc5b154108dc40c9de0db1d5c10038e904f04
unknown <bill@.(none)>
2012-01-24 13:40:16 -0600

    Clean up bad integration.

--- src/proccontrol/pc_irpc_mutatee.c
+++ src/proccontrol/pc_irpc_mutatee.c
@@ -101,22 +101,7 @@ int pc_irpc_mutatee()
       return -1;
    }
 
-   // Windows won't let us boot threads out of system calls.
-   // Rather than test whether we can work around this, we'll instead fake it
-   // by having this thread cycle in and out of Sleep() for a bit so that it can
-   // actually run RPCs.
-#if defined(os_windows_test)
-   for(i = 0; i < 1000; i++)
-   {
-	   Sleep(1);
-   }
-#endif
-   result = recv_message((unsigned char *) &msg, sizeof(syncloc));
-   if (result == -1) {
-      output->log(STDERR, "Failed to recv sync message\n");
-      testUnlock(&init_lock);
-      return -1;
-   }
+
    if (msg.code != SYNCLOC_CODE) {
       output->log(STDERR, "Recieved unexpected sync message\n");
       testUnlock(&init_lock);
