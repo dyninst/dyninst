683c088bbdb8a8c3b2d2713971c8ec9ddb4b3ed6
Matthew P. Legendre <legendre@cab668.llnl.gov>
2012-05-15 10:16:03 -0700

    Clean code related to local variable/parameter parsing, no behavior changes.

--- symtabAPI/src/Function.C
+++ symtabAPI/src/Function.C
@@ -244,11 +244,11 @@ bool Function::addLocalVar(localVar *locVar)
 
       if (!addAnnotation(lvs, FunctionLocalVariablesAnno))
       {
-         fprintf(stderr, "%s[%d]:  failed to add local var collecton anno\n", 
-               FILE__, __LINE__);
          return false;
       }
    }
+   if (!lvs)
+      return false;
 
    lvs->addLocalVar(locVar);
    return true;
@@ -257,22 +257,16 @@ bool Function::addLocalVar(localVar *locVar)
 bool Function::addParam(localVar *param)
 {
 	localVarCollection *ps = NULL;
+   if (!setupParams())
+   {
+      return false;
+   }
 
 	if (!getAnnotation(ps, FunctionParametersAnno))
 	{
-                if (!setupParams())
-                {
-                        return false;
-                }
-
-                if (!getAnnotation(ps, FunctionParametersAnno))
-                {
-			fprintf(stderr, "%s[%d]:  failed to get local var collecton anno\n", 
-					FILE__, __LINE__);
-			return false;
-                }
+      return false;
 	}
-
+   
 	ps->addLocalVar(param);
 
 	return true;
@@ -294,7 +288,7 @@ bool Function::setupParams()
 		}
 	}
         
-        return true;
+   return true;
 }
 
 Function::~Function()
