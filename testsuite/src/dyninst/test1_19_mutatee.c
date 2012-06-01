83b72f811d0f69b814dc7e7894bd0bd3a0131373
legendre1 <legendre1@auk60.llnl.gov>
2012-06-01 15:37:00 -0700

    Remove debugging print

--- testsuite/src/dyninst/test1_19_mutatee.c
+++ testsuite/src/dyninst/test1_19_mutatee.c
@@ -64,7 +64,6 @@ static int globalVariable19_2 = (int)0xdeadbeef;
 int test1_19_mutatee() {
     int retval = 0;
 
-	fprintf(stderr, "Mutatee calling stop_process_() time 1\n");
     stop_process_();
 
     if (globalVariable19_1 != MAGIC19_1) {
