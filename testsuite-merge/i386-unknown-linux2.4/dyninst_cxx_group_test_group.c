3169303456cd32dd5db9e2a4dbaa872bdf2e2649
Andrew Bernat <bernat@cumin.cs.wisc.edu>
2012-06-21 16:57:56 -0500

    Reference copy of merged test suite branch.

---
+++ testsuite-merge/i386-unknown-linux2.4/dyninst_cxx_group_test_group.c
@@ -0,0 +1,33 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "../src/mutatee_call_info.h"
+
+extern int test5_3_mutatee();
+extern int test5_2_mutatee();
+extern int test5_1_mutatee();
+extern int test5_7_mutatee();
+extern int test5_6_mutatee();
+extern int test5_5_mutatee();
+extern int test5_4_mutatee();
+extern int test5_9_mutatee();
+extern int test5_8_mutatee();
+
+mutatee_call_info_t mutatee_funcs[] = {
+  {"test5_3", test5_3_mutatee, GROUPED, "test5_3"},
+  {"test5_2", test5_2_mutatee, GROUPED, "test5_2"},
+  {"test5_1", test5_1_mutatee, GROUPED, "test5_1"},
+  {"test5_7", test5_7_mutatee, GROUPED, "test5_7"},
+  {"test5_6", test5_6_mutatee, GROUPED, "test5_6"},
+  {"test5_5", test5_5_mutatee, GROUPED, "test5_5"},
+  {"test5_4", test5_4_mutatee, GROUPED, "test5_4"},
+  {"test5_9", test5_9_mutatee, GROUPED, "test5_9"},
+  {"test5_8", test5_8_mutatee, GROUPED, "test5_8"}
+};
+
+int max_tests = 9;
+int runTest[9];
+int passedTest[9];
+#ifdef __cplusplus
+}
+#endif
