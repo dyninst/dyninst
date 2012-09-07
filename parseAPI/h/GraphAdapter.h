49b33e067420828eed0064c8802a8123af9a20ae
Bill Williams <bill@cs.wisc.edu>
2012-09-07 15:50:40 -0500

    Build fix

--- parseAPI/h/GraphAdapter.h
+++ parseAPI/h/GraphAdapter.h
@@ -3,6 +3,7 @@
 #include "CFG.h"
 #include <boost/graph/graph_traits.hpp>
 #include <boost/property_map/property_map.hpp>
+#include <boost/graph/properties.hpp>
 
 using namespace Dyninst;
 using namespace ParseAPI;
