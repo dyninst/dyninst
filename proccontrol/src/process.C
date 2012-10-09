455a4f410837a45c54832d2aaf61def52b7d3934
Matthew P. Legendre <legendre@rzmerl156.llnl.gov>
2012-10-09 12:51:41 -0700

    Update release version numbers for ProcControlAPI

--- proccontrol/src/process.C
+++ proccontrol/src/process.C
@@ -71,8 +71,8 @@ bool int_process::in_callback = false;
 std::set<int_thread::continue_cb_t> int_thread::continue_cbs;
 SymbolReaderFactory *int_process::user_set_symbol_reader;
 
-static const int ProcControl_major_version = 0;
-static const int ProcControl_minor_version = 1;
+static const int ProcControl_major_version = 8;
+static const int ProcControl_minor_version = 0;
 static const int ProcControl_maintenance_version = 0;
 
 void Process::version(int& major, int& minor, int& maintenance)
