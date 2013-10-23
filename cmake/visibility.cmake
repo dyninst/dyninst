98da2f2d52d2a35882924a82788aae8efef68c3f
Josh Stone <jistone@redhat.com>
2013-10-23 15:48:50 -0700

    Really enable -fvisibility=hidden for GCC builds
    The visibility flags were being added after the component directories
    were specified, which apparently means those subdirectories don't get
    the same flags.  Also, the flags weren't quoted correctly when defined,
    and -fvisibility-inlines-hidden is only valid for C++.
    
    Now the flags are defined using cmake/visibility.cmake, mimicking
    warnings.cmake, and included in the same place in CMakeLists.txt.
    
    I've found no regressions from this change, and there's a substantial
    reduction in the size of the libraries.
    
    Signed-off-by: Josh Stone <jistone@redhat.com>

---
+++ cmake/visibility.cmake
@@ -0,0 +1,5 @@
+if(CMAKE_COMPILER_IS_GNUCXX)
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+    message(STATUS "Found g++, enabling -fvisibility=hidden")
+endif()
