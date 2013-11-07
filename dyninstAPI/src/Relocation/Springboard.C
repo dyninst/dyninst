dyninst/dyninst

commit 8a035430164e89f3eeaa1e6dca96b13486f68dce

dyninstAPI: fix warnings given by gcc 4.8
Compiling ../../dyninstAPI/src/linux-x86.C
../../dyninstAPI/src/linux-x86.C: In member function ‘AstNodePtr PCProcess::createUnprotectStackAST()’:
../../dyninstAPI/src/linux-x86.C:179:68: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
     args.push_back(AstNode::operandNode(AstNode::Constant, (void *)size));
                                                                    ^
Compiling ../../dyninstAPI/src/emit-x86.C
../../dyninstAPI/src/emit-x86.C: In member function ‘virtual bool EmitterIA32::emitBTRestores(baseTramp*, codeGen&)’:
../../dyninstAPI/src/emit-x86.C:702:10: warning: variable ‘localSpace’ set but not used [-Wunused-but-set-variable]
     bool localSpace;
          ^
Compiling ../../dyninstAPI/src/unix.C
../../dyninstAPI/src/unix.C:286:6: warning: unused parameter ‘rights’ [-Wunused-parameter]
 bool PCProcess::getMemoryAccessRights(Address start, PCMemPerm& rights) {
      ^
Compiling ../../dyninstAPI/src/stackwalk-x86.C
../../dyninstAPI/src/stackwalk-x86.C: In member function ‘virtual bool StackwalkInstrumentationHelper::isInstrumentation(Dyninst::Address, Dyninst::Address*, unsigned int*, bool*, bool*)’:
../../dyninstAPI/src/stackwalk-x86.C:131:18: warning: variable ‘func’ set but not used [-Wunused-but-set-variable]
   func_instance *func = NULL;
                  ^
Compiling ../../dyninstAPI/src/IAPI_to_AST.C
../../dyninstAPI/src/IAPI_to_AST.C: In member function ‘virtual void ASTFactory::visit(Dyninst::InstructionAPI::RegisterAST*)’:
../../dyninstAPI/src/IAPI_to_AST.C:93:54: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
                       (void*)(convertRegID(r, unused))));
                                                      ^
Compiling ../src/Relocation/Widgets/CFWidget-x86.C

dyninstAPI/src/Relocation/Springboard.C
@@ -214,7 +214,6 @@ SpringboardBuilder::generateSpringboard(std::list<codeGen> &springboards,
    codeGen gen;
 
    bool usedTrap = false;
-   unsigned size;
    // Arbitrarily select the first function containing this springboard, since only one can win. 
    generateBranch(r.from, r.destinations.begin()->second, gen);
 
@@ -226,7 +225,6 @@ SpringboardBuilder::generateSpringboard(std::list<codeGen> &springboards,
       if (conflict(r.from, r.from + 1, r.fromRelocatedCode)) return Failed;
 
       generateTrap(r.from, r.destinations.begin()->second, gen);
-      size = 1;
    }
 
    if (r.includeRelocatedCopies) {

dyninstAPI/src/BPatch_image.C
@@ -696,7 +696,6 @@ BPatch_variableExpr *BPatch_image::findVariable(const char *name,
     pdvector<int_variable *> vars;
     std::vector<AddressSpace *> as;
     int_variable *var = NULL;
-    AddressSpace *var_as = NULL;
     
     if (!addSpace)
        return NULL;
@@ -707,7 +706,6 @@ BPatch_variableExpr *BPatch_image::findVariable(const char *name,
       (void)as[i]->findVarsByAll(name, vars);
        if (vars.size()) {
           var = vars[0];
-          var_as = as[i];
           break;
        }
     }
@@ -718,7 +716,6 @@ BPatch_variableExpr *BPatch_image::findVariable(const char *name,
 	 (void)as[i]->findVarsByAll(under_name, vars);
           if (vars.size()) {
              var = vars[0];
-             var_as = as[i];
              break;
           }
        }
@@ -732,7 +729,6 @@ BPatch_variableExpr *BPatch_image::findVariable(const char *name,
           (void)as[i]->findVarsByAll(prefix_name, vars);
           if (vars.size()) {
              var = vars[0];
-             var_as = as[i];
              break;
           }
        }

dyninstAPI/src/BPatch_process.C
@@ -1006,7 +1006,7 @@ BPatch_object *BPatch_process::loadLibrary(const char *libname, bool)
        if (!stopExecution()) {
           BPatch_reportError(BPatchWarning, 0, 
                   "Failed to stop process for loadLibrary");
-          return false;
+          return NULL;
        }
    }
 
@@ -1033,7 +1033,7 @@ BPatch_object *BPatch_process::loadLibrary(const char *libname, bool)
       BPatch_reportError(BPatchSerious, 100, msg.c_str());
    }
    BPatch_function *dlopen_func = bpfv[0];
-   if (dlopen_func == NULL) return false;
+   if (dlopen_func == NULL) return NULL;
 
    /**
     * Generate a call to DYNINSTloadLibrary, and then run the generated code.
@@ -1744,5 +1744,5 @@ bool BPatch_process::protectAnalyzedCode()
            ret = false;
        }
     }
-    return false;
+    return ret;
 }

dyninstAPI/src/BPatch_snippet.C
@@ -964,7 +964,7 @@ BPatch_registerExpr::BPatch_registerExpr(Dyninst::MachRegister mach) {
    bool whocares;
    Register reg = convertRegID(mach, whocares);
    ast_wrapper = AstNodePtr(AstNode::operandNode(AstNode::origRegister,
-                                                 (void *)reg));
+                                                 (void *)(intptr_t)reg));
     assert(BPatch::bpatch != NULL);
 
     // Registers can hold a lot of different types...
@@ -1627,14 +1627,14 @@ static void constructorHelper(
     }
 
     // create callback ID argument
-    int cb_id = BPatch::bpatch->getStopThreadCallbackID(bp_cb); 
-    idNode = AstNode::operandNode(AstNode::Constant, (void*)(int) cb_id );
+    intptr_t cb_id = BPatch::bpatch->getStopThreadCallbackID(bp_cb); 
+    idNode = AstNode::operandNode(AstNode::Constant, (void*) cb_id );
     BPatch_type *inttype = BPatch::bpatch->stdTypes->findType("int");
     assert(inttype != NULL);
     idNode->setType(inttype);
 
     // create interpret/usecache argument
-    int ic = 0;
+    intptr_t ic = 0;
     if (useCache)
         ic += 1;
     if (interp == BPatch_interpAsTarget)

dyninstAPI/src/IAPI_to_AST.C
@@ -90,7 +90,7 @@ void ASTFactory::visit(RegisterAST* r)
 #if defined(arch_x86) || defined(arch_x86_64)  
     bool unused;
     m_stack.push_back(AstNode::operandNode(AstNode::origRegister,
-                      (void*)(convertRegID(r, unused))));
+                      (void*)(intptr_t)(convertRegID(r, unused))));
 #else
     MachRegister reg = r->getID();
     reg = reg.getBaseRegister();

dyninstAPI/src/Relocation/Widgets/CFWidget-x86.C
@@ -82,7 +82,7 @@ bool CFWidget::generateIndirect(CodeBuffer &buffer,
    ia32_condition cond;
 
    ia32_instruction orig_instr(memacc, &cond, &loc);
-   ia32_decode(IA32_FULL_DECODER, (unsigned char *)insn->ptr(), orig_instr);
+   ia32_decode(IA32_FULL_DECODER, (const unsigned char *)insn->ptr(), orig_instr);
    const unsigned char *ptr = (const unsigned char *)insn->ptr();
 
    std::vector<unsigned char> raw (ptr,

dyninstAPI/src/ast.C
@@ -899,11 +899,10 @@ bool AstOperatorNode::generateOptimizedAssignment(codeGen &gen, bool noCost)
    if (!arithl || !arithr)
       return false;
    
-   AstNode *data_oper = NULL, *const_oper = NULL;
+   AstNode *const_oper = NULL;
    if (arithl->getoType() == DataAddr && arithr->getoType() == Constant &&
        laddr == (Address) arithl->getOValue())
    {
-      data_oper = arithl;
       const_oper = arithr;
    }
    else if (arithl->getoType() == variableValue && arithr->getoType() == Constant)
@@ -914,14 +913,12 @@ bool AstOperatorNode::generateOptimizedAssignment(codeGen &gen, bool noCost)
          return false;
       addr = var->getAddress();
       if (addr == laddr) {
-         data_oper = arithl;
          const_oper = arithr;
       }
    }
    else if (arithr->getoType() == DataAddr && arithl->getoType() == Constant &&
             laddr == (Address) arithr->getOValue() && roper->op == plusOp)
    {
-      data_oper = arithr;
       const_oper = arithl;
    }
    else if (arithl->getoType() == variableValue && arithr->getoType() == Constant)
@@ -932,7 +929,6 @@ bool AstOperatorNode::generateOptimizedAssignment(codeGen &gen, bool noCost)
          return false;
       addr = var->getAddress();
       if (addr == laddr) {
-         data_oper = arithr;
          const_oper = arithl;
       }
    }
@@ -2284,6 +2280,7 @@ BPatch_type *AstOperatorNode::checkType() {
     if (roperand) rType = roperand->checkType();
 
     if (eoperand) eType = eoperand->checkType();
+    (void)eType; // unused...
 
     if (lType == BPatch::bpatch->type_Error ||
         rType == BPatch::bpatch->type_Error)

dyninstAPI/src/codegen.C
@@ -773,7 +773,7 @@ std::string codeGen::format() const {
    Instruction::Ptr insn = deco.decode();
    ret << hex;
    while(insn) {
-     ret << "\t" << base << ": " << insn->format(base) << " / " << *((unsigned *)insn->ptr()) << endl;
+     ret << "\t" << base << ": " << insn->format(base) << " / " << *((const unsigned *)insn->ptr()) << endl;
       base += insn->size();
       insn = deco.decode();
    }

commit 698521d826a4676fd3cd66be3ce7e52d5250b38d

Fix a bad merge that broke Springboard
In commit 8a035430164e, I removed the 'size' variable from
SpringboardBuilder::generateSpringboard(), as it was not used.

In commit 31b4cd373cd6, Bill initialized 'size' with a proper
value and used it in the call to registerBranch().

When git merged these, it reconciled the changed initialization on its
own, but also kept out a 'size = 1' that I had removed and Bill hadn't
touched.  That assignment is needed now that the variable is used.

dyninstAPI/src/Relocation/Springboard.C
@@ -227,6 +227,7 @@ SpringboardBuilder::generateSpringboard(std::list<codeGen> &springboards,
       if(!addrSpace_->canUseTraps()) return Failed;
       
       generateTrap(r.from, r.destinations.begin()->second, gen);
+      size = 1;
    }
 
    if (r.includeRelocatedCopies) {