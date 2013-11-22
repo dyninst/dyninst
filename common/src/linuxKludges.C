36bdda433436c4887a1e0b107d55287d60d6b5f0
Josh Stone <jistone@redhat.com>
2013-11-22 11:18:11 -0800

    Reimplement Linux P_copy with fstream
    Coverity reported RESOURCE_LEAK for the fds on error paths, and I see it
    also doesn't check write() return value.  It's much easier to just let
    fstream do the buffer management and IO.
    
    Note: P_copy doesn't appear to actually be used anywhere, and the former
    destination mode 0 was also suspicious.  Maybe it should be removed.

--- common/src/linuxKludges.C
+++ common/src/linuxKludges.C
@@ -56,31 +56,12 @@ int P_getopt(int argc, char *argv[], const char *optstring)
 }
 
 int P_copy(const char *from, const char *to) {
-    int from_fd = P_open(from, O_RDONLY, 0);
-    if (from_fd == -1)  {
-        perror("Opening from file in copy"); 
-        return -1;
-    }
-    int to_fd = P_open(to, O_WRONLY | O_APPEND | O_CREAT | O_TRUNC, 0);
-    if (to_fd == -1) {
-        perror("Opening to file in copy");
-        close(from_fd);
-        return -1;
-    }
-
-    char buffer[1048576];
-    while(true) {
-        int amount = read(from_fd, buffer, 1048576);
-        if (amount == -1) {
-            perror("Reading in file copy");
-            return -1;
-        }
-        write(to_fd, buffer, amount);
-        if (amount < 1048576) break;
-    }
-    close(to_fd);
-    close(from_fd);
-    return 0;
+    std::ifstream src(from, std::ios::binary);
+    std::ofstream dst(to, std::ios::binary | std::ios::trunc);
+    dst << src.rdbuf();
+    dst.close();
+    src.close();
+    return (src && dst) ? 0 : -1;
 }
 
 
