100ba895a27adc2589817469cfa8c5342258ec14
Paradyn <paradyn@cs.wisc.edu>
2014-02-04 14:10:12 -0600

    More windows fixes

--- symtabAPI/src/annotations.C
+++ symtabAPI/src/annotations.C
@@ -34,14 +34,14 @@
 #include "Annotatable.h"
 #include "common/src/serialize.h"
 
-AnnotationClass<localVarCollection> FunctionLocalVariablesAnno(std::string("FunctionLocalVariablesAnno"));
-AnnotationClass<localVarCollection> FunctionParametersAnno(std::string("FunctionParametersAnno"));
+AnnotationClass<localVarCollection> FunctionLocalVariablesAnno("FunctionLocalVariablesAnno");
+AnnotationClass<localVarCollection> FunctionParametersAnno("FunctionParametersAnno");
 AnnotationClass<std::vector<std::string> > SymbolVersionNamesAnno("SymbolVersionNamesAnno");
 AnnotationClass<std::string> SymbolFileNameAnno("SymbolFileNameAnno");
-AnnotationClass<std::vector<Function *> > UserFuncsAnno(std::string("UserFuncsAnno"));
-AnnotationClass<std::vector<Region *> > UserRegionsAnno(std::string("UserRegionsAnno"));
-AnnotationClass<std::vector<Type *> > UserTypesAnno(std::string("UserTypesAnno"));
-AnnotationClass<std::vector<Symbol *> > UserSymbolsAnno(std::string("UserSymbolsAnno"));
-AnnotationClass<LineInformation> ModuleLineInfoAnno(std::string("ModuleLineInfoAnno"));
-AnnotationClass<typeCollection> ModuleTypeInfoAnno(std::string("ModuleTypeInfoAnno"));
+AnnotationClass<std::vector<Function *> > UserFuncsAnno("UserFuncsAnno");
+AnnotationClass<std::vector<Region *> > UserRegionsAnno("UserRegionsAnno");
+AnnotationClass<std::vector<Type *> > UserTypesAnno("UserTypesAnno");
+AnnotationClass<std::vector<Symbol *> > UserSymbolsAnno("UserSymbolsAnno");
+AnnotationClass<LineInformation> ModuleLineInfoAnno("ModuleLineInfoAnno");
+AnnotationClass<typeCollection> ModuleTypeInfoAnno("ModuleTypeInfoAnno");
 AnnotationClass<dyn_hash_map<Address, Symbol *> > IdToSymAnno("IdToSymMap");
