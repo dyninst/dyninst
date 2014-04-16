e9b143e24c022e907e06d88bb29b619f88e41062
Emily Jacobson <jacobson@cs.wisc.edu>
2014-04-16 13:53:43 -0500

    We weren't ensuring that our section's alignment was at least equal to     its data's alignment. Fixed. (Part 2, from Bill.)

--- symtabAPI/src/emitElf.C
+++ symtabAPI/src/emitElf.C
@@ -1549,6 +1549,10 @@ bool emitElf::createLoadableSections(Symtab*obj, Elf32_Shdr* &shdr, unsigned &ex
         newdata64->d_size = newSecs[i]->getDiskSize();
         if (!newdata64->d_align)
            newdata64->d_align = newshdr->sh_addralign;
+        if(newshdr->sh_addralign < newdata64->d_align) 
+        {
+            newshdr->sh_addralign = newdata64->d_align;
+        }
         newshdr->sh_size = newdata64->d_size;
         memcpy(newdata, newdata64, sizeof(Elf_Data));
      }
@@ -1560,6 +1564,10 @@ bool emitElf::createLoadableSections(Symtab*obj, Elf32_Shdr* &shdr, unsigned &ex
         newdata->d_size = newSecs[i]->getDiskSize();
         if (!newdata->d_align)
            newdata->d_align = newshdr->sh_addralign;
+        if(newshdr->sh_addralign < newdata->d_align) 
+        {
+            newshdr->sh_addralign = newdata->d_align;
+        }
         newshdr->sh_size = newdata->d_size;
      }
 
