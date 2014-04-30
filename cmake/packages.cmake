f2c4a898711fb39eb215ed82e58c9c5794460ba4
Bill Williams <bill@cs.wisc.edu>
2014-04-30 10:51:44 -0500

    C++11 detection on a per-feature basis via BSD-licensed CMake scripts (that will become part of 2.8.x at some point).

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -136,3 +136,12 @@ message(STATUS "Boost includes: ${Boost_INCLUDE_DIRS}")
 message(STATUS "Boost library dirs: ${Boost_LIBRARY_DIRS}")
 message(STATUS "Boost thread library: ${Boost_THREAD_LIBRARY}")
 message(STATUS "Boost libraries: ${Boost_LIBRARIES}")
+
+include(${DYNINST_ROOT}/cmake/CheckCXX11Features.cmake)
+if(NOT HAS_CXX11_AUTO)
+  message(FATAL_ERROR "No support for C++11 auto found. Dyninst requires this compiler feature.")
+else()
+  message(STATUS "C++11 support found, required flags are: ${CXX11_COMPILER_FLAGS}")
+endif()
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX11_COMPILER_FLAGS}")
