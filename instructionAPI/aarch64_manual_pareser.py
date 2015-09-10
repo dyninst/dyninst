15a3e0ec7d8be08375d3816a8c2bb47d4b60ffce
SteveXiSong <songxi.buaa@gmail.com>
2015-09-10 17:59:27 -0500

    mask table for aarch64 has been generated, prototype

--- instructionAPI/aarch64_manual_pareser.py
+++ instructionAPI/aarch64_manual_pareser.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+"""
+Author: Steve Song
+Email:  songxi.buaa@gmail.com
+Date:   Sep 2015
+"""
+
 import os, sys
 from sets import Set
+from array import *
 #from bitarray import BAarray
 
 #MACRO used as a flag for vector and floating points
@@ -38,11 +46,12 @@ def getOpcodes():
     print('enum {')
     printP('INVALID')
     printP('extended')
-    for ele in op_set:
+    #for ele in op_set:
+    for ele in sorted(insn_set):
         printP(ele)
 
     print('}')
-    print 'number of instructions: ', len(op_set)
+    print 'number of instructions: ', len(insn_set)
 
 
 # to find all control field names
@@ -50,7 +59,6 @@ control_field_set = Set()
 non_ctl_field_set = Set(['sf'] )
 
 def getDecodeFieldNames():
-    global  control_field_set
 
     decodeFile = open(ISA_dir + 'encodingindex.xml')
 
@@ -60,7 +68,6 @@ def getDecodeFieldNames():
         if startReadDecodeField == True:
             if line.find('\"bitfields\"') != -1:
                 control_field_set.add(line.split('>')[1].split('<')[0])
-
 
         if line.find('Decode fields') !=-1:
             startReadDecodeField = True
@@ -82,19 +89,22 @@ def shifting(bitlist):
     return out
 
 # to get the encoding table
+masksArray = list()
+encodingsArray = list()
+insnArray = list()
+
 def getOpTable():
 
     # open all op files
     commonBits = int(str('1'*32), 2)
     for files in sorted(files_dir):
         if files.endswith('.xml'):
-            if files.split('_')[0] in op_set:
-                instruction = files.split('.xml')[0]
+            instruction = files.split('.xml')[0]
+            if instruction in insn_set:
                 #print files
                 curFile = open(ISA_dir+files)
 
                 startDiagram = False
-                maskBitArray = list('0'*32)
                 maskBitArrayNew = list('0'*32)
                 encodingArray = list('0'*32)
                 maskStartBit = 31
@@ -110,35 +120,11 @@ def getOpTable():
 
                     #diagram starts
                     if startDiagram == True and line.find('<box')!=-1:
-
-                        not_control_field = False
-                        if line.find('name=') !=-1:
-
-                            for field in line.split(' '):
-                                if field.find('name') != -1 and field.find('usename')==-1:
-                                    control_field = field.split('\"')[1]
-                                    #print control_field
-
-                                    # if this is control field
-                                    if control_field not in control_field_set:
-                                        not_control_field = True
-                                    break
-
                         #maskStartBit = 31
                         for x in line.split(' '):
                             if x.find('hibit') != -1:
                                 maskStartBit = int(x.split('\"')[1])
                                 break
-
-                        maskLen = 1
-                        for i in line.split(' '):
-                            if i.find('width')!=-1:
-                                maskLen = int(i.split('\"')[1])
-                                break
-
-                        if not_control_field == False:
-                            for s in range(1, maskLen+1):
-                                maskBitArray[(31-maskStartBit)+s-1] = '1'
 
                         startBox = True
 
@@ -162,19 +148,115 @@ def getOpTable():
 
 
                 # print instruction and encoding mask per file
-                maskBitInt = int(''.join(maskBitArray), 2)
-                maskBitNewInt = int(''.join(maskBitArrayNew), 2)
+                maskBitInt = int(''.join(maskBitArrayNew), 2)
+                encodingBitInt = int( ''.join(encodingArray),2)
 
-                #print "%22s"%instruction, '\t\t', ''.join(maskBitArray), '\t', ''.join(maskBitArrayNew), '\t', ''.join(encodingArray)
-                print "%22s"%instruction, '\t\t', ''.join(maskBitArrayNew), '\t', ''.join(encodingArray)
+                masksArray.append(maskBitInt)
+                encodingsArray.append(encodingBitInt)
+                insnArray.append(instruction)
+
+                print "%22s"%instruction, '\t', ''.join(maskBitArrayNew),'(', hex(maskBitInt),')', '\t', ''.join(encodingArray), '(', hex(encodingBitInt), ')'
 
     insn_unallocated = (2**28+2**27)
 
     commonBits = commonBits & insn_unallocated
-    print "%22s"%'unallocated', '\t\t', bin(insn_unallocated), '\t', hex(insn_unallocated)
+    masksArray.append(insn_unallocated)
+    encodingsArray.append(int(0))
+    insnArray.append('unallocated')
+
+    print "%22s"%'unallocated', '\t', bin(insn_unallocated), '(', hex(insn_unallocated), ')'
+
+def buildInsnTable():
+    assert len(masksArray) == len(encodingsArray) ==len(insnArray)
+    print len(insnArray)
+    for i in range(1, len(insnArray)):
+        print '\taarch64_insn_table.push_back(aarch64_entry(aarch64_op_'+insnArray[i]+', \t\"'+insnArray[i].split('_')[0]+'\t\", list_of() ));'
+
+
+numOfLeafNodes=0
+# need to initialize the mask_table with unallocated entries
+
+def buildDecodeTable(inInsnIndex , processedMask, entryToPlace):
+    global numOfLeafNodes
+
+    # guards
+    if entryToPlace > 2**32:
+        #numOfLeafNodes +=1
+        return
+
+    if processedMask == 0xffffffff:
+        #numOfLeafNodes +=1
+        return
+
+    # terminated condition 1
+    if len(inInsnIndex) <=1:
+        numOfLeafNodes +=1
+        return
+
+    validMaskBits = int(''.join('1'*32), 2)
+    for i in inInsnIndex:
+        validMaskBits = validMaskBits & masksArray[i]
+    validMaskBits = validMaskBits&(~processedMask)
+    #print hex(validMaskBits), bin(validMaskBits)
+
+    # till here we should have got the mask bits
+    MSBmask = 0x80000000
+    curMask = 0
+    for bit in range(0, 31):
+        if (MSBmask & validMaskBits) == 0 :
+            validMaskBits = validMaskBits << 1
+        else:
+            curMask = 1<<(31-bit)
+            break
+
+    # terminated condition 2
+    if curMask == 0:
+        numOfLeafNodes+=1
+        return
+
+    #print hex(curMask)
+    zeroIndexes = list()
+    oneIndexes = list()
+
+    # typedef mask_table vector<mask_entry>;
+    zeroEntryToPlace = entryToPlace*2+1;
+    oneEntryToPlace = entryToPlace*2+2;
+
+    #TODO
+    print 'mask_table['+str(entryToPlace)+']=mask_entry('+str(hex(curMask))+', '+str(zeroEntryToPlace)+', '+str(oneEntryToPlace)+',0'+');'
+
+    #print hex(curMask)
+    for index in inInsnIndex:
+        if encodingsArray[index] & curMask == 0:
+            zeroIndexes.append(index)
+            #print index, bin(encodingsArray[index]), encodingsArray[index]&curMask
+        else:
+            oneIndexes.append(index)
+            #print '\t\t', index, bin(encodingsArray[index]), encodingsArray[index]&curMask
+
+    """
+    print '%34s'%bin(curMask)
+    for i in zeroIndexes:
+        print '%34s'%bin(encodingsArray[i])
+
+    for i in oneIndexes:
+        print '%34s'%bin(encodingsArray[i])
+    """
+    processedMask0 = processedMask | curMask
+    processedMask1 = processedMask | curMask
+
+    buildDecodeTable(zeroIndexes, processedMask0, zeroEntryToPlace)
+    buildDecodeTable(oneIndexes, processedMask1, oneEntryToPlace)
+
 
 
 getOpcodes()
-getDecodeFieldNames()
+#getDecodeFieldNames()
 getOpTable()
+
+buildInsnTable()
+validInsnIndex = list( range(0, len(insnArray) ) )
+#print validInsnIndex
+buildDecodeTable(validInsnIndex, 0 , 0)
+print numOfLeafNodes
 
