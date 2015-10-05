4a1315544aa866937f55c495fb653943547b985b
SteveXiSong <songxi.buaa@gmail.com>
2015-10-05 14:45:10 -0500

    fixed generator bugs. It is nealy good. update gnerated output

--- instructionAPI/aarch64_manual_pareser.py
+++ instructionAPI/aarch64_manual_pareser.py
@@ -10,17 +10,23 @@ Date:   Sep 2015
 import os, sys
 from sets import Set
 from array import *
-#from bitarray import BAarray
 
-#MACRO used as a flag for vector and floating points
+#######################################
+# a flag for vector and floating points
+#######################################
 VEC_SIMD_SWITCH = False
 #VEC_SIMD_SWITCH = True
 
+####################################
 # dir to store aarch64 ISA xml files
+####################################
 ISA_dir = "../../ISA_xml_v2_00rel11/"
 files_dir = os.listdir(ISA_dir)
 
-# opcodes and instructions
+##############################
+# parse xml files
+# get opcodes
+##############################
 op_set = Set()
 insn_set = Set()
 
@@ -53,8 +59,11 @@ def getOpcodes():
     print('}')
     print 'number of instructions: ', len(insn_set)
 
-
+#################################
 # to find all control field names
+# discarded
+#################################
+'''
 control_field_set = Set()
 non_ctl_field_set = Set(['sf'] )
 
@@ -80,6 +89,7 @@ def getDecodeFieldNames():
 
     control_field_set.difference_update(non_ctl_field_set)
     print sorted(control_field_set)
+    '''
 
 
 def shifting(bitlist):
@@ -88,107 +98,195 @@ def shifting(bitlist):
         out = (out<<1) | (bit=='1' )
     return out
 
-# to get the encoding table
+##########################
+# parse xml files and get
+# opcodes and operands
+##########################
+
+def getOperand_Insn(line):
+    '''
+    analyze one <box> line </box> to get operand
+    '''
+    for field in line.split(' '):
+        if field.find('name') != -1 and field.find('usename') == -1:
+            return field.split('\"')[1]
+
+
+###########################
+# to store all masks
+# to store all encodings
+# to store all instructions
+###########################
 masksArray = list()
 encodingsArray = list()
 insnArray = list()
+operandsArray = list()
+operandsSet = set()
 
-def getOpTable():
+# to get the encoding table
+def getOpTable( filename = 'NULL' ):
 
-    # open all op files
+    global masksArray
+    global encodingsArray
+    global insnArray
+    global operandsSet
 
+    #### some init ####
     insn_unallocated = (2**28+2**27)
     masksArray.append(insn_unallocated)
     encodingsArray.append(int(0))
     insnArray.append('unallocated')
+    operandsArray.append('')
+    indexOfInsn = 1
 
     print 0, '%22s'%'unallocated',  '%34s'%bin(insn_unallocated), '(', hex(insn_unallocated), ')'
-    index = 1
-    for files in sorted(files_dir):
-        if files.endswith('.xml'):
-            instruction = files.split('.xml')[0]
+
+    for file in sorted(files_dir):
+        if file.endswith('.xml'):
+            instruction = file.split('.xml')[0]
             if instruction in insn_set:
-                #print files
-                curFile = open(ISA_dir+files)
+                #print file
+                curFile = open(ISA_dir+file)
 
                 startDiagram = False
-                maskBitArrayNew = list('0'*32)
-                encodingArray = list('0'*32)
-                maskStartBit = 31
                 startBox = False
+                maskBit = list('0'*32)
+                encodingArray = list('0'*32)
+                operands_Insn = list()
+                operands_pos_Insn = list()
+                reserve_operand = list()
+                reserve_operand_pos = list()
+                maskStartBit = 31
+
+                foundOneEncoding = False
 
                 # to analyze lines
                 for line in curFile:
 
-                    #diagram starts
+                    #diagram starts ##
                     if line.find('<regdiagram')!=-1:
                         startDiagram = True
-                    if line.find('</regdiagram')!=-1:
-                        break
+                        continue
 
-                    #analyze each box
-                    if startDiagram == True and line.find('<box')!=-1:
+                    if line.find('</regdiagram')!=-1:
+                        startDiagram = False
+                        foundOneEncoding = True
+                        continue
+                        #break
+                    # end of diagram
+
+                    #analyze each box ##
+                    if startDiagram == True and line.find('<box') != -1:
                         #name, start bit, length
                         for x in line.split(' '):
                             if x.find('hibit') != -1:
                                 maskStartBit = int(x.split('\"')[1])
                                 break
 
+                        reserve_operand = getOperand_Insn(line)
+                        reserve_operand_pos = getOperandValues(line)
                         startBox = True
 
                     if line.find('</box') != -1:
                         startBox = False
+                        continue
+                    # end of box #######
 
-                    # read box content
+                    # read box content #################################
                     if startBox == True:
-                        if line.find('<c>') != -1:
-                            encodeBit = line.split('>')[1].split('<')[0]
-                            if encodeBit == '1' or encodeBit == '0':
-                                maskBitArrayNew[31-maskStartBit] = '1'
-                                encodingArray[31-maskStartBit] = encodeBit
-                            else:
-                                maskBitArrayNew[31-maskStartBit] = '0'
-                                encodingArray[31-maskStartBit] = '0'
-                            maskStartBit = maskStartBit - 1
-                        '''
+                        # start of <c>
+                        #if line.find('<c>') != -1:
                         if line.find('<c') != -1:
-                            if line.split('>')[1].split('<')[0] =='':
-                                operandSet.add(operandValues)
-                        '''
-                    # end of <box line>
+                            encodeBit = line.split('>')[1].split('<')[0]
+
+                            # control field
+                            if encodeBit != '' and encodeBit != 'x':
+                                if encodeBit == '1' or encodeBit == '0':
+                                    maskBit[31-maskStartBit] = '1'
+                                    encodingArray[31-maskStartBit] = encodeBit
+                                else:
+                                    maskBit[31-maskStartBit] = '0'
+                                    encodingArray[31-maskStartBit] = '0'
+
+                            if encodeBit == '':
+                                operands_Insn.append(reserve_operand)
+                                operands_pos_Insn.append(reserve_operand_pos)
+                                '''
+                                if reserve_operand not in operandsSet:
+                                    operandsSet.add(reserve_operand)
+                                    '''
+
+                            maskStartBit = maskStartBit - 1
+                    # end of <box line> ################################
+
+                    #print instruction in file. Might be multiples in one file
+                    if foundOneEncoding == True:
+                        #printInstnEntry(maskBit, encodingArray, indexOfInsn, instruction, operands_Insn)
+                        printInstnEntry(maskBit, encodingArray, indexOfInsn, instruction, operands_pos_Insn)
+
+                        maskBit = list('0'*32)
+                        encodingArray = list('0'*32)
+                        operands_Insn = list()
+                        operands_pos_Insn = list()
+
+                        foundOneEncoding = False
+                        indexOfInsn +=1
+
+                    #end of print instruction
 
                 # end of each line
 
+####################################
+# generate instructions
+####################################
+def printInstnEntry( maskBit, encodingArray, index, instruction, operands):
+    global masksArray
+    global encodingsArray
+    global insnArray
+    global operandsArray
 
-                # print instruction and encoding mask per file
-                maskBitInt = int(''.join(maskBitArrayNew), 2)
-                encodingBitInt = int( ''.join(encodingArray),2)
+     # print instruction and encoding mask per file
+    maskBitInt = int(''.join(maskBit), 2)
+    encodingBitInt = int( ''.join(encodingArray),2)
 
-                masksArray.append(maskBitInt)
-                encodingsArray.append(encodingBitInt)
-                insnArray.append(instruction)
+    masksArray.append(maskBitInt)
+    encodingsArray.append(encodingBitInt)
+    insnArray.append(instruction)
+    operandsArray.append(operands)
 
-                print index, "%22s"%instruction, '\t', ''.join(maskBitArrayNew),'(', hex(maskBitInt),')', '\t', ''.join(encodingArray), '(', hex(encodingBitInt), ')'
-                index += 1
+    print index, "%22s"%instruction, '\t', ''.join(maskBit),'(', hex(maskBitInt),')', '\t', ''.join(encodingArray), '(', hex(encodingBitInt), ')', operands
 
 
+####################################
+# generate the c++ code
+# which builds the instruction table
+####################################
 def buildInsnTable():
     assert len(masksArray) == len(encodingsArray) ==len(insnArray)
     print len(insnArray)
     print '*** instruction table ***'
+
     for i in range(0, len(insnArray)):
-        print '\taarch64_insn_table.push_back(aarch64_insn_entry(aarch64_op_'+insnArray[i]+', \t\"'+insnArray[i].split('_')[0]+'\t\", list_of() ));'
+
+        if len(operandsArray[i]) == 0:
+            operands = 'operandSpec()'
+        else:
+            operands = 'list_of'
+            for operand in operandsArray[i]:
+                if len(operand) != 1:
+                    operands += '('+operand[0]+'<'+ str(operand[1][0])+',' + str(operand[1][1])+'>)'
+                else:
+                    operands += '('+operand[0]+')'
+
+        print '\tmain_insn_table.push_back(aarch64_insn_entry(aarch64_op_'+insnArray[i]+', \t\"'+insnArray[i].split('_')[0]+'\",\t'+ operands +' ));'
 
 
+# generate decoding tree ####
 numOfLeafNodes=0
 processedIndex = Set()
 notProcessedIndex = Set()
 numNotProcNodes = 0
 numNodes = 0
-
-# need to initialize the mask_table with unallocated entries
-#def printDecodertable(entryToPlace, curMask=0, zeroEntryToPlace=-1, oneEntryToPlace=-1, index=-1 ):
-    #print '\taarch_decoder_table['+str(entryToPlace)+']=aarch64_mask_entry('+str(hex(curMask))+', '+str(zeroEntryToPlace)+', '+str(oneEntryToPlace)+','+str(index)+');'
 
 def clapseMask(encoding, curMask):
     ret = 0
@@ -200,6 +298,10 @@ def clapseMask(encoding, curMask):
     return ret
 
 
+####################################
+# generate the c++ code
+# which builds the decoding table
+####################################
 def printDecodertable(entryToPlace, curMask=0, entryList=list(), index=-1 ):
     entries = 'map_list_of'
     if len(entryList) == 0:
@@ -208,7 +310,7 @@ def printDecodertable(entryToPlace, curMask=0, entryList=list(), index=-1 ):
         for ent in entryList:
             entries += '('+str(ent[0])+','+str(ent[1])+')'
 
-    print '\taarch_decoder_table['+str(entryToPlace)+']=aarch64_mask_entry('+str(hex(curMask))+', '+entries+','+str(index)+');'
+    print '\tmain_decoder_table['+str(entryToPlace)+']=aarch64_mask_entry('+str(hex(curMask))+', '+entries+','+str(index)+');'
 
 entryAvailable = 1
 
@@ -217,7 +319,6 @@ def buildDecodeTable(inInsnIndex , processedMask, entryToPlace):
     global numNotProcNodes
     global numNodes
     global entryAvailable
-
 
     # guards, redundant
     '''
@@ -259,16 +360,26 @@ def buildDecodeTable(inInsnIndex , processedMask, entryToPlace):
     addMask = 0
     if validMaskBits == 0:
         #print '[WARN] all mask bits have been processed', len(inInsnIndex)
-        #print '%25s'%'processed mask'+'%34s'%bin(processedMask)
+
+        # relaxed solution to alias
+        numOfLeafNodes += len(inInsnIndex)
+        for i in inInsnIndex:
+            processedIndex.add(i)
+        printDecodertable(entryToPlace, 0, list(), inInsnIndex[0]);
+        numNodes += 1
+        return
+
+        '''
         for i in inInsnIndex:
             #print '%3d'%i+'%22s'%insnArray[i]+'%34s'%bin(masksArray[i])+'%34s'%bin(encodingsArray[i])
-            addMask ^= encodingsArray[i] &(~processedMask)
+            addMask |= masksArray[i] &(~processedMask)
+            #addMask ^= encodingsArray[i] &(~processedMask)
+
         # handle alias, merge them into one node
         if addMask == 0:
             numOfLeafNodes += len(inInsnIndex)
             for i in inInsnIndex:
                 processedIndex.add(i)
-            #printDecodertable(entryToPlace, 0, -1, -1, inInsnIndex[0]);
             printDecodertable(entryToPlace, 0, list(), inInsnIndex[0]);
             numNodes += 1
             return
@@ -276,6 +387,7 @@ def buildDecodeTable(inInsnIndex , processedMask, entryToPlace):
         else:
             validMaskBits = addMask
             #print bin(addMask)
+            '''
 
     # till here we should have got the mask bits
     MSBmask = 0x80000000
@@ -356,23 +468,32 @@ the following section is for parsing and generating operands.
 """
 
 def getOperandValues(line):
+    multiOperandSet = set(['S', 'imm', 'option', 'opt', 'N', 'cond'])
+
     #operandValues #= {'name':'', 'bit':32, 'width':0}
     if line.find(' name') != -1:
+        width = 1
         tokens = line.split(' ')
         for token in tokens:
             if token.find('name')!=-1 and token.find('usename')== -1:
                 name = token.split('\"')[1]
+                if name.find('imm') != -1:
+                    name = 'imm'
             if token.find('hibit')!=-1:
                 bit = int(token.split('\"')[1])
             if token.find('width') != -1:
                 width = int(token.split('\"')[1])
-            else:
-                width = 1
     else:
-        return ('', [(0, 0)])
+        return ('', [0, 0])
 
-    return (name, [(bit, width)])
+    endbit = bit - (width-1)
+    #return (name, [bit, width])
+    if name in multiOperandSet:
+        return (name, [bit, endbit])
+    else:
+        return (name,)
 
+'''
 operandList = list()
 operandDict = {}
 
@@ -409,25 +530,37 @@ def analyzeOperands():
                     if startBox == True:
                         if line.find('<c') != -1:
                             if line.split('>')[1].split('<')[0] =='':
-                                '''
-                                print type(operandDict)
+                                #print type(operandDict)
                                 print operandValues
                                 #operandDict[operandValues[0]].append(operandValues[1])
                                 #operandList.append(operandValues)
-                                #operandSet.add(operandValues)
-                                #print line
-                                '''
                                 continue
+                            '''
 
+def printOperandFuncs(operandsSet):
+    for operand in operandsSet:
+        print 'void '+operand + '(){ }'
+
+
+#####################
+####### main ########
+#####################
 
 getOpcodes()
 #getDecodeFieldNames()
 getOpTable()
 
+#instruction table
 buildInsnTable()
-validInsnIndex = list( range(0, len(insnArray) ) )
 
+#operands
+print '*** operands ***'
+print operandsSet
+printOperandFuncs(operandsSet)
+
+#decoder table
 print '*** decoder table ***'
+validInsnIndex = list( range(0, len(insnArray) ) )
 buildDecodeTable(validInsnIndex, 0 , 0)
 print 'num of vaild leaves', numOfLeafNodes
 allIndex = Set(range(0, len(insnArray)) )
@@ -437,5 +570,7 @@ print 'processed indexex: ', processedIndex, len(processedIndex)
 print 'missing indexes:', sorted(allIndex - processedIndex), len(allIndex-processedIndex)
 print 'number of total nodes in the tree:', numNodes
 
+'''
 analyzeOperands()
 print 'operands:', operandDict
+'''
