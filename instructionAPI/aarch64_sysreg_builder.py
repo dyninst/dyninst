dff19b519f485e48c51bfcaba02cccb1f60a7bc8
Sunny Shah <shah28@wisc.edu>
2016-04-01 13:40:59 -0500

    Generating output for system register declarations in the dyn_regs header
    The Python script that parses system register XMLs now generates the code that needs to be added to dyn_regs.h for register declarations.
    This also required the computation of register sizes from the XMLs.

--- instructionAPI/aarch64_sysreg_builder.py
+++ instructionAPI/aarch64_sysreg_builder.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 sys_reg_dir = "/p/paradyn/arm/arm-download-1350222/AR100-DA-70000-r0p0-00rel10/AR100-DA-70000-r0p0-00rel10/SysReg_xml/ARMv8-SysReg-00bet12/SysReg_xml-00bet12"
 reg_names = list()
 reg_encodings = OrderedDict()
+reg_sizes = dict()
 
 reg_names = [f.split('.')[0].split("AArch64-")[1] for f in os.listdir(sys_reg_dir) if (path.isfile(sys_reg_dir + os.sep + f) and f.find("AArch64") != -1)]
 
@@ -28,9 +29,16 @@ for name in reg_names:
 	encoding_fieldvalue =  encoding_dict[encoding_fieldname]
 	for c in encoding_fieldvalue:
 	    encoding_val.append(c)
+
+    attributes = collection.getElementsByTagName("reg_attributes")
+    for attr in attributes:
+	para = attr.getElementsByTagName("para")[0].childNodes[0].data
+    if(para.find("-bit") != -1 and para.find("is a ") != -1):
+	size = para.split("-bit")[0].split("is a ")[1]
     
     try: 
         reg_encodings[name] = hex(int(''.join(str(i) for i in encoding_val), 2))
+	reg_sizes[name] = size
     except:
 	encoding_str = ''.join(encoding_val)
 	if len(encoding_str) < 1 or encoding_str.find("n") == -1:
@@ -69,6 +77,18 @@ for name in reg_names:
 		cur_encoding = cur_encoding[:(len(encoding_str) - 1 - int(val))] + bin_arr[len(replace_list) - 1 - int(val)] + cur_encoding[(len(encoding_str) - int(val)):]
 	    #print(hex(int(cur_encoding, 2)))
 	    reg_encodings[name.replace("n_", str(idx) + "_")] = hex(int(cur_encoding, 2))
+	    reg_sizes[name.replace("n_", str(idx) + "_")] = size
 
 for elem in reg_encodings:
     print("sysRegMap[" + reg_encodings[elem] + "] = aarch64::" + elem + ";");
+
+print()
+ct = 0
+for elem in reg_encodings:
+    size_str = ""
+    if reg_sizes[elem] == "32":
+	size_str = "D_REG"
+    else:
+	size_str = "FULL"
+    print("DEF_REGISTER(" + elem + ",\t\t" + str(ct) + " | " + size_str + " |SYSREG | Arch_aarch64, \"aarch64\");");
+    ct += 1
