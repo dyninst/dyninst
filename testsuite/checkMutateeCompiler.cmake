4d934adb3b5f8057ba5758dfb59c35af49fc83d8
Peter Foley <pefoley2@pefoley.com>
2016-05-08 22:56:53 -0400

    travis debugging
    Signed-off-by: Peter Foley <pefoley2@pefoley.com>

--- testsuite/checkMutateeCompiler.cmake
+++ testsuite/checkMutateeCompiler.cmake
@@ -7,10 +7,8 @@ MACRO (CHECK_MUTATEE_COMPILER _COMPILER _COMP_FLAG _LINK_FLAG _LANG _MSG _RESULT
       if(${_LANG} MATCHES CXX)
             execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/CMakeTmp/CompilerTest
                             RESULT_VARIABLE COMPILER_RESULT
-                            OUTPUT_QUIET
-                            ERROR_QUIET
-#							OUTPUT_VARIABLE COMPILER_OUTPUT
-#							ERROR_VARIABLE COMPILER_OUTPUT
+							OUTPUT_VARIABLE COMPILER_OUTPUT
+							ERROR_VARIABLE COMPILER_OUTPUT
                             COMMAND ${CMAKE_COMMAND}
                             -DCMAKE_CXX_COMPILER=${_COMPILER}
                             -DCMAKE_CXX_FLAGS=${_COMP_FLAG}
@@ -20,10 +18,8 @@ MACRO (CHECK_MUTATEE_COMPILER _COMPILER _COMP_FLAG _LINK_FLAG _LANG _MSG _RESULT
       elseif (${_LANG} MATCHES Fortran)
             execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/CMakeTmp/CompilerTest
                             RESULT_VARIABLE COMPILER_RESULT
-                            OUTPUT_QUIET
-                            ERROR_QUIET
-#							OUTPUT_VARIABLE COMPILER_OUTPUT
-#							ERROR_VARIABLE COMPILER_OUTPUT
+							OUTPUT_VARIABLE COMPILER_OUTPUT
+							ERROR_VARIABLE COMPILER_OUTPUT
                             COMMAND ${CMAKE_COMMAND}
                             -DCMAKE_Fortran_COMPILER=${_COMPILER}
                             -DCMAKE_Fortran_FLAGS=${_COMP_FLAG}
@@ -33,10 +29,8 @@ MACRO (CHECK_MUTATEE_COMPILER _COMPILER _COMP_FLAG _LINK_FLAG _LANG _MSG _RESULT
       elseif (${_LANG} MATCHES C)
             execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/CMakeTmp/CompilerTest
                             RESULT_VARIABLE COMPILER_RESULT
-                            OUTPUT_QUIET
-                            ERROR_QUIET
-#							OUTPUT_VARIABLE COMPILER_OUTPUT
-#							ERROR_VARIABLE COMPILER_OUTPUT
+							OUTPUT_VARIABLE COMPILER_OUTPUT
+							ERROR_VARIABLE COMPILER_OUTPUT
                             COMMAND ${CMAKE_COMMAND}
                             -DCMAKE_C_COMPILER=${_COMPILER}
                             -DCMAKE_C_FLAGS=${_COMP_FLAG}
@@ -47,10 +41,8 @@ MACRO (CHECK_MUTATEE_COMPILER _COMPILER _COMP_FLAG _LINK_FLAG _LANG _MSG _RESULT
       if (${COMPILER_RESULT} MATCHES 0)
             execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/CMakeTmp/CompilerTest
                             RESULT_VARIABLE COMPILER_RESULT
-                            OUTPUT_QUIET
-                            ERROR_QUIET
-#							OUTPUT_VARIABLE COMPILER_OUTPUT
-#							ERROR_VARIABLE COMPILER_OUTPUT
+							OUTPUT_VARIABLE COMPILER_OUTPUT
+							ERROR_VARIABLE COMPILER_OUTPUT
                             COMMAND ${CMAKE_COMMAND}
                             --build ${CMAKE_BINARY_DIR}/CMakeTmp/CompilerTest)
       endif()
