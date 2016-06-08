a1dae47b85b471cac4ca54f06a17b28a711f93be
Bill Williams <bill@cs.wisc.edu>
2016-06-08 13:19:09 -0500

    remove mallocs from the RTlib's os_malloc call when we're on the mmap path

--- dyninstAPI_RT/src/RTposix.c
+++ dyninstAPI_RT/src/RTposix.c
@@ -242,10 +242,18 @@ try_again:
   return 0;
 }
 
+// Important note: addr will be zero in two cases here
+// One is the case where we're doing a constrained low mmap, in which case MAP_32BIT
+// is precisely correct. The other is the case where our
+// constrained map attempts have failed, and we're doing a scan for first available
+// mappable page. In that case, MAP_32BIT does no harm.
 void *map_region(void *addr, int len, int fd) {
      void *result;
-     result = mmap(addr, len, PROT_READ|PROT_WRITE|PROT_EXEC, 
-                   DYNINSTheap_mmapFlags, fd, 0);
+    int flags = DYNINSTheap_mmapFlags;
+    if(addr == 0) flags |= MAP_32BIT;
+     result = mmap(addr, len, PROT_READ|PROT_WRITE|PROT_EXEC,
+                   flags, fd, 0);
+                   flags, fd, 0);
      if (result == MAP_FAILED)
          return NULL;
      return result;
