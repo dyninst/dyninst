a575ab3dd1528d3d23aa69d76aeb2cbfa55e2d2b
Blair Archibald <b.archibald.1@research.gla.ac.uk>
2016-09-12 13:56:11 +0100

    Fix operand types for vpand
    The intel developer manual specifies vpand as having operands "xmm1 xmm2
    xmm3/m128". This change reflects this in dyninst.

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -6526,7 +6526,7 @@ ia32_entry sseMapMult[][3] =
     { e_vpminub, t_done, 0, true, { Vps, Hps, Wps }, 0, s1W2R3R },
     { e_vpminub, t_done, 0, true, { Vps, Hps, Wps }, 0, s1W2R3R },
   }, { /* SSEDB_66 */
-    { e_vpand, t_done, 0, true, { Vps, Hps, Wps }, 0, s1RW2R },
+    { e_vpand, t_done, 0, true, { Vps, Hps, Wps }, 0, s1RW2R3R },
     { e_No_Entry, t_vexw, VEXW91, false, { Zz, Zz, Zz }, 0, 0 },
     { e_No_Entry, t_vexw, VEXW64, false, { Zz, Zz, Zz }, 0, 0 }
   }, { /* SSEDC_66 */
