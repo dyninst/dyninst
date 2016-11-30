f158e967c532e8f171094e965286f883bbccc3a8
Bill Williams <bill@cs.wisc.edu>
2016-11-30 14:02:06 -0600

    Added interface to query whether a module already has address range information, and reindented--this had become a ragged mess of 3 space/4 space/tab indents. Fixed merge issues from architecture auto-detection.

--- symtabAPI/src/Object-elf.C
+++ symtabAPI/src/Object-elf.C
@@ -1105,7 +1105,7 @@ bool Object::get_relocation_entries( Elf_X_Shdr *&rel_plt_scnp,
 
             } else if (plt_entry_size_ == 16) {
                 // New style secure PLT
-                Region *plt = NULL, /* *relplt = NULL, */ *dynamic = NULL,
+                Region *plt = NULL, *relplt = NULL, *dynamic = NULL,
                         *got = NULL, *glink = NULL;
                 unsigned int glink_addr = 0;
                 unsigned int stub_addr = 0;
@@ -1116,7 +1116,7 @@ bool Object::get_relocation_entries( Elf_X_Shdr *&rel_plt_scnp,
                 for (unsigned iter = 0; iter < regions_.size(); ++iter) {
                     std::string name = regions_[iter]->getRegionName();
                     if (name == PLT_NAME) plt = regions_[iter];
-                    // else if (name == REL_PLT_NAME) relplt = regions_[iter];
+                    else if (name == REL_PLT_NAME) relplt = regions_[iter];
                     else if (name == DYNAMIC_NAME) dynamic = regions_[iter];
                     else if (name == GOT_NAME) got = regions_[iter];
                 }
