a00056bec6a0265afb592944dde2ff461b525e8d
John Mellor-Crummey <johnmc@rice.edu>
2017-11-24 12:34:45 -0600

    add support for MCS locks and phase-fair reader writer locks

---
+++ common/h/mcs-lock.h
@@ -0,0 +1,80 @@
+//***************************************************************************
+//
+// File:
+//   mcs_lock.h
+//
+// Purpose:
+//   Define an API for the MCS lock: a fair queue-based lock.
+//
+// Reference:
+//   John M. Mellor-Crummey and Michael L. Scott. 1991. Algorithms for scalable
+//   synchronization on shared-memory multiprocessors. ACM Transactions on
+//   Computing Systems 9, 1 (February 1991), 21-65.
+//   http://doi.acm.org/10.1145/103727.103729
+//***************************************************************************
+
+
+
+#ifndef _mcs_lock_h_
+#define _mcs_lock_h_
+
+//******************************************************************************
+// global includes
+//******************************************************************************
+
+#include <atomic>
+
+
+
+//******************************************************************************
+// local includes
+//******************************************************************************
+
+#include "util.h"
+
+
+
+//******************************************************************************
+// types
+//******************************************************************************
+
+typedef struct mcs_node_s {
+  std::atomic<struct mcs_node_s*> next;
+  std::atomic<bool> blocked;
+} mcs_node_t;
+
+
+typedef struct {
+  std::atomic<mcs_node_t *> tail;
+} mcs_lock_t;
+
+
+
+//******************************************************************************
+// constants
+//******************************************************************************
+
+#define mcs_nil (struct mcs_node_s*) 0
+
+//******************************************************************************
+// interface functions
+//******************************************************************************
+
+static inline void
+mcs_init(mcs_lock_t &l)
+{
+  atomic_init(&l.tail, mcs_nil);
+}
+
+COMMON_EXPORT void
+mcs_lock(mcs_lock_t &l, mcs_node_t &me);
+
+
+COMMON_EXPORT bool
+mcs_trylock(mcs_lock_t &l, mcs_node_t &me);
+
+
+COMMON_EXPORT void
+mcs_unlock(mcs_lock_t &l, mcs_node_t &me);
+
+#endif
