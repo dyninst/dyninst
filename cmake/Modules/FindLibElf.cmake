364ac431248f9a6fe345b9c6b4b8913a5d73c7d9
Tim <thaines.astro@gmail.com>
2019-03-01 12:52:59 -0600

    Add version checking
    find_package_handle_standard_args doesn't check versions of shared
    libraries, so we need to do it manually.

--- cmake/Modules/FindLibElf.cmake
+++ cmake/Modules/FindLibElf.cmake
@@ -51,13 +51,34 @@ find_library (LIBELF_LIBRARIES
       ENV LIBRARY_PATH
       ENV LD_LIBRARY_PATH)
 
-include (FindPackageHandleStandardArgs)
-
-
-# handle the QUIETLY and REQUIRED arguments and set LIBELF_FOUND to TRUE if all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibElf DEFAULT_MSG
-    LIBELF_LIBRARIES
-    LIBELF_INCLUDE_DIR)
-
-
-# mark_as_advanced(LIBELF_INCLUDE_DIRS LIBELF_LIBRARIES)
+# Enforce required version, if enabled
+if(LibElf_FIND_VERSION AND LIBELF_LIBRARIES)
+	get_filename_component(LibElf_realpath ${LIBELF_LIBRARIES} REALPATH)
+	string(REGEX MATCH "libelf\\-(.+)\\.so$" res ${LibElf_realpath})
+	
+	# The library version number is stored in CMAKE_MATCH_1
+	set(LibELf_version ${CMAKE_MATCH_1})
+	
+	if(NOT res OR NOT ${LibELf_version})
+		message(WARNING "Found libelf library '${LibElf_realpath}', but does not match known name format")
+		set(LibElf_FOUND FALSE)
+		set(LIBELF_FOUND FALSE)
+		return()
+	endif()
+	
+	if(${LibELf_version} VERSION_LESS ${LibElf_FIND_VERSION})
+		message(WARNING "Found libelf version '${LibELf_version}', but required version '${LibElf_FIND_VERSION}'")
+		set(LibElf_FOUND FALSE)
+		set(LIBELF_FOUND FALSE)
+		return()
+	endif()
+	set(LIBELF_FOUND TRUE)
+	set(LibElf_FOUND TRUE)
+	message(STATUS "Found libelf '${LibELf_version}' in '${LibElf_realpath}'")
+	return()
+else()
+	include (FindPackageHandleStandardArgs)
+	
+	#handle the QUIETLY and REQUIRED arguments and set LIBELF_FOUND to TRUE if all listed variables are TRUE
+	find_package_handle_standard_args(LibElf LIBELF_LIBRARIES LIBELF_INCLUDE_DIR)
+endif()
