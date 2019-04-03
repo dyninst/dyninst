c8e3d2f41288ee3228502376c91fce6d1162a85a
Tim <thaines.astro@gmail.com>
2019-04-03 17:01:38 -0500

    Update comments

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -27,9 +27,11 @@ set(Boost_USE_STATIC_RUNTIME OFF CACHE STRING
 # Disable auto-linking
 add_definitions(-DBOOST_ALL_NO_LIB=1)
 
+# Disable generating serialization code in boost::multi_index
+add_definitions(-DBOOST_MULTI_INDEX_DISABLE_SERIALIZATION)
+
 # There are broken versions of MSVC that won't handle variadic templates
-# correctly (despite the C++11 test case passing). Just build vanilla versions,
-# boost can handle it.
+# correctly (despite the C++11 test case passing).
 if(MSVC)
   add_definitions(-DBOOST_NO_CXX11_VARIADIC_TEMPLATES)
 endif()
