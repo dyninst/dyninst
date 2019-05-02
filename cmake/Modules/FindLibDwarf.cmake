227677a69185330f4a8bb7cd9d4bf91425808831
Tim Haines <thaines.astro@gmail.com>
2019-05-02 15:54:12 -0500

    Elfutils cleanup (#591)
    fixes #557
    fixes #547
    fixes #561
    
    - Consolidate all elfutils options into cmake/ElfUtils.cmake
    - Allow user to supply installation hint directories
    - Allow user to specify minimum version
    - Use user-specified minimum version when building from source instead of 'elfutils-latest.tar.gz'
    - Unify system paths when searching for libelf and libdw (fixes #557)
    - Correctly install headers/libs when building from source (fixes #547)
    - Exports variables for use in Testsuite (fixes #561)
    - Clean up formatting

--- cmake/Modules/FindLibDwarf.cmake
+++ cmake/Modules/FindLibDwarf.cmake
@@ -1,52 +1,90 @@
-# - Try to find libdwarf
-# Once done this will define
+#===================================================================================
+# FindLibDwarf.cmake
 #
-#  LIBDWARF_FOUND - system has libdwarf
-#  LIBDWARF_INCLUDE_DIRS - the libdwarf include directory
-#  LIBDWARF_LIBRARIES - Link these to use libdwarf
-#  LIBDWARF_DEFINITIONS - Compiler switches required for using libdwarf
+# Find libdw include dirs and libraries
 #
+#		----------------------------------------
+#
+# Use this module by invoking find_package with the form::
+#
+#  find_package(LibDwarf
+#    [version] [EXACT]      # Minimum or EXACT version e.g. 0.173
+#    [REQUIRED]             # Fail with error if libdw is not found
+#  )
+#
+# This module reads hints about search locations from variables::
+#
+#	LibDwarf_ROOT_DIR		- Base directory the of libdw installation
+#	LibDwarf_INCLUDEDIR		- Hint directory that contains the libdw headers files
+#	LibDwarf_LIBRARYDIR		- Hint directory that contains the libdw library files
+#
+# and saves search results persistently in CMake cache entries::
+#
+#	LibDwarf_FOUND			- True if headers and requested libraries were found
+#	LibDwarf_INCLUDE_DIRS 	- libdw include directories
+#	LibDwarf_LIBRARY_DIRS	- Link directories for libdw libraries
+#	LibDwarf_LIBRARIES		- libdw library files
+#
+#===================================================================================
 
+include(DyninstSystemPaths)
 
-if (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
-  set (LibDwarf_FIND_QUIETLY TRUE)
-endif (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
+# Non-standard subdirectories to search
+set(_path_suffixes libdw libdwarf elfutils)
 
-find_path (LIBDWARF_INCLUDE_DIR
-  NAMES
-  elfutils/libdw.h
-  HINTS
-  ${LIBDWARF_INCLUDE_DIRS}
-  PATHS
-  /usr/include
-  /usr/local/include
-  /opt/local/include
-  /sw/include
-  ENV CPATH) # PATH and INCLUDE will also work
+find_path(LibDwarf_INCLUDE_DIR
+          NAMES libdw.h
+          HINTS ${LibDwarf_ROOT_DIR}/include ${LibDwarf_ROOT_DIR} ${LibDwarf_INCLUDEDIR}
+          PATHS ${DYNINST_SYSTEM_INCLUDE_PATHS}
+          PATH_SUFFIXES ${_path_suffixes}
+          DOC "libdw include directories")
 
-find_library (LIBDWARF_LIBRARIES
-  NAMES
-  dw
-  HINTS
-  ${LIBDWARF_LIBRARIES}
-  PATHS
-  /usr/lib
-  /usr/lib64
-  /usr/local/lib
-  /usr/local/lib64
-  /opt/local/lib
-  /opt/local/lib64
-  /sw/lib
-  ENV LIBRARY_PATH   # PATH and LIB will also work
-  ENV LD_LIBRARY_PATH)
-include (FindPackageHandleStandardArgs)
+find_library(LibDwarf_LIBRARIES
+             NAMES libdw.so.1 libdw.so
+             HINTS ${LibDwarf_ROOT_DIR}/lib ${LibDwarf_ROOT_DIR} ${LibDwarf_LIBRARYDIR}
+             PATHS ${DYNINST_SYSTEM_LIBRARY_PATHS}
+             PATH_SUFFIXES ${_path_suffixes})
 
+# Find the library with the highest version
+set(_max_ver 0.0)
+set(_max_ver_lib)
+foreach(l ${LibDwarf_LIBRARIES})
+  get_filename_component(_dw_realpath ${LibDwarf_LIBRARIES} REALPATH)
+  string(REGEX MATCH
+               "libdw\\-(.+)\\.so\\.*$"
+               res
+               ${_dw_realpath})
 
-# handle the QUIETLY and REQUIRED arguments and set LIBDWARF_FOUND to TRUE
-# if all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibDwarf DEFAULT_MSG
-  LIBDWARF_LIBRARIES
-  LIBDWARF_INCLUDE_DIR)
+  # The library version number is stored in CMAKE_MATCH_1
+  set(_cur_ver ${CMAKE_MATCH_1})
 
-#mark_as_advanced(LIBDW_INCLUDE_DIR DWARF_INCLUDE_DIR)
-#mark_as_advanced(LIBDWARF_INCLUDE_DIRS LIBDWARF_LIBRARIES)
+  if(${_cur_ver} VERSION_GREATER ${_max_ver})
+    set(_max_ver ${_cur_ver})
+    set(_max_ver_lib ${l})
+  endif()
+endforeach()
+
+# Set the exported variables to the best match
+set(LibDwarf_LIBRARIES ${_max_ver_lib})
+set(LibDwarf_VERSION ${_max_ver})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibDwarf
+                                  FOUND_VAR
+                                  LibDwarf_FOUND
+                                  REQUIRED_VARS
+                                  LibDwarf_LIBRARIES
+                                  LibDwarf_INCLUDE_DIR
+                                  VERSION_VAR
+                                  LibDwarf_VERSION)
+
+# Export cache variables
+if(LibDwarf_FOUND)
+  set(LibDwarf_INCLUDE_DIRS ${LibDwarf_INCLUDE_DIR})
+  set(LibDwarf_LIBRARIES ${LibDwarf_LIBRARIES})
+
+  # Because we only report the library with the largest version, we are
+  # guaranteed there is only one file in LibDwarf_LIBRARIES
+  get_filename_component(_dw_dir ${LibDwarf_LIBRARIES} DIRECTORY)
+  set(LibDwarf_LIBRARY_DIRS ${_dw_dir})
+endif()
--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,39 +1,4 @@
 if (UNIX)
-  find_package (LibDwarf)
-  find_package (LibElf 0.173)
-  if(NOT LIBELF_FOUND OR NOT LIBDWARF_FOUND)
-    message(STATUS "Attempting to build elfutils as external project")
-    cmake_minimum_required (VERSION 2.8.11)
-    include(ExternalProject)
-    ExternalProject_Add(LibElf
-      PREFIX ${CMAKE_BINARY_DIR}/elfutils
-      URL https://sourceware.org/elfutils/ftp/elfutils-latest.tar.bz2
-      CONFIGURE_COMMAND CFLAGS=-g <SOURCE_DIR>/configure --enable-install-elfh --enable-shared --prefix=${CMAKE_BINARY_DIR}/elfutils
-      BUILD_COMMAND make
-      INSTALL_COMMAND make install
-      )
-    set(LIBELF_INCLUDE_DIR ${CMAKE_BINARY_DIR}/elfutils/include)
-    set(LIBELF_LIBRARIES ${CMAKE_BINARY_DIR}/elfutils/lib/libelf.so)
-    set(LIBDWARF_INCLUDE_DIR ${CMAKE_BINARY_DIR}/elfutils/include)
-    set(LIBDWARF_LIBRARIES ${CMAKE_BINARY_DIR}/elfutils/lib/libdw.so)
-    set(SHOULD_INSTALL_LIBELF 1)
-  else()
-    set(SHOULD_INSTALL_LIBELF 0)
-  endif()
-  add_library(libelf_imp SHARED IMPORTED)
-  set_property(TARGET libelf_imp
-    PROPERTY IMPORTED_LOCATION ${LIBELF_LIBRARIES})
-  if(NOT LIBELF_FOUND)
-    add_dependencies(libelf_imp LibElf)
-  endif()
-
-  add_library(libdwarf_imp SHARED IMPORTED)
-  set_property(TARGET libdwarf_imp 
-    PROPERTY IMPORTED_LOCATION ${LIBDWARF_LIBRARIES})
-  if(NOT LIBDWARF_FOUND)
-    add_dependencies(libdwarf_imp LibDwarf)
-  endif()
-
   if (NOT USE_GNU_DEMANGLER)
     find_package (LibIberty)
 
@@ -63,10 +28,6 @@ if (UNIX)
   endif()
 
   find_package (ThreadDB)
-  include_directories (
-    ${LIBELF_INCLUDE_DIR}
-    ${LIBDWARF_INCLUDE_DIR}
-    )
 endif()
 
 if (PLATFORM MATCHES "bgq")
