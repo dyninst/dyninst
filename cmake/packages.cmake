f8b94f51ebac1742de94f90be0b217225f6b570d
Tim <thaines.astro@gmail.com>
2019-06-13 17:34:27 -0500

    Delete old C++11 feature-checking CMake files

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -5,13 +5,3 @@ if (PLATFORM MATCHES "bgq")
     include_directories (${PATH_BGQ})
   endif()
 endif()
-
-
-include(${DYNINST_ROOT}/cmake/CheckCXX11Features.cmake)
-if(NOT HAS_CXX11_AUTO)
-  message(FATAL_ERROR "No support for C++11 auto found. Dyninst requires this compiler feature.")
-else()
-  message(STATUS "C++11 support found, required flags are: ${CXX11_COMPILER_FLAGS}")
-endif()
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX11_COMPILER_FLAGS}")
