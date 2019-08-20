e0cd4dd242f06adaa6e606da2cbbbf51130e88b2
Xiaozhu Meng <mxz297@gmail.com>
2019-08-20 17:20:50 -0500

    Fix loop tree callee construction on Power

--- parseAPI/src/LoopAnalyzer.C
+++ parseAPI/src/LoopAnalyzer.C
@@ -121,6 +121,23 @@ void LoopAnalyzer::createLoopHierarchy()
           Function* callee = target->obj()->findFuncByEntry(target->region(), target->start());
           if (callee)
               insertCalleeIntoLoopHierarchy(callee, b->last());
+          if (b->obj()->cs()->getArch() == Arch_ppc64) {
+              // Since Power 8, the new ABI will typically create two entries for a function
+              callee = target->obj()->findFuncByEntry(target->region(), target->start() - 8);
+              if (callee == NULL) continue;
+              // Make sure that the function entry block indeed aligns with the call target
+              if (callee->entry()->end() != target->start()) continue;
+              // Make sure that the function entry block will fall through to the call target
+              bool findFT = false;
+              for (auto eit = callee->entry()->targets().begin(); eit != callee->entry()->targets().end(); ++eit) {
+                  if ((*eit)->type() == FALLTHROUGH) {
+                      findFT = true;
+                      break;
+                  }
+              }
+              if (findFT)
+                  insertCalleeIntoLoopHierarchy(callee, b->last());
+          }
       }
     }
   }
