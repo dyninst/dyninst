aca6cd81d93b8e33ebc7177951a1e26472446e31
Jokeren <robinho364@gmail.com>
2019-10-20 08:44:03 -0500

    Add cuda registers and ops

--- common/h/entryIDs.h
+++ common/h/entryIDs.h
@@ -3002,6 +3002,8 @@ power_op_dxex,
   aarch64_op_yield_hint,
   aarch64_op_zip1_advsimd,
   aarch64_op_zip2_advsimd,
+  cuda_op_general,
+  cuda_op_call,
   _entry_ids_max_
 };
 enum prefixEntryID : unsigned int {
