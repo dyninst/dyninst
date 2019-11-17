18364c8a6160925f582ffa5bc37b24cc54854d45
Sasha Nícolas <sasha@cs.wisc.edu>
2019-11-16 20:28:17 -0600

    Parse dwarf variables with abstract origin attribute (#642)

--- symtabAPI/src/dwarfWalker.C
+++ symtabAPI/src/dwarfWalker.C
@@ -892,6 +892,15 @@ bool DwarfWalker::parseVariable() {
       }
    }
 
+   /* If the DIE has an _abstract_origin, we'll use that for the
+            remainder of our inquiries. */
+   bool hasAbstractOrigin;
+   if (!handleAbstractOrigin(hasAbstractOrigin)) return false;
+   if (hasAbstractOrigin) {
+          // Clone to spec entry too
+          setSpecEntry(abstractEntry());
+   }
+
    Type *type = NULL;
    if (!findType(type, false))
        return false;
