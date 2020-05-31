952dae51a3b9ce9a84e20ea30f91ffe742060436
Tim Haines <thaines.astro@gmail.com>
2020-05-31 18:36:07 -0500

    C++ cleanup (#610)
    * Delete old C++11 feature-checking CMake files
    
    * Move C++ ABI check into cmake/c++11.cmake
    
    * Use built-in CMake features for detecting C and C++ language support
    
    * Rename cmake/c++11.cmake -> cmake/language_features.cmake
    
    * Rename cmake/language_features.cmake -> cmake/LanguageStandards.cmake
    
    * Use the LanguageStandards CMake module to set the C and C++ standards
    
    * Add comments for CMake third-party library includes
    
    * Whitespace cleanup in cmake/LanguageStandards.cmake

---
+++ cmake/LanguageStandards.cmake
@@ -0,0 +1,43 @@
+#=========================================================================
+# LanguageStandards.cmake
+#
+# Configure C++ and C Language API and ABI standards for Dyninst
+#
+#   ----------------------------------------
+#
+# Accepts the following CMake variables
+#
+# USE_CXX11_ABI - Enable using the GNU C++11 ABI (aka, the post gcc-5 ABI)
+#
+#=========================================================================
+
+#
+# --------  C++ language features ----------------
+#
+
+# Disable compiler-specific C++ language extensions (e.g., gnu++11)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+# Require C++11 support
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set(USE_CXX11_ABI "" CACHE STRING "Override the default GNU C++11 ABI setting")
+if(NOT ("${USE_CXX11_ABI}" STREQUAL ""))
+  if(${USE_CXX11_ABI})
+    add_definitions(-D_GLIBCXX_USE_CXX11_ABI=1)
+  else()
+    add_definitions(-D_GLIBCXX_USE_CXX11_ABI=0)
+  endif()
+endif()
+
+#
+# --------  C language features ----------------
+#
+
+# Disable compiler-specific C language extensions (e.g., gnu99)
+set(CMAKE_C_EXTENSIONS OFF)
+
+# Require C99 support
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED ON)
