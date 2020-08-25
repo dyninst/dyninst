af02e34b4140623fab4e5514697ef5ded499f067
Tim Haines <thaines.astro@gmail.com>
2020-08-25 17:43:13 -0500

    Remove cotire
    Versions 9.3.x used cotire, but have no knob to turn it off. Cotire has
    no real use for one-time builds and can break parallel builds with both
    static and shared libs.

--- cmake/ElfUtils.cmake
+++ cmake/ElfUtils.cmake
@@ -154,7 +154,3 @@ include_directories(${ElfUtils_INCLUDE_DIRS})
 message(STATUS "ElfUtils includes: ${ElfUtils_INCLUDE_DIRS}")
 message(STATUS "ElfUtils library dirs: ${ElfUtils_LIBRARY_DIRS}")
 message(STATUS "ElfUtils libraries: ${ElfUtils_LIBRARIES}")
-
-if(USE_COTIRE)
-  cotire(ElfUtils)
-endif()
