dyninst/dyninst

commit 3f55f75bb2c41a6e71a6d682f9638ddd0a8233b1

cleanup memoryTracker memory handling (#876)
* Use a unique_ptr to manage object
* Use inline initializer for public members
     This makes the constructors simpler.
* Whitespace cleanup
* Use new/delete to fix realloc

dyninstAPI/src/binaryEdit.h
@@ -34,8 +34,7 @@
 #define BINARY_H
 
 #include <map>
-#include <functional>
-#include <queue>
+#include <memory>
 
 #include "infHeap.h"
 #include "addressSpace.h"
@@ -248,44 +247,46 @@ class depRelocation {
 };
 
 class memoryTracker : public codeRange {
- public:
-    memoryTracker(Address a, unsigned s) :
-        alloced(false),  dirty(false), a_(a), s_(s) {
-        b_ = malloc(s_);
+public:
+  memoryTracker(Address a, unsigned s) : memoryTracker(a, s, nullptr) {}
+
+  memoryTracker(Address a, unsigned s, void *b)
+      : a_(a), s_(s) {
+    b_.reset(new char[s_]);
+    if (b) {
+      memcpy(b_.get(), b, s_);
     }
+  }
+  ~memoryTracker() = default;
 
-    memoryTracker(Address a, unsigned s, void *b) :
-    alloced(false), dirty(false), a_(a), s_(s)
-        {
-            if(b) {
-                b_ = malloc(s_);
-                memcpy(b_, b, s_);
-            } else {
-                b_ = calloc(1, s_);
-            }
-        }
-    ~memoryTracker() { free(b_); }
+  // Not copyable
+  memoryTracker(memoryTracker const &) = delete;
+  memoryTracker &operator=(memoryTracker const &) = delete;
 
-    Address get_address() const { return a_; }
-    unsigned get_size() const { return s_; }
-    void *get_local_ptr() const { return b_; }
-    void realloc(unsigned newsize) {
-      b_ = ::realloc(b_, newsize);
-      s_ = newsize;
-      if (!b_ && newsize) {
-	cerr << "Odd: failed to realloc " << newsize << endl;
-	assert(b_);
-      }
+  // move-only
+  memoryTracker(memoryTracker &&) = default;
+  memoryTracker &operator=(memoryTracker &&) = default;
+
+  Address get_address() const { return a_; }
+  unsigned get_size() const { return s_; }
+  void *get_local_ptr() const { return static_cast<void*>(b_.get()); }
+  void realloc(unsigned newsize) {
+    if(newsize == s_) return;
+    b_.reset(new char[newsize]);
+    s_ = newsize;
+    if (!b_ && newsize) {
+      cerr << "Odd: failed to realloc " << newsize << endl;
+      assert(b_);
     }
+  }
 
-    bool alloced;
-    bool dirty;
+  bool alloced{false};
+  bool dirty{false};
 
- private:
-    Address a_;
-    unsigned s_;
-    void *b_;
-    
+private:
+  Address a_;
+  unsigned s_;
+  std::unique_ptr<char[]> b_;
 };
 
 #endif // BINARY_H

commit e2187576b0c821548f4d5a69f0c0a70f6a00e1d3

Ensure data is copied in memoryTracker::realloc
This bug was introduced by #876.

dyninstAPI/src/binaryEdit.h
@@ -271,13 +271,15 @@ public:
   unsigned get_size() const { return s_; }
   void *get_local_ptr() const { return static_cast<void*>(b_.get()); }
   void realloc(unsigned newsize) {
-    if(newsize == s_) return;
-    b_.reset(new char[newsize]);
-    s_ = newsize;
-    if (!b_ && newsize) {
-      cerr << "Odd: failed to realloc " << newsize << endl;
-      assert(b_);
+    if(newsize <= s_) {
+    	// No need to fiddle with the data, just change the size
+    	s_ = newsize;
+    	return;
     }
+    auto *ptr = new char[newsize];
+    std::copy(b_.get(), b_.get()+s_, ptr);
+    b_.reset(ptr);
+    s_ = newsize;
   }
 
   bool alloced{false};