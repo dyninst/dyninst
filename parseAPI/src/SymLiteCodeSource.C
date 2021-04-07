fde464a26f8091a6feeb0d1dd7dfc32174ff3cb5
Tim Haines <thaines.astro@gmail.com>
2021-04-07 14:28:55 -0500

    Update copyrights to 2021 (#1015)
    NB: Only the copyrights explicitly belonging to Dyninst were updated

--- parseAPI/src/SymLiteCodeSource.C
+++ parseAPI/src/SymLiteCodeSource.C
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996-2011 Barton P. Miller
+ * Copyright (c) 1996-2021 Barton P. Miller
  * 
  * We provide the Paradyn Parallel Performance Tools (below
  * described as "Paradyn") on an AS IS basis, and do not warrant its
