b96fa72a026eb703a4fc742c08180a47b01d914c
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2021-09-17 20:43:36 -0500

    Format doc/examples/test

--- patchAPI/test/findPoint/mutatee/main.c
+++ patchAPI/test/findPoint/mutatee/main.c
@@ -1,28 +1,28 @@
 /*
  * See the dyninst/COPYRIGHT file for copyright information.
- * 
+ *
  * We provide the Paradyn Tools (below described as "Paradyn")
  * on an AS IS basis, and do not warrant its validity or performance.
  * We reserve the right to update, modify, or discontinue this
  * software at any time.  We shall have no obligation to supply such
  * updates or modifications or any other form of support to you.
- * 
+ *
  * By your use of Paradyn, you understand and agree that we (or any
  * other person or entity with proprietary rights in Paradyn) are
  * under no obligation to provide either maintenance services,
  * update services, notices of latent defects, or correction of
  * defects for Paradyn.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -30,17 +30,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void foo1() {
-	printf("i'm foo1\n");
+void
+foo1()
+{
+    printf("i'm foo1\n");
 }
-void foo2() {
-	printf("i'm foo2\n");
+void
+foo2()
+{
+    printf("i'm foo2\n");
 }
 
-int main(int argc, const char *argv[])
+int
+main(int argc, const char* argv[])
 {
-  if (argc > 2) return 0;
-  foo1();
-  foo2();
-	return 0;
+    if(argc > 2)
+        return 0;
+    foo1();
+    foo2();
+    return 0;
 }
