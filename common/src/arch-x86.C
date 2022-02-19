30efedf407c70a39da9e9da1f109ba9eae6938ee
Tim Haines <thaines.astro@gmail.com>
2022-02-18 21:24:55 -0600

    Remove redundant 'vblendvpd'

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -1483,7 +1483,6 @@ COMMON_EXPORT dyn_hash_map<entryID, std::string> entryNames_IAPI = map_list_of
   (e_vblendps, "vblendps")
   (e_vblendvpd, "vblendvpd")
   (e_vblendvps, "vblendvps")
-  (e_vblendvpd, "vblendvpd")
   (e_vpblendmb, "vpblendmb")
   (e_vpblendmw, "vpblendmw")
   (e_vpblendvb, "vpblendvb")
