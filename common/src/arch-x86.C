35ba9dccddcb1f44a95ff1974c8c40855b5d0957
Tim Haines <thaines.astro@gmail.com>
2022-02-18 21:32:34 -0600

    Remove redundant 'vpmovsqw'

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -2020,7 +2020,6 @@ COMMON_EXPORT dyn_hash_map<entryID, std::string> entryNames_IAPI = map_list_of
 
 /* What are these? */
  (e_vpmovswb, "vpmovswb")
- (e_vpmovsqw, "vpmovsqw")
 
  (e_fp_generic, "[FIXME: GENERIC FPU INSN]")
  (e_3dnow_generic, "[FIXME: GENERIC 3DNow INSN]")
