3a433cf7f1976c58c212ce7ee1f57a3d25eb1b55
Tim Haines <thaines.astro@gmail.com>
2022-02-18 21:26:15 -0600

    Remove redundant 'vcmpsd'

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -1982,7 +1982,6 @@ COMMON_EXPORT dyn_hash_map<entryID, std::string> entryNames_IAPI = map_list_of
   (e_ktestd, "ktestd")
   (e_ktestw, "ktestw")
   (e_ktestq, "ktestq")
-  (e_vcmpsd, "vcmpsd")
   (e_vcmpss, "vcmpss")
   (e_vmovntpd, "vmovntpd")
   (e_vcvttsd2usi, "vcvttsd2usi")
