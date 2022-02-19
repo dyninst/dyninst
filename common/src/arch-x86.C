2cfc41c5f2f6b8f33ddafcef7575c258264579ff
Tim Haines <thaines.astro@gmail.com>
2022-02-18 21:30:28 -0600

    Remove redundant 'vpmovsdb'

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -2020,7 +2020,6 @@ COMMON_EXPORT dyn_hash_map<entryID, std::string> entryNames_IAPI = map_list_of
 
 /* What are these? */
  (e_vpmovswb, "vpmovswb")
- (e_vpmovsdb, "vpmovsdb")
  (e_vpmovsqb, "vpmovsqb")
  (e_vpmovsdw, "vpmovsdw")
  (e_vpmovsqw, "vpmovsqw")
