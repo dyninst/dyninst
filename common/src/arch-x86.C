b6e85e261febb09fdb30ff42727eaa4e1ba6381b
Tim Haines <thaines.astro@gmail.com>
2022-02-18 21:14:36 -0600

    Fix spelling of 'vblendpd' mnemonic

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -1485,7 +1485,7 @@ COMMON_EXPORT dyn_hash_map<entryID, std::string> entryNames_IAPI = map_list_of
   (e_vblendmps, "vblendmps")
   (e_vblendmpd, "vblendmpd")
   (e_vblendps, "vblendps")
-  (e_vblendvpd, "vblendpd")
+  (e_vblendvpd, "vblendvpd")
   (e_vblendvps, "vblendvps")
   (e_vblendvpd, "vblendvpd")
   (e_vpblendmb, "vpblendmb")
