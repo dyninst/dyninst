10639343a20dbdb2658509653c74aa07d8063fb6
Tim Haines <thaines.astro@gmail.com>
2022-03-03 22:47:26 -0600

    Use CMake's -S instead of undocumented -H

--- docker/build.sh
+++ docker/build.sh
@@ -12,7 +12,7 @@ mkdir -p build/dyninst
 printf "⭐️ Preparing to build Dyninst\n"
 echo "::group::build dyninst"   
 cd build/dyninst
-cmake -H/code -B. -DCMAKE_INSTALL_PREFIX=. > >(tee config.out) 2> >(tee config.err >&2)
+cmake -S /code -B. -DCMAKE_INSTALL_PREFIX=. > >(tee config.out) 2> >(tee config.err >&2)
 make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
 make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
 echo "::endgroup::"
@@ -32,7 +32,7 @@ cd /opt/dyninst-env/
 mkdir -p build/testsuite/tests
 cd build/testsuite
 
-cmake -H/opt/testsuite -B. -DCMAKE_INSTALL_PREFIX=$PWD/tests -DDyninst_DIR=/opt/dyninst-env/build/dyninst/lib/cmake/Dyninst > >(tee config.out) 2> >(tee config.err >&2)
+cmake -S /opt/testsuite -B. -DCMAKE_INSTALL_PREFIX=$PWD/tests -DDyninst_DIR=/opt/dyninst-env/build/dyninst/lib/cmake/Dyninst > >(tee config.out) 2> >(tee config.err >&2)
 make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
 make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
 echo "::endgroup::"
