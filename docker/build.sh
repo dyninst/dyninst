9b5ffe39187cc0de148a7ca686c1548396e496c4
Tim Haines <thaines.astro@gmail.com>
2022-03-07 17:37:50 -0600

    Docker: simplify build script (#1212)
    * Use CMake's -S instead of undocumented -H
    
    * Remove log captures
    
    The reports are best used within the context of the PR, so storing them
    in the dashboard wouldn't be much use.
    
    * Reduce duplication of directory names
    
    * Use CMake commands to build/install
    
    * Separate build and install trees for Dyninst
    
    * Separate build and install directories for test suite
    
    * Simplify 'git pull' for test suite
    
    * Explicitly override test suite install directory
    
    The test suite reads Dyninst's cache which poisons the CMAKE_INSTALL_PREFIX. This overrides that and then puts the test files in the root install directory.

--- docker/build.sh
+++ docker/build.sh
@@ -6,33 +6,41 @@ set -euo pipefail
 printf "⭐️ Setting up spack environment for Dyninst\n"
 . /opt/spack/share/spack/setup-env.sh
 spack env activate .
-mkdir -p build/dyninst
 
 # 1. Build Dyninst
 printf "⭐️ Preparing to build Dyninst\n"
-echo "::group::build dyninst"   
-cd build/dyninst
-cmake -H/code -B. -DCMAKE_INSTALL_PREFIX=. > >(tee config.out) 2> >(tee config.err >&2)
-make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
-make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
+echo "::group::build dyninst"
+
+DYNINST_BUILD_DIR=/opt/dyninst-env/build/dyninst
+mkdir -p $DYNINST_BUILD_DIR
+
+DYNINST_INSTALL_DIR=/opt/dyninst-env/install/dyninst
+mkdir -p $DYNINST_INSTALL_DIR
+
+cmake -S /code -B $DYNINST_BUILD_DIR -DCMAKE_INSTALL_PREFIX=$DYNINST_INSTALL_DIR
+cmake --build $DYNINST_BUILD_DIR -- -j2
+cmake --install $DYNINST_BUILD_DIR
+
 echo "::endgroup::"
 
 # 2. Update the test suite
 printf "⭐️ Updating the testsuite\n"
 echo "::group::update testsuite"   
-cd /opt/testsuite
-git pull origin master
-cd -
+git -C /opt/testsuite pull origin master
 echo "::endgroup::"
 
 # 3. Build the test suite
 printf "⭐️ Preparing to build the testsuite\n"
-echo "::group::build tests"   
-cd /opt/dyninst-env/
-mkdir -p build/testsuite/tests
-cd build/testsuite
+echo "::group::build tests"
 
-cmake -H/opt/testsuite -B. -DCMAKE_INSTALL_PREFIX=$PWD/tests -DDyninst_DIR=/opt/dyninst-env/build/dyninst/lib/cmake/Dyninst > >(tee config.out) 2> >(tee config.err >&2)
-make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
-make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
+TESTSUITE_BUILD_DIR=/opt/dyninst-env/build/testsuite
+mkdir -p $TESTSUITE_BUILD_DIR
+
+TESTSUITE_INSTALL_DIR=/opt/dyninst-env/install/testsuite
+mkdir -p $TESTSUITE_INSTALL_DIR
+
+cmake -S /opt/testsuite -B $TESTSUITE_BUILD_DIR -DCMAKE_INSTALL_PREFIX=$TESTSUITE_INSTALL_DIR -DDyninst_DIR=$DYNINST_INSTALL_DIR/lib/cmake/Dyninst
+cmake --build $TESTSUITE_BUILD_DIR -- -j2
+cmake --install $TESTSUITE_BUILD_DIR --prefix $TESTSUITE_INSTALL_DIR
+mv $TESTSUITE_INSTALL_DIR/bin/testsuite/* $TESTSUITE_INSTALL_DIR
 echo "::endgroup::"
