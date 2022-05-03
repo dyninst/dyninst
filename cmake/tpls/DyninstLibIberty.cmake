b951a13889f819c5492ff81a4260c6783fe59da6
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2022-05-03 05:36:44 -0500

    Update DyninstLibIberty.cmake

--- cmake/tpls/DyninstLibIberty.cmake
+++ cmake/tpls/DyninstLibIberty.cmake
@@ -21,9 +21,12 @@
 #
 #======================================================================================
 
-if(LibIberty_FOUND)
-    return()
-endif()
+include_guard(GLOBAL)
+
+include(DyninstUtilities)
+
+# always provide Dyninst::LibIberty even if it is empty
+dyninst_add_interface_library(LibIberty "LibIberty interface library")
 
 if(NOT UNIX)
     return()
@@ -43,31 +46,105 @@ set(LibIberty_LIBRARYDIR
 
 # -------------- PACKAGES -----------------------------------------------------
 
-find_package(LibIberty REQUIRED)
+if(NOT BUILD_LIBIBERTY)
+    find_package(LibIberty)
+endif()
+
+# -------------- SOURCE BUILD -------------------------------------------------
+if(LibIberty_FOUND)
+    set(_li_root ${LibIberty_ROOT_DIR})
+    set(_li_inc_dirs ${LibIberty_INCLUDE_DIRS})
+    set(_li_lib_dirs ${LibIberty_LIBRARY_DIRS})
+    set(_li_libs ${LibIberty_LIBRARIES})
+elseif(STERILE_BUILD)
+    dyninst_message(
+        FATAL_ERROR
+        "LibIberty not found and cannot be downloaded because build is sterile.")
+elseif(NOT BUILD_LIBIBERTY)
+    dyninst_message(
+        FATAL_ERROR
+        "LibIberty was not found. Either configure cmake to find TBB properly or set BUILD_LIBIBERTY=ON to download and build"
+        )
+else()
+    dyninst_message(STATUS "${LibIberty_ERROR_REASON}")
+    dyninst_message(STATUS "Attempting to build LibIberty as external project")
+
+    include(ExternalProject)
+    externalproject_add(
+        LibIberty-External
+        PREFIX ${PROJECT_BINARY_DIR}/binutils
+        URL http://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.gz
+        BUILD_IN_SOURCE 1
+        CONFIGURE_COMMAND
+            ${CMAKE_COMMAND} -E env CC=${CMAKE_C_COMPILER} CFLAGS=-fPIC\ -O2\ -g
+            CXX=${CMAKE_CXX_COMPILER} CXXFLAGS=-fPIC\ -O2\ -g <SOURCE_DIR>/configure
+            --prefix=${PROJECT_BINARY_DIR}/binutils
+        BUILD_COMMAND make
+        INSTALL_COMMAND "")
+
+    add_custom_command(
+        TARGET LibIberty-External
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${TPL_STAGING_PREFIX}/lib
+        COMMAND
+            install ARGS -C
+            ${PROJECT_BINARY_DIR}/binutils/src/LibIberty-External/libiberty/libiberty.a
+            ${TPL_STAGING_PREFIX}/lib
+        COMMENT "Installing LibIberty...")
+
+    # target for re-executing the installation
+    add_custom_target(
+        install-libiberty-external
+        COMMAND
+            install -C
+            ${PROJECT_BINARY_DIR}/binutils/src/LibIberty-External/libiberty/libiberty.a
+            ${TPL_STAGING_PREFIX}/lib
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/binutils/src/LibIberty-External
+        COMMENT "Installing LibIberty...")
+
+    set(_li_root ${TPL_STAGING_PREFIX})
+    set(_li_inc_dirs
+        $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/binutils/src/LibIberty-External/include>)
+    set(_li_lib_dirs $<BUILD_INTERFACE:${_li_root}/lib>
+                     $<INSTALL_INTERFACE:${INSTALL_LIB_DIR}/${TPL_INSTALL_LIB_DIR}>)
+    set(_li_libs
+        $<BUILD_INTERFACE:${_li_root}/lib/libiberty${CMAKE_STATIC_LIBRARY_SUFFIX}>
+        $<INSTALL_INTERFACE:$<INSTALL_PREFIX>/${INSTALL_LIB_DIR}/${TPL_INSTALL_LIB_DIR}/libiberty${CMAKE_STATIC_LIBRARY_SUFFIX}>
+        )
+
+    # For backward compatibility
+    set(IBERTY_FOUND TRUE)
+    set(IBERTY_BUILD TRUE)
+endif()
 
 # -------------- EXPORT VARIABLES ---------------------------------------------
 
-add_library(LibIberty STATIC IMPORTED GLOBAL)
-set_target_properties(LibIberty PROPERTIES IMPORTED_LOCATION ${LibIberty_LIBRARIES})
-set_target_properties(LibIberty PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                           ${LibIberty_INCLUDE_DIRS})
+foreach(_DIR_TYPE inc lib)
+    if(_li_${_DIR_TYPE}_dirs)
+        list(REMOVE_DUPLICATES _li_${_DIR_TYPE}_dirs)
+    endif()
+endforeach()
+
+target_include_directories(LibIberty INTERFACE ${_li_inc_dirs})
+target_link_directories(LibIberty INTERFACE ${_lib_lib_dirs})
+target_link_libraries(LibIberty INTERFACE ${_li_libs})
 
 set(LibIberty_ROOT_DIR
-    ${LibIberty_ROOT_DIR}
+    ${_li_root}
     CACHE PATH "Base directory the of LibIberty installation" FORCE)
 set(LibIberty_INCLUDE_DIRS
-    ${LibIberty_INCLUDE_DIRS}
+    ${_li_inc_dirs}
     CACHE PATH "LibIberty include directories" FORCE)
 set(LibIberty_LIBRARY_DIRS
-    ${LibIberty_LIBRARY_DIRS}
+    ${_li_lib_dirs}
     CACHE PATH "LibIberty library directory" FORCE)
 set(LibIberty_LIBRARIES
-    ${LibIberty_LIBRARIES}
+    ${_li_libs}
     CACHE FILEPATH "LibIberty library files" FORCE)
 
 # For backward compatibility only
 set(IBERTY_LIBRARIES ${LibIberty_LIBRARIES})
 
-message(STATUS "LibIberty include dirs: ${LibIberty_INCLUDE_DIRS}")
-message(STATUS "LibIberty library dirs: ${LibIberty_LIBRARY_DIRS}")
-message(STATUS "LibIberty libraries: ${LibIberty_LIBRARIES}")
+dyninst_message(STATUS "LibIberty include dirs: ${LibIberty_INCLUDE_DIRS}")
+dyninst_message(STATUS "LibIberty library dirs: ${LibIberty_LIBRARY_DIRS}")
+dyninst_message(STATUS "LibIberty libraries: ${LibIberty_LIBRARIES}")
