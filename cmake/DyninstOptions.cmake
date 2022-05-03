12cb85c979aa6098ff5cba9762aac97854d3578d
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2022-05-03 05:30:35 -0500

    Reworked dyninst options

--- cmake/DyninstOptions.cmake
+++ cmake/DyninstOptions.cmake
@@ -1,30 +1,51 @@
+include_guard(DIRECTORY)
+
+include(DyninstUtilities)
+
+dyninst_add_option(BUILD_SHARED_LIBS "Build shared libraries" ON)
+dyninst_add_option(BUILD_STATIC_LIBS "Build static libraries" OFF)
+
 # Use OpenMP?
-option(USE_OpenMP "Use OpenMP for parallel parsing" ON)
+dyninst_add_option(USE_OpenMP "Use OpenMP for parallel parsing" ON)
 
 # Use SymtabAPI or SymLite?
-option(
+dyninst_add_option(
     LIGHTWEIGHT_SYMTAB
     "Use lightweight symtab interface for ParseAPI, ProcControl, and Stackwalker; disables DyninstAPI build"
     OFF)
 
 # Use ParseAPI analysis in Stackwalker?
-option(SW_ANALYSIS_STEPPER "Use ParseAPI-based analysis stepper in Stackwalker" ON)
+dyninst_add_option(SW_ANALYSIS_STEPPER
+                   "Use ParseAPI-based analysis stepper in Stackwalker" ON)
+dyninst_add_option(BUILD_RTLIB_32 "Build 32-bit runtime library on mixed 32/64 systems"
+                   OFF)
+dyninst_add_option(ENABLE_LTO "Enable Link-Time Optimization" OFF)
+dyninst_add_option(ENABLE_DEBUGINFOD "Enable debuginfod support" OFF)
+dyninst_add_option(BUILD_PARSE_THAT "Enable building parseThat executable" ON)
+dyninst_add_option(BUILD_TBB "Enable building TBB internally" OFF)
+dyninst_add_option(BUILD_BOOST "Enable building Boost internally" OFF)
+dyninst_add_option(BUILD_ELFUTILS "Enable building elfutils internally" OFF)
+dyninst_add_option(BUILD_LIBIBERTY "Enable building libiberty internally" OFF)
+dyninst_add_option(DISABLE_DIAGNOSTIC_SUPPRESSIONS
+                   "Disable all warning suppressions and frame size overrides." OFF)
+dyninst_add_option(WARNINGS_AS_ERRORS "Treat compilation warnings as errors" OFF)
 
-option(BUILD_RTLIB_32 "Build 32-bit runtime library on mixed 32/64 systems" OFF)
+option(CMAKE_INSTALL_RPATH_USE_LINK_PATH "Enable rpath to linked libraries" ON)
+dyninst_add_feature(CMAKE_INSTALL_RPATH_USE_LINK_PATH "Enable rpath to linked libraries")
 
-option(ENABLE_LTO "Enable Link-Time Optimization" OFF)
-
-option(ENABLE_DEBUGINFOD "Enable debuginfod support" OFF)
+if(STERILE_BUILD)
+    foreach(_SUBPACKAGE TBB BOOST ELFUTILS LIBIBERTY)
+        dyninst_force_option(BUILD_${_SUBPACKAGE} OFF)
+    endforeach()
+endif()
 
 # Some global on/off switches
 if(LIGHTWEIGHT_SYMTAB)
-    add_definitions(-DWITHOUT_SYMTAB_API -DWITH_SYMLITE)
+    add_compile_definitions(WITH_SYMLITE)
 else()
-    add_definitions(-DWITH_SYMTAB_API -DWITHOUT_SYMLITE)
+    add_compile_definitions(WITH_SYMTAB_API)
 endif()
 
 if(SW_ANALYSIS_STEPPER)
-    add_definitions(-DUSE_PARSE_API)
+    add_compile_definitions(USE_PARSE_API)
 endif()
-
-message(STATUS "Options set")
