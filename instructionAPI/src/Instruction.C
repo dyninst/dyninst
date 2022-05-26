f23fc812601eb2b7993f34c0569d654cad877cc1
Tim Haines <thaines.astro@gmail.com>
2022-05-25 23:33:56 -0500

    make isValid the same as isLegalInsn
    This makes it impossible to make an Instruction object from a failed
    decoding process. This only affects x86.

--- instructionAPI/src/Instruction.C
+++ instructionAPI/src/Instruction.C
@@ -84,8 +84,8 @@ namespace Dyninst
         INSTRUCTION_EXPORT Instruction::Instruction(Operation what,
                 size_t size, const unsigned char* raw,
                 Dyninst::Architecture arch)
-            : m_InsnOp(what), m_Valid(true), arch_decoded_from(arch),
-            formatter(&ArchSpecificFormatter::getFormatter(arch))
+            : m_InsnOp(what), m_Valid(what.getID() != e_No_Entry), arch_decoded_from(arch),
+            formatter(ArchSpecificFormatter::getFormatter(arch))
         {
             copyRaw(size, raw);
 
