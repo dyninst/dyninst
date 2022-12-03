5998f2dad03846496f7884123a64be41d09e04da
Tim Haines <thaines.astro@gmail.com>
2022-12-02 22:43:36 -0600

    Whitespace formatting

--- cmake/tpls/DyninstBoost.cmake
+++ cmake/tpls/DyninstBoost.cmake
@@ -27,18 +27,29 @@ mark_as_advanced(Boost_ROOT_DIR)
 
 # Library components that need to be linked against
 set(_boost_components atomic chrono date_time filesystem thread timer)
-find_package(Boost ${Boost_MIN_VERSION} QUIET REQUIRED HINTS ${Boost_ROOT_DIR} ${PATH_BOOST} ${BOOST_ROOT} COMPONENTS ${_boost_components})
+find_package(
+    Boost
+    ${Boost_MIN_VERSION}
+    QUIET
+    REQUIRED
+    HINTS
+    ${Boost_ROOT_DIR}
+    ${PATH_BOOST}
+    ${BOOST_ROOT}
+    COMPONENTS ${_boost_components})
 
 # Make an interface dummy target to force includes to be treated as SYSTEM
 add_library(Dyninst::Boost INTERFACE IMPORTED)
 target_link_libraries(Dyninst::Boost INTERFACE ${Boost_LIBRARIES})
 target_include_directories(Dyninst::Boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
-target_compile_definitions(Dyninst::Boost INTERFACE BOOST_MULTI_INDEX_DISABLE_SERIALIZATION)
+target_compile_definitions(Dyninst::Boost
+                           INTERFACE BOOST_MULTI_INDEX_DISABLE_SERIALIZATION)
 
 # Just the headers (effectively a simplified Boost::headers target)
 add_library(Dyninst::Boost_headers INTERFACE IMPORTED)
 target_include_directories(Dyninst::Boost_headers SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
-target_compile_definitions(Dyninst::Boost_headers INTERFACE BOOST_MULTI_INDEX_DISABLE_SERIALIZATION)
+target_compile_definitions(Dyninst::Boost_headers
+                           INTERFACE BOOST_MULTI_INDEX_DISABLE_SERIALIZATION)
 
 message(STATUS "Found Boost ${Boost_VERSION}")
 message(STATUS "Boost include directories: ${Boost_INCLUDE_DIRS}")
