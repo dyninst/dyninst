c6b36486fc282f02e62922963bfa4fea2fe49179
Tim Haines <thaines.astro@gmail.com>
2022-12-13 12:09:52 -0600

    Libiberty cmake modernization (#1334)
    * LibIberty cmake modernization
    
    * Use INCLUDE_DIRS directly

--- cmake/tpls/DyninstLibIberty.cmake
+++ cmake/tpls/DyninstLibIberty.cmake
@@ -1,73 +1,33 @@
-#======================================================================================
-# LibIberty.cmake
+#================================================================
 #
-# Configure LibIberty for Dyninst
+# Configure libiberty
 #
 #   ----------------------------------------
 #
-# Directly exports the following CMake variables
+# LibIberty_ROOT_DIR - Directory hint for libiberty installation
 #
-# LibIberty_ROOT_DIR       - Computed base directory the of LibIberty installation
-# LibIberty_LIBRARY_DIRS   - Link directories for LibIberty libraries
-# LibIberty_LIBRARIES      - LibIberty library files
-# LibIberty_INCLUDE        - LibIberty include files
-#
-# NOTE:
-# The exported LibIberty_ROOT_DIR can be different from the value provided by the user
-# in the case that it is determined to build LibIberty from source. In such a case,
-# LibIberty_ROOT_DIR will contain the directory of the from-source installation.
-#
-# See Modules/FindLibIberty.cmake for details
-#
-#======================================================================================
+#================================================================
 
-if(LibIberty_FOUND)
-    return()
-endif()
+include_guard(GLOBAL)
 
+# libiberty is only available on Unixes; provide a dummy target on other platforms
 if(NOT UNIX)
+    if(NOT TARGET Dyninst::LibIberty)
+        add_library(Dyninst::LibIberty INTERFACE)
+    endif()
     return()
 endif()
-
-# -------------- PATHS --------------------------------------------------------
 
 # Base directory the of LibIberty installation
 set(LibIberty_ROOT_DIR
     "/usr"
     CACHE PATH "Base directory the of LibIberty installation")
-
-# Hint directory that contains the LibIberty library files
-set(LibIberty_LIBRARYDIR
-    "${LibIberty_ROOT_DIR}/lib"
-    CACHE PATH "Hint directory that contains the LibIberty library files")
-
-# -------------- PACKAGES -----------------------------------------------------
+mark_as_advanced(LibIberty_ROOT_DIR)
 
 find_package(LibIberty REQUIRED)
 
-# -------------- EXPORT VARIABLES ---------------------------------------------
-
-add_library(LibIberty STATIC IMPORTED GLOBAL)
-set_target_properties(LibIberty PROPERTIES IMPORTED_LOCATION ${LibIberty_LIBRARIES})
-set_target_properties(LibIberty PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                           ${LibIberty_INCLUDE_DIRS})
-
-set(LibIberty_ROOT_DIR
-    ${LibIberty_ROOT_DIR}
-    CACHE PATH "Base directory the of LibIberty installation" FORCE)
-set(LibIberty_INCLUDE_DIRS
-    ${LibIberty_INCLUDE_DIRS}
-    CACHE PATH "LibIberty include directories" FORCE)
-set(LibIberty_LIBRARY_DIRS
-    ${LibIberty_LIBRARY_DIRS}
-    CACHE PATH "LibIberty library directory" FORCE)
-set(LibIberty_LIBRARIES
-    ${LibIberty_LIBRARIES}
-    CACHE FILEPATH "LibIberty library files" FORCE)
-
-# For backward compatibility only
-set(IBERTY_LIBRARIES ${LibIberty_LIBRARIES})
-
-message(STATUS "LibIberty include dirs: ${LibIberty_INCLUDE_DIRS}")
-message(STATUS "LibIberty library dirs: ${LibIberty_LIBRARY_DIRS}")
-message(STATUS "LibIberty libraries: ${LibIberty_LIBRARIES}")
+if(NOT TARGET Dyninst::LibIberty)
+	add_library(Dyninst::LibIberty INTERFACE IMPORTED)
+	target_include_directories(Dyninst::LibIberty SYSTEM INTERFACE ${LibIberty_INCLUDE_DIRS})
+	target_link_libraries(Dyninst::LibIberty INTERFACE LibIberty::LibIberty)
+endif()
