ce8b8ea9aa013fadab47ae2de159296794d46b71
Tim Haines <thaines.astro@gmail.com>
2022-12-16 22:32:20 -0600

    Move all options to DyninstOptions

--- cmake/DyninstOptions.cmake
+++ cmake/DyninstOptions.cmake
@@ -13,6 +13,32 @@ option(ENABLE_LTO "Enable Link-Time Optimization" OFF)
 
 option(ENABLE_DEBUGINFOD "Enable debuginfod support" OFF)
 
+set(STERILE_BUILD
+    ON
+    CACHE BOOL "Do not download/build any third-party dependencies from source")
+
+set(ADD_VALGRIND_ANNOTATIONS
+    OFF
+    CACHE BOOL "Enable annotations for Valgrind analysis")
+
+set(ENABLE_STATIC_LIBS
+    NO
+    CACHE STRING "Build static libraries as well?")
+
+option(DYNINST_DISABLE_DIAGNOSTIC_SUPPRESSIONS
+       "Disable all warning suppressions and frame size overrides." OFF)
+
+set(DYNINST_EXTRA_WARNINGS
+    ""
+    CACHE
+        STRING
+        "Additional warning options to enable if available.  ;-separated without leading '-' (Wopt1[;Wopt2]...)."
+    )
+
+option(DYNINST_WARNINGS_AS_ERRORS "Treat compilation warnings as errors" OFF)
+
+option(ENABLE_PARSE_API_GRAPHS "Enable Boost Graph wrappers for parseAPI Functions" OFF)
+
 # Some global on/off switches
 if(LIGHTWEIGHT_SYMTAB)
     add_definitions(-DWITHOUT_SYMTAB_API -DWITH_SYMLITE)
