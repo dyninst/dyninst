5c0c2990dc1293bcd2b623dbe4eada175781ef73
Tim Haines <thaines.astro@gmail.com>
2022-12-16 22:31:18 -0600

    Remove unneeded comments in DyninstOptions

--- cmake/DyninstOptions.cmake
+++ cmake/DyninstOptions.cmake
@@ -1,13 +1,10 @@
-# Use OpenMP?
 option(USE_OpenMP "Use OpenMP for parallel parsing" ON)
 
-# Use SymtabAPI or SymLite?
 option(
     LIGHTWEIGHT_SYMTAB
     "Use lightweight symtab interface for ParseAPI, ProcControl, and Stackwalker; disables DyninstAPI build"
     OFF)
 
-# Use ParseAPI analysis in Stackwalker?
 option(SW_ANALYSIS_STEPPER "Use ParseAPI-based analysis stepper in Stackwalker" ON)
 
 option(BUILD_RTLIB_32 "Build 32-bit runtime library on mixed 32/64 systems" OFF)
@@ -26,5 +23,3 @@ endif()
 if(SW_ANALYSIS_STEPPER)
     add_definitions(-DUSE_PARSE_API)
 endif()
-
-message(STATUS "Options set")
