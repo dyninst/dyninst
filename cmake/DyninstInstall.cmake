0545ce61577304989bf50da224b1239ae0a9735f
Tim Haines <thaines.astro@gmail.com>
2022-12-19 09:15:52 -0600

    Automatically generate DyninstConfigVersion.cmake

--- cmake/DyninstInstall.cmake
+++ cmake/DyninstInstall.cmake
@@ -5,3 +5,12 @@ install(DIRECTORY ${DYNINST_ROOT}/cmake/Modules DESTINATION ${INSTALL_CMAKE_DIR}
 install(DIRECTORY ${DYNINST_ROOT}/cmake/tpls DESTINATION ${INSTALL_CMAKE_DIR})
 
 configure_file(cmake/version.h.in common/h/dyninstversion.h)
+
+write_basic_package_version_file(
+    ${PROJECT_BINARY_DIR}/DyninstConfigVersion.cmake
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY SameMajorVersion)
+
+install(
+    FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+    DESTINATION ${INSTALL_CMAKE_DIR})
