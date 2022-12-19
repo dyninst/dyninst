c00ba4e0dbbaa2efb06fc7ae740f644363c96771
Tim Haines <thaines.astro@gmail.com>
2022-12-19 09:18:49 -0600

    Use configure_package_config_file to generate DyninstConfig.cmake
    This will provide more utilities to make a more robust Config.cmake

--- cmake/DyninstInstall.cmake
+++ cmake/DyninstInstall.cmake
@@ -6,11 +6,19 @@ install(DIRECTORY ${DYNINST_ROOT}/cmake/tpls DESTINATION ${INSTALL_CMAKE_DIR})
 
 configure_file(cmake/version.h.in common/h/dyninstversion.h)
 
+configure_package_config_file(
+    ${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake.in
+    ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+    INSTALL_DESTINATION ${INSTALL_CMAKE_DIR}
+    INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}
+    PATH_VARS INSTALL_LIB_DIR INSTALL_INCLUDE_DIR)
+
 write_basic_package_version_file(
     ${PROJECT_BINARY_DIR}/DyninstConfigVersion.cmake
     VERSION ${PROJECT_VERSION}
     COMPATIBILITY SameMajorVersion)
 
 install(
-    FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+    FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+          ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
     DESTINATION ${INSTALL_CMAKE_DIR})
