c42f4d3fb714cc3311b320c08a5bc9f60d0a130c
Tim Haines <thaines.astro@gmail.com>
2023-01-05 17:17:48 -0600

    Remove custom fixed-width types and limit macros
    We require C++11, so just use the [c]stdint headers.

--- common/src/Types.h
+++ common/src/Types.h
@@ -36,96 +36,10 @@
 #if !defined(_Types_h_)
 #define _Types_h_
 
-/* Sets up 64 and 32 bit
-   types:
-      int64_t      uint64_t      int32_t       uint32_t
-   constant macros:
-      I64_C(x)     UI64_C(x)
-   limits:
-      I64_MAX      I64_MIN       UI64_MAX
-      I32_MAX      I32_MIN       UI32_MAX
-
-   note: needs to be included before anything that includes inttypes.h
-         (eg. stdio on some systems)
-*/
-
-/* Set up the 32 AND 64 BIT TYPES ===================================== */
-/*
-   --- inttypes.h ---
-             int32_t  uint32_t  int64_t uint64_t 32B lmts 64Blmts 64BlitMacros#
-Linux        yes      yes       yes     yes      yes      yes     yes
-FreeBSD      yes      yes       yes     yes      yes      yes     yes
-WindowsNT    nonexistant
-
-  # we rename all of the 64 bit literal macros to our shortened name
-*/
-
-#if defined(os_windows)
-   typedef signed __int64 int64_t;
-   typedef signed __int32 int32_t;
-   typedef signed __int16 int16_t;
-   typedef signed __int8 int8_t;
-   typedef unsigned __int64 uint64_t;
-   typedef unsigned __int32 uint32_t;
-   typedef unsigned __int16 uint16_t;
-   typedef unsigned __int8 uint8_t;
-
-#elif defined(os_linux)
-#if !defined(__STDC_CONSTANT_MACROS)
-#define __STDC_CONSTANT_MACROS
-#endif
-#if !defined(__STDC_LIMIT_MACROS)
-#define __STDC_LIMIT_MACROS
-#endif
-#include <stdint.h>
-#if defined(arch_x86_64) || defined(arch_64bit)
-#define TYPE64BIT
-#endif
-
-#elif defined(os_freebsd)
-#if !defined(__STDC_CONSTANT_MACROS)
-#define __STDC_CONSTANT_MACROS
-#endif
-#if !defined(__STDC_LIMIT_MACROS)
-#define __STDC_LIMIT_MACROS
-#endif
-#include <stdint.h>
-
-/* FreeBSD doesn't define this */
-typedef int64_t off64_t;
-
+#if defined __cplusplus
+#  include <cstdint>
 #else
-#error Unknown architecture
-#endif
-
-
-/* Set up the 64 BIT LITERAL MACROS =================================== */
-#if defined(os_windows)
-				   /* nt ----------------------------- */
-#define I64_C(x)  (x##i64)
-#define UI64_C(x) (x##ui64)
-#else                               /* linux, freebsd ----------------- */
-#define I64_C(x)  INT64_C(x)
-#define UI64_C(x) UINT64_C(x)
-#endif
-
-/* Set up the 32 and 64 BIT LIMITS for those not already set up ======= */
-#if defined(os_windows)
-			 /* nt ----------------------------- */
-#include <limits.h>
-#define I64_MAX  _I64_MAX
-#define UI64_MAX _UI64_MAX
-#define I64_MIN  _I64_MIN
-#define I32_MAX  _I32_MAX
-#define I32_MIN  _I32_MIN
-#define UI32_MAX  _UI32_MAX
-#else                              /* linux, freebsd ----------------- */
-#define I64_MAX  INT64_MAX
-#define UI64_MAX UINT64_MAX
-#define I64_MIN  INT64_MIN
-#define I32_MAX  INT32_MAX
-#define I32_MIN  INT32_MIN
-#define UI32_MAX UINT32_MAX
+#  include <stdint.h>
 #endif
 
    /*
