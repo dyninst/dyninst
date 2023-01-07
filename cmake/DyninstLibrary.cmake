ef5cc25b801e2591b75419e68557d4ea48e0397e
Tim Haines <thaines.astro@gmail.com>
2023-01-07 00:10:59 -0600

    DyninstLibrary - whitespace

--- cmake/DyninstLibrary.cmake
+++ cmake/DyninstLibrary.cmake
@@ -3,64 +3,59 @@ set(ALL_DYNINST_TARGETS
     CACHE INTERNAL "")
 
 function(dyninst_library target)
-    add_library(${target} ${SRC_LIST})
-    target_link_libraries(${target} PRIVATE ${ARGN})
-    file(GLOB headers "h/*.h" "${CMAKE_CURRENT_BINARY_DIR}/h/*.h")
-    set(ACTUAL_TARGETS ${target})
-    set(ALL_TARGETS "${ARGN};${target}")
-    if(${ENABLE_STATIC_LIBS})
-        set(ACTUAL_TARGETS ${ACTUAL_TARGETS} ${target}_static)
-        add_library(${target}_static STATIC ${SRC_LIST})
+  add_library(${target} ${SRC_LIST})
+  target_link_libraries(${target} PRIVATE ${ARGN})
+  file(GLOB headers "h/*.h" "${CMAKE_CURRENT_BINARY_DIR}/h/*.h")
+  set(ACTUAL_TARGETS ${target})
+  set(ALL_TARGETS "${ARGN};${target}")
+  if(${ENABLE_STATIC_LIBS})
+    set(ACTUAL_TARGETS ${ACTUAL_TARGETS} ${target}_static)
+    add_library(${target}_static STATIC ${SRC_LIST})
+  endif()
+  message(STATUS "Building ${ACTUAL_TARGETS}...")
+  set_target_properties(
+    ${ACTUAL_TARGETS}
+    PROPERTIES PUBLIC_HEADER "${headers}"
+               LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+               INSTALL_RPATH "${DYNINST_RPATH_DIRECTORIES}"
+               SOVERSION ${DYNINST_SOVERSION}
+               VERSION ${DYNINST_LIBVERSION}
+               CLEAN_DIRECT_OUTPUT 1)
+
+  set(_defs)
+  if(DYNINST_OS_Windows)
+    list(APPEND _defs WIN32_LEAN_AND_MEAN)
+    if(CMAKE_C_COMPILER_VERSION VERSION_GREATER 19)
+      list(APPEND _defs _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS=1)
+    else()
+      list(APPEND _defs snprintf=_snprintf)
     endif()
-    message(STATUS "Building ${ACTUAL_TARGETS}...")
-    set_target_properties(${ACTUAL_TARGETS}
-    		PROPERTIES
-    			PUBLIC_HEADER
-    				"${headers}"
-    			LIBRARY_OUTPUT_DIRECTORY
-    				${CMAKE_CURRENT_BINARY_DIR}
-    			INSTALL_RPATH
-    				"${DYNINST_RPATH_DIRECTORIES}"
-					SOVERSION
-						${DYNINST_SOVERSION}
-          VERSION
-          	${DYNINST_LIBVERSION}
-        	CLEAN_DIRECT_OUTPUT 1)
+  endif()
 
-		set(_defs)
-		if(DYNINST_OS_Windows)
-		    list(APPEND _defs WIN32_LEAN_AND_MEAN)
-		    if(CMAKE_C_COMPILER_VERSION VERSION_GREATER 19)
-		        list(APPEND _defs _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS=1)
-		    else()
-		        list(APPEND _defs snprintf=_snprintf)
-		    endif()
-		endif()
-		
-	 	if(LIGHTWEIGHT_SYMTAB)
-	    list(APPEND _defs WITHOUT_SYMTAB_API WITH_SYMLITE)
-		else()
-		    list(APPEND _defs WITH_SYMTAB_API WITHOUT_SYMLITE)
-		endif()
-		
-		if(SW_ANALYSIS_STEPPER)
-		    list(APPEND _defs USE_PARSE_API)
-		endif()
+  if(LIGHTWEIGHT_SYMTAB)
+    list(APPEND _defs WITHOUT_SYMTAB_API WITH_SYMLITE)
+  else()
+    list(APPEND _defs WITH_SYMTAB_API WITHOUT_SYMLITE)
+  endif()
 
-    foreach(t ${ACTUAL_TARGETS})
-    	target_compile_definitions(${t} PRIVATE ${_defs})
-    endforeach()
+  if(SW_ANALYSIS_STEPPER)
+    list(APPEND _defs USE_PARSE_API)
+  endif()
+  
+  foreach(t ${ACTUAL_TARGETS})
+    target_compile_definitions(${t} PRIVATE ${_defs})
+  endforeach()
 
-    install(
-        TARGETS ${ACTUAL_TARGETS}
-        EXPORT ${target}Targets
-        COMPONENT ${target}
-        RUNTIME DESTINATION ${DYNINST_INSTALL_LIBDIR}
-        LIBRARY DESTINATION ${DYNINST_INSTALL_LIBDIR}
-        ARCHIVE DESTINATION ${DYNINST_INSTALL_LIBDIR}
-        PUBLIC_HEADER DESTINATION ${DYNINST_INSTALL_INCLUDEDIR})
-    set(ALL_DYNINST_TARGETS
-        "${ALL_DYNINST_TARGETS};${target}"
-        CACHE INTERNAL "")
-    install(EXPORT ${target}Targets DESTINATION "${DYNINST_INSTALL_INCLUDEDIR}")
+  install(
+      TARGETS ${ACTUAL_TARGETS}
+      EXPORT ${target}Targets
+      COMPONENT ${target}
+      RUNTIME DESTINATION ${DYNINST_INSTALL_LIBDIR}
+      LIBRARY DESTINATION ${DYNINST_INSTALL_LIBDIR}
+      ARCHIVE DESTINATION ${DYNINST_INSTALL_LIBDIR}
+      PUBLIC_HEADER DESTINATION ${DYNINST_INSTALL_INCLUDEDIR})
+  set(ALL_DYNINST_TARGETS
+      "${ALL_DYNINST_TARGETS};${target}"
+      CACHE INTERNAL "")
+  install(EXPORT ${target}Targets DESTINATION "${DYNINST_INSTALL_INCLUDEDIR}")
 endfunction()
