3ac13c33c804a9afa4bf57d561bbbc7a4c96ae5a
Tim Haines <thaines.astro@gmail.com>
2023-01-10 16:20:09 -0600

    Refactor common/src/Types.h (#1351)
    * Remove double128_t
    
    It's never used
    
    * Replace custom limit macros with the ones from stdint
    
    * Remove custom fixed-width types and limit macros
    
    We require C++11, so just use the [c]stdint headers.
    
    * Use correct signedness for Address comparison

--- common/src/arch-x86.h
+++ common/src/arch-x86.h
@@ -1159,13 +1159,13 @@ inline bool is_disp16(long disp) {
 }
 
 inline bool is_disp32(long disp) {
-  return (disp <= I32_MAX && disp >= I32_MIN);
+  return (disp <= INT32_MAX && disp >= INT32_MIN);
 }
 inline bool is_disp32(Address a1, Address a2) {
   return is_disp32(a2 - (a1 + JUMP_REL32_SZ));
 }
 inline bool is_addr32(Address addr) {
-    return (addr < UI32_MAX);
+    return (addr < UINT32_MAX);
 }
 
 COMMON_EXPORT void decode_SIB(unsigned sib, unsigned& scale, 
