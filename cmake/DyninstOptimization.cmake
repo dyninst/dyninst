582807132d94345e9b2587425327dd0e631d1b77
Tim Haines <thaines.astro@gmail.com>
2023-01-11 14:16:47 -0600

    DyninstOptimization - don't modify CMAKE_*_LINKER_FLAGS
    DYNINST_LINK_FLAGS will be used later.

--- cmake/DyninstOptimization.cmake
+++ cmake/DyninstOptimization.cmake
@@ -15,7 +15,7 @@ set(_linux_compilers "GNU" "Clang" "Intel" "IntelLLVM")
 
 if(${CMAKE_CXX_COMPILER_ID} IN_LIST _linux_compilers)
   if(ENABLE_LTO)
-    set(LTO_LINK_FLAGS "-fuse-ld=bfd")
+    list(APPEND DYNINST_LINK_FLAGS "-fuse-ld=bfd")
   endif()
 	list(APPEND DYNINST_FLAGS_DEBUG   "-Og -g3")
 	list(APPEND DYNINST_FLAGS_RELEASE "-O2 -g3")
@@ -24,10 +24,7 @@ if(${CMAKE_CXX_COMPILER_ID} IN_LIST _linux_compilers)
 
   set(FORCE_FRAME_POINTER "-fno-omit-frame-pointer")
   # Ensure each library is fully linked
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
-
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${LTO_LINK_FLAGS}")
-  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${LTO_LINK_FLAGS}")
+	list(APPEND DYNINST_LINK_FLAGS "-Wl,--no-undefined")
 else(MSVC)
 	list(APPEND DYNINST_FLAGS_DEBUG "/MP /Od /Zi /MDd /D_DEBUG")
 	list(APPEND DYNINST_FLAGS_RELEASE "/MP /O2 /MD")
@@ -35,9 +32,5 @@ else(MSVC)
 	list(APPEND DYNINST_FLAGS_MINSIZEREL"/MP /O1 /MD")
 
   set(FORCE_FRAME_POINTER "/Oy-")
-
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${LTO_LINK_FLAGS}")
-  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${LTO_LINK_FLAGS}")
-  set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} ${LTO_LINK_FLAGS}")
 endif()
 message(STATUS "Set optimization flags")
