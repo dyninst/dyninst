8e2f44c470b18a120d63105b9c4a6298fdbfe551
Tim Haines <thaines.astro@gmail.com>
2023-01-15 09:28:02 -0600

    DyninstLibrary - simplify regex for header install

--- cmake/DyninstLibrary.cmake
+++ cmake/DyninstLibrary.cmake
@@ -158,7 +158,7 @@ function(dyninst_library _target)
   # Install headers, preserving the directory structure under h/.
   # Note: By convention, headers are stored in "h/"
   foreach(h ${_public_headers})
-    string(REGEX MATCH "^h\\/(.*)" _file ${h})
+    string(REGEX MATCH "^h/(.*)" _file ${h})
     get_filename_component(_dir ${CMAKE_MATCH_1} DIRECTORY)
     install(FILES ${h} DESTINATION "${DYNINST_INSTALL_INCLUDEDIR}/${_dir}")
   endforeach()
