6407563245bede14957e62d04d9269903f2a8e7f
Tim Haines <thaines.astro@gmail.com>
2023-02-09 11:53:47 -0600

    Fix bug in dwarf_result::operator bool

--- dwarf/src/dwarf_subrange.h
+++ dwarf/src/dwarf_subrange.h
@@ -14,7 +14,7 @@ template <typename T> struct dwarf_result {
   dwarf_result() = default;
   dwarf_result(T &&t) : value{std::move(t)} {}
   dwarf_result(dwarf_error) : error{true} {}
-  explicit operator bool() const { return error; }
+  explicit operator bool() const { return !error; }
 };
 
 DYNDWARF_EXPORT dwarf_result<long> dwarf_subrange_upper_bound(Dwarf_Die *die);
