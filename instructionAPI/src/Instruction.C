5ad79439de5468982f9ba878e916403f2c8671ec
Bolo <bolo@cs.wisc.edu>
2023-02-20 13:58:46 -0600

    Prevent fallthrough analysis of amdgpu swap/set pc instructions.
    Both amdgpu setpc and swappc instructions are _not_
    fallthrough branch instructions.  We need to stop analysis
    at that instruction.
    
    The CDNA2 (MI200) instructions of swappc and ssetpc were
    missing.  They have been added for similar fallthrough
    prevention on that architecture.

--- instructionAPI/src/Instruction.C
+++ instructionAPI/src/Instruction.C
@@ -540,7 +540,10 @@ namespace Dyninst
                 case e_sysexit:
                 case e_call:
                 case e_syscall:
+                case amdgpu_op_s_setpc_b64:
                 case amdgpu_op_s_swappc_b64:
+                case amdgpu_cdna2_op_S_SETPC_B64:
+                case amdgpu_cdna2_op_S_SWAPPC_B64:
                     return false;
                 case e_jnb:
                 case e_jb:
