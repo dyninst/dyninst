358db4610e4919690d928797713bef3cf67ef61d
kupsch <kupsch@cs.wisc.edu>
2023-02-22 09:51:01 -0600

    explicitly include <array> (#1384)
    - std::array is used in this file, but not included explicitly as
      on many systems this header is implicitly included recursively
      via another included file.  On new versions of headers this may
      no longer be the case, so explicitly include it.

--- instructionAPI/src/InstructionDecoder-aarch64.h
+++ instructionAPI/src/InstructionDecoder-aarch64.h
@@ -28,6 +28,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <array>
 #include "InstructionDecoderImpl.h"
 #include <iostream>
 #include "Immediate.h"
