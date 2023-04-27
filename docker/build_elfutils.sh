719036aeb55c459a355afb91188cfa4d7d465bc9
Tim Haines <thaines.astro@gmail.com>
2023-04-27 18:51:51 -0500

    Refactor Dockerfile.ubuntu (#1420)
    * Move elfutils logic into build_elfutils.sh
    
    * Whitespace
    
    * Copy in dependencies.versions
    
    This is no longer done in the base container because it didn't make
    sense there as that image should never change.

--- docker/build_elfutils.sh
+++ docker/build_elfutils.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+set -e
+
+if test x"$1" != xyes; then
+  apt install -qq -y --no-install-recommends elfutils libelf-dev libdw-dev libdebuginfod-dev
+  exit
+fi
+
+apt install -qq -y --no-install-recommends libcurl4-openssl-dev wget libzstd-dev libbz2-dev liblzma-dev
+
 version=$(grep elfutils dependencies.versions | awk '{split($0,a,":"); print a[2]}')
 
 wget --no-check-certificate https://sourceware.org/elfutils/ftp/${version}/elfutils-${version}.tar.bz2
