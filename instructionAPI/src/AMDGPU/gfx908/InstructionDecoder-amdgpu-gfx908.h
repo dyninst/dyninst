648010d849143619b58d00f29708c831474b79c2
wuxx1279 <bbiiggppiigg@gmail.com>
2023-08-02 03:03:57 -0500

    Fix missing parameter for makeRegisterExpression
    During one of the PRs that targets compiler warning,
    the num_elements parameter to makeRegisterExpression is omitted,
    resulting in all register operands are displayed separately,
    whether they should be displayed as a group or not.
    
    In addition to adding the parameter, certern unused helper functions
    and fields of InstructionDecoder-amdgpu-gfx*** has been removed
    to avoid shadowing of variable names.
    
    Note that this change isn't applied to vega as we don't have ISA-XML for
    it.

--- instructionAPI/src/AMDGPU/gfx908/InstructionDecoder-amdgpu-gfx908.h
+++ instructionAPI/src/AMDGPU/gfx908/InstructionDecoder-amdgpu-gfx908.h
@@ -216,21 +216,9 @@ namespace Dyninst {
             uint32_t imm_at_64{};
             uint32_t imm_at_96{};
 
-            bool setSCC{};
-
-#define IS_LD_ST() (isLoad || isStore )
-
-            bool isSMEM{}; // this is set when using smem instruction
-            bool isLoad{}; // this is set when a smem instruction is load, will set number of elements that are loaded at the same time
-            bool isStore{}; // similar to isLoad, but for store instructions
-            bool isBuffer{}; //
-            bool isScratch{};
-
             bool isBranch{}; // this is set for all branch instructions,
             bool isConditional{}; // this is set for all conditional branch instruction, will set branchCond
             bool isCall{}; // this is a call function
-
-
 
             // this is set for instructions that directly modify pc
             // namely s_setpc and s_swappc
@@ -269,14 +257,6 @@ namespace Dyninst {
                     }
 
                 }
-
-            void setSMEM() {isSMEM = true;}
-
-
-
-            void setScratch() {isScratch = true;}
-
-            void setBuffer() {isBuffer = true;}
 
             typedef struct buffer_resource_desc{
                 unsigned long long base_address;
