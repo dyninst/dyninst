cd6e83bf0f1bbb53d75be4a22bdd9b88c0ec455e
wuxx1279 <bbiiggppiigg@gmail.com>
2023-09-01 08:56:34 -0500

    Add a method for updating mnemonic(required for adding extensions)

--- instructionAPI/h/Operation_impl.h
+++ instructionAPI/h/Operation_impl.h
@@ -62,127 +62,135 @@
 // Operation + ExpressionPtrs -> Instruction + Operands
 
 namespace NS_x86 {
-struct ia32_entry;
-class ia32_prefixes;
+    struct ia32_entry;
+    class ia32_prefixes;
 }
 class ia32_locations;
 
 namespace Dyninst
 {
-  namespace InstructionAPI
-  {
-    /// An %Operation object represents a family of opcodes (operation encodings)
-    /// that perform the same task (e.g. the \c MOV family).  It includes
-    /// information about the number of operands, their read/write semantics,
-    /// the implicit register reads and writes, and the control flow behavior
-    /// of a particular assembly language operation.  It additionally provides
-    /// access to the assembly mnemonic, which allows any semantic details that
-    /// are not encoded in the %Instruction representation to be added by higher
-    /// layers of analysis.
-    ///
-    /// As an example, the \c CMP operation on IA32/AMD64 processors has the following
-    /// properties:
-    ///   - %Operand 1 is read, but not written
-    ///   - %Operand 2 is read, but not written
-    ///   - The following flags are written:
-    ///     - Overflow
-    ///     - Sign
-    ///     - Zero
-    ///     - Parity
-    ///     - Carry
-    ///     - Auxiliary
-    ///   - No other registers are read, and no implicit memory operations are performed
-    ///
-    /// %Operations are constructed by the %InstructionDecoder as part of the process
-    /// of constructing an %Instruction.
-    
-    class Operation_impl : public boost::lockable_adapter<boost::recursive_mutex>
+    namespace InstructionAPI
     {
-    public:
-      typedef std::set<RegisterAST::Ptr> registerSet;
-      typedef std::set<Expression::Ptr> VCSet;
-      friend class InstructionDecoder_power; // for editing mnemonics after creation
-      friend class InstructionDecoder_aarch64;
-      friend class InstructionDecoder_amdgpu_vega;
-      friend class InstructionDecoder_amdgpu_gfx908;
-      friend class InstructionDecoder_amdgpu_gfx90a;
-      friend class Instruction; // to make use of the update size function
-      
-    public:
-      INSTRUCTION_EXPORT Operation_impl(NS_x86::ia32_entry* e, NS_x86::ia32_prefixes* p = NULL, ia32_locations* l = NULL,
-                                  Architecture arch = Arch_none);
-      INSTRUCTION_EXPORT Operation_impl(const Operation_impl& o);
-      INSTRUCTION_EXPORT Operation_impl();
-      INSTRUCTION_EXPORT Operation_impl(entryID id, std::string m, Architecture arch);
-      
-      INSTRUCTION_EXPORT const Operation_impl& operator=(const Operation_impl& o);
-      
-      /// Returns the set of registers implicitly read (i.e. those not included in the operands, but read anyway)
-      INSTRUCTION_EXPORT const registerSet& implicitReads() ;
-      /// Returns the set of registers implicitly written (i.e. those not included in the operands, but written anyway)
-      INSTRUCTION_EXPORT const registerSet& implicitWrites() ;
-      /// Returns the mnemonic for the operation.  Like \c instruction::format, this is exposed for debugging
-      /// and will be replaced with stream operators in the public interface.
-      INSTRUCTION_EXPORT std::string format() const;
-      /// Returns the entry ID corresponding to this operation.  Entry IDs are enumerated values that correspond
-      /// to assembly mnemonics.
-      INSTRUCTION_EXPORT entryID getID() const;
-      /// Returns the prefix entry ID corresponding to this operation, if any.
-      /// Prefix IDs are enumerated values that correspond to assembly prefix mnemonics.
-      INSTRUCTION_EXPORT prefixEntryID getPrefixID() const;
+        /// An %Operation object represents a family of opcodes (operation encodings)
+        /// that perform the same task (e.g. the \c MOV family).  It includes
+        /// information about the number of operands, their read/write semantics,
+        /// the implicit register reads and writes, and the control flow behavior
+        /// of a particular assembly language operation.  It additionally provides
+        /// access to the assembly mnemonic, which allows any semantic details that
+        /// are not encoded in the %Instruction representation to be added by higher
+        /// layers of analysis.
+        ///
+        /// As an example, the \c CMP operation on IA32/AMD64 processors has the following
+        /// properties:
+        ///   - %Operand 1 is read, but not written
+        ///   - %Operand 2 is read, but not written
+        ///   - The following flags are written:
+        ///     - Overflow
+        ///     - Sign
+        ///     - Zero
+        ///     - Parity
+        ///     - Carry
+        ///     - Auxiliary
+        ///   - No other registers are read, and no implicit memory operations are performed
+        ///
+        /// %Operations are constructed by the %InstructionDecoder as part of the process
+        /// of constructing an %Instruction.
 
-      /// Returns true if the expression represented by \c candidate is read implicitly.
-      INSTRUCTION_EXPORT bool isRead(Expression::Ptr candidate) ;
-      /// Returns true if the expression represented by \c candidate is written implicitly.
-      INSTRUCTION_EXPORT bool isWritten(Expression::Ptr candidate) ;
-      /// Returns the set of memory locations implicitly read.
-      INSTRUCTION_EXPORT const VCSet& getImplicitMemReads() ;
-      /// Returns the set of memory locations implicitly written.
-      INSTRUCTION_EXPORT const VCSet& getImplicitMemWrites() ;
-      friend std::size_t hash_value(Operation_impl const& op)
-      {
-        size_t seed = 0;
-        boost::hash_combine(seed, op.operationID);
-        boost::hash_combine(seed, op.prefixID);
-        boost::hash_combine(seed, op.archDecodedFrom);
-        boost::hash_combine(seed, op.addrWidth);
-        boost::hash_combine(seed, op.segPrefix);
-        boost::hash_combine(seed, op.isVectorInsn);
-        return seed;
-      }
-      bool operator==(const Operation_impl& rhs) const {
-        return hash_value(*this) == hash_value(rhs);
-      }
-      INSTRUCTION_EXPORT const VCSet& getImplicitMemWrites() const;
-      bool isVectorInsn;
+        class Operation_impl : public boost::lockable_adapter<boost::recursive_mutex>
+        {
+            public:
+                typedef std::set<RegisterAST::Ptr> registerSet;
+                typedef std::set<Expression::Ptr> VCSet;
+                friend class InstructionDecoder_power; // for editing mnemonics after creation
+                friend class InstructionDecoder_aarch64;
+                friend class InstructionDecoder_amdgpu_vega;
+                friend class InstructionDecoder_amdgpu_gfx908;
+                friend class InstructionDecoder_amdgpu_gfx90a;
+                friend class Instruction; // to make use of the update size function
 
-    private:
-        std::once_flag data_initialized;
-      void SetUpNonOperandData();
-      
-      mutable registerSet otherRead;
-      mutable registerSet otherWritten;
-      mutable VCSet otherEffAddrsRead;
-      mutable VCSet otherEffAddrsWritten;
+            public:
+                INSTRUCTION_EXPORT Operation_impl(NS_x86::ia32_entry* e, NS_x86::ia32_prefixes* p = NULL, ia32_locations* l = NULL,
+                        Architecture arch = Arch_none);
+                INSTRUCTION_EXPORT Operation_impl(const Operation_impl& o);
+                INSTRUCTION_EXPORT Operation_impl();
+                INSTRUCTION_EXPORT Operation_impl(entryID id, std::string m, Architecture arch);
 
-    protected:
-        mutable entryID operationID;
-      Architecture archDecodedFrom;
-      prefixEntryID prefixID;
-      Result_Type addrWidth;
-      int segPrefix;
-      mutable std::string mnemonic;
+                INSTRUCTION_EXPORT const Operation_impl& operator=(const Operation_impl& o);
 
-      
-    };
-    struct Operation: public Operation_impl {
-        Operation(entryID id, std::string m, Architecture arch)
+                /// Returns the set of registers implicitly read (i.e. those not included in the operands, but read anyway)
+                INSTRUCTION_EXPORT const registerSet& implicitReads() ;
+                /// Returns the set of registers implicitly written (i.e. those not included in the operands, but written anyway)
+                INSTRUCTION_EXPORT const registerSet& implicitWrites() ;
+                /// Returns the mnemonic for the operation.  Like \c instruction::format, this is exposed for debugging
+                /// and will be replaced with stream operators in the public interface.
+                INSTRUCTION_EXPORT std::string format() const;
+                /// Returns the entry ID corresponding to this operation.  Entry IDs are enumerated values that correspond
+                /// to assembly mnemonics.
+                INSTRUCTION_EXPORT entryID getID() const;
+                /// Returns the prefix entry ID corresponding to this operation, if any.
+                /// Prefix IDs are enumerated values that correspond to assembly prefix mnemonics.
+                INSTRUCTION_EXPORT prefixEntryID getPrefixID() const;
+
+                /// Returns true if the expression represented by \c candidate is read implicitly.
+                INSTRUCTION_EXPORT bool isRead(Expression::Ptr candidate) ;
+                /// Returns true if the expression represented by \c candidate is written implicitly.
+                INSTRUCTION_EXPORT bool isWritten(Expression::Ptr candidate) ;
+                /// Returns the set of memory locations implicitly read.
+                INSTRUCTION_EXPORT const VCSet& getImplicitMemReads() ;
+                /// Returns the set of memory locations implicitly written.
+                INSTRUCTION_EXPORT const VCSet& getImplicitMemWrites() ;
+
+                friend std::size_t hash_value(Operation_impl const& op)
+                {
+                    size_t seed = 0;
+                    boost::hash_combine(seed, op.operationID);
+                    boost::hash_combine(seed, op.prefixID);
+                    boost::hash_combine(seed, op.archDecodedFrom);
+                    boost::hash_combine(seed, op.addrWidth);
+                    boost::hash_combine(seed, op.segPrefix);
+                    boost::hash_combine(seed, op.isVectorInsn);
+                    return seed;
+                }
+
+                bool operator==(const Operation_impl& rhs) const {
+                    return hash_value(*this) == hash_value(rhs);
+                }
+
+                void updateMnemonic(std::string new_mnemonic){
+                    mnemonic = new_mnemonic;
+                }
+
+                INSTRUCTION_EXPORT const VCSet& getImplicitMemWrites() const;
+
+                bool isVectorInsn;
+
+            private:
+                std::once_flag data_initialized;
+                void SetUpNonOperandData();
+
+                mutable registerSet otherRead;
+                mutable registerSet otherWritten;
+                mutable VCSet otherEffAddrsRead;
+                mutable VCSet otherEffAddrsWritten;
+
+            protected:
+                mutable entryID operationID;
+                Architecture archDecodedFrom;
+                prefixEntryID prefixID;
+                Result_Type addrWidth;
+                int segPrefix;
+                mutable std::string mnemonic;
+
+
+        };
+        struct Operation: public Operation_impl {
+            Operation(entryID id, std::string m, Architecture arch)
                 : Operation_impl(id, m, arch)  {}
-        Operation(NS_x86::ia32_entry* e, NS_x86::ia32_prefixes* p = NULL, ia32_locations* l = NULL,
-                Architecture arch = Arch_none) : Operation_impl(e, p, l, arch) {}
-        Operation() : Operation_impl() {}
-    };
-  }
+            Operation(NS_x86::ia32_entry* e, NS_x86::ia32_prefixes* p = NULL, ia32_locations* l = NULL,
+                    Architecture arch = Arch_none) : Operation_impl(e, p, l, arch) {}
+            Operation() : Operation_impl() {}
+        };
+    }
 }
 
 
