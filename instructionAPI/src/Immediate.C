5ea47b80c1658996f40947d5b9fe38245379c385
wuxx1279 <bbiiggppiigg@gmail.com>
2023-09-05 14:31:55 -0500

    redo whitespace and indentation changes

--- instructionAPI/src/Immediate.C
+++ instructionAPI/src/Immediate.C
@@ -100,10 +100,9 @@ namespace Dyninst {
         std::string NamedImmediate::format(formatStyle f) const {
             return name_+std::string(":0x")+Immediate::format(f);
         }
-
         ArmConditionImmediate::ArmConditionImmediate(const Result &val) : Immediate(val) {
             m_condLookupMap = boost::assign::map_list_of(0, "eq")(1, "ne")(2, "cs")(3, "cc")(4, "mi")(5, "pl")(6, "vs")(7, "vc")
-                (8, "hi")(9, "ls")(10, "ge")(11, "lt")(12, "gt")(13, "le")(14, "al")(15, "nv").convert_to_container<std::map<unsigned int, std::string> >();
+                    (8, "hi")(9, "ls")(10, "ge")(11, "lt")(12, "gt")(13, "le")(14, "al")(15, "nv").convert_to_container<std::map<unsigned int, std::string> >();
         }
 
         Immediate::Ptr ArmConditionImmediate::makeArmConditionImmediate(const Result &val) {
@@ -118,31 +117,31 @@ namespace Dyninst {
         std::string ArmConditionImmediate::format(formatStyle) const {
             unsigned int cond_val = eval().convert<unsigned int>();
             if(m_condLookupMap.count(cond_val) > 0)
-                return m_condLookupMap.find(cond_val)->second;
-            else
-                return "Error: Invalid condition code for ARM64!";
+        return m_condLookupMap.find(cond_val)->second;
+        else
+        return "Error: Invalid condition code for ARM64!";
         }
 
-        ArmPrfmTypeImmediate::ArmPrfmTypeImmediate(const Result &val) : Immediate(val) {
-            m_prfmTypeLookupMap = boost::assign::map_list_of(0, "PLDL1KEEP")(1, "PLDL1STRM")(2, "PLDL2KEEP")(3, "PLDL2STRM")(4, "PLDL3KEEP")(5, "PLDL3STRM")(8, "PLIL1KEEP")(9, "PLIL1STRM")(10, "PLIL2KEEP")(11, "PLIL2STRM")(12, "PLIL3KEEP")(13, "PLIL3STRM")(16, "PSTL1KEEP")(17, "PSTL1STRM")(18, "PSTL2KEEP")(19, "PSTL2STRM")(20, "PSTL3KEEP")(21, "PSTL3STRM").convert_to_container<std::map<unsigned int, std::string> >();
-        }
+	ArmPrfmTypeImmediate::ArmPrfmTypeImmediate(const Result &val) : Immediate(val) {
+	    m_prfmTypeLookupMap = boost::assign::map_list_of(0, "PLDL1KEEP")(1, "PLDL1STRM")(2, "PLDL2KEEP")(3, "PLDL2STRM")(4, "PLDL3KEEP")(5, "PLDL3STRM")(8, "PLIL1KEEP")(9, "PLIL1STRM")(10, "PLIL2KEEP")(11, "PLIL2STRM")(12, "PLIL3KEEP")(13, "PLIL3STRM")(16, "PSTL1KEEP")(17, "PSTL1STRM")(18, "PSTL2KEEP")(19, "PSTL2STRM")(20, "PSTL3KEEP")(21, "PSTL3STRM").convert_to_container<std::map<unsigned int, std::string> >();
+	}
 
-        Immediate::Ptr ArmPrfmTypeImmediate::makeArmPrfmTypeImmediate(const Result &val) {
-            Immediate::Ptr ret = make_shared(singleton_object_pool<ArmPrfmTypeImmediate>::construct(val));
-            return ret;
-        }
+	Immediate::Ptr ArmPrfmTypeImmediate::makeArmPrfmTypeImmediate(const Result &val) {
+	    Immediate::Ptr ret = make_shared(singleton_object_pool<ArmPrfmTypeImmediate>::construct(val));
+	    return ret;
+	}
 
-        std::string ArmPrfmTypeImmediate::format(Architecture, formatStyle f) const {
-            return format(f);
-        }
+	std::string ArmPrfmTypeImmediate::format(Architecture, formatStyle f) const {
+	    return format(f);
+	}
 
-        std::string ArmPrfmTypeImmediate::format(formatStyle) const {
-            unsigned prfm_type = eval().convert<unsigned int>();
-            if(m_prfmTypeLookupMap.count(prfm_type) > 0)
-                return m_prfmTypeLookupMap.find(prfm_type)->second;
-            else
-                return "Error: Invalid prefetech memory type for ARM64!";
-        }
+    std::string ArmPrfmTypeImmediate::format(formatStyle) const {
+        unsigned prfm_type = eval().convert<unsigned int>();
+        if(m_prfmTypeLookupMap.count(prfm_type) > 0)
+        return m_prfmTypeLookupMap.find(prfm_type)->second;
+        else
+        return "Error: Invalid prefetech memory type for ARM64!";
+    }
 
 
     }
