8bd073e5bc13932b095ef971fe755f1d1a9512fb
Tim Haines <thaines.astro@gmail.com>
2023-09-07 18:19:01 -0500

    Rename is_fullcu -> is_full_unit

--- dwarf/h/dwarf_cu_info.h
+++ dwarf/h/dwarf_cu_info.h
@@ -38,7 +38,9 @@
 
 namespace Dyninst { namespace DwarfDyninst {
 
-  inline bool is_fullcu(Dwarf_Die die) { return dwarf_tag(&die) == DW_TAG_compile_unit; }
+  // We purposefully don't include DW_TAG_skeleton_unit here as
+  // libdw should merge those into a single CU for us.
+  inline bool is_full_unit(Dwarf_Die die) { return dwarf_tag(&die) == DW_TAG_compile_unit; }
   inline bool is_partialcu(Dwarf_Die die) { return dwarf_tag(&die) == DW_TAG_partial_unit; }
   inline bool is_typecu(Dwarf_Die die) { return dwarf_tag(&die) == DW_TAG_type_unit; }
 
@@ -50,10 +52,7 @@ namespace Dyninst { namespace DwarfDyninst {
     // These are best guess. Ideally, we'd like to interrogate
     // the internals of the die, but that's not currently possible
     // with libdw. The internal function there is `is_cudie`.
-    //
-    // We purposefully don't include DW_TAG_skeleton_unit here as
-    // libdw should merge those into a single CU for us.
-    return is_fullcu(die) || is_partialcu(die) || is_typecu(die);
+    return is_full_unit(die) || is_partialcu(die) || is_typecu(die);
   }
 
   /*
