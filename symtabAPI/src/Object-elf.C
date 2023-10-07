27cf0cae8b01e91ed7d510a0cc3ddd3d716b1388
Tim Haines <thaines.astro@gmail.com>
2023-10-06 23:45:16 -0500

    Remove dead debug code

--- symtabAPI/src/Object-elf.C
+++ symtabAPI/src/Object-elf.C
@@ -2194,7 +2194,6 @@ bool Object::fix_global_symbol_modules_static_dwarf() {
 
         std::string modname = DwarfDyninst::die_name(cu_die);
 
-        //cerr << "Processing CU DIE for " << modname << " offset: " << next_cu_off << endl;
         Address tempModLow;
         Address modLow = 0;
         if (DwarfWalker::findConstant(DW_AT_low_pc, tempModLow, &cu_die, dbg)) {
@@ -2211,7 +2210,6 @@ bool Object::fix_global_symbol_modules_static_dwarf() {
         }
         if (!m->hasRanges())
         {
-//            cout << "No ranges for module " << modname << ", need to extract from statements\n";
             Dwarf_Lines *lines;
             size_t num_lines;
             if (dwarf_getsrclines(&cu_die, &lines, &num_lines) == 0)
@@ -2237,8 +2235,6 @@ bool Object::fix_global_symbol_modules_static_dwarf() {
                             }
 
                         }
-//                        cout << "Adding range [" << hex << low << ", " << high << ") to " << dec <<
-//                             m->fileName() << " based on statements" << endl;
                         m->addRange(low, high);
                     }
                 }
@@ -2246,7 +2242,6 @@ bool Object::fix_global_symbol_modules_static_dwarf() {
         }
         #pragma omp critical
         m->addDebugInfo(cu_die);
-        //cerr << "Files in module " << modname << endl;
         DwarfWalker::buildSrcFiles(dbg, cu_die, m->getStrings());
         // dies_seen.insert(cu_die_off);
     }
