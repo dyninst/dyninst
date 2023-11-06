f133f43af74222c62c36b88f6fff41cda7799aa6
Tim Haines <thaines.astro@gmail.com>
2023-11-06 13:16:25 -0600

    Remove ~AstOperatorNode

--- dyninstAPI/src/ast.h
+++ dyninstAPI/src/ast.h
@@ -534,12 +534,6 @@ class AstOperatorNode : public AstNode {
  public:
 
     AstOperatorNode(opCode opC, AstNodePtr l, AstNodePtr r = AstNodePtr(), AstNodePtr e = AstNodePtr());
-    
-    ~AstOperatorNode() {
-        //printf("at ~AstOperatorode()\n");
-        //debugPrint();
-    }
-
 
    virtual std::string format(std::string indent);
     virtual int costHelper(enum CostStyleType costStyle) const;	
