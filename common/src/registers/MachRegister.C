cfa0e7041bfb2f6775e6c1a76976bf451bc613ec
Tim Haines <thaines.astro@gmail.com>
2023-11-17 16:31:46 -0600

    Add conversion to ROSE register for Resume Flag

--- common/src/registers/MachRegister.C
+++ common/src/registers/MachRegister.C
@@ -813,6 +813,7 @@ namespace Dyninst {
               case x86_64::FLAGD: n = x86_flag_iopl1; break;
               case x86_64::FLAGF: n = x86_flag_15; break;
               case x86_64::VM: n = x86_flag_vm; break;
+              case x86_64::RF: n = x86_flag_rf; break;
               default:
                 c = -1;
                 return;
