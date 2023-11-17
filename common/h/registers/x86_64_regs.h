7a6f22a66434e2a0baa72acabc581661bbf5e7d7
Tim Haines <thaines.astro@gmail.com>
2023-11-17 15:24:38 -0600

    Add comments for aliased GPRs
    No real comments here. Just added a separator.

--- common/h/registers/x86_64_regs.h
+++ common/h/registers/x86_64_regs.h
@@ -88,6 +88,8 @@ namespace Dyninst { namespace x86_64 {
   const signed int YMM    = 0x000B0000;  // YMM0-YMM7 Registers from AVX2/FMA
   const signed int ZMM    = 0x000C0000;  // ZMM0-ZMM7 Registers from AVX-512
   const signed int KMASK  = 0x000D0000;  // K0-K7 opmask Registers from AVX-512
+
+  /* Base IDs for aliased GPRs */
   const signed int BASEA  = 0x0;
   const signed int BASEC  = 0x1;
   const signed int BASED  = 0x2;
