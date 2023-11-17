bf5d43a54cd123ad073926285f17306a775944fa
Tim Haines <thaines.astro@gmail.com>
2023-11-17 15:15:28 -0600

    Improve comments for register categories

--- common/h/registers/x86_64_regs.h
+++ common/h/registers/x86_64_regs.h
@@ -73,20 +73,22 @@ namespace Dyninst { namespace x86_64 {
   const signed int OCT    = 0x00000800; // 128-bit SSE, FC16, XOP, AVX, and FMA3/4
   const signed int YMMS   = 0x00000900; // 256-bit SSE, AVX2, FMA3/4
   const signed int ZMMS   = 0x00000A00; // 512-bit AVX-512/AVX10
-  const signed int GPR    = 0x00010000;
-  const signed int SEG    = 0x00020000;
-  const signed int FLAG   = 0x00030000;
-  const signed int MISC   = 0x00040000;
-  const signed int KMASK  = 0x00050000;
-  const signed int XMM    = 0x00060000;
-  const signed int YMM    = 0x00070000;
-  const signed int ZMM    = 0x00080000;
-  const signed int MMX    = 0x00090000;
-  const signed int CTL    = 0x000A0000;
-  const signed int DBG    = 0x000B0000;
-  const signed int TST    = 0x000C0000;
+
+  /* Register Categories */
+  const signed int GPR    = 0x00010000;  // General-Purpose Registers
+  const signed int SEG    = 0x00020000;  // Segment Registers
+  const signed int FLAG   = 0x00030000;  // EFLAGS Register
+  const signed int MISC   = 0x00040000;  // Internal ProcControlAPI Register
+  const signed int KMASK  = 0x00050000;  // K0-K7 opmask Registers from AVX-512
+  const signed int XMM    = 0x00060000;  // XMM0-XMM7 Registers from SSE
+  const signed int YMM    = 0x00070000;  // YMM0-YMM7 Registers from AVX2/FMA
+  const signed int ZMM    = 0x00080000;  // ZMM0-ZMM7 Registers from AVX-512
+  const signed int MMX    = 0x00090000;  // MM0-MM7 Registers
+  const signed int CTL    = 0x000A0000;  // Control Registers CR0-CR7
+  const signed int DBG    = 0x000B0000;  // Debug Registers DR0-DR7
+  const signed int TST    = 0x000C0000;  // Internal InstructionAPI Registers
   const signed int X87    = 0x000D0000;  // x87 FPU Registers
-  const signed int FLAGS  = 0x00000000;
+  const signed int FLAGS  = 0x00000000;  // RFLAGS Register
   const signed int BASEA  = 0x0;
   const signed int BASEC  = 0x1;
   const signed int BASED  = 0x2;
