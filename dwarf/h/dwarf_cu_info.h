8d922cf3e3ebf3287a2771879cfca4929aef8437
Tim Haines <thaines.astro@gmail.com>
2024-02-12 00:44:55 -0600

    Add dwarf/dwarf_cu_info.h

--- dwarf/h/dwarf_cu_info.h
+++ dwarf/h/dwarf_cu_info.h
@@ -50,39 +50,14 @@ namespace Dyninst { namespace DwarfDyninst {
     return is_full_unit(die) || is_partial_unit(die);
   }
 
-  /*
-   *  Check if `die` corresponds to a DWARF unit that should be parsed
-   *
-   *  DW_TAG_imported_unit may need to be included here, but is currently handled
-   *  separately in DwarfWalker::parse_int.
-   *
-   */
   inline bool is_parseable_unit(Dwarf_Die die) {
     return is_cudie(die) || is_type_unit(die);
   }
 
-  /*
-   * The name of the directory where the source file was compiled.
-   *
-   * If present, it is always an absolute path. Returns an empty string if not found.
-   *
-   */
   inline std::string cu_dirname(Dwarf_Die cuDie) {
       return detail::comp_dir_name(cuDie);
   }
-  /*
-   * The name of the compilation unit (CU) referred to by `cuDie`
-   *
-   *   This is the name of the source file used to create the CU (e.g., test.cpp)
-   *   made into an absolute path (if present, see below).
-   *
-   * The user MUST ensure that `cuDie` refers to a CU (see `dwarf_is_cudie` above). Otherwise,
-   * the returned name is nonsense. It would be very weird- but technically possible- for
-   * a CU die to be artificial. In that case, a unique name is generated.
-   *
-   * If the CU has no name- which would be even weirder- a unique one is created for it.
-   *
-   */
+
   inline std::string cu_name(Dwarf_Die cuDie) {
 
     // This takes care of the DW_AT_artificial case for us.
@@ -92,10 +67,6 @@ namespace Dyninst { namespace DwarfDyninst {
     return detail::absolute_path(name, detail::comp_dir_name(cuDie));
   }
 
-  /*
-   * Find the compilation unit (CU) die that starts at the address `addr`.
-   *
-   */
   inline Dwarf_Die *find_cu(Dwarf *dbg, Dwarf_Addr addr, Dwarf_Die *result) {
     // The .debug_info DIE offset is the starting address for modules.
     if(dwarf_offdie(dbg, addr, result)) {
