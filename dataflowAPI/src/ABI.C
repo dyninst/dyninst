69e18d7a089bef7b85eb1f53d1bd67218b2afe1a
Tim Haines <thaines.astro@gmail.com>
2024-03-15 14:16:15 -0500

    Add aarch64
    I'll break these up later. I don't want to go through the ABI docs
    right now.

--- dataflowAPI/src/ABI.C
+++ dataflowAPI/src/ABI.C
@@ -565,99 +565,108 @@ void ABI::initialize64(){
 //#warning "This is not verified!"
 #if defined(arch_aarch64)
 void ABI::initialize64(){
-    RegisterMap aarch64Map = machRegIndex_aarch64();
-	int sz = aarch64Map.size();
+  auto &reg_index = machRegIndex_aarch64();
+  auto const num_bits = reg_index.size();
 
-	returnRegs64_ = getBitArray(sz);
-    (*returnRegs64_)[aarch64Map[aarch64::x0]] = true;
-    (*returnRegs64_)[aarch64Map[aarch64::q0]] = true;
+  arch64.all.resize(num_bits);
+  arch64.callRead.resize(num_bits);
+  arch64.callWritten.resize(num_bits);
+  arch64.params.resize(num_bits);
+  arch64.returnRead.resize(num_bits);
+  arch64.returnValues.resize(num_bits);
+  arch64.syscallRead.resize(num_bits);
+  arch64.syscallWritten.resize(num_bits);
 
-	returnRead64_ = getBitArray(sz);
-    (*returnRead64_)[aarch64Map[aarch64::x0]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q0]] = true;
-    //Callee-saved registers
-    //First, GPRs...
-    (*returnRead64_)[aarch64Map[aarch64::x19]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x20]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x21]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x22]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x23]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x24]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x25]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x26]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x27]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::x28]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::sp]] = true;
-    //Now, SIMD regs...
-    (*returnRead64_)[aarch64Map[aarch64::q8]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q9]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q10]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q11]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q12]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q13]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q14]] = true;
-    (*returnRead64_)[aarch64Map[aarch64::q15]] = true;
+  arch64.returnValues[reg_index[aarch64::x0]] = true;
+  arch64.returnValues[reg_index[aarch64::q0]] = true;
 
-    callParam64_ = getBitArray(sz);
-    (*callParam64_)[aarch64Map[aarch64::x0]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x1]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x2]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x3]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x4]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x5]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x6]] = true;
-    (*callParam64_)[aarch64Map[aarch64::x7]] = true;
+  arch64.returnRead[reg_index[aarch64::x0]] = true;
+  arch64.returnRead[reg_index[aarch64::q0]] = true;
 
-	callRead64_ = getBitArray(sz);
-	//First, GPRs...
-	(*callRead64_)[aarch64Map[aarch64::x0]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x1]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x2]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x3]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x4]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x5]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x6]] = true;
-	(*callRead64_)[aarch64Map[aarch64::x7]] = true;
-	//Now, SIMD regs...
-	(*callRead64_)[aarch64Map[aarch64::q0]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q1]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q2]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q3]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q4]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q5]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q6]] = true;
-	(*callRead64_)[aarch64Map[aarch64::q7]] = true;
+  //Callee-saved registers
+  //First, GPRs...
+  arch64.returnRead[reg_index[aarch64::x19]] = true;
+  arch64.returnRead[reg_index[aarch64::x20]] = true;
+  arch64.returnRead[reg_index[aarch64::x21]] = true;
+  arch64.returnRead[reg_index[aarch64::x22]] = true;
+  arch64.returnRead[reg_index[aarch64::x23]] = true;
+  arch64.returnRead[reg_index[aarch64::x24]] = true;
+  arch64.returnRead[reg_index[aarch64::x25]] = true;
+  arch64.returnRead[reg_index[aarch64::x26]] = true;
+  arch64.returnRead[reg_index[aarch64::x27]] = true;
+  arch64.returnRead[reg_index[aarch64::x28]] = true;
+  arch64.returnRead[reg_index[aarch64::sp]] = true;
 
-	callWritten64_ = callRead64_;
-	//First, GPRs...
-	(*callWritten64_)[aarch64Map[aarch64::x9]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::x10]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::x11]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::x12]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::x13]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::x14]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::x15]] = true;
-	//Now, SIMD regs...
-	(*callWritten64_)[aarch64Map[aarch64::q16]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q17]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q18]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q19]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q20]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q21]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q22]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q23]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q24]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q25]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q26]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q27]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q28]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q29]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q30]] = true;
-	(*callWritten64_)[aarch64Map[aarch64::q31]] = true;
+  //Now, SIMD regs...
+  arch64.returnRead[reg_index[aarch64::q8]] = true;
+  arch64.returnRead[reg_index[aarch64::q9]] = true;
+  arch64.returnRead[reg_index[aarch64::q10]] = true;
+  arch64.returnRead[reg_index[aarch64::q11]] = true;
+  arch64.returnRead[reg_index[aarch64::q12]] = true;
+  arch64.returnRead[reg_index[aarch64::q13]] = true;
+  arch64.returnRead[reg_index[aarch64::q14]] = true;
+  arch64.returnRead[reg_index[aarch64::q15]] = true;
 
-	syscallRead64_ = &getBitArray(sz)->set();
-	syscallWritten64_ = &getBitArray(sz)->set();
+  arch64.params[reg_index[aarch64::x0]] = true;
+  arch64.params[reg_index[aarch64::x1]] = true;
+  arch64.params[reg_index[aarch64::x2]] = true;
+  arch64.params[reg_index[aarch64::x3]] = true;
+  arch64.params[reg_index[aarch64::x4]] = true;
+  arch64.params[reg_index[aarch64::x5]] = true;
+  arch64.params[reg_index[aarch64::x6]] = true;
+  arch64.params[reg_index[aarch64::x7]] = true;
 
-	allRegs64_ = &getBitArray(sz)->set();
+  //First, GPRs...
+  arch64.callRead[reg_index[aarch64::x0]] = true;
+  arch64.callRead[reg_index[aarch64::x1]] = true;
+  arch64.callRead[reg_index[aarch64::x2]] = true;
+  arch64.callRead[reg_index[aarch64::x3]] = true;
+  arch64.callRead[reg_index[aarch64::x4]] = true;
+  arch64.callRead[reg_index[aarch64::x5]] = true;
+  arch64.callRead[reg_index[aarch64::x6]] = true;
+  arch64.callRead[reg_index[aarch64::x7]] = true;
+  //Now, SIMD regs...
+  arch64.callRead[reg_index[aarch64::q0]] = true;
+  arch64.callRead[reg_index[aarch64::q1]] = true;
+  arch64.callRead[reg_index[aarch64::q2]] = true;
+  arch64.callRead[reg_index[aarch64::q3]] = true;
+  arch64.callRead[reg_index[aarch64::q4]] = true;
+  arch64.callRead[reg_index[aarch64::q5]] = true;
+  arch64.callRead[reg_index[aarch64::q6]] = true;
+  arch64.callRead[reg_index[aarch64::q7]] = true;
+
+  arch64.callWritten = arch64.callRead;
+
+  //First, GPRs...
+  arch64.callWritten[reg_index[aarch64::x9]] = true;
+  arch64.callWritten[reg_index[aarch64::x10]] = true;
+  arch64.callWritten[reg_index[aarch64::x11]] = true;
+  arch64.callWritten[reg_index[aarch64::x12]] = true;
+  arch64.callWritten[reg_index[aarch64::x13]] = true;
+  arch64.callWritten[reg_index[aarch64::x14]] = true;
+  arch64.callWritten[reg_index[aarch64::x15]] = true;
+
+  //Now, SIMD regs...
+  arch64.callWritten[reg_index[aarch64::q16]] = true;
+  arch64.callWritten[reg_index[aarch64::q17]] = true;
+  arch64.callWritten[reg_index[aarch64::q18]] = true;
+  arch64.callWritten[reg_index[aarch64::q19]] = true;
+  arch64.callWritten[reg_index[aarch64::q20]] = true;
+  arch64.callWritten[reg_index[aarch64::q21]] = true;
+  arch64.callWritten[reg_index[aarch64::q22]] = true;
+  arch64.callWritten[reg_index[aarch64::q23]] = true;
+  arch64.callWritten[reg_index[aarch64::q24]] = true;
+  arch64.callWritten[reg_index[aarch64::q25]] = true;
+  arch64.callWritten[reg_index[aarch64::q26]] = true;
+  arch64.callWritten[reg_index[aarch64::q27]] = true;
+  arch64.callWritten[reg_index[aarch64::q28]] = true;
+  arch64.callWritten[reg_index[aarch64::q29]] = true;
+  arch64.callWritten[reg_index[aarch64::q30]] = true;
+  arch64.callWritten[reg_index[aarch64::q31]] = true;
+
+  arch64.syscallRead.set();
+  arch64.syscallWritten.set();
+
+  arch64.all.set();
 }
 #endif
