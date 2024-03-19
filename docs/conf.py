469c0a3fe109f32fdcfa2d4246d4a57af0ab9b77
Tim Haines <thaines.astro@gmail.com>
2024-03-19 08:26:53 -0500

    Don't use expandable TOC

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "collapse_navigation" : False,
+    "collapse_navigation" : True,
     "sticky_navigation" : False,
     "display_version" : True
 }
