0b94a5cfa903a81820001392e2a13b14dd9468de
Tim Haines <thaines.astro@gmail.com>
2024-05-03 16:10:36 -0500

    Remove re-def guard for DYNINST_EXPORT

--- common/h/dyninst_visibility.h
+++ common/h/dyninst_visibility.h
@@ -31,8 +31,6 @@
 #ifndef DYNINST_COMMON_DYNINST_VISIBILITY_H
 #define DYNINST_COMMON_DYNINST_VISIBILITY_H
 
-#ifndef DYNINST_EXPORT
-# define DYNINST_EXPORT __attribute__((visibility ("default")))
-#endif
+#define DYNINST_EXPORT __attribute__((visibility ("default")))
 
 #endif
