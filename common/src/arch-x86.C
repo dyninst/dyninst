e2d9a449f7af941c0aadafee121eea7b9b7067c1
Kirill Batuzov <batuzovk@ispras.ru>
2024-05-23 17:00:48 +0300

    x86: only group opcodes from groupMap2 can be VEX-prefixed

--- common/src/arch-x86.C
+++ common/src/arch-x86.C
@@ -9949,7 +9949,6 @@ int ia32_decode_opcode(unsigned int capa, const unsigned char *addr, ia32_instru
                 {
                     idx = gotit->tabidx;
                     unsigned int reg  = (addr[0] >> 3) & 7;
-                    vextab = true;
                     if(idx < Grp12)
                         switch(idx)
                         {
@@ -9972,6 +9971,7 @@ int ia32_decode_opcode(unsigned int capa, const unsigned char *addr, ia32_instru
                         unsigned int mod = addr[0] >> 6;
                         gotit = &groupMap2[idx-Grp12][mod==3][reg];
                         nxtab = gotit->otable;
+                        vextab = true;
                     }
                     break;
                 }
