36024678d22acf43d30e00dbaffbae7b58f9466f
wxrdnx <wxrdnx@protonmail.com>
2024-09-16 21:07:17 -0500

    Use array instead of hash

--- dataflowAPI/sail/sail_lex.pl
+++ dataflowAPI/sail/sail_lex.pl
@@ -12,78 +12,79 @@ sub tokenize {
         my $token_type = "";
 
         # Note that the regex order is important!
-        my %regexes = (
-            "L_UNDEFINED" => qr/^(undefined)/,
-            "L_TRUE" => qr/^(true)/,
-            "L_FALSE" => qr/^(false)/,
-            "L_LET" => qr/^(let)/,
-            "L_MATCH" => qr/^(match)/,
-            "L_IF" => qr/^(if)/,
-            "L_THEN" => qr/^(then)/,
-            "L_ELSE" => qr/^(else)/,
-            "L_ASSERT" => qr/^(assert)/,
-            "L_SIZEOF" => qr/^(sizeof)/,
-            "L_IDENT" => qr/^([0-9a-zA-Z_\']+)/,
+        my @regexes = (
+            ["L_UNDEFINED", qr/^(undefined)/],
+            ["L_TRUE", qr/^(true)/],
+            ["L_FALSE", qr/^(false)/],
+            ["L_LET", qr/^(let)/],
+            ["L_MATCH", qr/^(match)/],
+            ["L_IF", qr/^(if)/],
+            ["L_THEN", qr/^(then)/],
+            ["L_ELSE", qr/^(else)/],
+            ["L_ASSERT", qr/^(assert)/],
+            ["L_SIZEOF", qr/^(sizeof)/],
+            ["L_IDENT", qr/^([0-9a-zA-Z_\']+)/],
 
-            "L_BIN" => qr/^(0b[0-1_]+)/,
-            "L_HEX" => qr/^(0x[A-Fa-f0-9_]+)/,
-            "L_POWER" => qr/^(2\s*\^)/,
+            ["L_BIN", qr/^(0b[0-1_]+)/],
+            ["L_HEX", qr/^(0x[A-Fa-f0-9_]+)/],
+            ["L_POWER", qr/^(2\s*\^)/],
 
-            "L_STRING" => qr/^"([^"\\]|\\.)*"/,
+            ["L_STRING", qr/^"([^"\\]|\\.)*"/],
 
-            "L_ADD" => qr/^(\+)/,
-            "L_SUB" => qr/^(\-)/,
-            "L_AND" => qr/^(\&)/,
-            "L_OR" => qr/^(\|)/,
-            "L_XOR" => qr/^(\^)/,
-            "L_AT" => qr/^(\@)/,
-            "L_MUL" => qr/^(\*)/,
+            ["L_ADD", qr/^(\+)/],
+            ["L_SUB", qr/^(\-)/],
+            ["L_AND", qr/^(\&)/],
+            ["L_OR", qr/^(\|)/],
+            ["L_XOR", qr/^(\^)/],
+            ["L_AT", qr/^(\@)/],
+            ["L_MUL", qr/^(\*)/],
 
-            "L_COMMA" => qr/^(\,)/,
-            "L_COLON" => qr/^(\:)/,
-            "L_SEMICOLON" => qr/^(\;)/,
+            ["L_COMMA", qr/^(\,)/],
+            ["L_COLON", qr/^(\:)/],
+            ["L_SEMICOLON", qr/^(\;)/],
 
-            "L_RANGE" => qr/^(\.\.)/,
-            "L_DOT" => qr/^(\.)/,
+            ["L_RANGE", qr/^(\.\.)/],
+            ["L_DOT", qr/^(\.)/],
 
-            "L_EQUAL" => qr/^(\=\=)/,
-            "L_ARROW" => qr/^(\=\>)/,
-            "L_ASSIGN" => qr/^(\=)/,
+            ["L_EQUAL", qr/^(\=\=)/],
+            ["L_MATCH_ARROW", qr/^(\=\>)/],
+            ["L_ASSIGN", qr/^(\=)/],
 
-            "L_NOT_EQUAL" => qr/^(\!\=)/,
+            ["L_NOT_EQUAL", qr/^(\!\=)/],
 
-            "L_SHIFT_LEFT" => qr/^(\<\<)/,
-            "L_LESS_THAN" => qr/^(\<)/,
-            "L_LESS_THAN_EQUALS" => qr/^(\<\=)/,
-            "L_LESS_THAN_SIGNED" => qr/^(\<_s)/,
-            "L_LESS_THAN_UNSIGNED" => qr/^(\<_u)/,
-            "L_LESS_THAN_EQUALS_SIGNED" => qr/^(\<\=_s)/,
-            "L_LESS_THAN_EQUALS_UNSIGNED" => qr/^(\<\=_u)/,
+            ["L_SHIFT_LEFT", qr/^(\<\<)/],
+            ["L_LESS_THAN", qr/^(\<)/],
+            ["L_LESS_THAN_EQUALS", qr/^(\<\=)/],
+            ["L_LESS_THAN_SIGNED", qr/^(\<_s)/],
+            ["L_LESS_THAN_UNSIGNED", qr/^(\<_u)/],
+            ["L_LESS_THAN_EQUALS_SIGNED", qr/^(\<\=_s)/],
+            ["L_LESS_THAN_EQUALS_UNSIGNED", qr/^(\<\=_u)/],
 
-            "L_SHIFT_RIGHT" => qr/^(\>\>)/,
-            "L_GREATER_THAN" => qr/^(\>)/,
-            "L_GREATER_THAN_EQUALS" => qr/^(\>\=)/,
-            "L_GREATER_THAN_SIGNED" => qr/^(\>_s)/,
-            "L_GREATER_THAN_UNSIGNED" => qr/^(\>_u)/,
-            "L_GREATER_THAN_EQUALS_SIGNED" => qr/^(\>\=_s)/,
-            "L_GREATER_THAN_EQUALS_UNSIGNED" => qr/^(\>\=_u)/,
+            ["L_SHIFT_RIGHT", qr/^(\>\>)/],
+            ["L_GREATER_THAN", qr/^(\>)/],
+            ["L_GREATER_THAN_EQUALS", qr/^(\>\=)/],
+            ["L_GREATER_THAN_SIGNED", qr/^(\>_s)/],
+            ["L_GREATER_THAN_UNSIGNED", qr/^(\>_u)/],
+            ["L_GREATER_THAN_EQUALS_SIGNED", qr/^(\>\=_s)/],
+            ["L_GREATER_THAN_EQUALS_UNSIGNED", qr/^(\>\=_u)/],
 
-            "L_UNIT" => qr/^(\(\))/,
-            "L_LEFT_PAREN" => qr/^(\()/,
-            "L_RIGHT_PAREN" => qr/^(\))/,
-            "L_LEFT_BRACK" => qr/^(\[)/,
-            "L_RIGHT_BRACK" => qr/^(\])/,
-            "L_LEFT_BRACE" => qr/^(\{)/,
-            "L_RIGHT_BRACE" => qr/^(\})/,
+            ["L_UNIT", qr/^(\(\))/],
+            ["L_LEFT_PAREN", qr/^(\()/],
+            ["L_RIGHT_PAREN", qr/^(\))/],
+            ["L_LEFT_BRACK", qr/^(\[)/],
+            ["L_RIGHT_BRACK", qr/^(\])/],
+            ["L_LEFT_BRACE", qr/^(\{)/],
+            ["L_RIGHT_BRACE", qr/^(\})/],
 
             # Comments and spaces
-            "L_SINGLE_LINE_COMMENT" => qr/^\/\/.*\n/,
-            "L_DOUBLE_LINE_COMMENT" => qr/^\/\*.*\*\//,
-            "L_SPACE" => qr/^\s+/,
+            ["L_SINGLE_LINE_COMMENT", qr/^\/\/.*\n/],
+            ["L_DOUBLE_LINE_COMMENT", qr/^\/\*.*\*\//],
+            ["L_SPACE", qr/^\s+/],
         );
 
         my $match = 0;
-        while (my ($token_type, $regex) = each %regexes) {
+        foreach my $r (@regexes) {
+            my ($token_type, $regex) = @$r;
             if ($string =~ /$regex/gms) {
                 $match = 1;
                 $string =~ s/$regex//s;
