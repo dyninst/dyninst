fb575d40325deff1a1272708eb265ac861c93ec3
Tim Haines <thaines.astro@gmail.com>
2024-10-13 21:47:21 -0500

    Add external control instructions missing from Capstone
    These come from Chapter 5 External Control of v2.0 of the ISA manual.
    These aren't in Capstone because they are missing from LLVM.

--- instructionAPI/capstone/ppc/mnemonics.py
+++ instructionAPI/capstone/ppc/mnemonics.py
@@ -67,6 +67,9 @@ class mnemonics:
       # 4.6.5 Floating-Point Move Instructions added in v2.07
       "fmrgew", "fmrgow",
       
+      # 5.1.1 External Access Instructions (added before v2.01, removed in v3.0)
+      "eciwx", "ecowx",
+      
       # 5.6.1 DFP Arithmetic Instructions added in v2.05 (Decimal Floating-Point)
       "dadd", "daddq", "dcffix", "dcffixq", "dcffixqq", "dcmpo", "dcmpoq", "dcmpu", 
       "dcmpuq", "dctdp", "dctfix", "dctfixq", "dctqpq", "ddedpd", "ddedpdq", "ddiv", 
