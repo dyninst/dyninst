de98a78e00642c240d19f38933a4eff0f1ddba7b
Tim Haines <thaines.astro@gmail.com>
2024-11-20 15:59:10 -0600

    Clean up, extend, and document Instruction categories (#1817)

--- instructionAPI/src/InstructionCategories.C
+++ instructionAPI/src/InstructionCategories.C
@@ -1,28 +1,28 @@
 /*
  * See the dyninst/COPYRIGHT file for copyright information.
- * 
+ *
  * We provide the Paradyn Tools (below described as "Paradyn")
  * on an AS IS basis, and do not warrant its validity or performance.
  * We reserve the right to update, modify, or discontinue this
  * software at any time.  We shall have no obligation to supply such
  * updates or modifications or any other form of support to you.
- * 
+ *
  * By your use of Paradyn, you understand and agree that we (or any
  * other person or entity with proprietary rights in Paradyn) are
  * under no obligation to provide either maintenance services,
  * update services, notices of latent defects, or correction of
  * defects for Paradyn.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -31,26 +31,25 @@
 #include "InstructionCategories.h"
 #include "entryIDs.h"
 
-namespace Dyninst
-{
-  namespace InstructionAPI
-  {
-    InsnCategory entryToCategory(entryID e)
-    {
-      switch(e)
-      {
+namespace Dyninst { namespace InstructionAPI {
+
+  InsnCategory entryToCategory(entryID e) {
+    switch(e) {
       case e_ret_near:
       case e_ret_far:
       case aarch64_op_ret:
-	return c_ReturnInsn;
+        return c_ReturnInsn;
+
       case amdgpu_gfx908_op_S_ENDPGM: // special treatment for endpgm
       case amdgpu_gfx90a_op_S_ENDPGM: // special treatment for endpgm
       case amdgpu_gfx940_op_S_ENDPGM: // special treatment for endpgm
-    return c_GPUKernelExitInsn;
+        return c_GPUKernelExitInsn;
+
       case e_call:
       case aarch64_op_bl:
       case aarch64_op_blr:
-	return c_CallInsn;
+        return c_CallInsn;
+
       case e_jmp:
       case e_jb:
       case e_jb_jnaej_j:
@@ -80,47 +79,48 @@ namespace Dyninst
       case aarch64_op_tbnz:
       case aarch64_op_cbz:
       case aarch64_op_cbnz:
-      case aarch64_op_br: 
+      case aarch64_op_br:
+      case power_op_b:
+      case power_op_bc:
+      case power_op_bcctr:
+      case power_op_bclr:
 #include "amdgpu_branchinsn_table.h"
-	return c_BranchInsn;
-          case e_cmp:
-          case e_cmppd:
-          case e_cmpps:
-          case e_cmpsb:
-          case e_cmpsd:
-          case e_cmpss:
-          case e_cmpsw:
-          case e_cmpxchg:
-          case e_cmpxchg8b:
-          case power_op_cmp:
-          case power_op_cmpi:
-          case power_op_cmpl:
-          case power_op_cmpli:
-              return c_CompareInsn;
-          case e_prefetch:
-          case e_prefetchnta:
-          case e_prefetcht0:
-          case e_prefetcht1:
-          case e_prefetcht2:
-          case e_prefetch_w:
-          case e_prefetchw:
-              return c_PrefetchInsn;
-          case power_op_b:
-          case power_op_bc:
-          case power_op_bcctr:
-          case power_op_bclr:
-              return c_BranchInsn;        
+        return c_BranchInsn;
+
+      case e_cmp:
+      case e_cmppd:
+      case e_cmpps:
+      case e_cmpsb:
+      case e_cmpsd:
+      case e_cmpss:
+      case e_cmpsw:
+      case e_cmpxchg:
+      case e_cmpxchg8b:
+      case power_op_cmp:
+      case power_op_cmpi:
+      case power_op_cmpl:
+      case power_op_cmpli:
+        return c_CompareInsn;
+
+      case e_prefetch:
+      case e_prefetchnta:
+      case e_prefetcht0:
+      case e_prefetcht1:
+      case e_prefetcht2:
+      case e_prefetch_w:
+      case e_prefetchw:
+        return c_PrefetchInsn;
+
       case e_sysenter:
-	return c_SysEnterInsn;
+        return c_SysEnterInsn;
+
       case e_syscall:
       case e_int:
-    return c_SyscallInsn;
-          default:
-	return c_NoCategory;
-      }
-      
+        return c_SyscallInsn;
 
+      default:
+      	return c_NoCategory;
     }
-    
   }
-}
+
+}}
