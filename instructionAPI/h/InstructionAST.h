13e2a882fd7b460ad4cb472a9803ffdd4c18633a
Tim Haines <thaines.astro@gmail.com>
2024-11-22 14:32:44 -0600

    InstructionAST - whitespace

--- instructionAPI/h/InstructionAST.h
+++ instructionAPI/h/InstructionAST.h
@@ -1,28 +1,28 @@
 /*
  * See the dyninst/COPYRIGHT file for copyright information.
- * 
+ *
  * We provide the Paradyn Tools (below described as "Paradyn")
  * on an AS IS basis, and do not warrant its validity or performance.
  * We reserve the right to update, modify, or discontinue this
  * software at any time.  We shall have no obligation to supply such
  * updates or modifications or any other form of support to you.
- * 
+ *
  * By your use of Paradyn, you understand and agree that we (or any
  * other person or entity with proprietary rights in Paradyn) are
  * under no obligation to provide either maintenance services,
  * update services, notices of latent defects, or correction of
  * defects for Paradyn.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -31,62 +31,51 @@
 #if !defined(INSTRUCTIONAST_H)
 #define INSTRUCTIONAST_H
 
-
+#include "ArchSpecificFormatters.h"
+#include "Result.h"
+#include "boost/enable_shared_from_this.hpp"
 #include "util.h"
+
+#include <iostream>
+#include <set>
 #include <string>
 #include <vector>
-#include <set>
-#include <iostream>
-#include "Result.h"
-#include "ArchSpecificFormatters.h"
-#include "boost/enable_shared_from_this.hpp"
 
-namespace Dyninst
-{
-  namespace InstructionAPI
-  {
-  
-    class InstructionAST;
-  
-    using std::vector;
-    using std::set;
-    enum formatStyle
-    {
-        defaultStyle,
-        memoryAccessStyle
-    };
+namespace Dyninst { namespace InstructionAPI {
 
-    class DYNINST_EXPORT InstructionAST : public boost::enable_shared_from_this<InstructionAST>
-    {
-    public:
-      typedef boost::shared_ptr<InstructionAST> Ptr;
+  class InstructionAST;
 
-      InstructionAST();
-      InstructionAST(const InstructionAST&) = default;
-      virtual ~InstructionAST();
+  using std::set;
+  using std::vector;
 
-      bool operator==(const InstructionAST& rhs) const;
+  enum formatStyle { defaultStyle, memoryAccessStyle };
 
-      virtual void getChildren(vector<InstructionAST::Ptr>& children) const = 0;
+  class DYNINST_EXPORT InstructionAST : public boost::enable_shared_from_this<InstructionAST> {
+  public:
+    typedef boost::shared_ptr<InstructionAST> Ptr;
 
-      virtual void getUses(set<InstructionAST::Ptr>& uses) = 0;
+    InstructionAST();
+    InstructionAST(const InstructionAST&) = default;
+    virtual ~InstructionAST();
 
-      virtual bool isUsed(InstructionAST::Ptr findMe) const = 0;
+    bool operator==(const InstructionAST& rhs) const;
 
-      virtual std::string format(Architecture arch, formatStyle how = defaultStyle) const = 0;
+    virtual void getChildren(vector<InstructionAST::Ptr>& children) const = 0;
 
-      virtual std::string format(formatStyle how = defaultStyle) const = 0;
-  
-    protected:
-      friend class MultiRegisterAST;
-      friend class RegisterAST;
-      friend class Immediate;
-      virtual bool isStrictEqual(const InstructionAST& rhs) const= 0;
-      virtual bool checkRegID(MachRegister, unsigned int = 0, unsigned int = 0) const;
-      virtual const Result& eval() const = 0;
-    };
-  }
-}
+    virtual void getUses(set<InstructionAST::Ptr>& uses) = 0;
+    virtual bool isUsed(InstructionAST::Ptr findMe) const = 0;
 
+    virtual std::string format(Architecture arch, formatStyle how = defaultStyle) const = 0;
+    virtual std::string format(formatStyle how = defaultStyle) const = 0;
+
+  protected:
+    friend class MultiRegisterAST;
+    friend class RegisterAST;
+    friend class Immediate;
+    virtual bool isStrictEqual(const InstructionAST& rhs) const = 0;
+    virtual bool checkRegID(MachRegister, unsigned int = 0, unsigned int = 0) const;
+    virtual const Result& eval() const = 0;
+  };
+}}
 
 #endif
--- instructionAPI/src/InstructionAST.C
+++ instructionAPI/src/InstructionAST.C
@@ -1,68 +1,54 @@
 /*
  * See the dyninst/COPYRIGHT file for copyright information.
- * 
+ *
  * We provide the Paradyn Tools (below described as "Paradyn")
  * on an AS IS basis, and do not warrant its validity or performance.
  * We reserve the right to update, modify, or discontinue this
  * software at any time.  We shall have no obligation to supply such
  * updates or modifications or any other form of support to you.
- * 
+ *
  * By your use of Paradyn, you understand and agree that we (or any
  * other person or entity with proprietary rights in Paradyn) are
  * under no obligation to provide either maintenance services,
  * update services, notices of latent defects, or correction of
  * defects for Paradyn.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "../h/InstructionAST.h"
 #include "../h/Expression.h"
+#include "../h/InstructionAST.h"
+
 #include <string>
 
-namespace Dyninst
-{
-  namespace InstructionAPI
-  {
-    static DummyExpr dummyExpr;
+namespace Dyninst { namespace InstructionAPI {
+  static DummyExpr dummyExpr;
 
-    InstructionAST::InstructionAST()
-    {
-    }
-    InstructionAST::~InstructionAST()
-    {
-    }
-    
+  InstructionAST::InstructionAST() {}
 
-    bool InstructionAST::operator==(const InstructionAST& rhs) const
-    {
-      // isStrictEqual assumes rhs and this to be of the same derived type
-      // so isSameType enforces this restriction
-        if((typeid(*this) == typeid(dummyExpr)) ||
-            (typeid(rhs) == typeid(dummyExpr)))
-        {
-            return true;
-        }
-        return((typeid(*this) == typeid(rhs)) && isStrictEqual(rhs));
-    }
-    bool InstructionAST::checkRegID(MachRegister, unsigned int, unsigned int) const
-    {
-        return false;
-    }
+  InstructionAST::~InstructionAST() {}
 
+  bool InstructionAST::operator==(const InstructionAST& rhs) const {
+    // isStrictEqual assumes rhs and this to be of the same derived type
+    // so isSameType enforces this restriction
+    if((typeid(*this) == typeid(dummyExpr)) || (typeid(rhs) == typeid(dummyExpr))) {
+      return true;
+    }
+    return ((typeid(*this) == typeid(rhs)) && isStrictEqual(rhs));
   }
-}
 
-  
+  bool InstructionAST::checkRegID(MachRegister, unsigned int, unsigned int) const { return false; }
+
+}}
