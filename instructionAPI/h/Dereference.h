dd3465055e67935f1ec045fb7d22d5e664c7b94e
Tim Haines <thaines.astro@gmail.com>
2024-11-22 14:07:27 -0600

    Dereference - remove dead debug macros

--- instructionAPI/h/Dereference.h
+++ instructionAPI/h/Dereference.h
@@ -81,95 +81,12 @@ namespace Dyninst
       virtual std::string format(formatStyle) const
       {
     std::string retVal;
-#if defined(DEBUG_MEMORY_ACCESS_WIDTH)
-        switch(Expression::userSetValue.type)
-        {
-            case u8:
-                retVal += "u8 @ ";
-                break;
-            case s8:
-                retVal += "s8 @ ";
-                break;
-            case u16:
-                retVal += "u16 @ ";
-                break;
-            case s16:
-                retVal += "s16 @ ";
-                break;
-            case u32:
-                retVal += "u32 @ ";
-                break;
-            case s32:
-                retVal += "s32 @ ";
-                break;
-            case u64:
-                retVal += "u64 @ ";
-                break;
-            case s64:
-                retVal += "s64 @ ";
-                break;
-            case sp_float:
-                retVal += "float @ ";
-                break;
-            case dp_float:
-                retVal += "double @ ";
-                break;
-            case dbl128:
-                retVal += "packed double @ ";
-                break;
-            default:
-                retVal += "UNKNOWN SIZE @ ";
-                break;
-        }
-#endif
     retVal += "[" + addressToDereference->format() + "]";
         return retVal;
       }
 
         virtual std::string format(Architecture arch, formatStyle) const
         {
-#if defined(DEBUG_MEMORY_ACCESS_WIDTH)
-            switch(Expression::userSetValue.type)
-            {
-                case u8:
-                    retVal << "u8 @ ";
-                    break;
-                case s8:
-                    retVal << "s8 @ ";
-                    break;
-                case u16:
-                    retVal << "u16 @ ";
-                    break;
-                case s16:
-                    retVal << "s16 @ ";
-                    break;
-                case u32:
-                    retVal << "u32 @ ";
-                    break;
-                case s32:
-                    retVal << "s32 @ ";
-                    break;
-                case u64:
-                    retVal << "u64 @ ";
-                    break;
-                case s64:
-                    retVal << "s64 @ ";
-                    break;
-                case sp_float:
-                    retVal << "float @ ";
-                    break;
-                case dp_float:
-                    retVal << "double @ ";
-                    break;
-                case dbl128:
-                    retVal << "packed double @ ";
-                    break;
-                default:
-                    retVal << "UNKNOWN SIZE @ ";
-                    break;
-            }
-#endif
-
             return ArchSpecificFormatter::getFormatter(arch).formatDeref(addressToDereference->format(arch));
         }
 
