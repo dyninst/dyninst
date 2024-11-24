61648d633489a191074e386b8bc84e6397de3076
wxrdnx <wxrdnx@protonmail.com>
2024-11-24 14:38:20 -0600

    Add RISC-V guards

--- proccontrol/src/loadLibrary/codegen.C
+++ proccontrol/src/loadLibrary/codegen.C
@@ -128,6 +128,8 @@ bool Codegen::generateCall(Address addr, const std::vector<Address> &args) {
          return generateCallPPC32(addr, args);
       case Arch_ppc64:
          return generateCallPPC64(addr, args);
+      case Arch_riscv64:
+         return generateCallRISCV64(addr, args);
 #endif //!defined(os_windows)
 #if defined(DYNINST_HOST_ARCH_AARCH64)
       case Arch_aarch64:
@@ -154,7 +156,10 @@ bool Codegen::generateNoops() {
          break;
       case Arch_aarch64:
          copyInt(0xd503201f);
-	 break;
+         break;
+      case Arch_riscv64:
+         // TODO
+         break;
       default:
          return false;
          break;
@@ -174,7 +179,10 @@ bool Codegen::generateTrap() {
          break;
       case Arch_aarch64:
          copyInt(0xd4200000);
-	 break;
+         break;
+      case Arch_riscv64:
+         // TODO
+         break;
       default:
          return false;
          break;
@@ -194,6 +202,8 @@ bool Codegen::generatePreamble() {
          return generatePreamblePPC32();
       case Arch_ppc64:
          return generatePreamblePPC64();
+      case Arch_riscv64:
+         return generatePreambleRISCV64();
 #endif //!defined(os_windows)
 #if defined(DYNINST_HOST_ARCH_AARCH64)
       case Arch_aarch64:
