7b290041f1565d3f932b927d4919b19fb25db252
wxrdnx <wxrdnx@protonmail.com>
2024-11-24 14:56:52 -0600

    Add RISC-V stackwalk guard

--- dyninstAPI/src/binaryEdit.C
+++ dyninstAPI/src/binaryEdit.C
@@ -418,6 +418,7 @@ bool BinaryEdit::getResolvedLibraryPath(const std::string &, std::vector<std::st
 #if !(defined(cap_binary_rewriter) && (defined(DYNINST_HOST_ARCH_X86) || defined(DYNINST_HOST_ARCH_X86_64)\
 		|| defined(DYNINST_HOST_ARCH_POWER)   \
 		|| defined(DYNINST_HOST_ARCH_AARCH64) \
+        || defined(DYNINST_HOST_ARCH_RISCV64) \
 		)) 
 bool BinaryEdit::doStaticBinarySpecialCases() {
     return true;
