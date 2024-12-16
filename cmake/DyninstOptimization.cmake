ed83d385554ab36e264d789ccc6d8260c7c47095
Tim Haines <thaines.astro@gmail.com>
2024-12-16 10:53:41 -0600

    Treat linker warnings as errors when DYNINST_WARNINGS_AS_ERRORS is on

--- cmake/DyninstOptimization.cmake
+++ cmake/DyninstOptimization.cmake
@@ -74,6 +74,10 @@ if(${CMAKE_CXX_COMPILER_ID} IN_LIST _linux_compilers)
   if(DYNINST_FORCE_RUNPATH)
     list(APPEND DYNINST_LINK_FLAGS "-Wl,--enable-new-dtags")
   endif()
+  
+  if(DYNINST_WARNINGS_AS_ERRORS)
+    list(APPEND DYNINST_LINK_FLAGS "-Wl,--fatal-warnings")
+  endif()
 elseif(MSVC)
   set(DYNINST_FORCE_FRAME_POINTER /Oy-)
 
