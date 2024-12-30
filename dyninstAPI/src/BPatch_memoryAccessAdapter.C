6e1895fe4e862de85e8de1aa746a903d4c1b8710
wxrdnx <wxrdnx@protonmail.com>
2024-12-30 15:21:57 -0600

    Rename arch_riscv64 to DYNINST_HOST_ARCH_AARCH64

--- dyninstAPI/src/BPatch_memoryAccessAdapter.C
+++ dyninstAPI/src/BPatch_memoryAccessAdapter.C
@@ -253,7 +253,7 @@ BPatch_memoryAccess* BPatch_memoryAccessAdapter::convert(Instruction insn,
         }
     }
 	return NULL;
-#elif defined(arch_riscv64)
+#elif defined(DYNINST_HOST_ARCH_RISCV64)
     // TODO
     assert(!"Unimplemented architecture");
 #else 
