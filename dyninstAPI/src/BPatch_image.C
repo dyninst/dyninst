a0d0948c6c60e2bf2d97608b9ed57dcc32251373
Ronak Chauhan <ronak@cs.wisc.edu>
2025-01-05 06:44:42 -0600

    [BPatch] Add API function std::string BPatch_image::getProgramFileName()

--- dyninstAPI/src/BPatch_image.C
+++ dyninstAPI/src/BPatch_image.C
@@ -935,6 +935,21 @@ char *BPatch_image::getProgramFileName(char *name, unsigned int len)
    return name;
 }
 
+std::string BPatch_image::getProgramFileName() const {
+   std::vector<AddressSpace*> addrSpaces;
+   addSpace->getAS(addrSpaces);
+   AddressSpace *addrSpace = addrSpaces[0];
+
+   if (!addrSpace->mappedObjects().size()) {
+      // No program defined yet
+      return "<no program defined>";
+   }
+
+   string fileName =  addrSpace->getAOut()->fileName();
+   if (fileName.empty()) fileName = "<unnamed file>";
+   return fileName;
+}
+
 BPatch_module *BPatch_image::findModule(mapped_module *base) 
 {
    ModMap::iterator iter = modmap.find(base);
