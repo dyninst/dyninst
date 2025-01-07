13a3dd3738684ea03f48ceaabe925ba8779f55c3
wxrdnx <wxrdnx@protonmail.com>
2025-01-06 21:00:50 -0600

    Add missing RISC-V ROSE register conversion

--- dataflowAPI/rose/registers/convert.C
+++ dataflowAPI/rose/registers/convert.C
@@ -1,6 +1,7 @@
 #include "rose/registers/convert.h"
 
 #include "dataflowAPI/rose/registers/aarch64.h"
+#include "dataflowAPI/rose/registers/riscv64.h"
 #include "dataflowAPI/rose/registers/amdgpu.h"
 #include "dataflowAPI/rose/registers/ppc32.h"
 #include "dataflowAPI/rose/registers/ppc64.h"
@@ -93,6 +94,12 @@ namespace Dyninst { namespace DataflowAPI {
           return std::make_tuple(armv8_pstatefield_pstate, 0, 0, num_bits);
         }
         return aarch64Rose(category, baseID, subrange, num_bits);
+      }
+      case Arch_riscv64: {
+        if(reg.isPC()) {
+          return std::make_tuple(riscv64_regclass_pc, 0, 0, num_bits);
+        }
+        return riscv64Rose(category, baseID, subrange, num_bits);
       }
       case Arch_aarch32:
       case Arch_cuda:
