6e6e00b9718fb07a8eeca522f61381ffe31f99ae
Tim Haines <thaines.astro@gmail.com>
2025-01-12 11:09:05 -0600

    isVector - add stub

--- common/src/registers/MachRegister.C
+++ common/src/registers/MachRegister.C
@@ -804,6 +804,28 @@ namespace Dyninst {
     // clang-format: on
   }
 
+  bool MachRegister::isVector() const {
+    // clang-format: off
+    auto const category = regClass();
+    switch(getArchitecture()) {
+      case Arch_x86:
+      case Arch_x86_64:
+      case Arch_aarch64:
+      case Arch_amdgpu_gfx908:
+      case Arch_amdgpu_gfx90a:
+      case Arch_amdgpu_gfx940:
+      case Arch_ppc64:
+      case Arch_intelGen9:
+      case Arch_aarch32:
+      case Arch_none:
+      case Arch_ppc32:
+      case Arch_cuda:
+        return false;
+    }
+    return false;
+    // clang-format: on
+  }
+
   MachRegister MachRegister::getArchReg(unsigned int regNum, Dyninst::Architecture arch) {
     switch(arch) {
       case Arch_aarch64:
