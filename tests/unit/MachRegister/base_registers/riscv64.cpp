9dfdeedbb9ff9015f81ca279b14a8f3663878279
wxrdnx <wxrdnx@protonmail.com>
2025-07-22 14:47:22 -0500

    Change f<N>_32 and f<N>_64 to alias check

--- tests/unit/MachRegister/base_registers/riscv64.cpp
+++ tests/unit/MachRegister/base_registers/riscv64.cpp
@@ -13,16 +13,6 @@ int main() {
   BASEREG_CHECK(riscv64::f8, riscv64::f8);
   BASEREG_CHECK(riscv64::f31, riscv64::f31);
 
-  // FPR 64 Boxed -> 64 Boxed
-  BASEREG_CHECK(riscv64::f0_32, riscv64::f0);
-  BASEREG_CHECK(riscv64::f8_32, riscv64::f8);
-  BASEREG_CHECK(riscv64::f31_32, riscv64::f31);
-
-  // FPR 64 Boxed -> 64 Boxed
-  BASEREG_CHECK(riscv64::f0_64, riscv64::f0);
-  BASEREG_CHECK(riscv64::f8_64, riscv64::f8);
-  BASEREG_CHECK(riscv64::f31_64, riscv64::f31);
-
   // Aliases
   BASEREG_CHECK(riscv64::zero, riscv64::x0);
   BASEREG_CHECK(riscv64::ra, riscv64::x1);
@@ -41,6 +31,14 @@ int main() {
   BASEREG_CHECK(riscv64::s11, riscv64::x27);
   BASEREG_CHECK(riscv64::t3, riscv64::x28);
   BASEREG_CHECK(riscv64::t6, riscv64::x31);
+
+  BASEREG_CHECK(riscv64::f0_32, riscv64::f0);
+  BASEREG_CHECK(riscv64::f8_32, riscv64::f8);
+  BASEREG_CHECK(riscv64::f31_32, riscv64::f31);
+
+  BASEREG_CHECK(riscv64::f0_64, riscv64::f0);
+  BASEREG_CHECK(riscv64::f8_64, riscv64::f8);
+  BASEREG_CHECK(riscv64::f31_64, riscv64::f31);
 
   return EXIT_SUCCESS;
 }
