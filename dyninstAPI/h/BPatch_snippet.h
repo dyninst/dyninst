52fed5a7284a1789f86ccc5ed1fb1497b84a7132
Ronak Chauhan <ronak@cs.wisc.edu>
2025-08-28 15:54:57 -0500

    [AMDGPU] Use existing constructor for variables

--- dyninstAPI/h/BPatch_snippet.h
+++ dyninstAPI/h/BPatch_snippet.h
@@ -376,17 +376,6 @@ class DYNINST_EXPORT BPatch_variableExpr : public BPatch_snippet
     BPatch_variableExpr(BPatch_addressSpace *in_addSpace,
 			AddressSpace *ll_addSpace, int_variable *iv,
 			BPatch_type *type);
-
-    // Used for AMDGPU
-    // The body of this function contains references to conditionally compiled code.
-    // Hence keeping it under an ifdef
-#if defined(DYNINST_CODEGEN_ARCH_AMDGPU_GFX908)
-    BPatch_variableExpr(const std::string& varName,
-                        BPatch_addressSpace *in_addSpace,
-                        AddressSpace *ll_addSpace,
-                        BPatch_type *type_);
-#endif
-
  public:
     static BPatch_variableExpr* makeVariableExpr(BPatch_addressSpace* in_addSpace,
 						 int_variable* v,
@@ -397,9 +386,7 @@ class DYNINST_EXPORT BPatch_variableExpr : public BPatch_snippet
 						 void* offset,
 						 BPatch_type* type);
 
-   // Wrapper around the constructor for AMDGPU
-   // The body of this function eventually calls conditionally compiled code.
-   // Hence keeping it under an ifdef
+    // This refers to conditionally compiled code, hence is under an ifdef.
 #if defined(DYNINST_CODEGEN_ARCH_AMDGPU_GFX908)
    static BPatch_variableExpr* makeVariableExpr(const std::string& name,
                                                 BPatch_addressSpace *in_addSpace,
