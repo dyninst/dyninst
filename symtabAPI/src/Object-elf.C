7190abcdeb88f50aea83379d188c9675b2df45c4
wxrdnx <wxrdnx@protonmail.com>
2025-10-13 17:34:04 -0500

    No need to default initialize struct

--- symtabAPI/src/Object-elf.C
+++ symtabAPI/src/Object-elf.C
@@ -2312,8 +2312,7 @@ ObjectELF::ObjectELF(MappedFile *mf_, bool, void (*err_func)(const char *),
         obj_type_(ObjectType::Unknown),
         DbgSectionMapSorted(false),
         soname_(NULL),
-        containingFunc(nullptr),
-        riscv_attrs()
+        containingFunc(nullptr)
 {
     li_for_object = NULL; 
     file_format_ = FileFormat::ELF;
