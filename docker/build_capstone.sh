3fdbf5a841feba2d211fc293eb435759d9ca219a
wxrdnx <67510189+wxrdnx@users.noreply.github.com>
2025-11-06 10:48:14 -0600

    Add Capstone Workflow (#2053)

---
+++ docker/build_capstone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+jobs=1   # run builds with N jobs (same as 'make -jN')
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -j|--jobs) jobs="$2"; shift 2; ;;
+  esac
+done
+
+version=$(grep capstone dependencies.versions | awk '{split($0,a,":"); print a[2]}')
+
+git clone --branch=${version} https://github.com/capstone-engine/capstone
+cd capstone
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF
+cmake --build . --parallel ${jobs}
+cmake --install .
+cd /
+rm -rf capstone
