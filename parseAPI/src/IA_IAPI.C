dyninst/dyninst

commit f7a19747c96aeacda6a1c3611a61258818f28b8b

Add category helpers to Instruction class (#1813)
getCategory() assumes there is only one category per instruction, but
that restriction will be removed when using the Capstone decoder and the
function deprecated. These helpers will make it easier for users to
check behavior without having to know how it's being tracked in
Instruction.

This makes IA_IAPI::hasCFT() more expensive, but it will be much
cheaper as we move more architectures to Capstone.

parseAPI/src/IA_IAPI.C
@@ -286,16 +286,14 @@ bool IA_IAPI::hasCFT() const
         parsing_printf("\t Returning cached entry: %d\n",hascftstatus.second);
         return hascftstatus.second;
     }
-    InsnCategory c = curInsn().getCategory();
     hascftstatus.second = false;
-    if(c == c_BranchInsn ||
-            c == c_ReturnInsn) {
+    if(curInsn().isBranch() || curInsn().isReturn()) {
         if ( likely ( ! (_obj->defensiveMode() && isNopJump()) ) ) {
             parsing_printf("\t branch or return, ret true\n");
             hascftstatus.second = true;
         }
     }
-    else if(c == c_CallInsn) {
+    else if(curInsn().isCall()) {
         if(isRealCall()) {
             hascftstatus.second = true;
         }
@@ -306,11 +304,11 @@ bool IA_IAPI::hasCFT() const
             hascftstatus.second = true;
         }
     }
-    else if(c == c_SysEnterInsn) 
+    else if(curInsn().isSysEnter())
     {
         hascftstatus.second = true;
     }
-    else if (c == c_SyscallInsn)
+    else if (curInsn().isSyscall())
     {
         hascftstatus.second = true;
     }
@@ -428,7 +426,7 @@ bool IA_IAPI::isFrameSetupInsn() const
 bool IA_IAPI::isDynamicCall() const
 {
     Instruction ci = curInsn();
-    if(ci.isValid() && (ci.getCategory() == c_CallInsn))
+    if(ci.isValid() && ci.isCall())
     {
         Address addr;
         bool success;
@@ -444,7 +442,7 @@ bool IA_IAPI::isDynamicCall() const
 bool IA_IAPI::isAbsoluteCall() const
 {
     Instruction ci = curInsn();
-    if(ci.getCategory() == c_CallInsn)
+    if(ci.isCall())
     {
         Expression::Ptr cft = ci.getControlFlowTarget();
         if(cft && boost::dynamic_pointer_cast<Immediate>(cft))
@@ -461,27 +459,27 @@ bool IA_IAPI::isAbsoluteCall() const
 
 bool IA_IAPI::isBranch() const
 {
-    return curInsn().getCategory() == c_BranchInsn;
+    return curInsn().isBranch();
 }
 bool IA_IAPI::isCall() const
 {
-    return curInsn().getCategory() == c_CallInsn;
+    return curInsn().isCall();
 }
 
 bool IA_IAPI::isInterruptOrSyscall() const
 {
-    return (Dyninst::InstructionAPI::isSoftwareInterrupt(curInsn()) || Dyninst::InstructionAPI::isSystemCall(curInsn()));
+    auto is_interrupt = Dyninst::InstructionAPI::isSoftwareInterrupt(curInsn());
+    return is_interrupt || curInsn().isSyscall();
 }
 
 bool IA_IAPI::isSysEnter() const
 {
-    Instruction ci = curInsn();
-    return (ci.getOperation().getID() == e_sysenter);
+    return curInsn().isSysEnter();
 }
 
 bool IA_IAPI::isIndirectJump() const {
     Instruction ci = curInsn();
-    if(ci.getCategory() != c_BranchInsn) return false;
+    if(ci.isBranch()) return false;
     if(ci.allowsFallThrough()) return false;
     bool valid;
     Address target;
@@ -506,7 +504,7 @@ void IA_IAPI::parseSysEnter(std::vector<std::pair<Address, EdgeTypeEnum> >& outE
     do {
         scratch->advance();
     } while(scratch->isNop());
-    if(scratch->curInsn().getCategory() == c_BranchInsn)
+    if(scratch->curInsn().isBranch())
     {
         parsing_printf("[%s:%d] Detected Linux-ish sysenter idiom at 0x%lx\n",
                 FILE__, __LINE__, getAddr());
@@ -535,7 +533,7 @@ void IA_IAPI::getNewEdges(std::vector<std::pair< Address, EdgeTypeEnum> >& outEd
     Instruction ci = curInsn();
 
     // Only call this on control flow instructions!
-    if(ci.getCategory() == c_CallInsn)
+    if(ci.isCall())
     {
         bool success; 
         Address target;
@@ -591,7 +589,7 @@ void IA_IAPI::getNewEdges(std::vector<std::pair< Address, EdgeTypeEnum> >& outEd
         }
         return;
     }
-    else if(ci.getCategory() == c_BranchInsn)
+    else if(ci.isBranch())
     {
         if(ci.allowsFallThrough())
         {
@@ -678,7 +676,7 @@ void IA_IAPI::getNewEdges(std::vector<std::pair< Address, EdgeTypeEnum> >& outEd
             return;
         }
     }
-    else if(ci.getCategory() == c_ReturnInsn)
+    else if(ci.isReturn())
     {
         parsing_printf("%s[%d]: return candidate %s at 0x%lx\n", FILE__, __LINE__,
                 ci.format().c_str(), current);
@@ -728,8 +726,7 @@ bool IA_IAPI::isIPRelativeBranch() const
     Address target;
     boost::tie(valid, target) = getCFT();
 
-    if(ci.getCategory() == c_BranchInsn &&
-            !valid) {
+    if(ci.isBranch() && !valid) {
         Expression::Ptr cft = ci.getControlFlowTarget();
         if(cft->isUsed(thePC[_isrc->getArch()]))
         {
@@ -841,7 +838,7 @@ std::pair<bool, Address> IA_IAPI::getCFT() const
 bool IA_IAPI::isRelocatable(InstrumentableLevel lvl) const
 {
     Instruction ci = curInsn();
-    if(ci.isValid() && (ci.getCategory() == c_CallInsn))
+    if(ci.isValid() && ci.isCall())
     {
         if(!isDynamicCall())
         {

parseAPI/src/IA_x86.C
@@ -271,7 +271,7 @@ bool IA_x86::isThunk() const {
     Instruction thunkFirst = targetChecker.decode();
     Instruction thunkSecond = targetChecker.decode();
     if((thunkFirst.getOperation().getID() == e_mov) &&
-        (thunkSecond.getCategory() == c_ReturnInsn))
+        (thunkSecond.isReturn()))
     {
         if(thunkFirst.isRead(stackPtr[_isrc->getArch()]))
         {
@@ -322,7 +322,7 @@ bool IA_x86::isTailCall(const Function *context, EdgeTypeEnum type, unsigned int
 
     Function* callee = _obj->findFuncByEntry(_cr, addr);
     Block* target = _obj->findBlockByEntry(_cr, addr);
-    if(curInsn().getCategory() == c_BranchInsn &&
+    if(curInsn().isBranch() &&
        valid &&
        callee && 
        callee != context &&
@@ -343,7 +343,7 @@ bool IA_x86::isTailCall(const Function *context, EdgeTypeEnum type, unsigned int
       return true;
     }    
 
-    if (curInsn().getCategory() == c_BranchInsn &&
+    if (curInsn().isBranch() &&
             valid &&
             !callee) {
     if (knownTargets.find(addr) != knownTargets.end()) {
@@ -354,7 +354,7 @@ bool IA_x86::isTailCall(const Function *context, EdgeTypeEnum type, unsigned int
     }
 
     if(allInsns.size() < 2) {
-      if(context->addr() == _curBlk->start() && curInsn().getCategory() == c_BranchInsn)
+      if(context->addr() == _curBlk->start() && curInsn().isBranch())
       {
 	parsing_printf("\tjump as only insn in entry block, TAIL CALL\n");
 	tailCalls[type] = true;
@@ -369,7 +369,7 @@ bool IA_x86::isTailCall(const Function *context, EdgeTypeEnum type, unsigned int
       }
     }
 
-    if ((curInsn().getCategory() == c_BranchInsn))
+    if (curInsn().isBranch())
     {
         //std::map<Address, Instruction::Ptr>::const_iterator prevIter =
                 //allInsns.find(current);
@@ -491,7 +491,7 @@ bool IA_x86::isStackFramePreamble() const
 bool IA_x86::cleansStack() const
 {
     Instruction ci = curInsn();
-	if (ci.getCategory() != c_ReturnInsn) return false;
+	if (!ci.isReturn()) return false;
     std::vector<Operand> ops;
 	ci.getOperands(ops);
 	return (ops.size() > 1);
@@ -504,7 +504,7 @@ bool IA_x86::isReturn(Dyninst::ParseAPI::Function * /*context*/,
     // However, for powerpc, the return instruction BLR can be a return or
     // an indirect jump used for jump tables etc. Hence, we need to function and block
     // to determine if an instruction is a return. But these parameters are unused for x86. 
-    return curInsn().getCategory() == c_ReturnInsn;
+    return curInsn().isReturn();
 }
 
 bool IA_x86::isReturnAddrSave(Dyninst::Address&) const
@@ -563,8 +563,7 @@ bool IA_x86::isFakeCall() const
 
     // follow ctrl transfers until you get a block containing non-ctrl 
     // transfer instructions, or hit a return instruction
-    while (insn.getCategory() == c_CallInsn ||
-           insn.getCategory() == c_BranchInsn)
+    while (insn.isCall() || insn.isBranch())
     {
        boost::tie(valid, entry) = ah->getCFT();
        if ( !valid || ! _cr->contains(entry) || ! _isrc->isCode(entry) ) {
@@ -595,9 +594,7 @@ bool IA_x86::isFakeCall() const
     while(true) {
 
         // exit condition 1
-        if (insn.getCategory() == c_CallInsn ||
-            insn.getCategory() == c_ReturnInsn ||
-            insn.getCategory() == c_BranchInsn)
+        if (insn.isCall() || insn.isReturn() || insn.isBranch())
         {
             break;
         }
@@ -730,7 +727,7 @@ bool IA_x86::isFakeCall() const
     } 
 
     // not a fake call if it ends w/ a return instruction
-    if (insn.getCategory() == c_ReturnInsn) {
+  if (insn.isReturn()) {
         delete ah;
         return false;
     }
@@ -814,8 +811,7 @@ bool IA_x86::isIATcall(std::string &calleeName) const
 
 bool IA_x86::isNopJump() const
 {
-    InsnCategory cat = curInsn().getCategory();
-    if (c_BranchInsn != cat) {
+    if (curInsn().isBranch()) {
         return false;
     }
     bool valid; Address addr;

dataflowAPI/src/liveness.C
@@ -526,10 +526,7 @@ ReadWriteInfo LivenessAnalyzer::calcRWSets(Instruction curInsn, Block *blk, Addr
       }
     }
   }
-  InsnCategory category = curInsn.getCategory();
-  switch(category)
-  {
-  case c_CallInsn:
+  if(curInsn.isCall()) {
       // Call instructions not at the end of a block are thunks, which are not ABI-compliant.
       // So make conservative assumptions about what they may read (ABI) but don't assume they write anything.
       ret.read |= (abi->getCallReadRegisters());
@@ -537,32 +534,26 @@ ReadWriteInfo LivenessAnalyzer::calcRWSets(Instruction curInsn, Block *blk, Addr
       {
           ret.written |= (abi->getCallWrittenRegisters());
       }
-    break;
-  case c_ReturnInsn:
+  }
+  else if(curInsn.isReturn()) {
     ret.read |= (abi->getReturnReadRegisters());
     // Nothing written implicitly by a return
-    break;
-  case c_BranchInsn:
+  }
+  else if(curInsn.isBranch()) {
     if(!curInsn.allowsFallThrough() && isExitBlock(blk))
     {
       //Tail call, union of call and return
-      ret.read |= ((abi->getCallReadRegisters()) |
-		   (abi->getReturnReadRegisters()));
+      ret.read |= ((abi->getCallReadRegisters()) | (abi->getReturnReadRegisters()));
       ret.written |= (abi->getCallWrittenRegisters());
     }
-    break;
-  default:
-    {
-      const bool isInterrupt = Dyninst::InstructionAPI::isSoftwareInterrupt(curInsn);
-      const bool isSyscall = Dyninst::InstructionAPI::isSystemCall(curInsn);
-
-      if (isInterrupt || isSyscall) {
-	ret.read |= (abi->getSyscallReadRegisters());
-	ret.written |= (abi->getSyscallWrittenRegisters());
-      }
+  }
+  else {
+    auto is_interrupt = Dyninst::InstructionAPI::isSoftwareInterrupt(curInsn);
+    if (is_interrupt || curInsn.isSyscall()) {
+      ret.read |= (abi->getSyscallReadRegisters());
+      ret.written |= (abi->getSyscallWrittenRegisters());
     }
-    break;
-  }	  
+  }
   return ret;
 }
 

dataflowAPI/src/stackanalysis.C
@@ -2435,11 +2435,11 @@ void StackAnalysis::handleDefault(Instruction insn, Block *block,
 }
 
 bool StackAnalysis::isCall(Instruction insn) {
-   return insn.getCategory() == c_CallInsn;
+   return insn.isCall();
 }
 
 bool StackAnalysis::isJump(Instruction insn) {
-   return insn.getCategory() == c_BranchInsn;
+   return insn.isBranch();
 }
 
 bool StackAnalysis::handleNormalCall(Instruction insn, Block *block,
@@ -2788,7 +2788,7 @@ bool StackAnalysis::handleThunkCall(Instruction insn, Block *block,
 
    // We know that we're not a normal call, so it depends on whether the CFT is
    // "next instruction" or not.
-   if (insn.getCategory() != c_CallInsn || !insn.getControlFlowTarget()) {
+   if (!insn.isCall() || !insn.getControlFlowTarget()) {
       return false;
    }
 

dyninstAPI/src/BPatch_basicBlock.C
@@ -358,7 +358,7 @@ bool isStore(Instruction i)
 }
 bool isPrefetch(Instruction i)
 {
-  return i.getCategory() == c_PrefetchInsn;
+  return i.isPrefetch();
 }
 
 struct funcPtrPredicate : public insnPredicate

dyninstAPI/src/Relocation/CFG/RelocBlock.C
@@ -317,9 +317,7 @@ void RelocBlock::createCFWidget() {
 
    InstructionAPI::Instruction insn = elements_.back()->insn();
    if (insn.isValid()) {
-      if (insn.getCategory() == c_CallInsn ||
-          insn.getCategory() == c_ReturnInsn ||
-          insn.getCategory() == c_BranchInsn) {
+      if (insn.isCall() || insn.isReturn() || insn.isBranch()) {
          hasCF = true;
       }
    }

dyninstAPI/src/Relocation/Transformers/Movement-adhoc.C
@@ -289,14 +289,8 @@ bool adhocMovementTransformer::isPCRelData(Widget::Ptr ptr,
                                            Instruction insn,
                                            Address &target) {
   target = 0;
-  switch(insn.getCategory())
-  {
-      case c_CallInsn:
-      case c_BranchInsn:
-      case c_ReturnInsn:
-          return false;
-      default:
-	  break;
+  if(insn.isCall() || insn.isBranch() || insn.isReturn()) {
+    return false;
   }
   if (insn.getControlFlowTarget()) return false;
 
@@ -411,7 +405,7 @@ bool adhocMovementTransformer::isGetPC(Widget::Ptr ptr,
   // Check for call to thunk.
   // TODO: need a return register parameter.
 
-   if (insn.getCategory() != InstructionAPI::c_CallInsn) return false;
+   if (!insn.isCall()) return false;
 
   // Okay: checking for call + size
   Expression::Ptr CFT = insn.getControlFlowTarget();
@@ -479,7 +473,7 @@ bool adhocMovementTransformer::isGetPC(Widget::Ptr ptr,
 
     if(firstInsn.isValid() && firstInsn.getOperation().getID() == e_mov
        && firstInsn.readsMemory() && !firstInsn.writesMemory()
-       && secondInsn.isValid() && secondInsn.getCategory() == c_ReturnInsn) {
+       && secondInsn.isValid() && secondInsn.isReturn()) {
 
       thunkVisitor visitor;
       relocation_cerr << "Checking operand "

dyninstAPI/src/Relocation/Transformers/Movement-analysis.C
@@ -138,7 +138,7 @@ bool PCSensitiveTransformer::process(RelocBlock *reloc, RelocGraph *g) {
             extSens_++;
             thunk_++;
             continue;
-        } else if (insn.getCategory() == c_CallInsn && exceptionSensitive(addr+insn.size(), block)) {
+        } else if (insn.isCall() && exceptionSensitive(addr+insn.size(), block)) {
             extSens = true;
             sensitivity_cerr << "\tException sensitive @ " << hex << addr << dec << endl;
         }
@@ -406,7 +406,7 @@ bool PCSensitiveTransformer::insnIsThunkCall(Instruction insn,
                                              Address addr,
                                              Absloc &destination) {
   // Should be able to handle this much more efficiently by following the CFG
-  if (insn.getCategory() != c_CallInsn) {
+  if (!insn.isCall()) {
     return false;
   }
   Expression::Ptr CFT = insn.getControlFlowTarget();
@@ -450,7 +450,7 @@ bool PCSensitiveTransformer::insnIsThunkCall(Instruction insn,
 
     if(firstInsn.isValid() && firstInsn.getOperation().getID() == e_mov
        && firstInsn.readsMemory() && !firstInsn.writesMemory()
-       && secondInsn.isValid() && secondInsn.getCategory() == c_ReturnInsn) {
+       && secondInsn.isValid() && secondInsn.isReturn()) {
 
       // Check to be sure we're reading memory
       std::set<RegisterAST::Ptr> reads;

commit 2bc10b4ded8b8e04157fd68962feab62d3e6a87c

Fix regression introduced in PR #1813
Certain if predicates are missing a not.
This fix the regression reported in #2050.

parseAPI/src/IA_IAPI.C
@@ -473,7 +473,7 @@ bool IA_IAPI::isSysEnter() const
 
 bool IA_IAPI::isIndirectJump() const {
     Instruction ci = curInsn();
-    if(ci.isBranch()) return false;
+    if(!ci.isBranch()) return false;
     if(ci.allowsFallThrough()) return false;
     bool valid;
     Address target;

parseAPI/src/IA_x86.C
@@ -809,7 +809,7 @@ bool IA_x86::isIATcall(std::string &calleeName) const
 
 bool IA_x86::isNopJump() const
 {
-    if (curInsn().isBranch()) {
+    if (!curInsn().isBranch()) {
         return false;
     }
     bool valid; Address addr;