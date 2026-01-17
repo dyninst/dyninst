b9be7bc7c7fcd72346e64ad4fbdd4ea3ca6a82f1
Tim Haines <thaines.astro@gmail.com>
2026-01-16 20:44:21 -0600

    Remove initPrimitiveCost
    Its usage was removed by b42dfb67 in 2011.

--- dyninstAPI/src/inst.h
+++ dyninstAPI/src/inst.h
@@ -140,12 +140,6 @@ public:
   std::vector<Dyninst::PatchAPI::InstancePtr> instances;
 };
 
-/*
- * get information about the cost of primitives.
- *
- */
-void initPrimitiveCost();
-
 //
 // Return the expected runtime of the passed function in instruction times.
 //
