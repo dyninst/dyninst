068cc4baed880e647e112da458d9eb4266af2fcb
Ronak Chauhan <ronak@cs.wisc.edu>
2026-01-18 14:06:27 -0600

    Define new register struct

--- common/src/dyn_register.h
+++ common/src/dyn_register.h
@@ -31,12 +31,73 @@
 #ifndef DYNINST_REGISTER
 #define DYNINST_REGISTER
 
+#include <functional>
+
 namespace Dyninst {
 /* This needs to be an int since it is sometimes used to pass offsets
    to the code generator (i.e. if-statement) - jkh 5/24/99 */
 
+#if defined(DYNINST_CODEGEN_ARCH_AMDGPU_GFX908)
+enum RegKind : uint32_t { SCALAR = 0, VECTOR = 1, MATRIX = 2, PREDICATE = 3, UNDEFINED_KIND = 4 };
+
+enum RegUsage : uint16_t {
+  GENERAL_PURPOSE = 0,
+  SPECIAL_PURPOSE = 1,
+  TEMPORARY = 2,
+  UNDEFINED_USAGE = 3
+};
+
+constexpr uint32_t REG_ID_WIDTH = 12;
+constexpr uint32_t REG_KIND_WIDTH = 4;
+constexpr uint32_t REG_USAGE_WIDTH = 4;
+constexpr uint32_t REG_COUNT_WIDTH = 4;
+constexpr uint32_t REG_RESERVED_WIDTH = 8;
+// total = 32
+
+struct __attribute__((packed)) RegisterFields {
+  uint32_t id : REG_ID_WIDTH;
+
+  uint32_t kind : REG_KIND_WIDTH;
+
+  uint32_t usage : REG_USAGE_WIDTH;
+
+  uint32_t count : REG_COUNT_WIDTH;
+
+  uint32_t reserved : REG_RESERVED_WIDTH;
+};
+
+union Register {
+  RegisterFields details;
+  uint32_t raw;
+
+  constexpr Register(uint32_t val) : raw(val) {}
+
+  constexpr Register() : raw(static_cast<uint32_t>(-1)) {}
+
+  // A register block represents consecutive reqisters starting from id. Not all architectures
+  // support this.
+  //
+  // For backward compatibility in codegen across architectures, we need to keep this 0 when we
+  // want to have a single register.
+  // So count = 0 and count = 1 will have the same effect.
+  constexpr bool isRegisterBlock() const { return details.count != 0; }
+
+  constexpr operator uint32_t() const { return raw; }
+
+  // Required for hash map lookup
+  constexpr bool operator==(const Register &other) const { return raw == other.raw; }
+
+  constexpr bool operator!=(const Register &other) const { return !(*this == other); }
+
+  // Required for compatibility with integers
+  constexpr bool operator==(const int &value) const { return raw == static_cast<uint32_t>(value); }
+
+  constexpr bool operator!=(const int &value) const { return !(*this == value); }
+};
+#else
 /* a register number, e.g., [0..31]  */
 typedef unsigned int Register;
+#endif
 
 /* register content 64-bit */
 typedef long long int RegValue;
@@ -46,4 +107,13 @@ constexpr Register Null_Register{static_cast<unsigned int>(-1)};
 
 }
 
+#if defined(DYNINST_CODEGEN_ARCH_AMDGPU_GFX908)
+namespace std {
+template <> struct hash<Dyninst::Register> {
+  size_t operator()(const Dyninst::Register &reg) const noexcept {
+    return std::hash<uint32_t>{}(reg.raw);
+  }
+};
+} // namespace std
+#endif
 #endif
