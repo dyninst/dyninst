279663b483af964a58aae9e8502df897fa7e0016
Tim Haines <thaines.astro@gmail.com>
2026-01-19 04:34:17 -0600

    Extract AstMemoryNode

--- dyninstAPI/src/ast.C
+++ dyninstAPI/src/ast.C
@@ -62,114 +62,6 @@ using namespace Dyninst;
 using namespace Dyninst::InstructionAPI;
 using PatchAPI::Point;
 
-AstMemoryNode::AstMemoryNode(memoryType mem,
-                             unsigned which,
-                             int size_) :
-    AstNode(),
-    mem_(mem),
-    which_(which) {
-
-    assert(BPatch::bpatch != NULL);
-    assert(BPatch::bpatch->stdTypes != NULL);
-
-
-    switch(mem_) {
-    case EffectiveAddr:
-        switch (size_) {
-            case 1:
-                bptype = BPatch::bpatch->stdTypes->findType("char");
-                break;
-            case 2:
-                bptype = BPatch::bpatch->stdTypes->findType("short");
-                break;
-            case 4:
-                bptype = BPatch::bpatch->stdTypes->findType("int");
-                break;
-            default:
-                bptype = BPatch::bpatch->stdTypes->findType("long");
-        }
-        break;
-    case BytesAccessed:
-        bptype = BPatch::bpatch->stdTypes->findType("int");
-        break;
-    default:
-        assert(!"Naah...");
-    }
-    size = bptype->getSize();
-    doTypeCheck = BPatch::bpatch->isTypeChecked();
-}
-
-bool AstMemoryNode::generateCode_phase2(codeGen &gen, bool noCost,
-                                        Address &,
-                                        Dyninst::Register &retReg) {
-
-	RETURN_KEPT_REG(retReg);
-
-    const BPatch_memoryAccess* ma;
-    const BPatch_addrSpec_NP *start;
-    const BPatch_countSpec_NP *count;
-    if (retReg == Dyninst::Null_Register)
-        retReg = allocateAndKeep(gen, noCost);
-    switch(mem_) {
-    case EffectiveAddr: {
-
-        // VG(11/05/01): get effective address
-        // VG(07/31/02): take care which one
-        // 1. get the point being instrumented & memory access info
-        assert(gen.point());
-
-        BPatch_addressSpace *bproc = (BPatch_addressSpace *)gen.addrSpace()->up_ptr();
-        BPatch_point *bpoint = bproc->findOrCreateBPPoint(NULL, gen.point(), BPatch_point::convertInstPointType_t(gen.point()->type()));
-        if (bpoint == NULL) {
-            fprintf(stderr, "ERROR: Unable to find BPatch point for internal point %p/0x%lx\n",
-                    (void*)gen.point(), gen.point()->insnAddr());
-        }
-        assert(bpoint);
-        ma = bpoint->getMemoryAccess();
-        if(!ma) {
-            bpfatal( "Memory access information not available at this point.\n");
-            bpfatal( "Make sure you create the point in a way that generates it.\n");
-            bpfatal( "E.g.: findPoint(const std::set<BPatch_opCode>& ops).\n");
-            assert(0);
-        }
-        if(which_ >= ma->getNumberOfAccesses()) {
-            bpfatal( "Attempt to instrument non-existent memory access number.\n");
-            bpfatal( "Consider using filterPoints()...\n");
-            assert(0);
-        }
-        start = ma->getStartAddr(which_);
-        emitASload(start, retReg, 0, gen, noCost);
-        break;
-    }
-    case BytesAccessed: {
-        // 1. get the point being instrumented & memory access info
-        assert(gen.point());
-
-        BPatch_addressSpace *bproc = (BPatch_addressSpace *)gen.addrSpace()->up_ptr();
-        BPatch_point *bpoint = bproc->findOrCreateBPPoint(NULL, gen.point(), BPatch_point::convertInstPointType_t(gen.point()->type()));
-        ma = bpoint->getMemoryAccess();
-        if(!ma) {
-            bpfatal( "Memory access information not available at this point.\n");
-            bpfatal("Make sure you create the point in a way that generates it.\n");
-            bpfatal( "E.g.: findPoint(const std::set<BPatch_opCode>& ops).\n");
-            assert(0);
-        }
-        if(which_ >= ma->getNumberOfAccesses()) {
-            bpfatal( "Attempt to instrument non-existent memory access number.\n");
-            bpfatal( "Consider using filterPoints()...\n");
-            assert(0);
-        }
-        count = ma->getByteCount(which_);
-        emitCSload(count, retReg, gen, noCost);
-        break;
-    }
-    default:
-        assert(0);
-    }
-	decUseCount(gen);
-    return true;
-}
-
 bool AstOriginalAddrNode::generateCode_phase2(codeGen &gen,
                                               bool noCost,
                                               Address &,
@@ -284,15 +176,6 @@ bool AstSnippetNode::generateCode_phase2(codeGen &gen,
    if (!snip_->generate(gen.point(), buf)) return false;
    gen.copy(buf.start_ptr(), buf.size());
    return true;
-}
-
-std::string AstMemoryNode::format(std::string indent) {
-   std::stringstream ret;
-   ret << indent << "Mem/" << hex << this << dec << "("
-       << ((mem_ == EffectiveAddr) ? "EffAddr" : "BytesAcc")
-       << ")" << endl;
-
-   return ret.str();
 }
 
 AstAtomicOperationStmtNode::AstAtomicOperationStmtNode(opCode astOpcode, AstNodePtr variableNode,
