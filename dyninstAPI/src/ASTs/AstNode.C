84fddbe560740462067e8b9882fd377f8e38e7f9
Tim Haines <thaines.astro@gmail.com>
2026-01-20 02:00:31 -0600

    Create a decorator for stack ASTs
    Eventually MSpecialType will go in there, but it's needed for AstNode to
    build right now.

--- dyninstAPI/src/ASTs/AstNode.C
+++ dyninstAPI/src/ASTs/AstNode.C
@@ -329,38 +329,6 @@ bool AstNode::initRegisters(codeGen &g) {
   return ret;
 }
 
-bool AstNode::allocateCanaryRegister(codeGen &gen, bool noCost, Dyninst::Register &reg,
-                                     bool &needSaveAndRestore) {
-  // Let's see if we can find a dead register to use!
-  instPoint *point = gen.point();
-
-  // Try to get a scratch register from the register space
-  registerSpace *regSpace = registerSpace::actualRegSpace(point);
-  bool realReg = true;
-  Dyninst::Register tmpReg = regSpace->getScratchRegister(gen, noCost, realReg);
-  if(tmpReg != Dyninst::Null_Register) {
-    reg = tmpReg;
-    needSaveAndRestore = false;
-    if(gen.getArch() == Arch_x86) {
-      gen.rs()->noteVirtualInReal(reg, RealRegister(reg));
-    }
-    return true;
-  }
-
-  // Couldn't find a dead register to use :-(
-  registerSpace *deadRegSpace = registerSpace::optimisticRegSpace(gen.addrSpace());
-  reg = deadRegSpace->getScratchRegister(gen, noCost, realReg);
-  if(reg == Dyninst::Null_Register) {
-    fprintf(stderr, "WARNING: using default allocateAndKeep in allocateCanaryRegister\n");
-    reg = allocateAndKeep(gen, noCost);
-  }
-  needSaveAndRestore = true;
-  fprintf(stderr, "allocateCanaryRegister will require save&restore at 0x%lx\n",
-          gen.point()->addr());
-
-  return true;
-}
-
 BPatch_type *AstNode::checkType(BPatch_function *) {
   return BPatch::bpatch->type_Untyped;
 }
