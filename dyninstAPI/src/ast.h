3c224eaf244e95a5a9f88726c060465c0d6ccf7b
Tim Haines <thaines.astro@gmail.com>
2026-01-20 18:41:23 -0600

    Remove SCAST_AST macro
    It's no longer used.

--- dyninstAPI/src/ast.h
+++ dyninstAPI/src/ast.h
@@ -723,7 +723,6 @@ class AstAtomicOperationStmtNode : public AstNode {
     AstNodePtr constant;
 };
 
-#define SCAST_AST(ast) boost::static_pointer_cast<AstNode>(ast)
 #define DCAST_AST(ast) boost::dynamic_pointer_cast<AstNode>(ast)
 
 
