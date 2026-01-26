f3ac26bcfd52fea31811634c98691b599de1bf7b
Ronak Chauhan <ronak@cs.wisc.edu>
2026-01-26 10:18:00 -0600

    Use getValue() instead of uint32_t casts

--- dyninstAPI/src/inst-power.C
+++ dyninstAPI/src/inst-power.C
@@ -366,7 +366,7 @@ void registerSpace::initialize() {
 
 unsigned registerSpace::SPR(Register x) {
     // Encodings from architecture manual
-    switch ((powerRegisters_t) uint32_t(x)) {
+    switch ((powerRegisters_t) x.getValue()) {
     case xer:
         return SPR_XER;
         break;
@@ -1626,7 +1626,7 @@ static inline void restoreGPRtoGPR(codeGen &gen,
 //        insnCodeGen::generateImm(gen, Lop, dest, REG_SP,
 //                                 gpr_off + reg*gpr_size);
     else {
-        bperr( "GPR %u should not be restored...", uint32_t(reg));
+        bperr( "GPR %u should not be restored...", reg.getValue());
         assert(0);
     }
 }
