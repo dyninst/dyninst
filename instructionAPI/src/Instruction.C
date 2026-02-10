dyninst/dyninst

commit e0c12bc4adfcd911e6a7427291699e923db34489

RISC-V Instruction API (#2046)

instructionAPI/src/Instruction.C
@@ -38,6 +38,7 @@
 #include "dyninstversion.h"
 #include "interrupts.h"
 #include "entryIDs.h"
+#include "dyn_regs.h"
 
 #include <algorithm>
 #include <boost/iterator/indirect_iterator.hpp>
@@ -97,7 +98,16 @@ namespace Dyninst { namespace InstructionAPI {
 
   DYNINST_EXPORT Instruction::Instruction(Operation what, size_t size, const unsigned char* raw,
                                           Dyninst::Architecture arch)
-      : m_InsnOp(what), m_Valid(is_valid_mnemonic(arch, what.getID())), m_size{static_cast<decltype(m_size)>(size)},
+      : m_InsnOp(what), m_EncodedInsnOp(what), m_Valid(is_valid_mnemonic(arch, what.getID())),
+        m_size{static_cast<decltype(m_size)>(size)},
+        arch_decoded_from(arch), formatter(&ArchSpecificFormatter::getFormatter(arch)) {
+    copyRaw(size, raw);
+  }
+
+  Instruction::Instruction(Operation what, Operation encoded_what, size_t size, const unsigned char* raw,
+                                          Dyninst::Architecture arch)
+      : m_InsnOp(what), m_EncodedInsnOp(encoded_what), m_Valid(is_valid_mnemonic(arch, what.getID())),
+        m_size{static_cast<decltype(m_size)>(size)},
         arch_decoded_from(arch), formatter(&ArchSpecificFormatter::getFormatter(arch)) {
     copyRaw(size, raw);
   }
@@ -125,7 +135,18 @@ namespace Dyninst { namespace InstructionAPI {
 
   DYNINST_EXPORT Operation& Instruction::getOperation() { return m_InsnOp; }
 
+  Operation& Instruction::getEncodedOperation() {
+    return m_EncodedInsnOp;
+  }
+  DYNINST_EXPORT std::vector<Operand> Instruction::getExplicitEncodedOperands() const {
+    return std::vector<Operand>(m_EncodedOperands.begin(), m_EncodedOperands.end());
+  }
+
   DYNINST_EXPORT const Operation& Instruction::getOperation() const { return m_InsnOp; }
+
+  DYNINST_EXPORT const Operation& Instruction::getEncodedOperation() const {
+    return m_EncodedInsnOp;
+  }
 
   std::vector<Operand> Instruction::getAllOperands() const {
     return std::vector<Operand>(m_Operands.begin(), m_Operands.end());
@@ -172,6 +193,16 @@ namespace Dyninst { namespace InstructionAPI {
       return Operand(Expression::Ptr(), false, false);
     }
     std::list<Operand>::const_iterator found = m_Operands.begin();
+    std::advance(found, index);
+    return *found;
+  }
+
+  DYNINST_EXPORT Operand Instruction::getEncodedExplicitOperand(int index) const {
+    if(index < 0 || index >= (int)(m_EncodedOperands.size())) {
+      // Out of range = empty operand
+      return Operand(Expression::Ptr(), false, false);
+    }
+    std::list<Operand>::const_iterator found = m_EncodedOperands.begin();
     std::advance(found, index);
     return *found;
   }
@@ -317,11 +348,11 @@ namespace Dyninst { namespace InstructionAPI {
 
     // remove this once ArchSpecificFormatter is extended for all architectures
 
-    std::string opstr = m_InsnOp.format();
+    std::string opstr = m_EncodedInsnOp.format();
     opstr += " ";
     std::list<Operand>::const_iterator currOperand;
     std::vector<std::string> formattedOperands;
-    for(currOperand = m_Operands.begin(); currOperand != m_Operands.end(); ++currOperand) {
+    for(currOperand = m_EncodedOperands.begin(); currOperand != m_EncodedOperands.end(); ++currOperand) {
       /* If this operand is implicit, don't put it in the list of operands. */
       if(currOperand->isImplicit())
         continue;
@@ -385,6 +416,18 @@ namespace Dyninst { namespace InstructionAPI {
   DYNINST_EXPORT Architecture Instruction::getArch() const { return arch_decoded_from; }
 
   DYNINST_EXPORT InsnCategory Instruction::getCategory() const {
+    if(m_InsnOp.isMultiInsnCall || m_InsnOp.isNonABICall)
+      return c_CallInsn;
+    if(m_InsnOp.isMultiInsnBranch)
+      return c_BranchInsn;
+    if(m_InsnOp.isNonABIReturn)
+      return c_ReturnInsn;
+    if(arch_decoded_from == Arch_riscv64) {
+      if(categories.categories.size()) {
+        return categories.categories[0];
+      }
+      return c_NoCategory;
+    }
     if(m_InsnOp.isVectorInsn)
       return c_VectorInsn;
     InsnCategory c = entryToCategory(m_InsnOp.getID());
@@ -418,6 +461,11 @@ namespace Dyninst { namespace InstructionAPI {
   void Instruction::appendOperand(Expression::Ptr e, bool isRead, bool isWritten, bool isImplicit,
                                   bool trueP, bool falseP) const {
     m_Operands.push_back(Operand(e, isRead, isWritten, isImplicit, trueP, falseP));
+  }
+
+  void Instruction::appendEncodedOperand(Expression::Ptr e, bool isRead, bool isWritten, bool isImplicit,
+                                         bool trueP, bool falseP) const {
+    m_EncodedOperands.push_back(Operand(e, isRead, isWritten, isImplicit, trueP, falseP));
   }
 
 }}

cmake/DyninstWarnings.cmake
@@ -172,6 +172,7 @@ if(HAS_CPP_FLAG_Wframe_larger_than AND NOT DYNINST_DISABLE_DIAGNOSTIC_SUPPRESSIO
     set(debugMaxFrameSizeOverrideFinalizeOperands 29000)
     set(nonDebugMaxFrameSizeOverrideFinalizeOperands 29000)
   endif()
+  set(maxFrameSizeOverrideRiscvInstructionApiTests 49152)
 endif()
 
 # clang >17.0 doesn't detect /usr/include/boost as a system directory

common/src/arch-riscv64.h
@@ -1,0 +1,140 @@
+/*
+ * See the dyninst/COPYRIGHT file for copyright information.
+ *
+ * We provide the Paradyn Tools (below described as "Paradyn")
+ * on an AS IS basis, and do not warrant its validity or performance.
+ * We reserve the right to update, modify, or discontinue this
+ * software at any time.  We shall have no obligation to supply such
+ * updates or modifications or any other form of support to you.
+ *
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _ARCH_RISCV64_H
+#define _ARCH_RISCV64_H
+
+#include <stdint.h>
+
+// In RISC-V, instruction length can be a multiple of 2 bytes
+
+// Standard RISC-V instructions (RVI, RVA, RVM, RVF, RVD, ...) are 4 bytes
+using rvInsn_t = uint32_t;
+constexpr int RISCV_INSN_SIZE = 4;
+static_assert(sizeof(rvInsn_t) == RISCV_INSN_SIZE, "rvInst_t size mismatch");
+
+// Compressed instructions (RVC) are 2 bytes
+using rvcInsn_t = uint16_t;
+constexpr int RISCVC_INSN_SIZE = 2;
+static_assert(sizeof(rvcInsn_t) == RISCVC_INSN_SIZE, "rvcInsn_t size mismatch");
+
+// The minimum instruction size is 2 bytes
+using rvMinInsn_t = rvcInsn_t;
+constexpr int RISCV_MIN_INSN_SIZE = RISCVC_INSN_SIZE;
+static_assert(sizeof(rvMinInsn_t) == RISCV_MIN_INSN_SIZE, "rvMinInst_t size mismatch");
+
+// The maximum instruction size is 4 bytes
+using rvMaxInsn_t = rvInsn_t;
+constexpr int RISCV_MAX_INSN_SIZE = RISCV_INSN_SIZE;
+static_assert(sizeof(rvMaxInsn_t) == RISCV_MAX_INSN_SIZE, "rvMaxInsn_t size mismatch");
+
+// The bits in the register is 64 bits
+constexpr int RISCV_REG_SIZE = 8;
+// The immediates in instructions varies.
+// The maximum is 21 bits, which can be stored in a 32 bit integer
+constexpr int RISCV_IMM_SIZE = 4;
+
+// Raw register encoding used in RISC-V instruction encoding
+// These values identify registers when decoding instructions
+// They are also used to emit register encoding during codegen
+constexpr int32_t GPR_X0     = 0;
+constexpr int32_t GPR_X1     = 1;
+constexpr int32_t GPR_X2     = 2;
+constexpr int32_t GPR_X3     = 3;
+constexpr int32_t GPR_X4     = 4;
+constexpr int32_t GPR_X5     = 5;
+constexpr int32_t GPR_X6     = 6;
+constexpr int32_t GPR_X7     = 7;
+constexpr int32_t GPR_X8     = 8;
+constexpr int32_t GPR_X9     = 9;
+constexpr int32_t GPR_X10    = 10;
+constexpr int32_t GPR_X11    = 11;
+constexpr int32_t GPR_X12    = 12;
+constexpr int32_t GPR_X13    = 13;
+constexpr int32_t GPR_X14    = 14;
+constexpr int32_t GPR_X15    = 15;
+constexpr int32_t GPR_X16    = 16;
+constexpr int32_t GPR_X17    = 17;
+constexpr int32_t GPR_X18    = 18;
+constexpr int32_t GPR_X19    = 19;
+constexpr int32_t GPR_X20    = 20;
+constexpr int32_t GPR_X21    = 21;
+constexpr int32_t GPR_X22    = 22;
+constexpr int32_t GPR_X23    = 23;
+constexpr int32_t GPR_X24    = 24;
+constexpr int32_t GPR_X25    = 25;
+constexpr int32_t GPR_X26    = 26;
+constexpr int32_t GPR_X27    = 27;
+constexpr int32_t GPR_X28    = 28;
+constexpr int32_t GPR_X29    = 29;
+constexpr int32_t GPR_X30    = 30;
+constexpr int32_t GPR_X31    = 31;
+
+constexpr int32_t GPR_ZERO   = 0;
+constexpr int32_t GPR_RA     = 1;
+constexpr int32_t GPR_SP     = 2;
+constexpr int32_t GPR_GP     = 3;
+constexpr int32_t GPR_TP     = 4;
+constexpr int32_t GPR_T0     = 5;
+constexpr int32_t GPR_T1     = 6;
+constexpr int32_t GPR_T2     = 7;
+constexpr int32_t GPR_S0     = 8;
+constexpr int32_t GPR_FP     = 8;
+constexpr int32_t GPR_S1     = 9;
+constexpr int32_t GPR_A0     = 10;
+constexpr int32_t GPR_A1     = 11;
+constexpr int32_t GPR_A2     = 12;
+constexpr int32_t GPR_A3     = 13;
+constexpr int32_t GPR_A4     = 14;
+constexpr int32_t GPR_A5     = 15;
+constexpr int32_t GPR_A6     = 16;
+constexpr int32_t GPR_A7     = 17;
+constexpr int32_t GPR_S2     = 18;
+constexpr int32_t GPR_S3     = 19;
+constexpr int32_t GPR_S4     = 20;
+constexpr int32_t GPR_S5     = 21;
+constexpr int32_t GPR_S6     = 22;
+constexpr int32_t GPR_S7     = 23;
+constexpr int32_t GPR_S8     = 24;
+constexpr int32_t GPR_S9     = 25;
+constexpr int32_t GPR_S10    = 26;
+constexpr int32_t GPR_S11    = 27;
+constexpr int32_t GPR_T3     = 28;
+constexpr int32_t GPR_T4     = 29;
+constexpr int32_t GPR_T5     = 30;
+constexpr int32_t GPR_T6     = 31;
+
+// Register encoding
+constexpr int32_t REG_RD_ENC_OFFSET = 7;
+constexpr int32_t REG_RS1_ENC_OFFSET = 15;
+constexpr int32_t REG_RS2_ENC_OFFSET = 20;
+constexpr int32_t REG_ENC_MASK = 0x1f;
+
+#endif

instructionAPI/CMakeLists.txt
@@ -38,6 +38,11 @@ set(_sources
     src/syscalls.C
     src/interrupts.C)
 
+if(DYNINST_ENABLE_CAPSTONE)
+  list(APPEND _sources src/decoder/riscv/decoder.C src/decoder/riscv/mem_xlat.C
+       src/decoder/riscv/opcode_xlat.C src/decoder/riscv/register_xlat.C)
+endif()
+
 set(_public_headers
     h/ArchSpecificFormatters.h
     h/BinaryFunction.h
@@ -62,12 +67,24 @@ set(_private_headers
     src/AMDGPU/gfx940/InstructionDecoder-amdgpu-gfx940.h
     src/AMDGPU/gfx90a/InstructionDecoder-amdgpu-gfx90a.h
     src/AMDGPU/gfx908/InstructionDecoder-amdgpu-gfx908.h
+    src/decoder/type_conversion.h
     src/InstructionDecoder-aarch64.h
     src/InstructionDecoderImpl.h
     src/InstructionDecoder-power.h
     src/InstructionDecoder-x86.h
     h/syscalls.h
     h/interrupts.h)
+
+if(DYNINST_ENABLE_CAPSTONE)
+  list(
+    APPEND
+    _private_headers
+    src/decoder/riscv/categories.h
+    src/decoder/riscv/decoder.h
+    src/decoder/riscv/mem_xlat.h
+    src/decoder/riscv/opcode_xlat.h
+    src/decoder/riscv/register_xlat.h)
+endif()
 
 set(instructionDecoderPowerExtraFlags "")
 if(${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
@@ -128,6 +145,12 @@ if(NOT finalizeOperandsExtraFlags STREQUAL "")
                               PROPERTIES COMPILE_FLAGS "${finalizeOperandsExtraFlags}")
 endif()
 
+if(DYNINST_ENABLE_CAPSTONE)
+  set(_capstone_dep Dyninst::Capstone)
+else()
+  set(_capstone_dep)
+endif()
+
 # cmake-format: off
 dyninst_library(
   instructionAPI
@@ -137,6 +160,7 @@ dyninst_library(
   DEFINES INSTRUCTION_LIB
   DYNINST_DEPS common
   PUBLIC_DEPS Dyninst::Boost_headers
+  PRIVATE_DEPS ${_capstone_dep}
 )
 # cmake-format: on
 

instructionAPI/h/ArchSpecificFormatters.h
@@ -78,6 +78,17 @@ namespace Dyninst {
             std::map<std::string, std::string> binaryFuncModifier;
         };
 
+        class RiscvFormatter : public ArchSpecificFormatter {
+        public:
+            RiscvFormatter();
+
+            std::string getInstructionString(const std::vector <std::string>&) const override;
+            std::string formatImmediate(const std::string&) const override;
+            std::string formatDeref(const std::string&) const override;
+            std::string formatRegister(const std::string&) const override;
+            std::string formatBinaryFunc(const std::string&, const std::string&, const std::string&) const override;
+        };
+
         class AmdgpuFormatter : public ArchSpecificFormatter {
         public:
             AmdgpuFormatter();

instructionAPI/h/Instruction.h
@@ -57,6 +57,7 @@ namespace Dyninst { namespace InstructionAPI {
     friend class InstructionDecoder_amdgpu_gfx908;
     friend class InstructionDecoder_amdgpu_gfx90a;
     friend class InstructionDecoder_amdgpu_gfx940;
+    friend class InstructionDecoder_riscv64;
 
     static const unsigned int maxInstructionLength = 16;
 
@@ -71,12 +72,40 @@ namespace Dyninst { namespace InstructionAPI {
           : target(t), isCall(call), isIndirect(indir), isConditional(cond), isFallthrough(ft) {}
     };
 
+    class category_t final {
+      friend class Instruction;
+      std::vector<InsnCategory> categories{};
+
+    public:
+      explicit category_t(std::vector<InsnCategory> c) noexcept : categories{std::move(c)} {}
+      category_t() = default;
+      category_t& operator=(std::vector<InsnCategory>&& c) {
+        categories = std::move(c);
+        return *this;
+      }
+
+      bool satisfies(InsnCategory category) const {
+        for(auto c : categories) {
+          if(c == category) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+
     DYNINST_EXPORT Instruction(Operation what, size_t size, const unsigned char* raw,
                                Dyninst::Architecture arch);
+    DYNINST_EXPORT Instruction(Operation what, Operation encoded_what, size_t size, const unsigned char* raw,
+                Dyninst::Architecture arch);
     DYNINST_EXPORT Instruction();
 
     DYNINST_EXPORT Operation& getOperation();
     DYNINST_EXPORT const Operation& getOperation() const;
+
+    DYNINST_EXPORT Operation& getEncodedOperation();
+    DYNINST_EXPORT const Operation& getEncodedOperation() const;
+    DYNINST_EXPORT std::vector<Operand> getExplicitEncodedOperands() const;
 
     DYNINST_EXPORT std::vector<Operand> getAllOperands() const;
     DYNINST_EXPORT std::vector<Operand> getExplicitOperands() const;
@@ -85,6 +114,7 @@ namespace Dyninst { namespace InstructionAPI {
     DYNINST_DEPRECATED("Use getallOperands()") DYNINST_EXPORT
     void getOperands(std::vector<Operand>& operands) const;
     DYNINST_EXPORT Operand getOperand(int index) const;
+    DYNINST_EXPORT Operand getEncodedExplicitOperand(int index) const;
 
     DYNINST_EXPORT std::vector<Operand> getDisplayOrderedOperands() const;
 
@@ -133,6 +163,11 @@ namespace Dyninst { namespace InstructionAPI {
     DYNINST_EXPORT bool isGPUKernelExit() const { return getCategory() == c_GPUKernelExitInsn; }
     DYNINST_EXPORT bool isSoftwareException() const { return isGPUKernelExit() || getCategory() == c_SoftwareExceptionInsn; }
 
+    DYNINST_EXPORT bool isMultiInsnCall() const { return isCall() && getOperation().isMultiInsnCall; }
+    DYNINST_EXPORT bool isMultiInsnBranch() const { return isBranch() && getOperation().isMultiInsnBranch; }
+    DYNINST_EXPORT bool isNonABICall() const { return isCall() && getOperation().isNonABICall; }
+    DYNINST_EXPORT bool isNonABIReturn() const { return isReturn() && getOperation().isNonABIReturn; }
+
     typedef std::list<CFT>::const_iterator cftConstIter;
     DYNINST_EXPORT cftConstIter cft_begin() const { return m_Successors.begin(); }
     DYNINST_EXPORT cftConstIter cft_end() const { return m_Successors.end(); }
@@ -154,12 +189,30 @@ namespace Dyninst { namespace InstructionAPI {
 
     void addSuccessor(Expression::Ptr e, bool isCall, bool isIndirect, bool isConditional, bool isFallthrough,
                       bool isImplicit = false) const;
+
+    void addSuccessor(CFT cft) {
+        m_Successors.push_back(std::move(cft));
+    }
+
     void appendOperand(Expression::Ptr e, bool isRead, bool isWritten, bool isImplicit = false,
                        bool trueP = false, bool falseP = false) const;
+    void appendEncodedOperand(Expression::Ptr e, bool isRead, bool isWritten, bool isImplicit = false,
+                              bool trueP = false, bool falseP = false) const;
     void copyRaw(size_t size, const unsigned char* raw);
 
+    bool checked_category(InsnCategory c) const {
+      if(arch_decoded_from == Arch_riscv64) {
+        return categories.satisfies(c);
+      }
+      return getCategory() == c;
+    }
+
     mutable std::list<Operand> m_Operands;
+    // Encoded instruction operands, for RISC-V compressed instructions
+    mutable std::list<Operand> m_EncodedOperands;
     mutable Operation m_InsnOp;
+    // Encoded instruction opcode, for RISC-V compressed instructions
+    mutable Operation m_EncodedInsnOp;
     bool m_Valid;
     std::array<uint8_t, maxInstructionLength> m_RawInsn;
     uint8_t m_size{};
@@ -167,6 +220,7 @@ namespace Dyninst { namespace InstructionAPI {
     mutable std::list<CFT> m_Successors;
     // formatter is a non-owning pointer to a singleton object
     ArchSpecificFormatter* formatter;
+    mutable category_t categories;
   };
 }}
 

instructionAPI/h/Operation_impl.h
@@ -60,6 +60,7 @@ namespace Dyninst { namespace InstructionAPI {
     friend class InstructionDecoder_amdgpu_gfx908;
     friend class InstructionDecoder_amdgpu_gfx90a;
     friend class InstructionDecoder_amdgpu_gfx940;
+    friend class InstructionDecoder_riscv64;
 
   public:
     DYNINST_EXPORT Operation(NS_x86::ia32_entry* e, NS_x86::ia32_prefixes* p = NULL,
@@ -87,6 +88,10 @@ namespace Dyninst { namespace InstructionAPI {
     void updateMnemonic(std::string new_mnemonic) { mnemonic = std::move(new_mnemonic); }
 
     bool isVectorInsn{};
+    bool isMultiInsnCall{};
+    bool isMultiInsnBranch{};
+    bool isNonABICall{};
+    bool isNonABIReturn{};
 
   private:
     std::once_flag data_initialized;

instructionAPI/src/ArchSpecificFormatters.C
@@ -143,6 +143,100 @@ std::string ArmFormatter::formatBinaryFunc(const std::string &left, const std::s
         return left + " " + func + " " + right;
 }
 
+///////// Formatter for RISC-V
+
+RiscvFormatter::RiscvFormatter() {
+}
+
+std::string RiscvFormatter::formatImmediate(const std::string &evalString) const  {
+    return "0x" + evalString;
+}
+
+std::string RiscvFormatter::formatRegister(const std::string &regName) const  {
+    constexpr bool use_register_aliases = true;
+    std::string::size_type substr = regName.rfind(':');
+    std::string ret = regName;
+
+    if (substr != std::string::npos) {
+        ret = ret.substr(substr + 1, ret.length());
+        if (use_register_aliases) {
+            if (ret[0] == 'x') {
+                int regID = std::stoi(ret.substr(1));
+                if (regID == 0) {
+                    return "zero";
+                } else if (regID == 1) {
+                    return "ra";
+                } else if (regID == 2) {
+                    return "sp";
+                } else if (regID == 3) {
+                    return "gp";
+                } else if (regID == 4) {
+                    return "tp";
+                } else if (regID >= 5 && regID <= 7) {
+                    return "t" + std::to_string(regID - 5);
+                } else if (regID >= 8 && regID <= 9) {
+                    return "s" + std::to_string(regID - 8);
+                } else if (regID >= 10 && regID <= 17) {
+                    return "a" + std::to_string(regID - 10);
+                } else if (regID >= 18 && regID <= 27) {
+                    return "s" + std::to_string(regID - 16);
+                } else if (regID >= 28 && regID <= 31) {
+                    return "t" + std::to_string(regID - 25);
+                }
+            }
+            else if (ret[0] == 'f') {
+                int regID = std::stoi(ret.substr(1));
+                if (regID >= 0 && regID <= 7) {
+                    return "ft" + std::to_string(regID);
+                } else if (regID >= 8 && regID <= 9) {
+                    return "fs" + std::to_string(regID - 8);
+                } else if (regID >= 10 && regID <= 17) {
+                    return "fa" + std::to_string(regID - 10);
+                } else if (regID >= 18 && regID <= 27) {
+                    return "fs" + std::to_string(regID - 16);
+                } else if (regID >= 28 && regID <= 31) {
+                    return "ft" + std::to_string(regID - 20);
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+std::string RiscvFormatter::formatDeref(const std::string &addrString) const  {
+    std::string out;
+    size_t pluspos = addrString.find("+");
+
+    if(pluspos != std::string::npos) {
+        out += addrString.substr(pluspos + 2) + "(" + addrString.substr(0, pluspos - 1) + ")";
+    } else {
+        out += addrString;
+    }
+
+    return out;
+}
+
+std::string RiscvFormatter::formatBinaryFunc(const std::string &left, const std::string &func, const std::string &right) const  {
+    if (left.find("pc") != std::string::npos) {
+        return right;
+    }
+    return left + " " + func + " " + right;
+}
+
+std::string RiscvFormatter::getInstructionString(const std::vector<std::string> &operands) const
+{
+    std::string out;
+
+    for(auto itr = operands.cbegin(); itr != operands.cend(); itr++) {
+        out += *itr;
+        if(itr != operands.cend() - 1)
+            out += ", ";
+    }
+
+    return out;
+}
+
+
 ///////// Formatter for AMDGPU
 
 AmdgpuFormatter::AmdgpuFormatter() {
@@ -446,6 +540,9 @@ ArchSpecificFormatter& ArchSpecificFormatter::getFormatter(Architecture a)
     auto found = theFormatters.find(a);
     if(found != theFormatters.end()) return *found->second;
     switch(a) {
+        case Arch_riscv64:
+            theFormatters[a] = boost::shared_ptr<ArchSpecificFormatter>(new RiscvFormatter());
+            break;
         case Arch_amdgpu_gfx908:
         case Arch_amdgpu_gfx90a:
         case Arch_amdgpu_gfx940:

commit fbc4b7250f222c6148141b6853b73cebe5448f01

Include all operands in Instruction::format
This was inadvertently broken by e0c12bc4ad in 2025.

instructionAPI/src/Instruction.C
@@ -346,18 +346,19 @@ namespace Dyninst { namespace InstructionAPI {
       return "ERROR_NO_ARCH_SET_FOR_INSTRUCTION";
     }
 
-    // remove this once ArchSpecificFormatter is extended for all architectures
 
     std::string opstr = m_EncodedInsnOp.format();
     opstr += " ";
-    std::list<Operand>::const_iterator currOperand;
-    std::vector<std::string> formattedOperands;
-    for(currOperand = m_EncodedOperands.begin(); currOperand != m_EncodedOperands.end(); ++currOperand) {
-      /* If this operand is implicit, don't put it in the list of operands. */
-      if(currOperand->isImplicit())
-        continue;
 
-      formattedOperands.push_back(currOperand->format(getArch(), addr));
+    std::vector<std::string> formattedOperands;
+    for(auto&& operands : {m_Operands, m_EncodedOperands}) {
+      for(auto&& op : operands) {
+        /* If this operand is implicit, don't put it in the list of operands. */
+        if(op.isImplicit())
+          continue;
+
+        formattedOperands.push_back(op.format(getArch(), addr));
+      }
     }
 
     return opstr + formatter->getInstructionString(formattedOperands);