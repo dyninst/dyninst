cfbaa7a64d37fb4d711aaf58afba9847b5fc80b8
Tim Haines <thaines.astro@gmail.com>
2026-03-26 10:47:21 -0500

    Remove Operation SMFs
    Now that the std::once_flag has been removed, the special member
    functions (SMFs) are no longer needed. The compiler-generated ones
    will work correctly.

--- instructionAPI/src/Operation.C
+++ instructionAPI/src/Operation.C
@@ -39,23 +39,6 @@ using namespace NS_x86;
 
 namespace Dyninst { namespace InstructionAPI {
 
-  Operation::Operation(const Operation& o) {
-    operationID = o.operationID;
-    archDecodedFrom = o.archDecodedFrom;
-    prefixID = o.prefixID;
-    isVectorInsn = o.isVectorInsn;
-    mnemonic = o.mnemonic;
-  }
-
-  const Operation& Operation::operator=(const Operation& o) {
-    operationID = o.operationID;
-    archDecodedFrom = o.archDecodedFrom;
-    prefixID = o.prefixID;
-    isVectorInsn = o.isVectorInsn;
-    mnemonic = o.mnemonic;
-    return *this;
-  }
-
   std::string Operation::format() const {
     if(mnemonic != "") {
       return mnemonic;
