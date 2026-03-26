865561c1f12b6071ae94468d60513a21d5e88a63
Tim Haines <thaines.astro@gmail.com>
2026-03-26 10:44:37 -0500

    Remove Operation::SetUpNonOperandData
    All implicit operands are now accounted for in the decoder.

--- instructionAPI/h/Operation_impl.h
+++ instructionAPI/h/Operation_impl.h
@@ -37,7 +37,6 @@
 #include "entryIDs.h"
 #include "dyninst_visibility.h"
 
-#include <mutex>
 #include <set>
 #include <string>
 
@@ -84,9 +83,6 @@ namespace Dyninst { namespace InstructionAPI {
     bool isVectorInsn{};
 
   private:
-    std::once_flag data_initialized;
-    void SetUpNonOperandData();
-
     mutable registerSet otherRead;
     mutable registerSet otherWritten;
     mutable VCSet otherEffAddrsRead;
@@ -96,7 +92,6 @@ namespace Dyninst { namespace InstructionAPI {
     Architecture archDecodedFrom{};
     prefixEntryID prefixID{};
     Result_Type addrWidth{};
-    int segPrefix{};
     mutable std::string mnemonic;
   };
 }}
