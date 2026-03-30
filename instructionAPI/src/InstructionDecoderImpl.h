900b8db0fe05a630e3d7178a98b60a7db4be4496
Tim Haines <thaines.astro@gmail.com>
2026-03-30 16:05:09 -0500

    InstructionDecoderImpl.h whitespace

--- instructionAPI/src/InstructionDecoderImpl.h
+++ instructionAPI/src/InstructionDecoderImpl.h
@@ -1,28 +1,28 @@
 /*
  * See the dyninst/COPYRIGHT file for copyright information.
- * 
+ *
  * We provide the Paradyn Tools (below described as "Paradyn")
  * on an AS IS basis, and do not warrant its validity or performance.
  * We reserve the right to update, modify, or discontinue this
  * software at any time.  We shall have no obligation to supply such
  * updates or modifications or any other form of support to you.
- * 
+ *
  * By your use of Paradyn, you understand and agree that we (or any
  * other person or entity with proprietary rights in Paradyn) are
  * under no obligation to provide either maintenance services,
  * update services, notices of latent defects, or correction of
  * defects for Paradyn.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -31,17 +31,15 @@
 #if !defined(INSTRUCTION_DECODER_IMPL_H)
 #define INSTRUCTION_DECODER_IMPL_H
 
-#include <stdint.h>
-#include "Expression.h"
 #include "Architecture.h"
 #include "entryIDs.h"
+#include "Expression.h"
 #include "Instruction.h"
 #include "InstructionDecoder.h"
 
-namespace Dyninst
-{
-namespace InstructionAPI
-{
+#include <stdint.h>
+
+namespace Dyninst { namespace InstructionAPI {
   namespace {
     constexpr bool CFT_CALL = true;
     constexpr bool CFT_INDIRECT = true;
@@ -53,65 +51,86 @@ namespace InstructionAPI
     constexpr bool OP_IMPLICIT = true;
   }
 
-class InstructionDecoderImpl
-{
-    public:
-        typedef boost::shared_ptr<InstructionDecoderImpl> Ptr;
-      
-        InstructionDecoderImpl(Architecture a) : m_Arch(a) {}
-        virtual ~InstructionDecoderImpl() {}
-        virtual Instruction decode(InstructionDecoder::buffer& b) = 0;
-        static Ptr makeDecoderImpl(Architecture a);
+  class InstructionDecoderImpl {
+  public:
+    typedef boost::shared_ptr<InstructionDecoderImpl> Ptr;
 
-    protected:
-      
-        virtual Expression::Ptr makeAddExpression(Expression::Ptr lhs, Expression::Ptr rhs, Result_Type resultType);
-        virtual Expression::Ptr makeMultiplyExpression(Expression::Ptr lhs, Expression::Ptr rhs, Result_Type resultType);
-        virtual Expression::Ptr makeLeftShiftExpression(Expression::Ptr lhs, Expression::Ptr rhs, Result_Type resultType);
-        virtual Expression::Ptr makeRightArithmeticShiftExpression(Expression::Ptr lhs, Expression::Ptr rhs, Result_Type resultType);
-        virtual Expression::Ptr makeRightLogicalShiftExpression(Expression::Ptr lhs, Expression::Ptr rhs, Result_Type resultType);
-		virtual Expression::Ptr makeRightRotateExpression(Expression::Ptr lhs, Expression::Ptr rhs, Result_Type resultType);
-        virtual Expression::Ptr makeDereferenceExpression(Expression::Ptr addrToDereference, Result_Type resultType);
+    InstructionDecoderImpl(Architecture a) : m_Arch(a) {}
 
+    virtual ~InstructionDecoderImpl() {}
 
+    virtual Instruction decode(InstructionDecoder::buffer &b) = 0;
 
-        virtual Expression::Ptr makeMultiRegisterExpression(MachRegister reg, uint32_t num_elements);
-        virtual Expression::Ptr makeRegisterExpression(MachRegister reg, uint32_t num_elements = 1);
-        // added version to support loading partial values out of register
-        virtual Expression::Ptr makeRegisterExpression(MachRegister reg, unsigned int start , unsigned int end);
-        virtual Expression::Ptr makeMaskRegisterExpression(MachRegister reg);
-        virtual Expression::Ptr makeRegisterExpression(MachRegister reg, Result_Type extendFrom);
+    static Ptr makeDecoderImpl(Architecture a);
 
-        // added to support ternary value 
-        virtual Expression::Ptr makeTernaryExpression(Expression::Ptr cond, Expression::Ptr first, Expression::Ptr second, Result_Type resultType);
-        boost::shared_ptr<Instruction> makeInstruction(entryID opcode, const char* mnem, unsigned int decodedSize,
-                                     const unsigned char* raw);
+  protected:
+    virtual Expression::Ptr makeAddExpression(Expression::Ptr lhs, Expression::Ptr rhs,
+                                              Result_Type resultType);
 
-        template<typename... Args>
-        void add_operand(Args&&... args) {
-          m_Operands.emplace_back(std::forward<Args>(args)...);
-        }
+    virtual Expression::Ptr makeMultiplyExpression(Expression::Ptr lhs, Expression::Ptr rhs,
+                                                   Result_Type resultType);
 
-        template<typename... Args>
-        void add_cft_target(Args&&... args) {
-          m_CFT_Targets.emplace_back(std::forward<Args>(args)...);
-        }
+    virtual Expression::Ptr makeLeftShiftExpression(Expression::Ptr lhs, Expression::Ptr rhs,
+                                                    Result_Type resultType);
 
-        void add_successor(Expression::Ptr e, bool isCall, bool isIndirect, bool isConditional,
-                           bool isFallthrough, bool isImplicit = false) {
-          add_cft_target(e, isCall, isIndirect, isConditional, isFallthrough);
-          if(!isFallthrough) {
-            add_operand(e, OP_READ, !OP_WRITTEN, isImplicit);
-          }
-        }
+    virtual Expression::Ptr makeRightArithmeticShiftExpression(Expression::Ptr lhs,
+                                                               Expression::Ptr rhs,
+                                                               Result_Type resultType);
 
-        Operation m_Operation;
-        Architecture m_Arch;
-        std::vector<Operand> m_Operands;
-        std::vector<Instruction::CFT> m_CFT_Targets;
-};
+    virtual Expression::Ptr makeRightLogicalShiftExpression(Expression::Ptr lhs,
+                                                            Expression::Ptr rhs,
+                                                            Result_Type resultType);
 
-}
-}
+    virtual Expression::Ptr makeRightRotateExpression(Expression::Ptr lhs, Expression::Ptr rhs,
+                                                      Result_Type resultType);
+
+    virtual Expression::Ptr makeDereferenceExpression(Expression::Ptr addrToDereference,
+                                                      Result_Type resultType);
+
+    virtual Expression::Ptr makeMultiRegisterExpression(MachRegister reg, uint32_t num_elements);
+
+    virtual Expression::Ptr makeRegisterExpression(MachRegister reg, uint32_t num_elements = 1);
+
+    // added version to support loading partial values out of register
+    virtual Expression::Ptr makeRegisterExpression(MachRegister reg, unsigned int start,
+                                                   unsigned int end);
+
+    virtual Expression::Ptr makeMaskRegisterExpression(MachRegister reg);
+
+    virtual Expression::Ptr makeRegisterExpression(MachRegister reg, Result_Type extendFrom);
+
+    // added to support ternary value
+    virtual Expression::Ptr makeTernaryExpression(Expression::Ptr cond, Expression::Ptr first,
+                                                  Expression::Ptr second, Result_Type resultType);
+
+    boost::shared_ptr<Instruction> makeInstruction(entryID opcode, const char *mnem,
+                                                   unsigned int decodedSize,
+                                                   const unsigned char *raw);
+
+    template<typename... Args>
+    void add_operand(Args &&...args) {
+      m_Operands.emplace_back(std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    void add_cft_target(Args &&...args) {
+      m_CFT_Targets.emplace_back(std::forward<Args>(args)...);
+    }
+
+    void add_successor(Expression::Ptr e, bool isCall, bool isIndirect, bool isConditional,
+                       bool isFallthrough, bool isImplicit = false) {
+      add_cft_target(e, isCall, isIndirect, isConditional, isFallthrough);
+      if(!isFallthrough) {
+        add_operand(e, OP_READ, !OP_WRITTEN, isImplicit);
+      }
+    }
+
+    Operation m_Operation;
+    Architecture m_Arch;
+    std::vector<Operand> m_Operands;
+    std::vector<Instruction::CFT> m_CFT_Targets;
+  };
+
+}}
 
 #endif
