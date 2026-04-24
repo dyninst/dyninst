3f51894542bd140b4d486797b69d430d47d67a29
Tim Haines <thaines.astro@gmail.com>
2026-04-24 01:47:31 -0500

    ppc - remove registerNames
    It was added by 3169f28af in 1995, but never used.

--- dyninstAPI/src/inst-power.C
+++ dyninstAPI/src/inst-power.C
@@ -67,11 +67,6 @@ using operandAST = Dyninst::DyninstAPI::operandAST;
 
 extern bool isPowerOf2(int value, int &result);
 
-const char *registerNames[] = { "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
-			"r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15",
-			"r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23",
-			"r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31"};
-
 /*
  * Saving and restoring registers
  * We create a new stack frame in the base tramp and save registers
