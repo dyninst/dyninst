277cebe69311dc7b0722bd5656f2a14baa3e57df
Tim Haines <thaines.astro@gmail.com>
2026-04-24 01:43:22 -0500

    ppc - remove floatingLiveRegList
    Its usage was removed by bb377beb9 in 2007.

--- dyninstAPI/src/inst-power.C
+++ dyninstAPI/src/inst-power.C
@@ -72,9 +72,6 @@ const char *registerNames[] = { "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
 			"r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23",
 			"r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31"};
 
-Dyninst::Register floatingLiveRegList[] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-unsigned int floatingLiveRegListSize = 14;
-
 /*
  * Saving and restoring registers
  * We create a new stack frame in the base tramp and save registers
