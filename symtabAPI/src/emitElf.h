000820c755fd51ea209b2840307e5efacfdaa191
James A. Kupsch <kupsch@cs.wisc.edu>
2026-04-28 22:19:21 -0500

    Revert "Clean up logic related to movePHdrFirst (#2165)"
    This reverts commit 0d206d395c6deee12bcdaa65614c5f438d0aaeaf.

--- symtabAPI/src/emitElf.h
+++ symtabAPI/src/emitElf.h
@@ -233,6 +233,7 @@ namespace Dyninst {
 
             //flags
             // Expand NOBITS sections within the object file to their size
+            bool movePHdrsFirst;
             unsigned loadSecTotalSize;
 
             bool isStripped;
@@ -246,6 +247,7 @@ namespace Dyninst {
             void findSegmentEnds();
             void renameSection(const std::string &oldStr, const std::string &newStr, bool renameAll=true);
             void fixPhdrs();
+            void createNewPhdrRegion(std::unordered_map<std::string, unsigned> &newNameIndexMapping);
 
             bool addSectionHeaderTable(Elf_Shdr *shdr);
 
@@ -279,6 +281,7 @@ namespace Dyninst {
             void addDTNeeded(std::string s);
 
             void log_elferror(void (*err_func)(const char *), const char* msg);
+            bool cannotRelocatePhdrs();
 
             bool isStaticBinary;
             std::vector<void*> buffers;
