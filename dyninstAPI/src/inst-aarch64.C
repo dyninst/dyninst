52db4473b9a79f1e3447928739913563dab95eec
Tim Haines <thaines.astro@gmail.com>
2026-05-15 01:48:07 -0500

    Clean up inst-aarch.C

--- dyninstAPI/src/inst-aarch64.C
+++ dyninstAPI/src/inst-aarch64.C
@@ -28,89 +28,51 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "common/src/headers.h"
-#include "codegen/RegControl.h"
-#include "dyninstAPI/h/BPatch_memoryAccess_NP.h"
-#include "dyninstAPI/src/image.h"
-#include "dyninstAPI/src/dynProcess.h"
-#include "dyninstAPI/src/inst-aarch64.h"
-#include "common/src/arch-aarch64.h"
-#include "dyninstAPI/src/codegen.h"
-#include "dyninstAPI/src/util.h"
-#include "common/src/stats.h"
-#include "dyninstAPI/src/os.h"
-#include "dyninstAPI/src/instPoint.h" // class instPoint
-#include "dyninstAPI/src/debug.h"
-#include "dyninstAPI/h/BPatch.h"
-#include "BPatch_collections.h"
-#include "registerSpace.h"
-#include "dyninstAPI/src/binaryEdit.h"
-#include "dyninstAPI/src/function.h"
-#include "dyninstAPI/src/mapped_object.h"
-#include "RegisterConversion.h"
-#include "parseAPI/h/CFG.h"
 #include "codegen/emitters/aarch64/EmitterAarch64Dyn.h"
 #include "codegen/emitters/aarch64/EmitterAarch64Stat.h"
 #include "Instruction.h"
 #include "Register.h"
-#include "registers/aarch64_regs.h"
-
-#include "emitter.h"
-#include "emit-aarch64.h"
-
-#include <boost/assign/list_of.hpp>
-using namespace boost::assign;
-#include <sstream>
-
-#include "dyninstAPI/h/BPatch_memoryAccess_NP.h"
+#include "RegisterConversion.h"
+#include "codegen.h"
+#include "registerSpace/registerSpace.h"
 
 using codeGenASTPtr = Dyninst::DyninstAPI::codeGenASTPtr;
 using operandAST = Dyninst::DyninstAPI::operandAST;
 
-void emitLoadPreviousStackFrameRegister(Address register_num,
-                                        Register dest,
-                                        codeGen &gen,
-                                        int /*size*/)
-{
-    gen.codeEmitter()->emitLoadOrigRegister(register_num, dest, gen);
+void emitLoadPreviousStackFrameRegister(Address register_num, Register dest,
+                                        codeGen &gen, int /*size*/) {
+  gen.codeEmitter()->emitLoadOrigRegister(register_num, dest, gen);
 }
 
 // First AST node: target of the call
 // Second AST node: source of the call
 // This can handle indirect control transfers as well
 bool AddressSpace::getDynamicCallSiteArgs(InstructionAPI::Instruction i,
-					  Address addr,
-					  std::vector<codeGenASTPtr> &args)
-{
-    namespace di = Dyninst::InstructionAPI;
+                                          Address addr, std::vector<codeGenASTPtr> &args) {
+  namespace di = Dyninst::InstructionAPI;
 
-    auto cft = i.getControlFlowTarget();
-    auto target_reg = boost::dynamic_pointer_cast<di::RegisterAST>(cft);
-    if(!target_reg) return false;
-    auto branch_target = convertRegID(target_reg);
+  auto cft = i.getControlFlowTarget();
+  auto target_reg = boost::dynamic_pointer_cast<di::RegisterAST>(cft);
+  if (!target_reg)
+    return false;
+  auto branch_target = convertRegID(target_reg);
 
-    if(branch_target == registerSpace::ignored) return false;
+  if (branch_target == registerSpace::ignored)
+    return false;
 
-    //jumping to Xn (BLR Xn)
-    args.push_back(operandAST::origRegister((void *)(long)branch_target));
-    args.push_back(operandAST::Constant((void *) addr));
+  // jumping to Xn (BLR Xn)
+  args.push_back(operandAST::origRegister((void *)(long)branch_target));
+  args.push_back(operandAST::Constant((void *)addr));
 
-    return true;
+  return true;
 }
 
 Emitter *AddressSpace::getEmitter() {
-    static Dyninst::DyninstAPI::EmitterAarch64Stat emitter64Stat;
-    static Dyninst::DyninstAPI::EmitterAarch64Dyn emitter64Dyn;
+  static Dyninst::DyninstAPI::EmitterAarch64Stat emitter64Stat;
+  static Dyninst::DyninstAPI::EmitterAarch64Dyn emitter64Dyn;
 
-    if (proc())
-        return &emitter64Dyn;
+  if (proc())
+    return &emitter64Dyn;
 
-    return &emitter64Stat;
+  return &emitter64Stat;
 }
-
-#define GET_IP      0x429f0005
-#define MFLR_30     0x7fc802a6
-#define ADDIS_30_30 0x3fde0000
-#define ADDI_30_30  0x3bde0000
-#define LWZ_11_30   0x817e0000
-#define ADDIS_11_30 0x3d7e0000
