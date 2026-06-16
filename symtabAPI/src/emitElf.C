2cfba58e2af4ab132314c6814d3d615c37a4f915
James A. Kupsch <kupsch@cs.wisc.edu>
2026-06-16 08:44:39 -0500

    make scncount be the section index; start at 1

--- symtabAPI/src/emitElf.C
+++ symtabAPI/src/emitElf.C
@@ -433,7 +433,7 @@ bool emitElf<ElfTypes>::driver(std::string fName) {
     unsigned scncount;
     unsigned sectionNumber = 0;
 
-    for (scncount = 0; (scn = elf_nextscn(oldElf, scn)); scncount++) {
+    for (scncount = 1; (scn = elf_nextscn(oldElf, scn)); scncount++) {
         //copy sections from oldElf to newElf
         shdr = ElfTypes::elf_getshdr(scn);
 
