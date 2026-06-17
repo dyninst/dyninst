3b1d91f30266834487c22fc801c58d4ed61dac00
Tim Haines <thaines.astro@gmail.com>
2026-06-17 10:12:42 -0500

    Add 32-bit emitter to AMD64

--- dyninstAPI/src/addressSpace-AMD64.C
+++ dyninstAPI/src/addressSpace-AMD64.C
@@ -1,4 +1,6 @@
 #include "addressSpace.h"
+#include "codegen/emitters/x86/IA32/EmitterIA32Dyn.h"
+#include "codegen/emitters/x86/IA32/EmitterIA32Stat.h"
 #include "emit-x86.h"
 
 namespace da = Dyninst::DyninstAPI;
@@ -7,10 +9,22 @@ Emitter *AddressSpace::getEmitter() {
   static EmitterAMD64Dyn emitter64Dyn;
   static EmitterAMD64Stat emitter64Stat;
 
+  if(getAddressWidth() == 8) {
+    if (proc()) {
+      return &emitter64Dyn;
+    } else {
+      assert(edit());
+      return &emitter64Stat;
+    }
+  }
+
+  static da::EmitterIA32Dyn emitter32Dyn;
+  static da::EmitterIA32Stat emitter32Stat;
+
   if (proc()) {
-    return &emitter64Dyn;
+    return &emitter32Dyn;
   } else {
     assert(edit());
-    return &emitter64Stat;
+    return &emitter32Stat;
   }
 }
