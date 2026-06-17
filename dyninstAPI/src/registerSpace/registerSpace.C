0699a8b43bb58c8f5b5376818f7e4e9b207dec4a
Tim Haines <thaines.astro@gmail.com>
2026-06-17 11:51:37 -0500

    Convert to runtime selection

--- dyninstAPI/src/registerSpace/registerSpace.C
+++ dyninstAPI/src/registerSpace/registerSpace.C
@@ -552,19 +552,34 @@ void registerSpace::cleanSpace() {
 bool registerSpace::readProgramRegister(codeGen &gen,
                                         Register source,
                                         Register destination,
-                                        unsigned
-#if !defined(DYNINST_CODEGEN_ARCH_POWER)
-                                        size
-#endif
-)
+                                        unsigned size)
 {
-#if !defined(DYNINST_CODEGEN_ARCH_POWER)
-    emitLoadPreviousStackFrameRegister((Address)source,
-                                       destination,
-                                       gen,
-                                       size);
+  switch (gen.getArch()) {
+  case Arch_x86:
+  case Arch_x86_64:
+  case Arch_aarch64:
+  case Arch_amdgpu_gfx908: {
+    emitLoadPreviousStackFrameRegister((Address)source, destination, gen, size);
     return true;
-#else
+  }
+
+  case Arch_ppc32:
+  case Arch_ppc64:
+    // Implemented below
+    break;
+
+  case Arch_none:
+  case Arch_aarch32:
+  case Arch_riscv64:
+  case Arch_cuda:
+  case Arch_amdgpu_gfx90a:
+  case Arch_amdgpu_gfx940:
+  case Arch_intelGen9:
+    regalloc_printf("readProgramRegister not supported for %s\n",
+                    Dyninst::getArchitectureName(gen.getArch()));
+    return false;
+  }
+
     // Real version that uses stored information
 
     // First step: identify the registerSlot that contains information
@@ -605,8 +620,6 @@ bool registerSpace::readProgramRegister(codeGen &gen,
         return false;
         break;
     }
-#endif
-
 }
 bool registerSpace::writeProgramRegister(codeGen &gen,
                                          Register destination,
