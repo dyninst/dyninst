1feae309d03eee16183e6edf69dc8bb610f79bb8
Tim Haines <thaines.astro@gmail.com>
2026-06-23 13:04:18 -0500

    Use instruction_cb on aarch64
    This doesn't actually do anything since 'isReturnAddrSave' is always false for
    aarch64, but it makes it consistent with the previous compile guards.

--- dyninstAPI/src/parsing/DynParseCallback.C
+++ dyninstAPI/src/parsing/DynParseCallback.C
@@ -41,7 +41,6 @@ namespace Dyninst { namespace DyninstAPI {
     case Arch_x86:
     case Arch_x86_64:
     case Arch_aarch32:
-    case Arch_aarch64:
     case Arch_riscv64:
     case Arch_cuda:
     case Arch_amdgpu_gfx908:
@@ -49,6 +48,7 @@ namespace Dyninst { namespace DyninstAPI {
     case Arch_amdgpu_gfx940:
     case Arch_intelGen9:
       return;
+    case Arch_aarch64:
     case Arch_ppc32:
     case Arch_ppc64:
       break;
