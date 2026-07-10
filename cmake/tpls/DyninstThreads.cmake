1962926ff82f889bcd3ec2d99a404d8a82395f13
bbiiggppiigg <bbiiggppiigg@gmail.com>
2026-07-10 09:58:29 -0500

    Revert "Compile parseAPI with -pthread to fix Clang thread-safety (#2311)" (#2322)
    This reverts commit 0a996ac60bb6e137d298ae76d6392a1e63273a64.

--- cmake/tpls/DyninstThreads.cmake
+++ cmake/tpls/DyninstThreads.cmake
@@ -4,14 +4,6 @@ if(DYNINST_OS_UNIX)
   set(_required REQUIRED)
 endif()
 
-# Prefer the compiler's -pthread flag over a bare -lpthread. On GCC and Clang,
-# -pthread also defines _REENTRANT, which Dyninst's bundled Sawyer/ROSE relies
-# on to compile its pool allocator and shared-pointer reference counting as
-# thread-safe (see dataflowAPI/rose/util/Sawyer.h). GCC's -fopenmp defines
-# _REENTRANT implicitly, but Clang's -fopenmp=libomp does not; using -pthread
-# makes the thread-safe path explicit and compiler-independent.
-set(THREADS_PREFER_PTHREAD_FLAG ON)
-
 find_package(Threads ${_required})
 
 if(NOT Threads_FOUND)
