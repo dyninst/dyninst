285283dfb4a753253ab58dbeb12fc7cf0fcbb137
Tim Haines <thaines.astro@gmail.com>
2026-09-23 22:52:04 -0500

    Return 'cur' calculation directly

--- symtabAPI/src/emitElfStatic.C
+++ symtabAPI/src/emitElfStatic.C
@@ -1742,8 +1742,8 @@ Offset emitElfStatic::allocateRelocationSection(std::map<Symbol *, std::pair<Off
   size_t relocEntries = entries.size() + (rela ? rela->getRelocations().size() : 0);
 
   size = relocEntries * relocSize;
-  Offset cur = relocOffset + size;
-  return cur;
+
+  return relocOffset + size;
 }
 
 Offset emitElfStatic::allocateRelGOTSection(const std::map<Symbol *, std::pair<Offset, Offset> > &entries,
