4c0dc02d56cd642e1aa78809830e7abe32d0a09f
Tim Haines <thaines.astro@gmail.com>
2026-09-24 16:42:05 -0500

    P_shmdt

--- common/src/freebsdHeaders.h
+++ common/src/freebsdHeaders.h
@@ -75,7 +75,6 @@ inline int P_getpagesize() { return getpagesize(); }
 /* SYSTEM-V shared memory */
 #include <sys/ipc.h>
 #include <sys/shm.h> /* shmid_ds */
-inline int P_shmdt(void *addr) {return shmdt((char*)addr);}
 inline int P_shmctl(int shmid, int cmd, struct shmid_ds *buf) {
    return shmctl(shmid, cmd, buf);
 }
