8d79e6c92e395a1ad7068ecde764a94e703f2b22
Tim Haines <thaines.astro@gmail.com>
2026-09-24 17:10:45 -0500

    P_munmap

--- common/src/freebsdHeaders.h
+++ common/src/freebsdHeaders.h
@@ -82,7 +82,6 @@ inline char *P_strdup(const char *S) { return (strdup(S));}
 
 /* BSD */
 
-inline int P_munmap(caddr_t addr, int i) { return (munmap(addr, i));}
 inline int P_socket (int NAMESPACE, int STYLE, int PROTOCOL) {
   return (socket(NAMESPACE, STYLE, PROTOCOL));}
 inline int P_socketpair(int namesp, int style, int protocol, int filedes[2]) {
